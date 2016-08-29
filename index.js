42771d035dd4377031acae0cf70b440daa701720
Aaron Miller <amiller719@gmail.com>
2016-08-29 01:33:32 +0000

    Done.

--- index.js
+++ index.js
@@ -52,17 +52,17 @@ function placeOrder(card) {
 }
 
 function setCart(newCart) {
-  cart = newCart;
+    cart = newCart;
 }
 
 function total() {
-  let t = 0;
+    let t = 0;
 
-  for (var i = 0, l = cart.length; i < l; i++) {
-    for (var item in cart[i]) {
-      t += cart[i][item];
+    for (var i = 0, l = cart.length; i < l; i++) {
+        for (var item in cart[i]) {
+            t += cart[i][item];
+        }
     }
-  }
 
-  return t;
+    return t;
 }
