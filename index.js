b3cd7d38132be3a1f941d4dbbe054143995fb90c
Marine Boudeau <m14868@gmail.com>
2016-09-02 03:04:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-var cart;
+var cart = [];
+
+function getCart() {
+  return cart
+}
+
+function addToCart(item) {
+  var price = Math.floor(Math.random()*100)
+  cart.push({'item' : price})
+  console.log(`${item} has been added to your cart.`)
+  return cart
+}
+
+function viewCart() {
+  if (cart.length === 0) {
+    console.log(`Your shopping cart is empty.`)    
+  } else {
+    var cartItems = Object.keys(cart)
+    var text = `In your cart you have `
+    for (var i = 0; i < cartItems.length; i++) {
+      text += `${cart[cartItems[i]]} at ${cart[i]}!`
+    }
+    console.log(text)
+  }
+}
 
 function setCart(newCart) {
   cart = newCart;
