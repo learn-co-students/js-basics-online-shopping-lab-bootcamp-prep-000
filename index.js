a711e81077219f209dbaf429f6d1be8e1231f903
Tekhaus Dev <dev@tekha.us>
2016-09-02 09:08:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
-var cart;
+var cart = [];
+
+function getCart() {
+  return cart;
+}
 
 function setCart(newCart) {
   cart = newCart;
 }
 
-function total() {
-  let t = 0
+function addToCart(item) {
+  cart.push({
+    item: Math.floor(Math.random() * 100)
+  });
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+}
 
-  for (var i = 0, l = cart.length; i < l; i++) {
-    for (var item in cart[i]) {
-      t += cart[i][item]
+function viewCart() {
+  if (cart.length > 0) {
+    var output = 'In your cart, you have';
+
+    for (var i = 0; i < cart.length; i++) {
+      for (var item in cart[i]) {
+        output+= ` ${item} at $${cart[i][item]},`;
+      }
+    }
+    console.log(output.slice(0, -1) + '.');
+  } else {
+    console.log('Your shopping cart is empty.');
+  }
+}
+
+function removeFromCart(item) {
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      cart.splice(i, 1);
+      return cart;
     }
   }
+  console.log('That item is not in your cart.');
+}
 
-  return t
+function placeOrder(cc) {
+  if (cc === undefined) {
+    console.log(`We don't have a credit card on file for you to place your order.`);
+  } else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cc}.`);
+    cart = [];
+  }
+}
+
+function total() {
+  var t = 0;
+
+  for (var i = 0; i < cart.length; i++) {
+    for (var item in cart[i]) {
+      t+= cart[i][item];
+    }
+  }
+  return t;
 }
