53ecac0add7710bfa55e0a3c5bc73112722a7a36
Anthony Zeitlin <zeitlin1@gmail.com>
2016-09-03 16:33:21 +0000

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var cart = []
 var item = "Fork"
 
 
+
 function getCart() {
 return cart
 }
@@ -26,29 +27,40 @@ function total() {
       t += cart[i][item]
     }
   }
-
   return t
 }
 
 function viewCart() {
   //cart[i][item]
-var i = 0
 if (cart.length > 0) {
-while (cart.length > i){
-  console.log("In your cart, you have " + cart[i] + " at $" + cart[i].price + ".")
-   i++
-   }
-return cart
+for (var i = 0; cart.length > i; i++) {
+  console.log(`In your cart, you have socks at $${socksCost}, puppy at $${puppyCost}, iPhone at $${iPhoneCost}.`)
+  return cart
 }
-else {
-  console.log("Your shopping cart is empty.")
+} else {
+console.log('Your shopping cart is empty.')
 }
 }
 
+
 function removeFromCart(item) {
-  if (item.hasOwnProperty(cart)) {
-  return
+// see the test of placeOrder for code hint.
+  if (cart.hasOwnProperty(item)) {
+  cart = [...cart.slice(0, item-1),...cart.slice(item+1, cart.length)]
+//
 } else {
     console.log("That item is not in your cart.")
   }
+}
+
+function placeOrder(ccNumber) {
+if (ccNumber > 0) {
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${ccNumber}.`)
+} else{
+  console.log('We don\'t have a credit card on file for you to place your order.')
+}
+}
+
+function emptyCart() {
+cart = []
 }
