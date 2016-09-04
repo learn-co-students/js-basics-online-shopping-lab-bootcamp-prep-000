8c4d0838c468e14dbf7017201534feba1b37b87f
Denis C <dvc.838@gmail.com>
2016-09-04 22:36:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,50 @@ function total() {
   }
 
   return t
+}
+
+function getCart() { return cart; }
+
+function addToCart(itemName) {
+  var itemPrice = Math.floor(Math.random() * 100);
+  var item = {[itemName]: itemPrice};
+  cart.push(item);
+  console.log(`${itemName} has been added to your cart.`);
+
+  return cart;
+}
+
+function viewCart() {
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.");
+  } else {
+    var itemName = '';
+    var msg = 'In your cart, you have';
+    for (var i in cart) {
+      itemName = Object.keys(cart[i]);
+      msg += ` ${itemName} at $${cart[i][itemName]},`;
+    }
+    console.log(msg.slice(0, -1) + '.');
+  }
+}
+
+function removeFromCart(itemName) {
+  for (var i in cart) {
+    if (cart[i].hasOwnProperty(itemName)) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+
+  console.log("That item is not in your cart.")
+}
+
+function placeOrder(cardNumber) {
+  if (arguments.length === 0) {
+    console.log("We don't have a credit card on file for you to place your order.");
+  } else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+  }
+
+  cart.length = 0;
 }
