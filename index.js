c6cd8dcf0393cdf222fe9c3ee86e721029f64c3d
Menachem Abraham <menachemabraham@gmail.com>
2016-09-04 05:40:04 +0000

    Done.

--- index.js
+++ index.js
@@ -13,5 +13,49 @@ function total() {
     }
   }
 
-  return t
+  return t;
+}
+
+function getCart(){
+  return cart
+}
+
+function addToCart(item){
+  var price = Math.floor(Math.random() * 100);
+  cart.push({});
+  cart[cart.length - 1][item] = price;
+  console.log(`${item} has been added to your cart.`);
+  return cart
+}
+
+function viewCart(){
+  if (cart.length < 1){
+    console.log("Your shopping cart is empty.");
+  }else{
+    var returnString = "In your cart, you have";
+    for (var i = 0, l = cart.length;i < l; i++){
+      returnString += ` ${Object.keys(cart[i])[0]} at $${cart[i][Object.keys(cart[i])[0]]},`;
+    }
+    console.log(returnString.slice(0, -1) +".");
+  }
+}
+
+function removeFromCart(item){
+  for (var i = 0, l = cart.length; i < l; i++){
+    if (cart[i].hasOwnProperty(item)){
+      cart.splice(i, 1);
+      return cart
+    }
+  }
+  console.log("That item is not in your cart.");
+  return cart
+}
+
+function placeOrder(cc){
+  if (!cc){
+    console.log("We don't have a credit card on file for you to place your order.");
+  }else{
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cc}.`);
+  }
+  cart = [];
 }
