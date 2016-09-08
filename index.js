d5826b9a0e4d3de7429df084e442c37cb416f86e
Kathy Shin <katchat143@gmail.com>
2016-09-08 21:40:04 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function setCart(newCart) {
   cart = newCart;
 }
 
+
+
 function total() {
   let t = 0
 
@@ -15,3 +17,78 @@ function total() {
 
   return t
 }
+
+function getCart() {
+  return cart
+}
+
+function addToCart(pitem) {
+  const price = Math.floor(Math.random() * 100)
+
+  cart.push({ [pitem]: price })
+
+  console.log(`${pitem} has been added to your cart.`)
+
+  return cart
+}
+
+function viewCart() {
+  const l = cart.length
+
+  if (!l) {
+    return console.log("Your shopping cart is empty.")
+  }
+
+  const itemsAndPrices = []
+
+  for (let i = 0; i < l; i++) {
+    let itemAndPrice = cart[i]
+    let item = Object.keys(itemAndPrice)[0]
+    let price = itemAndPrice[item]
+
+    itemsAndPrices.push(`${item} at \$${price}`)
+  }
+
+  console.log(`In your cart, you have ${itemsAndPrices.join(', ')}.`)
+}
+
+  function removeFromCart(item) {
+   let itemInCart= false
+
+   for (let i = 0, l = cart.length; i < l; i++) {
+     if (cart[i].hasOwnProperty(item)) {
+        itemInCart= true
+        cart = cart.slice(0, i).concat(cart.slice(i+1))
+     }
+   }
+
+   if (!itemInCart) {
+     console.log("That item is not in your cart.")
+   }
+   return cart
+ }
+   //if item is in cart
+   //remove the object from cart array
+   //return cart
+   //if item isnt in cart, print item isnt in cart
+
+  function placeOrder(n) {
+    if (!n) {
+      return console.log("We don't have a credit card on file for you to place your order.")
+    }
+
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${n}.`)
+
+    cart = []
+
+    }
+    function total() {
+      let t = 0
+
+      for (var i= 0, l = cart.length; i < l; i++) {
+        for (var item in cart[i]) {
+          t += cart[i][item]
+        }
+      }
+    return t
+  }
