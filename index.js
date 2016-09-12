8dd4aa379630e19e07824eddeb39380ce012cb8c
LAURA CHUNG <3llejeong@gmail.com>
2016-09-12 20:11:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,16 @@ function total() {
   }
 
   return t
+}
+
+
+function getCart () {
+  return cart;
+}
+
+function addToCart (item) {
+  var price = Math.floor(Math.randon() * 100);
+  cart.push({item : price});
+  console.log(item +' has been added to your cart.');
+  return cart;
 }
