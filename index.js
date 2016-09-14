6732f83bf67968c5e9c2e01e91da3ec73496a6a2
Pia Kim <pkim2320@gmail.com>
2016-09-14 00:33:49 +0000

    Done.

--- index.js
+++ index.js
@@ -15,3 +15,32 @@ function total() {
 
   return t
 }
+
+var cart = []
+
+function getCart() {
+  return cart;
+
+}
+
+function addToCart(item) {
+  var price = Math.floor(Math.random() * 100)
+  cart.push({[item]: price})
+  console.log(item + " has been added to your cart.")
+  return cart
+}
+
+var yourCart = []
+function viewCart() {
+
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.")
+  }
+  else {
+for (var i = 0; i < cart.length; i++) {
+    for (var key in cart) {
+      yourCart.push(`${key} at $${cart[key]}`)
+    console.log("In your cart, you have " + yourCart.join() + ".")
+
+}
+} }
