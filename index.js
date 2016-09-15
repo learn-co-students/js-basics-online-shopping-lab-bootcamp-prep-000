5ac4a80f19bcd098a74456547eb8dfef8c7b903e
Justin Mayerick <jmayeric@gmail.com>
2016-09-15 11:56:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-var cart;
+var cart = {}
+var keys = Object.keys(cart)
+var keyIndex = []
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +16,39 @@ function total() {
   }
 
   return t
+}
+
+function getCart() {
+  return cart
+}
+
+function addToCart(item) {
+  function generatePrice() {
+    var price = Math.random()
+    Math.floor(price)
+    return price
+  }
+  var object = {item: generatePrice()}
+  cart.push(object)
+  console.log(`${item} has been added to your cart.`)
+  return cart
+}
+
+function viewCart() {
+  if (cart.length < 1) {
+    console.log("Your shopping cart is empty.")
+  } else {
+    for (keys in cart) {
+    console.log(`In your cart you have ${keys} at $${cart[keys]}.`)
+    }
+
+  }
+}
+
+function removeFromCart(item) {
+  if (cart.hasOwnProperty(item)) {
+    delete cart[item]
+  } else {
+    console.log("That item is not in your cart.")
+  }
 }
