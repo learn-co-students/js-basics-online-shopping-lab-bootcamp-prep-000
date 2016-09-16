b0455d486a4e54b8d1439b3b59330cdc8d82f1fc
DJ Jaiswal <digvijays18@gmail.com>
2016-09-16 15:41:20 +0000

    Done.

--- index.js
+++ index.js
@@ -9,34 +9,44 @@ function getCart(){
 }
 
 function addToCart(item){
-    var price = Math.floor((Math.random()*100)+ 1);
-    cart.push({item : `${price}`});
+  var price = Math.floor((Math.random()*100));
+    var userItem = {};
+    userItem[item] = price;
+    cart.push(userItem);
     console.log(`${item} has been added to your cart.`);
     return cart;
 }
 
+/*************************
+REFACTORED WAY
+**************************/
 function viewCart(){
-  if(cart.length>0){
-    for (var i = 0;i < cart.length; i++){
-      for (var e in cart[i]){
-        console.log(`In your cart, you have ${e} at $${cart[i][e]}`);
-      }
+  if (cart.length>0){
+    var empArray = [];
+    for (var i=0; i<cart.length; i++){
+    	for(var keyItem in cart[i]){
+      empArray.push(` ${keyItem} at $${cart[i][keyItem]}`);
+    	}
     }
+    console.log(`In your cart, you have${empArray}.`);
   }
   else {
-    console.log('Your shopping cart is empty.');
+    console.log('Your shopping cart is empty.')
   }
 }
 
-// function removeFromCart(itemName){
-//   for (var itemName in cart){
-//     if (cart.hasOwnProperty(itemName)){
-//       delete itemName;
-//     } else {
-//       console.log("That item is not in your cart.")
-//     }
-//   }
-// }
+function removeFromCart(itemToBeRemoved){
+
+ for(var i=0; i < cart.length; i++){
+   var someObj = Object.assign(cart[i]);
+    if(someObj.hasOwnProperty(`${itemToBeRemoved}`)){
+       cart.splice(cart[i],1);
+    }
+  else {
+      console.log("That item is not in your cart.")
+      }
+  } return cart;
+}
 
 
 
