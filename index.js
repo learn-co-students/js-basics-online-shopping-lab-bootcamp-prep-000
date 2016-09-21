a75cddaa075b01ed51a614e8b1e1c6253c87aafe
Hewan Abreha <henebys@gmail.com>
2016-09-21 18:34:17 +0000

    Done.

--- index.js
+++ index.js
@@ -22,30 +22,51 @@ function getCart() {
 }
 
 function addToCart(item) {
-  var price = Math.floor(Math.random() * 100)
-  cart.push(`${item}: ${price}`)
-    console.log(`${item} has been added to your cart.`)
-    return cart;
-};
+  var price = Math.floor(Math.random() * 100);
+  //adds item and price as an object to cart then console.log...., and return cart.
+  cart.push({[item]: price})
+  console.log(`${item} has been added to your cart.`);
 
-function viewCart () {
-// if(!cart.length) if there isn't anything in your cart then console.log ...
-  if(cart.length === 0) {
-   console.log("Your shopping cart is empty.");
-  }
-  else {
-    var itemAndPrice = [];
-    for (let i = 0, l = cart.length; i < l; i++) {
-      for (var item in cart[i]) {
-        itemsAndPrice.push(`${item} at \$${price}`)
-    }
-  }
-
-  console.log (`In your cart, you have ${itemsAndPrice.join(", ")}.`)
+  return cart;
 }
 
-function removeFromCart(item) {
-  if(!cart.length) {
-    return console.log("That item is not in your cart");
+function viewCart () {
+  // if(!cart.length) if there isn't anything in your cart then console.log ...
+  if(cart.length === 0) {
+    console.log("Your shopping cart is empty.");
   }
+  else
+  {
+    var itemInCart = [];
+    for (let i = 0, c = cart.length; i < c; i++) {
+      for (var item in cart[i]) {
+        itemInCart.push(`${item} at $${cart[i][item]}`);
+      }
+    }
+    console.log(`In your cart, you have ${itemInCart.join(', ')}.`);
+  }
+}
+
+function removeFromCart(itemName) {
+  for ( let i = 0; i < cart.length; i++) {
+    var addItem = cart[i].hasOwnProperty(itemName);
+    if (addItem) {
+      cart.splice(i, 1);
+    }
+  }
+  if (!addItem){
+    console.log("That item is not in your cart.");
+  }
+  return cart;
+}
+
+function placeOrder(ccardNumber) {
+  if(ccardNumber == null) {
+    console.log("We don't have a credit card on file for you to place your order.");
+  }
+  else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${ccardNumber}.`);
+    cart = [];
+  }
+  return cart;
 }
