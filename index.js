481b9fa1e6d0b29b1903b489295d60c6639bc2cf
Salina Hum <salina.hum@gmail.com>
2016-09-22 04:49:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = new Array()
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,41 @@ function total() {
   }
 
   return t
+}
+
+function getCart() {
+  return cart
+}
+
+function addToCart(wantedItem) {
+  var price = Math.floor(Math.random() * 100)
+
+  cart.push({[wantedItem]: price})
+
+  console.log(`${wantedItem} has been added to your cart.`)
+
+  return cart
+}
+
+function viewCart() {
+  if (cart.length === 0) {
+    console.log(`Your shopping cart is empty.`)
+  }
+
+  else {
+    var itemAtPrice = [] //lists out all of the items and their prices in "item at price" format
+
+    //need a loop so that everything happens for each item in the cart
+    for (var i = 0; i < cart.length; i++) {
+      //gets one key from the cart, stores it in cart2
+      var cart2 = cart[i]
+      var item = Object.keys(cart2)[0]
+      var price = cart2[item]
+
+      //make an array, put items/prices in "item at price" format
+      itemAtPrice.push(`${item} at \$${price}`)
+    }
+
+    console.log(`In your cart, you have ${itemAtPrice.join(', ')}`)
+  }
 }
