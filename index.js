97c8bbd7a91a9f0c2277a5a456d7b2ae78c89260
Kristi Thompson <kristiforpresident@gmail.com>
2016-09-25 16:18:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
-var cart;
+var cart = [];
+
+function getCart(){
+  return cart;
+}
+
+function addToCart(item){
+  var newItem = {item : Math.floor(Math.random() * 100)}
+  cart.push(newItem)
+  console.log(`${item} has been added to your cart.`)
+  return cart
+}
+
+function viewCart(){
+  if (cart.length > 0){
+    console.log(`In your cart, you have `)
+    for (var item in cart){
+      console.log(`${item} at ${cart[item]},`)
+    }
+  }
+  else {
+    console.log('Your shopping cart is empty.')
+  }
+}
+
+function removeFromCart(item){
+  if (cart.hasOwnProperty(item)){
+    delete cart.item;
+  }
+  else {
+    return "That item is not in your cart."
+  }
+}
 
 function setCart(newCart) {
   cart = newCart;
