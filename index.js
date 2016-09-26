ff56e41fe4e2a2f596183469d40508842594add2
Alexis Hovey <alexis.hovey@gmail.com>
2016-09-26 19:20:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = []
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,31 @@ function total() {
   }
 
   return t
+}
+
+function getCart(){
+  return cart
+}
+
+function addToCart(item){
+  var item;
+  function itemPrice(){
+    math.floor(math.random()) * 101
+  }
+  var price = itemPrice
+  cart.push({item : price})
+  console.log(`${item} has been added to your cart.`)
+  return(cart)
+  }
+
+function viewCart(){
+  if(cart.length == 0){
+  console.log("Your shopping cart is empty.")}
+  else {
+    for (var i = 0; i < cart.length; i++){
+      for (var cartItem in cart[i]) {
+        console.log("In your cart you have: " + cartItem + " $" + cart[i][cartItem]);
+      }
+    }
+  }
 }
