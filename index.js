5feb6a31f28b78d7a8426340206278955c29648a
Laamia Islam <laamia.islam@gmail.com>
2016-09-28 04:44:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
-var cart;
+var cart = [];
+
+function addToCart(item) {
+  console.log(`${item} has been added to your cart.`)
+  cart.push({[item]: Math.floor((Math.random())*100)})
+  return cart
+}
+
+function getCart() {
+  return cart;
+}
 
 function setCart(newCart) {
   cart = newCart;
+}
+
+function viewCart(){
+  var cartKeys = Object.keys(cart)
+  if(cart.length == 0) {
+    console.log('Your shopping cart is empty.')
+  }
+  else {
+    var text = 'In your cart, you have'
+    for(var i=0; i<cart.length; i++){
+      var item = Object.keys(cart[i])
+      if(i<cart.length-1) {
+        text += ` ${item[0]} at $${cart[i][item[0]]},`
+      }
+      else {
+        text += ` ${item[0]} at $${cart[i][item[0]]}.`
+      }
+    }
+    console.log(text)
+  }
 }
 
 function total() {
@@ -14,4 +44,33 @@ function total() {
   }
 
   return t
+}
+
+function removeFromCart(item) {
+  var itemList = []
+  var originalLength = cart.length
+  for (var i = 0; i<cart.length; i++){
+    var key = Object.keys(cart[i])
+    itemList.push(key[0])
+  }
+  for(var i = 0; i<cart.length; i++){
+    if(item == itemList[i]){
+      cart.splice(i,i+1)
+    }
+  }
+  var finalLength = cart.length
+  if (originalLength == finalLength){
+    console.log("That item is not in your cart.")
+  }
+}
+
+function placeOrder(creditCard){
+  creditCard = creditCard || 'default';
+  if (creditCard == 'default') {
+    console.log("We don't have a credit card on file for you to place your order.")
+  }
+  else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${creditCard}.`)
+    cart.length = 0
+  }
 }
