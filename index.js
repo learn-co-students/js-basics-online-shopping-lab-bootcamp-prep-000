4b158e4012ed444df1d0e40ccc4fa1467b9d4e9a
Matt Borg <msimonborg@gmail.com>
2016-10-01 04:49:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -15,3 +15,41 @@ function total() {
 
   return t
 }
+
+function getCart() {
+  return cart
+}
+
+function addToCart(item) {
+  let price = Math.floor( Math.random() * 100 + 1)
+  cart.push({[item]: price})
+  console.log(`${item} has been added to your cart.`)
+  return cart
+}
+
+function viewCart() {
+  let cartDetails = [];
+
+  if (cart.length > 0) {
+    for (var i = 0, l = cart.length; i < l; i++) {
+      for (var itemName in cart[i]) {
+        cartDetails.push(` ${itemName} at $${cart[i][itemName]}`)
+      }
+    } console.log(`In your cart, you have${cartDetails}.`)
+  } else {
+    console.log('Your shopping cart is empty.')
+  }
+}
+
+function removeFromCart(item) {
+  for (var i = 0, l = cart.length; i < l; i++) {
+    if (cart[i].hasOwnProperty(item) === true) {
+      cart.splice(i, 1);
+      return cart;
+    } else {
+      if (i === l - 1) {
+      console.log(`That item is not in your cart.`);
+          }
+        }
+      }
+    }
