69978065932eb8f193882f813e5fe7c92138e2c7
Carlo Francisco <cfrancisco726@gmail.com>
2016-10-05 05:34:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,52 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
+}
+
+function getCart(){
+  return cart
+}
+
+function addToCart(item){
+  var price = Math.floor(Math.random() * 100);
+  cart.push(item);
+  cart.item = price;
+  console.log(item + " has been added to your cart");
+  return cart
+}
+
+function viewCart(){
+  for (var item in cart) {
+    if (cart > 0){
+  console.log("In your cart, you have " + item + " at " + cart.item)
+} else {
+    console.log("Your shopping cart is empty")
+    }
+  }
+}
+
+
+function removeFromCart(item){
+  for (var item in cart) {
+  if (cart.hasOwnProperty(item)) {
+    console.log(name);
+    cart.remove(item);
+    return cart
+  }
+  else {
+    console.log("That item is not in your cart.");
+    }
+  }
+}
+
+function placeOrder(ccNum){
+  if(ccNum == undefined){
+    console.log("We don't have a credit card on file for you to place your order.");
+  } else {
+    console.log("Your total cost is $${total()}, which will be charged to the card ${ccNum}.");
+    cart.length = 0;
+  }
 }
 
 function total() {
@@ -12,6 +57,5 @@ function total() {
       t += cart[i][item]
     }
   }
-
   return t
 }
