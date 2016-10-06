a0bbc2133f98b32c35957368c0c743874f75cfc4
Carlo Francisco <cfrancisco726@gmail.com>
2016-10-06 00:51:37 +0000

    Done.

--- index.js
+++ index.js
@@ -11,30 +11,30 @@ function getCart(){
 
 function addToCart(item){
   var price = Math.floor(Math.random() * 100);
-  cart.push(item);
-  cart.item = price;
+  cart.push({ [item]: price })
   console.log(item + " has been added to your cart.");
   return cart
 }
 
 function viewCart()
 {
+  if (cart.length == 0)
+    {
+		return console.log('Your shopping cart is empty');
+      }
+
   var newCartArr = [];
   for( var i = 0, l = cart.length; i < l; i++)
   {
       let itemAndPrice = cart[i]
-      let item = Object.keys(itemAndPrice)[0]
+      let item = Object.keys(itemAndPrice)[i]
       let price = itemAndPrice[item]
     newCartArr.push(item + " at " + "$" + price);
 }
-    if (newCartArr.length > 0){
+
       console.log('In your cart, you have ' + newCartArr.join(', ') + '.')
-    } else {
-      console.log('Your shopping cart is empty.')
-    }
 
   }
-
 
 
 function removeFromCart(item){
