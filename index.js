5c4007cc09cfbd88cb4d1b1b5ca1f11b532a5ebc
Gregory Fellis <gregfellis@gmail.com>
2016-10-07 12:16:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,36 @@ function total() {
   }
 
   return t
+}
+
+// Start GS code
+function getCart() {
+  return cart
+}
+
+function addToCart(item) {
+  var price = Math.floor(Math.random() * 100);
+  cart.push( {item: price} );
+  console.log(`${item} has been added to your cart.`);
+
+  return getCart()
+}
+
+function viewCart() {
+  var itemInCart = Object.keys(cart)
+  var msg = ""
+
+  if (cart.length === 0) {
+    console.log(`Your shopping cart is empty.`)
+    return getCart()
+  }
+
+
+  for (var items in cart) {
+    msg += `${cart} at ${cart[itemInCart]}, `
+
+    console.log(`In your cart, you have socks at ${msg}`)
+  }
+
+  return getCart()
 }
