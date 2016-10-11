033645d6fe256b1765416fa3371d9a6e146ca67b
chris gyasi <jblck09@gmail.com>
2016-10-11 01:30:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-var cart;
+var cart=[];
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +18,31 @@ function total() {
   }
 
   return t
+}
+
+function getCart(){
+  return cart
+}
+
+function addToCart(item){
+  var obj={};
+  obj.item=Math.floor(getRandomArbitrary(0,100))
+  cart.push(obj)
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+}
+
+function viewCart(){
+  if(cart.length==0){
+    return console.log("Your shopping cart is empty.")
+  }
+  else{
+    var tmpArry=[];
+    for(var i=0; i<cart.length; i++){
+      var obj= cart[i]    //objcet at position i of array is assigned to variable
+      var objNames= Object.keys(obj)    //object key is assined a new variable
+      tmpArry.push(` ${objNames} at $${obj[objNames]}`)
+    }
+    return console.log("In your cart, you have" + tmpArry);
+  }
 }
