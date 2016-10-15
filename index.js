32b2d3a7f8622241d413407ad3111f50833333ea
Dan Farrell <farrelld09@gmail.com>
2016-10-15 20:48:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
-var cart;
+var cart = [];
+
+function getCart () {
+  return cart;
+}
+
+
+
+function addToCart(item){
+  var price = Math.floor(Math.random()*100);
+  cart.push({[item]: price});
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+}
+
+function viewCart () {
+  var itemPrice = [];
+  if (cart.length > 0) {
+    for(var i = 0; i < cart.length; i ++) {
+      var pair = cart[i];
+      var cartItem = Object.keys(pair)
+      itemPrice.push (`${cartItem} at $${pair[cartItem[i]]}`)
+      var string = itemPrice.join(', ');
+}
+      console.log(`In your cart, you have ${string}.`)
+}else{
+  console.log("Your shopping cart is empty.")
+  }
+}
 
 function setCart(newCart) {
   cart = newCart;
