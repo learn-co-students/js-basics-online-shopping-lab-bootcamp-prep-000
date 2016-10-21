b9de3507a3f297a8a60b5cb5ea03c1f05132459f
Daniel Lempesis <danlrcn@hotmail.com>
2016-10-21 00:31:07 +0000

    Done.

--- index.js
+++ index.js
@@ -16,50 +16,73 @@ function total() {
   return t
 }
 
- function getCart(){
+function getCart(){
   return cart
 }
 
+function addToCart(item){
   var itemPrice = Math.floor(Math.random() * 100)
   var keyPair = {[item]:itemPrice}
+
  cart.push(keyPair)
  console.log(item + " has been added to your cart.")
+
  return cart
 }
 
-function viewCart(){
-  var cartItems = Object.keys(cart)
 
+function viewCart() {
+  if (cart.length != 0) { //if cartlength exists
+  var nCart = []        // create new cart
+   for (var i = 0, len = cart.length; i < len; i++) {  // for i, iterating via cartlength
+   for (var item in cart[i]) {                        //for item in cart at current iteration of index i
+      nCart.push(` ${item} at $${cart[i][item]}`)     //push  "$item at $cart[i][item]" into nCart
+    }
+  }
+
+ console.log("In your cart, you have" + nCart +".")
+}
+  console.log("Your shopping cart is empty.")
+}
+
+
+/*function viewCart() {
+   if (cart.length != 0) {
+   var nCart = []
+   for (var i = 0, len = cart.length; i < len; i++) {
+   for (var item in cart[i]) {
+   nCart.push(`${item} at $${[cart][i][item]}`)
+}
+   console.log("In your cart, you have " + nCart);
+ }
+ else{
+   console.log("Your shopping cart is empty.");
+ }}
+}*/
+/*function viewCart(){
+  var cartItems = Object.keys(cart)
+  var itemPrice = Math.floor(Math.random() * 100)
   if (cart.length === 0) {
     console.log("Your shopping cart is empty.")
   }
-
   else {
-
-for (var i = 0, len = cartItems.length; i < len; i++) {
-  for (var item in cart[i]) {
-  console.log(`In your cart, you have ${cartItems} at ${itemPrice[i]}!`)
+  console.log(`In your cart, you have ${cartItems} at ${cart[itemPrice]}!`)
 }
-  //console.log(`In your cart you have ${item} at ${itemPrice}`)
-//console.log(`In your cart you have ${cart[item]} at ${itemPrice}`);
-}
-}}
-
-////PADDING
+}*/
 
 
 function removeFromCart(item){
-  for (var item in cart){
-    if (cart.hasOwnProperty(item)){
-      delete cart[item]
-    }
-    else {
+  var check = false
+  for (var i in cart) {
+    if (cart[i] !== undefined) {  //if (cart[i].hasOwnProperty(item)) { ...yuck.
+      delete cart[item] // cart.splice([i], 1) ... no thanks
+    }}
+    if (check === false) {
       console.log("That item is not in your cart.")
     }
     cart = []
     return cart
     }
-  }
 
 function placeOrder(cardNumber) {
   if (cardNumber == undefined ) {
