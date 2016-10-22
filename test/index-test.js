fba13d77a3d2ea1754be182e54c89cdd36d0a21a
Ugljesa Ijacic <ugison@gmail.com>
2016-10-22 02:42:06 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -2,30 +2,30 @@
 
 
 beforeEach(function() {
-  setCart([])
+  setCart([]);
 
-  expect.spyOn(console, 'log')
+  expect.spyOn(console, 'log');
 })
 
 afterEach(function() {
-  expect.restoreSpies()
+  expect.restoreSpies();
 })
 
 describe('#addToCart', function() {
   it("should add an item to the cart", function() {
-    addToCart('pizza')
+    addToCart('pizza');
 
     expect(getCart().length).toEqual(1);
   });
 
   it("logs that the item has been added", function() {
-    addToCart('pizza')
+    addToCart('pizza');
 
-    expect(console.log).toHaveBeenCalledWith("pizza has been added to your cart.")
+    expect(console.log).toHaveBeenCalledWith("pizza has been added to your cart.");
   })
 
   it("returns the cart", function() {
-    expect(addToCart("pizza")).toEqual(getCart())
+    expect(addToCart("pizza")).toEqual(getCart());
   })
 });
 
@@ -43,13 +43,13 @@ describe('#viewCart', function() {
 
     expect(console.log).toHaveBeenCalledWith(
       `In your cart, you have socks at $${socksCost}, puppy at $${puppyCost}, iPhone at $${iPhoneCost}.`
-    )
+    );
   });
 
   it("should print 'Your shopping cart is empty.' if the cart is empty", function() {
     viewCart();
 
-    expect(console.log).toHaveBeenCalledWith("Your shopping cart is empty.")
+    expect(console.log).toHaveBeenCalledWith("Your shopping cart is empty.");
   });
 });
 
@@ -65,15 +65,15 @@ describe('#total', function() {
 
     const totalCost = socksCost + puppyCost + iPhoneCost;
 
-    expect(total()).toEqual(totalCost)
+    expect(total()).toEqual(totalCost);
   })
 })
 
 describe('#removeFromCart', function() {
   it("removes the item from the cart", function() {
-    addToCart('pizza')
+    addToCart('pizza');
 
-    expect(hasItem(getCart(), 'pizza')).toBe(true)
+    expect(hasItem(getCart(), 'pizza')).toBe(true);
 
     removeFromCart("pizza");
 
@@ -81,9 +81,9 @@ describe('#removeFromCart', function() {
   });
 
   it("alerts you if you're trying to remove an item that isn't in your cart", function() {
-    removeFromCart("sock")
+    removeFromCart("sock");
 
-    expect(console.log).toHaveBeenCalledWith("That item is not in your cart.")
+    expect(console.log).toHaveBeenCalledWith("That item is not in your cart.");
   });
 });
 
@@ -93,38 +93,37 @@ describe('#placeOrder', function() {
 
     expect(console.log).toHaveBeenCalledWith(
       "We don't have a credit card on file for you to place your order."
-    )
+    );
   });
 
   it("lets you place an order with a credit card", function() {
-    addToCart('pizza')
+    addToCart('pizza');
 
-    const t = total()
+    const t = total();
 
     placeOrder(123);
 
     expect(console.log).toHaveBeenCalledWith(
       `Your total cost is $${t}, which will be charged to the card 123.`
-    )
+    );
   });
 
   it('empties the cart', function() {
-    addToCart('pizza')
+    addToCart('pizza');
 
-    expect(hasItem(getCart(), 'pizza')).toBe(true)
+    expect(hasItem(getCart(), 'pizza')).toBe(true);
 
     placeOrder(123);
 
-    expect(getCart()).toEqual([])
+    expect(getCart()).toEqual([]);
   })
 })
 
 function hasItem(c, item) {
   for (let i = 0, l = c.length; i < l; i++) {
     if (c[i].hasOwnProperty(item)) {
-      return true
+      return true;
     }
   }
-
-  return false
+  return false;
 }
