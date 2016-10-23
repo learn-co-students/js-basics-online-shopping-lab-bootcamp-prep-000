ee6afa6def695676b7e1fcddc036c03d42c7d2c4
Zhanxi Ni <ni.zhanxi@gmail.com>
2016-10-23 01:11:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-var cart;
+var cart = [];
+
+function getCart(){
+  return cart;
+}
 
 function setCart(newCart) {
   cart = newCart;
+}
+
+function addToCart (item) {
+ var thing = {
+   item : Math.floor(Math.random()*100)
+ }
+ cart.push(thing);
+ console.log(`${item} has been added to your cart.`);
+ return cart;
+}
+
+function viewCart () {
+var string = "In your cart you have ";
+  if (cart.length >= 0) {
+    for (var i=0; i<cart.length; i++) {
+      string += cart[i];
+      return string;
+    }
+  }
+  else {
+    return "Your shopping cart is empty."
+  }
+
+}
+
+function removeFromCart (item) {
+
+}
+
+function placeOrder(number) {
+
 }
 
 function total() {
