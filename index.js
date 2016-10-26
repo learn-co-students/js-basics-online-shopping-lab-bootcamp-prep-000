98e246722460a37b24e107c0e2615353f4140371
Zachary Smith <zach.smith33@gmail.com>
2016-10-26 23:23:12 +0000

    Done.

--- index.js
+++ index.js
@@ -15,3 +15,25 @@ function total() {
 
   return t
 }
+
+var cart = [];
+
+function getCart() {
+  return cart;
+}
+
+function addToCart(item) {
+  var price = Math.floor(Math.random() * 100);
+  cart.push({item: price});
+  var added = item + " has been added to your cart.";
+  console.log( added );
+}
+
+function viewCart() {
+  var i;
+  var stuffInCart;
+  for (i = 0; i < cart.length; i += 1) {
+     "In your car you have" + cart[i]);
+  }
+  console.log(stuffInCart)
+}
