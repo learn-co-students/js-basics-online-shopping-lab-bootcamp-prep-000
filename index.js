8d66f244c907acdf942eb2eebcdb9eaa6f965de7
Tim nguyen <timdung_tim@yahoo.com>
2016-11-01 04:43:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-var cart;
+var cart = [];
+function setCart(cart) {
 
-function setCart(newCart) {
-  cart = newCart;
+
+}
+
+function getCart () { // this is the first function //
+
+  return cart;
+}
+
+function addToCart (item) {
+
+  var price = Math.floor(Math.random() * 100);
+
+  cart.push({item:price});
+
+  console.log(`${item} has been added to your cart.`)
+
+  return cart;
 }
 
 function total() {
@@ -14,4 +30,26 @@ function total() {
   }
 
   return t
+}
+
+
+
+
+
+function viewCart() {
+    var array = [];
+    if (cart.length===0) {
+    console.log("Your shopping cart is empty.");
+    }
+
+    for (var i = 0; i < cart.length ; i++) {
+    var itemAndPrice = cart[i]
+    var item = Object.keys(itemAndPrice)[0]
+    var price = itemAndPrice[item]
+
+    array.push(`${item} at \$${price}`)
+
+  }
+
+  console.log(`In your cart, you have ${array.join(', ')}.`);
 }
