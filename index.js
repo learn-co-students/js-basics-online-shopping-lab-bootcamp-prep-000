dfa611954fbfdf61de03c43e6d003ab06484ddc0
Nicholas Anaya <anaya.nicholas@gmail.com>
2016-11-08 03:43:27 +0000

    Done.

--- index.js
+++ index.js
@@ -15,3 +15,32 @@ function total() {
 
   return t
 }
+
+var cart = []
+
+function getCart() {
+  return cart
+}
+
+function addToCart(item){
+  var price = Math.floor((Math.random() * 100));
+  var obj = {[item]:price}
+
+  cart.push(obj)
+  console.log(`${item} has been added to your cart.`)
+
+  return cart
+}
+
+function viewCart() {
+  if (cart.length === 0){
+    console.log("Your shopping cart is empty.")
+  } else {
+    var items = Object.keys(cart)
+    for (var i = 0, l = items.length; i < l; i++) {
+      var array = []
+      array.push(`${items[i]} at ${cart[items[i]]}`)
+      console.log(`In your cart you have ${array.join(' ')}.`)
+    }
+  }
+}
