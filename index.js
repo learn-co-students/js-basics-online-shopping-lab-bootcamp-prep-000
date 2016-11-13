ccb66646236da75b09e1ede0eaf7b6be772ccd41
Ryan Schoolfield <rp0school@gmail.com>
2016-11-13 09:33:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,48 @@ function total() {
   }
 
   return t
+}
+
+function getCart(){
+  return cart;
+}
+
+function addToCart(item) {
+  var price = Math.floor(100 * Math.random());
+  cart.push({[item]: price});
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+}
+
+function viewCart() {
+  if (cart.length === 0){
+    console.log("Your shopping cart is empty.");
+  } else {
+    const itemPrice = [];
+    for (let i = 0; i < cart.length; i++) {
+      var cartObject = cart[i];
+      var item = Object.keys(cartObject)[0];
+      var price = cartObject[item];
+      itemPrice.push(`${item} at $${price}`);
+    }
+      console.log(`In your cart, you have ${itemPrice.join(', ')}.`);
+    }
+  }
+
+function removeFromCart(item) {
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty([item])) {
+      return cart.splice(i, 1);
+    }
+  }
+   console.log("That item is not in your cart.");
+ }
+
+function placeOrder(cardNumber) {
+  if (cardNumber == undefined) {
+    console.log("We don't have a credit card on file for you to place your order.");
+  } else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    cart = false;
+  }
 }
