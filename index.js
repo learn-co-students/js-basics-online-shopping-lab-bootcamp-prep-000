055b999ba1794c8cdb41a9af25bfb127daede91d
Chelsea Hicks <chelseamariehicks@gmail.com>
2016-11-13 15:32:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,35 @@ function total() {
   }
 
   return t
+}
+
+function getCart() {
+  return cart
+}
+
+function addToCart (item) {
+  var price = Math.floor(Math.random() * 100);
+  cart.push({[item]: price})
+console.log(`[item] has been added to your cart.`);
+return cart
+}
+
+function viewCart() {
+var cart = {item: price};
+for (var item in cart) {
+  console.log(`In your cart you have ${item} at ${price}.`)};
+if (cart.length = 0) {console.log(`Your shopping cart is empty.`)};
+}
+
+// function viewCart() {
+//   var i = cart[i];
+//   if (cart.length > 0) {"In your cart you have ${item} at {item: price}.";}
+//
+// else {"Your shopping cart is empty.";}
+//
+// }
+
+function removeFromCart(item) {
+
+
 }
