d11fd74a9d9c11331b6cda07e2229817842a34aa
jason brown <j.grant.brown@gmail.com>
2016-11-16 16:10:58 +0000

    Done.

--- index.js
+++ index.js
@@ -21,5 +21,32 @@ function getCart(){
 }
 
 function addToCart(newitem){
-  
+ (newitem*Math.random()).Math.floor()
+
+}
+
+function viewCart(){
+if (cart>=1){
+  console.log("In your cart you have [item,price].")
+}
+else{
+  return ("Your shopping cart is empty.");
+}
+
+function removeFromCArt(removethis){
+  if (removethis is not in cart){
+    return ("That Item is not in your cart.");
+  }
+  else{
+  return  cart.pop(removethis);
+  }
+}
+
+function placeOrder(cardNumber){
+  if (cardNumber==undefined):{
+    return ("We don't have a credit card on file for you to place your order.");
+  }
+  else{
+    return ("Your total cost is $${total()}, which will be charged to the card ${cardNumber}.")
+  }
 }
