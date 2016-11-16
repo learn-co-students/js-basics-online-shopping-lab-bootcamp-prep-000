f45a52bc459eac1434c788fdf2ac863d41be9b61
jason brown <j.grant.brown@gmail.com>
2016-11-16 18:09:32 +0000

    Done.

--- index.js
+++ index.js
@@ -36,25 +36,3 @@ if (cart>=1){
 else{
   return ("Your shopping cart is empty.");
 }
-
-
-/*
-function removeFromCArt(removethis){
-  if (removethis is not in cart){
-    return ("That Item is not in your cart.");
-
-  }
-  else{
-  return  cart.pop(removethis);
-  }
-}
-
-function placeOrder(cardNumber){
-  if (cardNumber==undefined):{
-    return ("We don't have a credit card on file for you to place your order.");
-  }
-  else{
-    return ("Your total cost is $${total()}, which will be charged to the card ${cardNumber}.")
-  }
-}
-*/
