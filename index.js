63d6045b04ed414bb3db0dd99a7d3b15229b226b
jason brown <j.grant.brown@gmail.com>
2016-11-16 16:35:35 +0000

    Done.

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ function getCart(){
 }
 
 function addToCart(item){
-var price=Math.floor(Math.random()* 101);
-cart[{item: price}];
+var price=Math.floor(Math.random() * 101);
+cart={item: price};
 return (item + " has been added to your cart.");
 }
 
