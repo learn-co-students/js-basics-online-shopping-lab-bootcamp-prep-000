9e94babbbe7d63936b79644f411d91ec94cf8f96
Ian McNair <imcnair13@gmail.com>
2016-11-25 21:50:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var cart;
-
+var cart = [];
+/////////////
 function setCart(newCart) {
   cart = newCart;
 }
-
+/////////////
 function total() {
   let t = 0
 
@@ -14,4 +14,48 @@ function total() {
   }
 
   return t
+}
+////////////
+function getCart(){
+  return cart
+}
+/////////// Returns the added key-pair, error has to do with something else?
+function addToCart(item){
+  console.log(`${item} has been added to your cart.`)
+  var price = Math.random() * 100
+  price=Math.floor(price)
+  cart[item] = price
+  return cart
+}
+///////////viewCart works with zero length, but not with non zero
+////////// length arrays
+function viewCart(){
+  if(Object.keys(cart).length === 0){
+    return "Your shopping cart is empty."
+  }
+  else{
+      var items = Object.keys(cart)
+      var annoucement = "In your cart you have \n"
+      for(var items in cart) {
+        annoucement += `${items}: ${cart[items]}\n`
+    }
+    return annoucement
+  }
+}
+////////////// haven't checked the below in console yet
+function removeFromCart(item){
+  if(cart.hasOwnProperty(item) === false){
+    return "That item is not in your cart."
+  }else{
+     delete cart[item]
+     return cart
+  }
+}
+/////////////
+function placeOrder(cardNumber){
+  if (cardNumber === null){
+    return "We don't have a credit card on file for you to place your order."
+  }else {
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  }
 }
