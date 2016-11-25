6991da47378db99f73aa14e38f61285d6a147e21
Catherine Luse <catherine.luse@gmail.com>
2016-11-25 19:54:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,105 @@
-var cart;
+/*var cart = {};
 
 function setCart(newCart) {
   cart = newCart;
+}
+
+function getCart(){
+  return cart;
+}
+
+function addToCart(item){
+  var price = Math.floor(Math.random() * 10) + 1;
+  cart[item] = price;
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+}
+
+function viewCart(cart){
+  if (cart !== {}){
+      console.log(`In your cart you have ${cart}.`)
+  } else {
+      console.log("Your shopping cart is empty.")
+  }
+}
+
+function removeFromCart(item){
+  if (!(item in cart)){
+    delete cart.item;
+    return cart;
+  } else {
+    return "That item is not in your cart.";
+  }
+}
+
+function total() {
+  let t = 0
+
+    for (var item in cart) {
+      t += cart.item;
+    }
+  return t;
+  }
+
+function placeOrder(cardNumber){
+    if (!cardNumber){
+        console.log("We don't have a credit card on file for you to place your order.");
+    } else {
+        console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    }
+    cart = [];
+}
+*/
+var cart = [];
+
+function setCart(newCart) {
+  cart = newCart;
+}
+
+function getCart(){
+  return cart;
+}
+
+function addToCart(item){
+  var price = Math.floor(Math.random() * 10);
+  cart.push({[item]: price});
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+}
+
+function viewCart(cart){
+  var itemPricePairs = [];
+  var object;
+  var price;
+  var item;
+
+  for (var i = 0; i < cart.length; i++){
+      object = cart[i];
+      item = Object.keys(cart[i]);
+      price = object[item];
+      itemPricePairs.push(`${item} ${price}`);
+  }
+
+  itemPricePairs = itemPricePairs.join(", ");
+
+  if (cart.length > 0){
+      console.log(`In your cart you have ${itemPricePairs}.`)
+  } else {
+      console.log("Your shopping cart is empty.")
+  }
+}
+
+function removeFromCart(item){
+    var items = Object.keys(cart);
+    var itemIndex = items.indexOf(item);
+
+
+    if (itemIndex !== -1){
+        cart.splice(itemIndex, itemIndex + 1);
+        return cart;
+    } else {
+        return "That item is not in your cart.";
+    }
 }
 
 function total() {
@@ -14,4 +112,16 @@ function total() {
   }
 
   return t
+}
+
+function placeOrder(cardNumber){
+    var tempTotal = total();
+    cart = [];
+
+    if (!cardNumber){
+        return "We don't have a credit card on file for you to place your order.";
+    } else {
+        return `Your total cost is ${tempTotal}, which will be charged to the card ${cardNumber}.`
+    }
+
 }
