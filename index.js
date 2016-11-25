9105ddbdfde60208616c2677707769a5bc51c961
Catherine Luse <catherine.luse@gmail.com>
2016-11-25 21:19:20 +0000

    Done.

--- index.js
+++ index.js
@@ -46,7 +46,8 @@ function removeFromCart(item){
         obj = cart[i];
         key = Object.keys(obj);
         if (key[0] === item){
-          return cart.splice(i, i + 1);
+          cart.splice(i, i + 1);
+          return cart;
         }
     }
   return "That item is not in your cart.";
