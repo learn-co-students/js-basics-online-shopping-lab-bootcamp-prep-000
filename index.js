28eb4a5fcd77010eed6ba7dee04ba4f008efa68c
Catherine Luse <catherine.luse@gmail.com>
2016-11-25 21:36:12 +0000

    Done.

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function placeOrder(cardNumber){
     if (!cardNumber){
         console.log("We don't have a credit card on file for you to place your order.");
     } else {
-        console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`);
+        console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
         cart = [];
     }
 }
