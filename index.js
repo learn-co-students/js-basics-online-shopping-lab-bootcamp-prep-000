8bdc154d8ceaf4b2d3d1954ccc09ce444653a78e
Catherine Luse <catherine.luse@gmail.com>
2016-11-25 21:29:12 +0000

    Done.

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function removeFromCart(item){
           return cart;
         }
     }
-  return "That item is not in your cart.";
+  console.log("That item is not in your cart.");
 }
 
 function total() {
