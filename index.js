fe6abbcadc82709cbcfc8dc3c2a7ce965278280a
jason brown <j.grant.brown@gmail.com>
2016-11-29 18:30:30 +0000

    Done.

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ if(cart.length<1){
 else{
 
 for (var itemName in cart){
-  var itemAndPrice= (` ${Object.keys(cart[itemName])} + " at $" + ${itemName}+","`)
+  var itemAndPrice= (` ${Object.keys(cart[itemName])}   at $${Object.keys(cart[itemName[0]])}`)
 console.log(`In your cart, you have ${itemAndPrice} .`)
 }
 }
