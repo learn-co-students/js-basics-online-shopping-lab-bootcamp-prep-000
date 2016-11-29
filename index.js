8561186f072de29461cd57d9c4ce0b45512d21c9
jason brown <j.grant.brown@gmail.com>
2016-11-29 14:20:23 +0000

    Done.

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ if(cart.length<1){
 else{
 
 for (var itemName in cart){
-  var itemAndPrice= (` ${cart[itemName]} + "at $" + ${cart[[itemname]]} `)
+  var itemAndPrice= (` ${cart[itemName]} + "at $" + ${cart[[itemName]]} `)
 console.log(`In your cart, you have ${itemAndPrice} .`)
 }
 }
