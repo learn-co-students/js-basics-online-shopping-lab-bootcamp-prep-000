c1c3544f0fbc025eb07bf4b1cf21af6f5ff55ea1
Elizabeth Hall <elizabethhall205@gmail.com>
2016-11-29 17:38:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var cart;
-
+var cart=[];
+// define global variable, variables deined w = operator, initialize an empty array
 function setCart(newCart) {
   cart = newCart;
 }
 
 function total() {
   let t = 0
-
+//total accepts no arguments. It iterates over
   for (var i = 0, l = cart.length; i < l; i++) {
     for (var item in cart[i]) {
       t += cart[i][item]
@@ -15,3 +15,13 @@ function total() {
 
   return t
 }
+
+function getCart() {
+
+  return cart
+}
+/*This function should automatically set a price for this item by generating a random number between 0 and 100.
+(Hint: Math.random() generates a random number in 0, 1; Math.floor() rounds a number down to the nearest integer.)
+//This function should add the item and the price as an object ({item: price}) to the cart array.
+This function should print out to the console <item> has been added to your cart. and return the cart.
+function addToCart(item) */
