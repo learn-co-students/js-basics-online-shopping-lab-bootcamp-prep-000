6af1548aff4e2b7b2c6843af9e128612c503123f
jason brown <j.grant.brown@gmail.com>
2016-12-01 14:28:14 +0000

    Done.

--- index.js
+++ index.js
@@ -25,23 +25,20 @@ var price=Math.floor(Math.random() * 101);
 cart.push({[item]:price});
 console.log(item + " has been added to your cart.");
 return cart
-
+}
 
 
 function viewCart(){
 var itemsInCart= "";
-if(cart.length<1){
-  console.log("Your shopping cart is empty.");
+  if(cart.length<1){
+    console.log("Your shopping cart is empty.");
 }
-else{
-for (var i in cart){
+  else{
+      for (var i in cart){
+        var itemAndPrice= (`${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}, `)
+        itemsInCart+= itemAndPrice; //add to items in cart before the loop stars again
+}
+}
+console.log("In your cart, you have " + itemsInCart.slic(0,-2) +".");
 
-  var itemAndPrice= (`${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}, `)
-
-itemsInCart+= itemAndPrice; //add to items in cart before the loop stars again
-
-}
-console.log("In your cart, you have " + itemsInCart +".");
-}
-}
 }
