7dde702b8c225db87695bd1f495e39d00471f92b
jason brown <j.grant.brown@gmail.com>
2016-12-01 23:31:46 +0000

    Done.

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function removeFromCart(removeitem){
 
 for (var i in cart){
     if(cart[i].hasOwnProperty(`${removeitem}`) == true){
-      delete  cart[]
+      delete  cart[i]
         return cart
   }
 
