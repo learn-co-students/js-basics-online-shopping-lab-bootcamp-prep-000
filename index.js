3ad81c1cf076b125efd922df2d2e044ce51de8f5
Kevin Adkins <kevinladkins@gmail.com>
2016-12-04 14:56:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
-var cart;
+var cart = [];
+
+function getCart() {
+  return cart;
+}
+
+function addToCart(item) {
+  var price
+  price = Math.floor((Math.random() * 100) + 1);
+  cart.push({[item] : price});
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+}
+
+function viewCart() {
+  var cartObject = {};
+  var cartArray = [];
+  if (cart.length > 0) {
+    for (i = 0, l = cart.length; i < l; i++) {
+      Object.assign(cartObject, cart[i]);
+    }
+    var cartKeys;
+    cartKeys = Object.keys(cartObject);
+    for (var cartKeys in cartObject) {
+      cartArray.push(`${cartKeys} at $${cartObject[cartKeys]}`);
+    }
+    console.log(`In your cart, you have ${cartArray.join(', ')}`);
+    } else {
+   console.log("Your shopping cart is empty.")
+   }
+}
+
+
+function removeFromCart() {
+
+}
 
 function setCart(newCart) {
   cart = newCart;
