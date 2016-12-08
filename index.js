862f9897680bc841d35ac5d683626d7af419ac4f
David Chase <david.chase4@gmail.com>
2016-12-08 16:31:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,56 @@
-var cart;
+var cart = [];
+
+function getCart() {
+  return cart;
+};
 
 function setCart(newCart) {
   cart = newCart;
+};
+
+function addToCart(item) {
+  var price = Math.floor(Math.random() * 100);
+  cart.push({item: price});
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+};
+
+var itemName = Object.keys(cart)
+
+function viewCart() {
+  for (var item in cart) {
+    var itemName = Object.keys(cart[item]);
+    var cost = item.value;
+    if(item.length > 0) {
+    console.log(`In your cart, you have ${itemName} at $${cost}.`);
+  } else {
+    console.log('Your shopping cart is empty.');
+  }
 }
+};
+
+function viewCart() {
+  for(var item in cart) {
+    if(cart.hasOwnProperty(item)) {
+      var itemName = Object.keys(cart[item]);
+      var value = cart[item];
+      console.log(`In your cart, you have ${itemName} at $${value}.`);
+      } else {
+      console.log('Your shopping cart is empty.');
+      }
+    }
+  };
+
+function removeFromCart(item) {
+  for(var stuff in cart) {
+    if(stuff.hasOwnProperty(item)) {
+      console.log(`That item is not in your cart.`)
+    } else {
+      delete cart.item;
+  }
+}
+  return cart;
+};
 
 function total() {
   let t = 0
@@ -11,7 +59,7 @@ function total() {
     for (var item in cart[i]) {
       t += cart[i][item]
     }
-  }
+  };
 
   return t
-}
+};
