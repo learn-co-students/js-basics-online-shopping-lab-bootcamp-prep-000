58375b068636570670559e88e8e8445198b76c4d
Timea Roik <timea.roik@gmail.com>
2016-12-16 22:06:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,38 @@ function total() {
   }
 
   return t
+}
+
+function getCart() {
+  return cart;
+}
+
+function addToCart(itemForPurchase) {
+  var priceOfItem = Math.floor(Math.random() * 100);
+  cart[itemForPurchase] = priceOfItem;
+  console.log(`${itemForPurchase} has been added to your cart.`)
+  return cart;
+}
+
+function viewCart() {
+  const itemForPurchaseAndPriceOfItem = [];
+  for (let i=0; l=cart.length, i<l, i++) {
+    console.log("In your cart, you have itemForPurchaseAndPriceOfItem.push(`${i + 1}, ${priceOfItem[i]}`)";
+  } else if (cart.length === 0) {
+    console.log("Your shopping cart is empty.")
+  }
+}
+
+function removeFromCart(item) {
+  let itemInCart = false
+
+  for (let i = 0, l = cart.length; i < l; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      itemInCart = true
+      cart = cart.slice(0, i).concat(cart.slice(i + 1))
+      return cart;
+    } else if (cart.length === 0) {
+      console.log("That item is not in your cart.")
+    }
+  }
 }
