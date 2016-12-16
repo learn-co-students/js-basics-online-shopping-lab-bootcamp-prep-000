f8420e9d20b6b0e3ec887a919054c4102d02a318
Duncan Maina <duncandevs@gmail.com>
2016-12-16 02:56:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,34 @@ function total() {
   }
 
   return t
+}
+
+function getCart(){
+  return cart;
+}
+
+function addToCart(item){
+  var price = Math.floor(Math.random());
+  cart.push({item : price});
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+}
+
+function viewCart(){
+  var str = "In your cart, you have "
+  if(cart.length==0){
+    console.log("Your shopping cart is empty.");
+  } else {
+//`In your cart, you have socks at $${socksCost}, puppy at $${puppyCost}, iPhone at $${iPhoneCost}.`
+    for(var i = 0; i < cart.length; i++){
+      var item = Object.keys(cart[i]);
+      if(i == cart.length-1){
+        str += `${item} at ${cart[i][item]}.`;
+      } else {
+        str += `${item} at ${cart[i][item]}, `;
+      }
+    }
+
+    }
+  }
 }
