0cbd7655c2af2dd9ef65765937adc70b346b9f9a
Scott Plunkett <scottyplunkett@gmail.com>
2016-12-19 17:42:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -15,3 +15,26 @@ function total() {
 
   return t
 }
+
+function getCart(){
+  return cart
+};
+
+function addToCart(item) {
+  var price = Math.floor(Math.random() * 101)
+  cart.push( {item: price} )
+  console.log(item + ` has been added to your cart.`)
+  return cart
+};
+
+function viewCart() {
+  if (cart.length === 0) {
+    console.log(`Your shopping cart is empty.`)
+  } else {
+    for (var i = 0, l = cart.length; i < l; i++) {
+      for (var item in cart[i]) {
+      console.log(`In your cart, you have ` + item + `at` + cart[item])
+    }
+  }
+}
+};
