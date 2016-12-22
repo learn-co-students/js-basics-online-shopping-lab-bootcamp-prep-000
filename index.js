8e4060e22d47967e4e943ae78627e208d8dbcdeb
LOI DINH <loipdinh@gmail.com>
2016-12-22 01:16:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = new Array();
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,45 @@ function total() {
   }
 
   return t
+}
+
+function getCart() {
+  return cart;
+}
+
+function addToCart(item) {
+  var price;
+  price = Math.floor(Math.random() * 100);
+  cart.push({[item]: price});
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+}
+
+function viewCart() {
+  if (cart[0] != null) {
+    var list = [];
+    for (var i = 0, l = cart.length; i < l; i++) {
+
+      const itemName = Object.keys(cart[i])[0];
+      const price = cart[i][itemName];
+      list.push(itemName + " at $" + price);
+  }
+  console.log("In your cart, you have " + list.join(", ") + ".");
+  //return cart;
+}
+else {
+  console.log("Your shopping cart is empty.");
+}
+}
+
+function removeFromCart(item) {
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item) == false && i == cart.length - 1) {
+      console.log('That item is not in your cart.');
+    }
+    else if (cart[i].hasOwnProperty(item) == true){
+      cart = [...cart.slice(0,i),...cart.slice(i+1)];
+      return cart;
+    }
+  }
 }
