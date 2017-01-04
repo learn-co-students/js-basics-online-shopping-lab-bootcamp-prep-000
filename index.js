bb5b284e5390c2441c61d2ce2d55acf632c4befd
Elizabeth Hall <elizabethhall205@gmail.com>
2017-01-04 02:54:32 +0000

    Done.

--- index.js
+++ index.js
@@ -17,11 +17,28 @@ function total() {
 }
 
 function getCart() {
-
   return cart
 }
+
 /*This function should automatically set a price for this item by generating a random number between 0 and 100.
 (Hint: Math.random() generates a random number in 0, 1; Math.floor() rounds a number down to the nearest integer.)
 //This function should add the item and the price as an object ({item: price}) to the cart array.
 This function should print out to the console <item> has been added to your cart. and return the cart.
 function addToCart(item) */
+function addToCart(item) {
+  var itemPrice = Math.floor(Math.random() * 100);
+  cart.push({ [item]: itemPrice })
+  console.log("<item> has been added to your cart.");
+  return cart
+}
+//This function should loop over every item in cart to print out
+//"In your cart, you have [item and price pairs]."
+//If there isn't anything in your cart, the function should print out
+function viewCart( ){
+  //declare a var and set it's value equal to something
+      if (cart.length === 0) {
+        //do something
+          console.log("Your shopping cart is empty.");
+        }
+ //compare conditional statements here
+}
