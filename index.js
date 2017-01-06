c5e4880370e0d0672875d9e2930570ec30c77658
Andre M <andre.mueller@zoho.com>
2017-01-06 20:10:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
-var cart;
+var cart = []
 
 function setCart(newCart) {
-  cart = newCart;
+  cart = newCart
+}
+
+function getCart() {
+  return cart
+}
+
+function addToCart(item) {
+  var price =  Math.floor((Math.random() * 100) + 1);
+  var justAddedObj = {}
+  justAddedObj[item] = price
+  cart.push(justAddedObj)
+  console.log(`${item} has been added to your cart.`)
+  return cart
+}
+
+function viewCart() {
+  const l = cart.length
+  if (l === 0) {
+    console.log(`Your shopping cart is empty.`)
+  } else {
+    var outputArray = []
+    for (var i = 0; i < l; i++) {
+      for (var item in cart[i]) {
+        outputArray.push(`${item} at $${cart[i][item]}`)
+      }
+    }
+  console.log(`In your cart, you have ${outputArray.join(', ')}.`)
+  }
+}
+
+function removeFromCart(item) {
+  var itemInCart = 0
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      cart = cart.slice(0, i).concat(cart.slice(i + 1))
+      itemInCart === 1
+      return cart
+    }
+  }
+  if (itemInCart === 0) {
+    console.log(`That item is not in your cart.`)
+  }
 }
 
 function total() {
@@ -14,4 +56,13 @@ function total() {
   }
 
   return t
+}
+
+function placeOrder(cardNumber) {
+  if (cardNumber === undefined) {
+    console.log(`We don't have a credit card on file for you to place your order.`)
+  } else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    cart = []
+  }
 }
