92a0ff188548df0f517dc84086835bb21245cd7e
Amanda Rager <mandarage@gmail.com>
2017-01-13 20:05:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -15,3 +15,28 @@ function total() {
 
   return t
 }
+
+function getCart() {
+  return cart;
+};
+
+function addToCart(item) {
+  var price = Math.floor(Math.random() * 100);
+  var displayItem = {item : price};
+  cart.push(displayItem);
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+};
+
+function viewCart() {
+  var itemAtPriceArray = [];
+  var itemNames = Object.keys(meals);
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  } else {
+    for (var item in cart) {
+      itemAtPriceArray.push(`${itemNames[i]} at ${cart[itemNames[i]]}`);
+      return `In your cart, you have ${itemAtPriceArray}.`
+    }
+  }
+};
