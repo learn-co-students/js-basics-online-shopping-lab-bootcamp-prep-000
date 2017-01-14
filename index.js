ab5e7fa26357d60f070c58b09202c8a9ce7acb52
natasha kelly <kellynatashav@yahoo.com>
2017-01-14 17:03:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
 }
+
 
 function total() {
   let t = 0
@@ -14,4 +15,71 @@ function total() {
   }
 
   return t
+}
+
+
+
+var getCart = () =>{
+  return cart
+}
+
+
+var cart = [];
+
+
+var addToCart = (item) =>{
+  var price = Math.floor(Math.random() * 100);
+  cart.push({[item]:price});
+  console.log(`${item} has been added to your cart.`)
+  return cart
+}
+
+
+//Math.random() gives us a big decimal value between 0 and 1 we multiplye that by 100 and that basically moves the decimal down 2 spots right
+
+var viewCart = () => {
+    if(cart.length === 0){
+      console.log("Your shopping cart is empty.")
+    } else {
+      var str = ""
+      cart.forEach(function(object, index){
+        var itemName = Object.keys(object)
+        var itemPrice = object[itemName]
+        if (index === cart.length-1) {
+          str += (`${itemName} at $${itemPrice}`)
+        }else {
+          str += (`${itemName} at $${itemPrice}, `)
+        }
+      });
+      console.log("In your cart, you have " + str + ".")
+    }
+  }
+
+
+
+
+var removeFromCart = (name) =>{
+  for(var i = 0; i < cart.length; i++){
+    for(var item in cart[i]){
+      if(item === name){
+         cart.splice(i, 1);
+        return cart
+      } else {
+        console.log("That item is not in your cart.");
+    }
+    }
+  }
+}
+// placed the print statement outside of the loop since you do not want it to print at each iteration.
+// iterating the array and nested objects to return keyd not indexes.
+
+
+var placeOrder = (cardNumber) =>{
+if (cardNumber !== undefined) {
+  cart = []
+  console.log(`"Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+  return cart
+}else {
+  return "We don't have a credit card on file for you to place your order."
+  }
 }
