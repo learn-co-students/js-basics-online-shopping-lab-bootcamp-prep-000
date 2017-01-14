623703a8fc76d54214ce422d11e32474670b6bb3
Amanda Rager <mandarage@gmail.com>
2017-01-14 02:05:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart = [];
+var cart = {};
 
 function setCart(newCart) {
   cart = newCart;
@@ -28,15 +28,40 @@ function addToCart(item) {
   return cart;
 };
 
+/*
 function viewCart() {
-  var itemAtPriceArray = [];
   var itemNames = Object.keys(cart);
   if (cart.length === 0) {
     return "Your shopping cart is empty."
   } else {
-    for (var item in cart) {
-      itemAtPriceArray.push(`${itemNames[i]} at ${cart[itemNames[i]]}`);
-      return `In your cart, you have ${itemAtPriceArray}.`
-    }
-  }
+      for (var i = 0, l = itemNames.length; i < l; i++) {
+        itemAtPriceArray.push(`${itemNames[i]} at ${cart[itemNames[i]]} `);
+      }
+      for (var i = 0, l = cart.length; i < l; i++) {
+        return `In your cart, you have ${itemAtPriceArray}.`
+      };
+  };
 };
+
+function removeFromCart(item) {
+  if () { // condition: item is in cart === true
+   console.log("That item is not in your cart.")
+  } else {
+    //delete item from array
+  }
+  return cart;
+};
+
+//HINT: Check each object's key to see if it matches the parameter, then remove it if it matches.
+//You might find hasOwnProperty to be useful.
+
+function placeOrder(cardNumber) {
+  if () { //condition: no argument is received
+    return "We don't have a credit card on file for you to place your order."
+  } else {
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  }
+  //empty the cart array at the end
+};
+
+*/
