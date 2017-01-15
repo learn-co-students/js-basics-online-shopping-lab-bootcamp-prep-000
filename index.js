288d9ea5b03b7515281f5df4e4c8e865d381c316
natasha kelly <kellynatashav@yahoo.com>
2017-01-15 07:23:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var cart = [];
 
+
 function setCart(newCart) {
   cart = newCart;
 }
@@ -13,7 +14,6 @@ function total() {
       t += cart[i][item]
     }
   }
-
   return t
 }
 
@@ -22,9 +22,6 @@ function total() {
 var getCart = () =>{
   return cart
 }
-
-
-var cart = [];
 
 
 var addToCart = (item) =>{
@@ -59,27 +56,30 @@ var viewCart = () => {
 
 
 var removeFromCart = (name) =>{
+  if (cart.length === 0) {
+    console.log("That item is not in your cart.");
+  } else {
   for(var i = 0; i < cart.length; i++){
     for(var item in cart[i]){
       if(item === name){
          cart.splice(i, 1);
         return cart
       } else {
-        console.log("That item is not in your cart.");
-    }
+      }
+      console.log("That item is not in your cart.");
     }
   }
-}
+}}
 // placed the print statement outside of the loop since you do not want it to print at each iteration.
 // iterating the array and nested objects to return keyd not indexes.
 
 
 var placeOrder = (cardNumber) =>{
 if (cardNumber !== undefined) {
+  const t = total()
   cart = []
-  console.log(`"Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
-  return cart
+  console.log(`Your total cost is $${t}, which will be charged to the card 123.`)
 }else {
-  return "We don't have a credit card on file for you to place your order."
+  console.log("We don't have a credit card on file for you to place your order.")
   }
 }
