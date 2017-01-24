8efa77b6a1cff5c88735af203b8d2d63f9a45dbc
Liliana Alonzo <lillyneveu@gmail.com>
2017-01-24 00:29:24 +0000

    Done.

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function getCart() {
 }
 
 function addToCart(item) {
-  var price = Math.random() >= 100
+  var price = Math.floor(Math.random() * 100);
   cart.push({item: price});
     console.log(`${item} has been added to your cart.`);
   return cart
