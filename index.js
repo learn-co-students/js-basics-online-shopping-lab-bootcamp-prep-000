38380ab8f3c67ed21de9f632e98dd4a16538b654
Hima Chitalia <hima_chhag@yahoo.com>
2017-01-27 18:44:16 +0000

    Done.

--- index.js
+++ index.js
@@ -15,3 +15,28 @@ function total() {
 
   return t
 }
+
+function getCart(){
+  return cart;
+}
+
+function addToCart(item){
+  var price = Math.floor(Math.random()*10);
+  cart.push(new Object({[item]: price}));
+  console.log (`${item} has been added to your cart.`)
+  return cart;
+}
+
+function viewCart(){
+    if (cart.length === 0){
+      return "Your shopping cart is empty."
+    }else {
+    var items = [];
+    for (var i = 0; i < cart.length; i++){
+      for(var item in cart[i]){
+        items.push ( item + "at $" + cart[i][item])
+      }
+  }
+    return ("In your cart, you have" + items.join(",") + ".");
+  }
+}
