4ee0f341e63ff247c22b8aece9592da1fe161591
Hima Chitalia <hima_chhag@yahoo.com>
2017-01-27 20:41:17 +0000

    Done.

--- index.js
+++ index.js
@@ -22,21 +22,42 @@ function getCart(){
 
 function addToCart(item){
   var price = Math.floor(Math.random()*10);
-  cart.push(new Object({[item]: price}));
+  cart.push({[item]: price});
   console.log (`${item} has been added to your cart.`)
   return cart;
 }
 
 function viewCart(){
     if (cart.length === 0){
-      return "Your shopping cart is empty."
+      console.log ("Your shopping cart is empty.");
     }else {
     var items = [];
     for (var i = 0; i < cart.length; i++){
       for(var item in cart[i]){
-        items.push ( item + "at $" + cart[i][item])
+        items.push ( item + " at $" + cart[i][item])
       }
   }
-    return ("In your cart, you have" + items.join(",") + ".");
+    console.log ("In your cart, you have " + items.join(", ") + ".");
   }
+}
+
+function removeFromCart(item){
+  for (var i = 0; i < cart.length; i++){
+    if (cart[i].hasOwnProperty(item)){
+    cart.splice(i, 1)
+    return cart
+  }
+}
+  return console.log ("That item is not in your cart.");
+}
+
+function placeOrder(cardNumber){
+  var emptyArray= [];
+  if (cardNumber === undefined){
+    return console.log (`We don\'t have a credit card on file for you to place your order.`)
+  }else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    setCart(emptyArray);
+  }
+
 }
