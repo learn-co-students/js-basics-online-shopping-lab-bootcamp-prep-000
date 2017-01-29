c67dabbcca73be8eb613c77e40171340bfcb72ee
Luis Rincon <rincon_92@hotmail.com>
2017-01-29 23:04:33 +0000

    Done.

--- index.js
+++ index.js
@@ -8,18 +8,42 @@ function addToCart(item) {
   var price = Math.floor(Math.random() * 10);
   cart.push({[item]: price});
   console.log(`${item} has been added to your cart.`)
+  return cart;
 }
 
 function viewCart() {
   if (cart.length === 0) {
     console.log('Your shopping cart is empty.')
   } else {
-    var cartString = 'In your cart you have';
+    var cartString = [];
     for (var i = 0, l = cart.length; i < l; i++) {
       var cartObject = cart[i];
       var cartItem = Object.keys(cartObject);
-      cartString += (`${[cartItem]} ${cart[i].cartItem}`);
+      cartString.push(` ${[cartItem]} at $${cart[i][cartItem]}`);
     }
+  } console.log(`In your cart, you have${cartString}.`);
+}
+
+function removeFromCart(item) {
+  for (var i = 0, l = cart.length; i < l; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      cart.splice(i,1);
+      return cart;
+    }
+  }console.log('That item is not in your cart.');
+}
+
+function placeOrder(cardNumber) {
+  if (isNaN(cardNumber) === false) {
+    var total = 0;
+    for (var i = 0, l = cart.length; i < l; i++) {
+      var cartObject = cart[i];
+      var cartItem = Object.keys(cartObject);
+      total += cart[i][cartItem];
+  } cart.splice(0);
+    console.log(`Your total cost is $${total}, which will be charged to the card ${cardNumber}.`);
+} if (isNaN(cardNumber) === true) {
+  console.log('We don\'t have a credit card on file for you to place your order.');
   }
 }
 
