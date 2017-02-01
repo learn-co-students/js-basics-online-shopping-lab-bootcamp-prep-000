694c88e4015123b23b19973c8060fb896c11ed09
Henno Fourie <bortch@gmail.com>
2017-02-01 18:37:05 +0000

    Done.

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function getCart() {
 
 function addToCart(item) {
   var price = Math.floor(Math.random() *100)
-  cart.push ({item:price});
+  cart.push ({[item]:price});
   console.log (item + " has been added to your cart.");
   return cart
 }
@@ -30,11 +30,33 @@ function addToCart(item) {
 function viewCart() {
   var superVar = [];
   if (cart.length > 0) {
-      for (var item in cart) {
-        superVar.push (item + " at " + cart[item])
+      for (var index in cart) {
+        var key = Object.keys(cart[index])
+        superVar.push (" " + key + " at $" + cart[index][key])
       }
-      console.log ("In your cart, you have " + superVar + ".")
+
+      console.log ("In your cart, you have" + superVar + ".")
   } else {
     console.log ("Your shopping cart is empty.")
   }
+}
+
+function removeFromCart(item) {
+  for (var i = 0, l = cart.length ; i<l; i++) {
+    debugger
+    if (cart[i].hasOwnProperty(item)) {
+      cart.splice (i,1)
+      return cart
+    }
+  }
+  console.log ("That item is not in your cart.")
+}
+
+function placeOrder(cardNumber) {
+  if(cardNumber == null || cardNumber.length === 0) {
+    console.log ("We don't have a credit card on file for you to place your order.")
+  } else {
+    console.log (`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+  }
+  cart = [];
 }
