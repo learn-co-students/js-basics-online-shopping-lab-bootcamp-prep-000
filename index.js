199703ec6af71e74521825376cf84f4c51c0c0fe
Ursh Khan <urshkhan@gmail.com>
2017-02-08 23:41:18 +0000

    Done.

--- index.js
+++ index.js
@@ -4,14 +4,66 @@ function getCart() {
   return cart
 }
 
+
 function addToCart(item) {
-  /* This function should automatically set a price
-  for this item by generating a random number in 0, 1;
-  use Math.floor() rounds a number down to the nearest integer.)
-  This function should add the item and the price as an object ({item: price})
-  to the cart array. And return the cart.
-  */
+  var price = Math.floor(Math.random() * 100);
+
+  cart.push({[item]: price });
+
+  console.log(item + ' has been added to your cart.');
+
+  return cart
 }
+
+function viewCart() {
+  var emptyCart = cart.length
+
+  if(!emptyCart) {
+    return console.log("Your shopping cart is empty.")
+  }
+
+var itemPrices = [];
+
+for (var i = 0; i < emptyCart; i++) {
+  var itemsAndPrices = cart[i];
+  var item = Object.keys(itemsAndPrices)[0];
+  var price = itemsAndPrices[item]
+
+  itemPrices.push(`${item} at \$${price}`);
+}
+
+console.log(`In your cart, you have ${itemPrices.join(', ')}.`)
+
+}
+
+
+
+function removeFromCart(item) {
+  var itemInCart = false
+
+  if(!itemInCart) {
+    console.log("That item is not in your cart.")
+  }
+
+for (var i = 0, l = cart.length; i < l; i++) {
+  if(cart[i].hasOwnProperty(item)) {
+    itemInCart = true
+    cart = cart.slice(0, i)
+  }
+}
+}
+
+
+function placeOrder(creditCard) {
+  if (!creditCard) {
+    return console.log('We don\'t have a credit card on file for you to place your order.')
+  }
+
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${creditCard}.`)
+
+cart = []
+}
+
 
 function setCart(newCart) {
   cart = newCart;
