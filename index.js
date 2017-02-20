caad6617f23237097f45448aed25952f0eb94b86
Noah Guillory <slokomisu4@gmail.com>
2017-02-20 17:24:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
-var cart;
+var cart = []
 
 function setCart(newCart) {
   cart = newCart;
+}
+
+function getCart() {
+  return cart
+}
+
+function addToCart(item) {
+  let itemObj = {
+    [item]: Math.floor(Math.random() * 100) + 1
+  }
+  cart.push(itemObj)
+  console.log(`${item} has been added to your cart.`)
+  return cart
+}
+
+function viewCart() {
+  if (cart.length > 0) {
+    let itemCostPairs = []
+    cart.forEach(line => {
+      for (let item in line) {
+        itemCostPairs.push(`${item} at $${line[item]}`)
+      }
+    })
+    console.log(`In your cart, you have ${itemCostPairs.join(', ')}.`)
+  } else {
+    console.log('Your shopping cart is empty.')
+  }
+}
+
+function removeFromCart(item) {
+  cart.forEach((line, index) => {
+    if (line.hasOwnProperty(item)) {
+      cart.splice(index, 1)
+      return cart
+    }
+  })
+  console.log('That item is not in your cart.')
+}
+
+function placeOrder(cardNumber) {
+  if (cardNumber === undefined) {
+    console.log("We don't have a credit card on file for you to place your order.")
+  } else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    cart = []
+  }
 }
 
 function total() {
