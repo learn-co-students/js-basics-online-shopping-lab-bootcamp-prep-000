8fb372492523fdcec4f58d7d1503c13ba8a29e7a
Graham Squires <graham.squires3@gmail.com>
2017-02-21 15:22:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,16 @@ function total() {
   }
 
   return t
+}
+
+function getCart(){
+
+}
+
+function addToCart(item){
+  var price = Math.random()
+  var itemObj = {item: price}
+  console.log(`${item} has been added to your cart`)
+  return cart
+
 }
