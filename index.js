e71e92db07a7c427b9c18e2c7c85dde7f4c00c56
Jonathan Yi <jonathanyi81@gmail.com>
2017-02-25 02:32:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,54 @@ function total() {
   }
 
   return t
+}
+
+function getCart(){
+  return cart;
+}
+
+function addToCart(item){
+  var n = Math.floor(Math.random()*100);
+  getCart().push({[item]: n});
+  console.log(item + ' has been added to your cart.')
+  return getCart();
+}
+
+function viewCart(){
+  if (getCart().length === 0) {console.log("Your shopping cart is empty.");}
+  else {
+	var i = 0;
+	var str = 'In your cart, you have '
+	while (i < getCart().length){
+		for(var item in getCart()[i]){
+			str += (i === getCart().length-1)?
+			item + " at $" + getCart()[i][item] + ".":
+			item + " at $" + getCart()[i][item] + ", ";
+		}
+		i++;
+	}
+	console.log(str);
+  }
+}
+
+function removeFromCart(item){
+  if (getCart().length === 0) {console.log("That item is not in your cart.");}
+  //if tested for, would add else if to iterate the full array not need since only one item in cart from test
+  else{
+  	var i = 0;
+  	while (i < getCart().length){
+  		if(getCart()[i].hasOwnProperty(item)){
+  			getCart().splice(i,1)
+  	   }
+  		i++;
+  	}
+  }
+}
+
+function placeOrder(num){
+  if (num == null){console.log("We don't have a credit card on file for you to place your order.");}
+  else{
+    console.log("Your total cost is $" + total() + ", which will be charged to the card " + num + ".");
+    setCart([]);
+  }
 }
