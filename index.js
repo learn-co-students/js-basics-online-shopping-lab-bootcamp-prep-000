9271cc081c1eeab95d75025ef9e614a7ce8b269e
Nathaniel Richardson <richardson.j.nathaniel@gmail.com>
2017-02-26 03:12:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var cart;
-
+cart = [];
 function setCart(newCart) {
   cart = newCart;
 }
-
-function total() {
-  let t = 0
-
-  for (var i = 0, l = cart.length; i < l; i++) {
-    for (var item in cart[i]) {
-      t += cart[i][item]
-    }
-  }
-
-  return t
+function gerCart() {
+  return cart
 }
+
+function addToCart(item) {
+  var price = Math.floor(Math.random() *100)
+   cart.push ({[item]:price});
+   console.log(item + " has been added to your cart.");
+   return cart
+ }
+
+ 
