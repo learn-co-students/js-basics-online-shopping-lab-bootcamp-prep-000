3bc36a6c68fcd319c8262056d6ee70691952a7f0
Kyle Andrews <kylemandrews@gmail.com>
2017-02-27 21:00:10 +0000

    Done.

--- index.js
+++ index.js
@@ -15,3 +15,52 @@ function total() {
 
   return t
 }
+
+var cart = []
+function getCart() {
+  return cart
+}
+
+function addToCart(item) {
+  var price = Math.floor(Math.random() * 100) // 0.546234352 * 100 54.6234352 >> 54
+  var object = {[item]: price}
+  cart.push(object);
+  console.log(`${item} has been added to your cart.`)
+  return cart
+}
+
+function viewCart() {
+if (cart.length > 0) {
+  var array = []
+for (var i = 0; i < cart.length; i++) {
+  var item = Object.keys(cart[i])[0]// Object.keys(obj)[0] === "pizza" // cart[i] == {pizza: 24}
+  var price = cart[i][item]// obj[key]
+  array.push(`${item} at $${price}`); // ['socks at $25', 'pizza at $24']
+}
+console.log(`In your cart, you have ${array.join(', ')}.`) // In your cart, you have socks at $25,pizza at $24.
+}
+else {
+  console.log("Your shopping cart is empty.")
+}
+}
+
+function removeFromCart(name) { // "pizza"
+    for (var i = 0; i < cart.length; i++) {
+      if (cart[i].hasOwnProperty(name)) { // object.hasOwnProperty(string)
+        delete cart.splice(i, 1) // [{}], destructively remove the object from the array (hint) splice(starting index, how many?)
+        return cart // what does return do for us?
+      }
+    }
+    console.log("That item is not in your cart.")
+  }
+
+
+function placeOrder(creditCardNumber) {
+  if (creditCardNumber === undefined) {
+    console.log("We don't have a credit card on file for you to place your order.")
+  }
+  else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${creditCardNumber}.`)
+    cart = []
+  }  
+}
