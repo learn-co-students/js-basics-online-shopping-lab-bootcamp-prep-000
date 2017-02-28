ce40be88eb617ef727dc93264818f2cfeee36ce6
Trac Nguyen <nguyendtrac@gmail.com>
2017-02-28 07:28:45 +0000

    Done.

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function getCart() {
 
 function addToCart(item) {
   var price = Math.floor(Math.random() * 10)
-  var obj = {item: price}
+  var obj = {[item]: price}
 
   cart.push(obj)
   console.log(`${item} has been added to your cart.`)
@@ -43,3 +43,26 @@ function viewCart() {
   }
   console.log('In your cart, you have ' + itemTemp.join(', ') + '.')
 }
+
+function removeFromCart(name) {
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(name)) {
+      cart.splice(i, 1)
+    }
+  }
+  var myObject = {}
+  myObject.prop = name
+  if (myObject.hasOwnProperty('prop')) {
+    console.log("That item is not in your cart.")
+  }
+}
+
+function placeOrder(ccnumber) {
+    if (!ccnumber) {
+      console.log("We don't have a credit card on file for you to place your order.")
+    }
+    else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${ccnumber}.`)
+    }
+    cart = []
+  }
