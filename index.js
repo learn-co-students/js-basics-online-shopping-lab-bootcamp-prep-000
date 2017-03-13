e9d54c36f1d87afc8dc8a7351b4706ef1413a027
Will Sauer <will@heueso.com>
2017-03-13 20:54:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-var cart;
+var cart = [];
 
-function setCart(newCart) {
+//---------------------------------------------------
+function setCart(newCart)
+{
   cart = newCart;
 }
 
-function total() {
+
+//---------------------------------------------------
+function total()
+{
   let t = 0
 
   for (var i = 0, l = cart.length; i < l; i++) {
@@ -14,4 +19,44 @@ function total() {
   }
 
   return t
+}
+
+
+//---------------------------------------------------
+var getCart = () =>
+{
+  return cart;
+}
+
+//---------------------------------------------------
+var addToCart = (item) =>
+{
+  let price = Math.floor(Math.random() * (100 - 1) + 1);
+  cart.push({[item]: price});
+  console.log(`${item} has been added to your cart.`)
+  return cart
+}
+
+//---------------------------------------------------
+var viewCart = () => {
+
+  if (cart.length === 0){
+    console.log('Your shopping cart is empty.');
+  }
+
+  else{
+        var cartContents = 'In your cart, you have';
+          for (var i = 0; i<cart.length; i++){
+            for (var eachItem in cart[i]){
+            cartContents = cartContents + ` ${eachItem} at $${cart[i][eachItem]}`;
+            if (i === cart.length - 1) {cartContents = cartContents +'.'; }
+            else { cartContents = cartContents +',';}
+            }
+          }
+      }
+
+        console.log(cartContents);
+      }
+
+
 }
