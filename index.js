6612fa4ce56c901d09f9eb6b3ccfcf56bfce2674
Samneang Seng <seng.samneang12@gmail.com>
2017-03-16 03:05:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,50 @@ function total() {
   }
 
   return t
+}
+
+function getCart(){
+  return cart;
+}
+
+function addToCart(item){
+  var price = Math.floor(Math.random()*100);
+  var itemPrice = {};
+  itemPrice[item] = price;
+  cart.push(itemPrice);
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+}
+
+function viewCart(){
+  if(cart.length>0){
+      const itemWithPrice = [];
+        for(let i = 0; i < cart.length; i++ ){
+          for(let item in cart[i]){
+            itemWithPrice.push(`${item} at $${cart[i][item]}`);
+          }
+        }
+            console.log(`In your cart, you have ${itemWithPrice.join(", ")}.`);
+        } else {
+            console.log ("Your shopping cart is empty.");
+  }
+}
+
+function removeFromCart(item){
+  for(var i = 0; i < cart.length; i++){
+    if (cart[i].hasOwnProperty(item)) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+  console.log("That item is not in your cart.");
+}
+
+function placeOrder(cardNumber){
+  if(!cardNumber){
+    console.log("We don't have a credit card on file for you to place your order.");
+  } else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    cart = [];
+  }
 }
