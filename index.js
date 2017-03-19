a6c3f71a7448ba116b20058cd9c1bd1654396e27
Josie Kelley <jcakelley@gmail.com>
2017-03-19 17:43:49 +0000

    Done.

--- index.js
+++ index.js
@@ -9,23 +9,36 @@ function getCart(){
 }
 
 function addToCart(item){
-  var number = Math.random()
-  var rounded = number * Math.floor()
-  var price = 1 + rounded * 100
-  cart.push(`{item:price}`)
+  var price = Math.floor(Math.random() * 100)
+  cart.push(`{item: price}`)
   console.log(`${item} has been added to your cart.`)
   return cart
 }
 
 function viewCart(){
+
   if(cart[0] === undefined){
     return "Your shopping cart is empty."
-  }
+    }
   else{
-    for(var item in cart){
-      console.log(`In your cart, you have ${item} for ${cart[item]}.`)
+  for(var i = 0; cart.length > i; i++){
+    Object.keys(cart[i])
+    console.log(`In your cart, you have ${item} at ${cart[item]}.`)
+
     }
   }
+}
+
+function removeFromCart(itemName){
+  if(cart.hasOwnProperty(`${itemName}`)){
+    var index = cart.indexOf(`${itemName}`)
+    cart.splice(index, 1)
+    return cart
+  }
+else{
+  return "That item is not in your cart."
+}
+
 
 }
 
