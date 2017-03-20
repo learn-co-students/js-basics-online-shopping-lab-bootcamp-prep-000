11d255544ddb02b9dc76edff87c345a87c1250bc
Kayvan Taherpour <kayvanote@gmail.com>
2017-03-20 00:40:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -13,5 +13,67 @@ function total() {
     }
   }
 
-  return t
+  return t;
+}
+
+function getCart() {
+
+
+  return cart;
+}
+
+function addToCart(item) {
+
+
+  getCart().push({[item]: Math.floor(Math.random() * 10)});
+  console.log(`${item} has been added to your cart.`);
+  return getCart();
+}
+
+function viewCart() {
+
+
+  if (cart.length > 0) {
+  var statement = "In your cart, you have";
+  var item;
+  for (var i = 0; i < cart.length ; i++){
+
+    item = Object.keys(cart[i]);
+    statement += ` ${item} at $${cart[i][item]}${(cart.length - 1 ) > i ? "," : "."}`;
+
+      }
+
+  
+      console.log(statement);
+
+    } else {
+
+  console.log("Your shopping cart is empty.");
+
+  }
+
+}
+
+function removeFromCart(item) {
+
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      cart.splice(i, 1)
+      return cart;
+    }
+  }
+     console.log("That item is not in your cart.");
+}
+
+
+function placeOrder(cardNumber) {
+
+  if (cardNumber > 0) {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+  //  for (var i = 0; i < cart.length ; i++) {
+       cart.splice(0, cart.length);
+  //  }
+  }
+
+    console.log("We don\'t have a credit card on file for you to place your order.");
 }
