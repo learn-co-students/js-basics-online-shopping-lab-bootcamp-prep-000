f4f0f27024d23ee22464572a2e4146564acccd5f
Mike Wong <michaelwong1391@gmail.com>
2017-03-31 03:58:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,44 @@ function total() {
   }
 
   return t
+}
+
+function getCart() {
+  return cart;
+}
+
+function addToCart(item) {
+  var price = Math.floor(Math.random() * 100);
+  cart = Object.keys([item]);
+  cart.item = price;
+  console.log(item + " has been added to your cart.");
+  return getCart();
+}
+
+function viewCart() {
+  if (cart = []) {
+    console.log("Your shopping cart is empty.");
+  } else {
+    for (var i = 0; i < cart.length; i++) {
+      return `In your cart, you have ${cart} ${cart[i]}.`;
+      }
+  }
+}
+
+function removeFromCart(item) {
+  if (cart.hasOwnProperty(item)) {
+    delete cart.item;
+    return array;
+  } else {
+    return "That item is not in your cart.";
+  }
+}
+
+function placeOrder(cardNumber) {
+  if (cardNumber = undefined) {
+    return "We don't have a credit card on file for you to place your order.";
+  } else {
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+  }
+  var cart = [];
 }
