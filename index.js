5006dc3be5ad5bd663b2c005d389881b0ffd8e9b
Nancy Noyes <nnoyes@comcast.net>
2017-04-04 14:40:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart =[];
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,34 @@ function total() {
   }
 
   return t
+}
+
+function getCart (){
+  var theCart=""
+  for (var i = 0, l = cart.length; i < l; i++) {
+    for (var item in cart[i]) {
+      theCart = theCart+Object.keys(cart[i])+" $"+cart[i][item]
+      if (i < cart.length-1) {
+        theCart = theCart+", "
+      }
+    }
+  }
+  return theCart
+}
+
+function addToCart (item) {
+  var price = Math.floor(Math.random()*100)
+  cart.push ({[item]:price})
+  console.log(item+" has been added to your cart.")
+  return getCart ()
+}
+
+function viewCart () {
+  if (cart.length === 0){
+    console.log("Your shopping cart is empty.")
+  } else {
+    var cartContents ="In your cart, you have "
+    cartContents =cartContents+getCart()
+  console.log(cartContents+".")
+  }
 }
