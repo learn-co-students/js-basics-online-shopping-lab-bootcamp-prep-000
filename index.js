fa8be988701c80ed3b1732954beecc2016857a4d
Mikka Pineda <mikkanthrope@gmail.com>
2017-04-07 21:07:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-var cart;
+var cart = [];
+
+function getCart() {
+  return cart;
+}
+
+function addToCart(item) {
+  var price = Math.floor(Math.random() * 100);
+  cart.push({ [item] : price });
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+}
+
+function viewCart() {
+
+  var itemNames = Object.keys(cart);
+  
+  while (cart.length > 0) {
+    for  (var itemNames in cart) {
+      return `In your cart, you have ${itemNames} at ${cart[price]}`;
+    }
+  }
+
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  }
+
+}
 
 function setCart(newCart) {
   cart = newCart;
