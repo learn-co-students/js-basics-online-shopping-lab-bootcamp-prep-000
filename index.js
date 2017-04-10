7bf095dbd13d3c019c7c418a4c76ca404f3cd129
Steven Ossorio <codebusiness2014@gmail.com>
2017-04-10 02:57:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
+}
+
+function getCart(){
+  return cart
+}
+
+function addToCart(item){
+  var newCart = {}
+  var price = Math.floor((Math.random() * 100) + 0);
+  newCart[item] = price
+  cart.push(newCart)
+  console.log(`${item} has been added to your cart.`)
+  return cart
+}
+
+function viewCart(){
+  if (cart.length == 0) {
+    console.log("Your shopping cart is empty.")
+  } else {
+    var anotherArr = [];
+    for (var newArr in cart) {
+        anotherArr.push(`${cart[newArr]} at $${newArr}`)
+    }
+    console.log(`In your cart, you have ${anotherArr.join(', ')}.`)
+  }
 }
 
 function total() {
