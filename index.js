83454f61ced1a79f777728b4c76a1465b9ab47ab
Steven Ossorio <codebusiness2014@gmail.com>
2017-04-11 00:48:59 +0000

    Done.

--- index.js
+++ index.js
@@ -23,11 +23,49 @@ function viewCart(){
   } else {
     var anotherArr = [];
     for (var newArr in cart) {
-        anotherArr.push(`${cart[newArr]} at $${newArr}`)
+      //debugger
+      if (cart.hasOwnProperty(newArr)) {
+        var item = Object.keys(cart[newArr])[0]
+        var price = cart[newArr][item]
+        anotherArr.push(`${item} at $${price}`)
+      }
     }
     console.log(`In your cart, you have ${anotherArr.join(', ')}.`)
   }
 }
+
+// Deletes an entire object from the array.
+function removeFromCart(itemName){
+  var inCart = false
+  for(var item in cart){
+    if(cart[item].hasOwnProperty(itemName)) {
+      //var item = Object.keys(cart[newArr])[0]
+      // if (item === itemName) {
+      //var index = cart[item].indexOf(itemName)
+      cart.splice(item, 1)
+      //delete cart[item]
+      inCart = true
+      //cart.splice(0, 1)
+      // var index = item.indexOf(itemName) other ways to do it and passing.
+      // }
+    }
+  }
+  if (!inCart) {
+    //debugger
+    console.log('That item is not in your cart.')
+  }
+  debugger
+  return cart
+}
+
+//delete the "key" in obj1 should look like delete array['obj1']['key']
+// Another variation but still deletes an entire object from the array.
+// function removeFromCart(item){
+//   for(var items in cart){
+//     if(cart[items].hasOwnProperty(item)){
+//       cart.splice(items,1);
+//       return cart;
+
 
 function total() {
   let t = 0
