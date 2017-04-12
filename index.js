768bfa4c70dbe4eb44912fc7d04f11975c764d2a
Venessa  Stewart <stewart.venessa@gmail.com>
2017-04-12 19:09:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +15,41 @@ function total() {
   }
 
   return t
+}
+
+function getCart() {
+  return cart;
+}
+
+function addToCart(item) {
+  var price = Math.floor((Math.random() * 100) + 0);
+  cart.push({[item] : price});
+  console.log (`${item} has been added to your cart.`);
+  return cart;
+}
+
+function viewCart() {
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.")
+  } else {
+    var have = "In your cart, you have";
+    for(var i = 0; i < cart.length; i++) {
+      for(var item in cart[i]) {
+      have += ` ${item} at $${cart[i] [item]}`;
+    }
+    have += i < cart.length -1 ? "," : ".";
+  }
+  console.log(have);
+}
+}
+
+function removeFromCart(item) {
+  for(var i = 0; i < cart.length; i++) {
+    if(cart[i].hasOwnProperty([item])) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+  console.log("That item is not in your cart.");
+  return cart;
 }
