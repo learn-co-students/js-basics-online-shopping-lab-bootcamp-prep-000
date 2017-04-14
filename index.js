265a7a173fb8c5bf6ad15cc7afc973199a8468c8
Avery Carty <avery.carty7@gmail.com>
2017-04-14 03:07:10 +0000

    Done.

--- index.js
+++ index.js
@@ -15,3 +15,52 @@ function total() {
 
   return t
 }
+
+var getCart = () => {return cart;}
+
+var addToCart = (item) => {
+    var itemObj = {}
+    var rand = Math.floor((Math.random() * 100) + 1)
+
+    itemObj[`${item}`] = rand
+
+    cart = [...cart, itemObj]
+
+    console.log(item + " has been added to your cart.")
+
+    return cart
+}
+
+var viewCart = () => {
+    var itemList = []
+    for(var i = 0; i < cart.length; i++) {
+        for (var item in cart[i]) {
+            itemList.push(" " + item + " at $" + cart[i][item])
+        }
+    }
+
+    console.log(cart.length > 0 ? "In your cart, you have" +
+        itemList.join() + "." : "Your shopping cart is empty.")
+}
+
+var removeFromCart = (itemToRemove) => {
+    var removed = false
+
+    for (var i = 0; i < cart.length; i++) {
+        if (cart[i].hasOwnProperty(`${itemToRemove}`)) {
+            cart.splice(i,1)
+            removed = true
+        }
+    }
+
+    if (!removed) {
+        console.log("That item is not in your cart.")
+    }
+
+    return cart
+}
+
+var placeOrder = (ccn) => {
+    console.log(ccn === undefined ? "We don't have a credit card on file for you to place your order." : `Your total cost is $${total()}, which will be charged to the card ${ccn}.`)
+    cart = []
+}
