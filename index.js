0d7aededc6eaf918187eac02b3a431f4f837cb1f
Matthew Van Dyke <matthewlvandyke@gmail.com>
2017-04-15 18:52:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var cart;
+var cart = [];
 
-function setCart(newCart) {
+function setCart(newCart) { //only assigns
   cart = newCart;
 }
 
@@ -14,4 +14,66 @@ function total() {
   }
 
   return t
+}
+
+function getCart() {
+  return cart;
+}
+
+function addToCart(item) {
+  var itemObj = {};
+  itemObj[item] = (Math.floor(Math.random() * 100));
+  cart.push(itemObj);
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+}
+
+function viewCart() {
+  if (cart.length > 0) {
+    var str = `In your cart, you have `;
+    for (var i = 0; i < cart.length - 1; i++) {
+      var item = cart[i];
+
+      for (var key in item) {
+        str += `${key} at $${item[key]}, `;
+      }
+
+    }
+
+    for (var k in cart[cart.length - 1]) {
+      str += `${k} at $${cart[cart.length - 1][k]}.`;
+    }
+
+    console.log(str);
+
+  } else {
+    console.log("Your shopping cart is empty.");
+  }
+}
+
+function removeFromCart(item) {
+  for (var i = 0; i < cart.length; i++) {
+
+    var itemObj = cart[i];
+
+    for (var key in itemObj) {
+
+      if (itemObj[key] !== undefined) {
+        cart = cart.slice(0, i) + cart.slice(i + 1);
+        return cart;
+      }
+
+    }
+  }
+
+  console.log("That item is not in your cart.");
+}
+
+function placeOrder(cardNumber) {
+  if (cardNumber === undefined) {
+    console.log("We don't have a credit card on file for you to place your order.");
+  } else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    cart.length = 0;
+  }
 }
