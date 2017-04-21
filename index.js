bd17558658abf4a9f1a3e99f0ced294380be6ed0
Joseph Doyle <j.doyle345@gmail.com>
2017-04-21 00:01:12 +0000

    Done.

--- index.js
+++ index.js
@@ -15,3 +15,39 @@ function total() {
 
   return t
 }
+
+function getCart() {
+  return cart;
+}
+
+function addToCart(item) {
+  var num = Math.random()*100;
+  var price = Math.floor(num);
+  var object = {};
+  object[item] = price;
+  cart.push(object);
+  console.log(`${item} has been added to your cart.`)
+  return cart;
+}
+
+function viewCart() {
+  var array = [];
+  if (cart.length>0){
+    for(var i=0; i<cart.length; i++) {
+      var cartNamesi = Object.keys(cart[i])
+      array.push(` ${cartNamesi} at $${cart[i][cartNamesi]}`)
+        }
+    console.log(`In your cart, you have${array}.`)
+  }
+  if (cart.length===0) {
+    console.log(`Your shopping cart is empty.`)
+  }
+}
+
+function removeFromCart(item) {
+  for(var i=0; i<cart.length; i++) {
+    var cartNamesi = Object.keys(cart[i])
+    if (if cart[i].hasOwnProperty(item)===true) {
+      cart.pop()
+    }
+}
