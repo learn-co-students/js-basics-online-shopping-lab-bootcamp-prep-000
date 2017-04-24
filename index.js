dc94f47132bec8b306b98fd5de6ae72316c9412b
Arjen Buising <arjenbuising@gmail.com>
2017-04-24 15:37:10 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,35 @@ function setCart(newCart) {
   cart = newCart;
 }
 
+function addToCart(item) {
+  console.log(`${item} has been added to your cart.`)
+  cart.push(item)
+  return cart
+}
+
+function getCart() {
+  return cart
+}
+
+function viewCart() {
+  if (cart.length < 1) {
+    console.log('Your shopping cart is empty.')
+    return
+  }
+  var cartContents
+  for (var key in cart) {
+    var cartLength = cart.length
+    cartContents = `${cart[key]} at $${cart[price]}`
+    cartLength--
+    if (cartLength < 1) {
+      cartContents = `${cartContents}.`
+    } else {
+      cartContents = `${cartContents}, `
+    }
+  }
+  console.log(`In your cart, you have ${cartContents}`)
+}
+
 function total() {
   let t = 0
 
