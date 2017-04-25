32cb2990b59c923e1c1c759de6f267f2a1b7e5e8
Arjen Buising <arjenbuising@gmail.com>
2017-04-25 10:33:04 +0000

    Done.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function setCart(newCart) {
 
 function addToCart(item) {
   console.log(`${item} has been added to your cart.`)
-  cart.push(item)
+  cart.push({item: woordwaarde(item)})
   return cart
 }
 
@@ -35,6 +35,7 @@ function viewCart() {
 }
 
 function total() {
+  var cart = getCart()
   let t = 0
 
   for (var i = 0, l = cart.length; i < l; i++) {
@@ -44,4 +45,30 @@ function total() {
   }
 
   return t
+}
+
+function woordwaarde(query) {
+  var queryNumbers = new Array(query.length);
+  // var vert = 150;
+  // var hor = 10;
+  for (i = 0; i < query.length; i++) {
+      queryNumbers[i] = Number(query.charCodeAt(i) != 32 ? query.charCodeAt(i) - 64 : 0);
+      // ctx.fillText(query.charCodeAt(i) != 32 ? query.charCodeAt(i) - 64 : 0, i * 1 + hor, vert);
+      // hor += 45;
+  }
+  for (var key in queryNumbers) {
+      woordwaarde += parseInt(queryNumbers[key]);
+  }
+
+  // var debug = 175;
+  while (woordwaarde > 9) {
+      // ctx.fillText(woordwaarde, 10, debug += 35);
+      var newWoordwaarde = Number(0);
+      for (j = 0; j < woordwaarde.toString().length; j++) {
+          newWoordwaarde += Number(woordwaarde.toString().charAt(j));
+      }
+      woordwaarde = newWoordwaarde;
+  }
+
+  return woordwaarde
 }
