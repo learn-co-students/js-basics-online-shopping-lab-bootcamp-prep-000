52c9d82f9f6397785be07770442673584e6ba14d
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-05-02 13:58:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,52 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
+}
+
+function getCart() {
+  return cart;
+}
+
+function addToCart(item){
+  var itemPrice = Math.floor(Math.random() * 100) + 1;
+  var obj = {[item]: itemPrice};
+  cart.push(obj);
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+}
+
+function viewCart() {
+  if (cart.length == 0){
+    console.log("Your shopping cart is empty.")
+  } else {
+    var s = "In your cart, you have"
+    for (var i = 0; i < cart.length; i++) {
+      for (var item in cart[i]) {
+        s += ` ${item} at $${cart[i][item]}${i == cart.length - 1 ? "." : ","}`;
+      }
+    }
+    console.log(s);
+  }
+}
+
+function removeFromCart (item) {
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+  console.log("That item is not in your cart.")
+}
+
+function placeOrder (cardNumber) {
+  if (cardNumber) {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    cart = [];
+  } else {
+    console.log('We don\'t have a credit card on file for you to place your order.')
+  }
 }
 
 function total() {
