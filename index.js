81eeb0cce755c5ac1dc4d3f949dda721c9d5942b
Adames Hodelin <adameshh@gmail.com>
2017-05-03 17:26:49 +0000

    Done.

--- index.js
+++ index.js
@@ -15,3 +15,57 @@ function total() {
 
   return t
 }
+
+function getCart() {
+  return cart
+}
+
+function addToCart(item) {
+  var price = Math.floor(Math.random() * 99)
+  cart.push({[item]:price})
+  console.log(`${item} has been added to your cart.`)
+  return cart
+}
+
+function viewCart() {
+  if(cart.length === 0){
+    console.log(`Your shopping cart is empty.`)
+  } else {
+      var myResponse = "In your cart, you have "
+      for (var i = 0, l = cart.length; i < l; i++){
+          for(var list in cart[i]){
+            myResponse += `${list} at $${cart[i][list]}`
+            if(i !== cart.length - 1){
+              myResponse+=", "
+            }
+            else{
+              myResponse+="."
+            }
+          }
+      }
+      console.log(myResponse)
+  }
+}
+
+function removeFromCart(item){
+  for(var i = 0, l = cart.length; i < l; i++){
+    for(var list in cart[i]){
+        if(item === list){
+          cart.splice(i,1)
+          return cart
+        }
+    }
+  }
+  console.log("That item is not in your cart.")
+  return cart
+}
+
+function placeOrder(cardNumber) {
+  if (cardNumber === undefined){
+    console.log("We don't have a credit card on file for you to place your order.")
+  } else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    cart = []
+    return cart
+  }
+}
