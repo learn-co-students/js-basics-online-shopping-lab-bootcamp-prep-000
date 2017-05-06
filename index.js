e56f0408f6fced6b986b9a6b46e0e6b5856e79a0
Sean Denison <sean.denison26@gmail.com>
2017-05-06 21:52:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,60 @@
-var cart;
+var cart = [];
+
+function getCart() {
+  return cart;
+}
+
+function addToCart(item) {
+  var price = Math.floor(Math.random() * 100);
+  cart = cart.concat({[item]: price});
+  console.log(item + " has been added to your cart.");
+  return cart;
+}
+
+addToCart("pizza");
+
+function viewCart() {
+      if (cart.length === 0) {
+      console.log("Your shopping cart is empty.");
+      }
+      else {
+      let cartStr = cart.map((obj) => {
+        let item = Object.keys(obj).join("");
+        return item + " at $" + obj[item];
+      });
+      console.log("In your cart, you have "  + cartStr.join(", ") + ".");
+      }
+}
+
+function removeFromCart(item) {
+  let newCart = cart, remove = false;
+  newCart = newCart.filter((obj) => {
+    if (obj.hasOwnProperty(item)) {
+      remove = true;
+      return false;
+    }
+    else {
+      return true;
+    }
+  });
+  if(remove === false) {
+    console.log("That item is not in your cart.")
+  }
+  else {
+    cart = newCart;
+    return cart;
+  }
+}
+
+function placeOrder(creditcard) {
+  if(!creditcard) {
+    console.log("We don't have a credit card on file for you to place your order.");
+  }
+  else {
+    console.log("Your total cost is $" + total() + ", which will be charged to the card " + creditcard + ".");
+  }
+  cart = [];
+}
 
 function setCart(newCart) {
   cart = newCart;
