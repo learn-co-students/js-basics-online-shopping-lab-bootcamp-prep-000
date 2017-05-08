67f239a719254ff5b546a9a032daefe260cf46d5
Jennifer Watson <jennifer.watson@me.com>
2017-05-08 19:42:33 +0000

    Done.

--- index.js
+++ index.js
@@ -21,21 +21,39 @@ function getCart() {
 }
 
 function addToCart(item) {
-  var price = //random price
-  cart.push(item}: price);
+  var price = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+  cart.push(`${item}: ${price}`);
   console.log(`${item} has been added to your cart.`);
   return cart;
 }
 
 function viewCart() {
+  var i = 0;
   if (cart.length > 0) {
-    //iterate over the object list
-    return `In your cart you have ${item[i]:${price[i]}`; //rewrite code
+    for (i = 0; i < cart.length - 1; i++)
+    var cartContents = [];
+    cartContents.push(`${item[i]} at ${price[i]}`);
+    return `In your cart, you have ${cartContents}.`
   } else {
     return "Your shopping cart is empty."
   }
 }
 
 function removeFromCart(item) {
-
+  if (cart.length > 0 && cart.hasOwnProperty(item)){
+  cart.pop(cart[item]);
+  return cart;
+  } else {
+  return "That item is not in your cart.";
+  }
 }
+
+/* function placeOrder(cardNumber) {
+  if (cardNumber !== null){
+
+    return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`;
+  } else {
+    return "We don't have a credit card on file for you to place your order.";
+  }
+  cart.length = 0;
+} */
