e71e1b922cb447c5689a3f066c37b668dcc577c2
Jennifer Watson <jennifer.watson@me.com>
2017-05-15 19:56:28 +0000

    Done.

--- index.js
+++ index.js
@@ -21,21 +21,28 @@ function getCart() {
 
 function addToCart(item) {
   var price = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
-  cart.push(`${item} : ${price}`);
+  cart.push({[item]: price});
   console.log(`${item} has been added to your cart.`);
   return cart;
 }
 
 function viewCart() {
-  //var cartContents = [];
-    for (var item in cart){
-    //cartContents.push(`${item} at ${cart[item]},`);
-    console.log(`In your cart, you have ${item} at ${cart[item]}`) ;
-    }
-    if (cart.length < 0) {
-      console.log("Your shopping cart is empty.");
+    var i = 0;
+    var cartContents = [];
+    if (cart.length > 0) {
+      for (i = 0; i < cart.length; i++) {
+        var cartKeys = Object.keys(cart[i]);
+        var item = cartKeys[0];
+        var price = cart[i][item];
+        cartContents.push(`${item} at ${cart[item]}`);
+        var cartList = cartContents.join();
+      }
+      console.log(`In your cart, you have ${cartList}`) ;
+    } else {
+        console.log("Your shopping cart is empty.");
     }
 }
+
 
 
 function removeFromCart(item) {
