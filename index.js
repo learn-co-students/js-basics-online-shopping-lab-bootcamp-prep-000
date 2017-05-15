a9377acf228c61bac1d299021ced40182d66ca95
brandon corey <bcorey85@gmail.com>
2017-05-15 22:58:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = []
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,37 @@ function total() {
   }
 
   return t
+}
+
+function getCart() {
+  return(cart)
+}
+
+function addToCart(item){
+  var price = Math.floor(Math.random() * 100)
+  cart.push(`{${item}: ${price}}`)
+  console.log(`${item} has been added to your cart.`)
+  return(cart)
+}
+
+
+
+function viewCart() {
+var itemName = Object.keys(cart)
+var itemList = []
+if(cart.length == 0){
+console.log('Your shopping cart is empty.')
+}else{
+  for(var itemName in cart)
+  itemList.push(`${itemName} at ${cart[itemName]}`)
+}
+  return(`In your cart, you have ${itemList.join(`, `)}. `)
+}
+
+function removeFromCart(name){
+
+}
+
+function placeOrder(number){
+
 }
