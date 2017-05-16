a6ccc0174d9d8c561acdba8072acb951af73eef2
Yosa Yon <yosa.yon@gmail.com>
2017-05-16 23:44:05 +0000

    Done.

--- index.js
+++ index.js
@@ -15,3 +15,48 @@ function total() {
 
   return t
 }
+
+function getCart(){
+  return cart;
+}
+
+function addToCart(item){
+  let cost = Math.floor(Math.random() * 100);
+  cart.push({[item]: cost});
+  console.log(item + ' has been added to your cart.');
+  return cart;
+}
+
+function viewCart(){
+  let printItemsAndCost = 'In your cart, you have ';
+  if (cart.length === 0){
+    console.log('Your shopping cart is empty.');
+  }else{
+    for(var item in cart){
+      for(var cost in cart[item]){
+        printItemsAndCost += Object.keys(cart[item]) + ' at $' + cart[item][cost] + ', ';
+    }
+  }
+}
+  printItemsAndCost = printItemsAndCost.slice(0,printItemsAndCost.length-2).concat('.');
+  console.log(printItemsAndCost);
+}
+
+function removeFromCart(item){
+  for(let x in cart){
+    if(cart[x].hasOwnProperty(item)){
+      cart.splice(cart.indexOf(cart[x]),1);
+      return cart;
+    }
+  }
+  console.log('That item is not in your cart.');
+}
+
+function placeOrder(ccNumber){
+  if(ccNumber === undefined){
+    console.log('We don\'t have a credit card on file for you to place your order.');
+  }else{
+    console.log('Your total cost is $' + total() + ', which will be charged to the card ' + ccNumber + '.');
+  }
+  cart = [];
+}
