8ecc3cd48c113a0eec8b9e08fee1067ee0dd45e9
Andrew Peters <andrewjoelpeters@gmail.com>
2017-05-18 14:53:20 +0000

    Done.

--- index.js
+++ index.js
@@ -28,12 +28,45 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  if (cart.length === 0) {
-    return "Your shopping cart is empty."
+  let l = cart.length
+
+  if (l === 0) {
+    return console.log("Your shopping cart is empty.")
+  }
+
+    const allItemsAndPrices = []
+
+  for (let i = 0; i < cart.length; i++) {
+      let itemPriceIndex = cart[i]
+      let item = Object.keys(itemPriceIndex)[0]
+      let price = itemPriceIndex[item]
+
+      allItemsAndPrices.push(`${item} at $${price}`)
+    }
+    return console.log(`In your cart, you have ${allItemsAndPrices.join(', ')}.`)
+}
+
+function removeFromCart(item) {
+  let itemInCart = false
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      cart.splice(i,1)
+      itemInCart = true
+      return cart
+    }
+  }
+  if (itemInCart === false) {
+    return console.log(`That item is not in your cart.`)
+  }
+}
+
+function placeOrder(cardNumber) {
+  if (typeof cardNumber === "undefined") {
+    return console.log(`We don't have a credit card on file for you to place your order.`)
   }
   else {
-    for (var item in cart) {
-      console.log(`You have ${item} at $${cart[item]}`)
-    }
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    cart = []
+    return
   }
 }
