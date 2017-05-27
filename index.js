ca54247c04cef50bb97aaedf8eeed392a990af7b
Myles Kilgallon <learn@myles.uk>
2017-05-27 13:07:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-var cart;
+
+
+/*for (var mealName in meals) {
+  console.log(`I ate ${meals[mealName]} for ${mealName}!`)
+}*/
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +19,36 @@ function total() {
   }
 
   return t
+}
+
+function getCart(){
+  return cart
+}
+
+function addToCart(item){
+  var cartItem = {}
+  cartItem.itemName = item
+  cartItem.itemPrice = Math.random
+  cart.push({itemPrice})
+  console.log(`${item} has been added to your cart.`);
+  return cart
+}
+
+function viewCart(){
+  if (cart.length > 0) {
+    for (var i = 0; i < cart.length; i++) {
+      console.log(`In your cart, you have ${cart.itemPrice[i]} at ${cart.itemPrice[i]}.`);
+    }
+    } else {
+      console.log("Your shopping cart is empty.");
+  }
+}
+
+function placeOrder(cardNumber){
+  if (cardNumber === ""){
+    console.log("We don't have a credit card on file for you to place your order.")
+  } else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    cart = []
+  }
 }
