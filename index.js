cbf2a14b420b11939e60a036704da147c5ca3488
Anderson Manosalva <tripkler@hotmail.com>
2017-06-02 17:33:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-var cart;
+var cart=[];
+
+function getCart(){
+  return cart;
+}
+
+function addToCart(newItem){
+  var price = Math.floor(Math.random()*10)
+  var obj = {[newItem]: price}
+  cart.push(obj)
+  console.log(`${newItem} has been added to your cart.`)
+  return cart
+}
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +26,48 @@ function total() {
   }
 
   return t
+}
+
+
+function viewCart(){
+  if (cart.length === 0){
+    console.log(`Your shopping cart is empty.`)
+  }
+  else {
+    var str=`In your cart, you have`
+    for (var i=0; i<cart.length; i++) {
+        var it = Object.keys(cart[i])
+         str += ` ${it} at $${cart[i][it]},`
+     }
+     str = str.slice(0, -1);
+     str += `.`
+     console.log(str)
+  }
+ return
+}
+
+
+function removeFromCart(itemName){
+  for (var i=0; i<cart.length; i++) {
+      if (`${Object.keys(cart[i])}` === itemName) {
+          cart.splice(i,1);
+          return cart
+      }
+  }
+  console.log(`That item is not in your cart.`)
+  return cart
+}
+
+function placeOrder(number){
+
+  if(number === undefined){
+    console.log(`We don't have a credit card on file for you to place your order.`)
+  }
+
+  else {
+    var tot = total()
+    console.log(`Your total cost is $${tot}, which will be charged to the card ${number}.`)
+  }
+  cart = []
+  return cart
 }
