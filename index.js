1e544997dd822f61cd84260cedce097f52fa6b88
Matt Faircloth <faircloth.matthew@gmail.com>
2017-06-03 16:14:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
-var cart;
+var cart = []
+
+function getCart() {
+  return cart
+}
 
 function setCart(newCart) {
-  cart = newCart;
+  cart = newCart
+}
+
+function addToCart(item) {
+  const price = Math.floor(Math.random() * 100)
+  cart.push({ [item]: price });
+  console.log(`${item} has been added to your cart.`);
+
+  return cart
+}
+
+function viewCart() {
+  if (!cart.length) {
+    console.log("Your shopping cart is empty.")
+  } else {
+    var items = [];
+    for (var i = 0; i < cart.length; i++) {
+      for (var item in cart[i]) {
+        items.push(item + " at $" + cart[i][item])
+      }
+    }
+    console.log("In your cart, you have " + items.join(", ") + ".");
+  }
+}
+
+function removeFromCart(item) {
+  var itemInCart = false
+  for(var i = 0; i < cart.length; i++){
+    if (cart[i].hasOwnProperty(item)) {
+      itemInCart = true;
+      cart.splice(i, 1);
+    }
+  }
+  if (!itemInCart) {
+    console.log("That item is not in your cart.");
+  }
+  return cart
 }
 
 function total() {
@@ -14,4 +54,14 @@ function total() {
   }
 
   return t
+}
+
+function placeOrder(cardNumber) {
+  if (!cardNumber) {
+    return console.log("We don't have a credit card on file for you to place your order.")
+  }
+
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+
+  cart = []
 }
