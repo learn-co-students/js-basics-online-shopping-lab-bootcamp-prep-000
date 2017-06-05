21a8b9aefdf2ef0d2be007fd91fed344fbe5319b
JGBrown <j.grant.brown@gmail.com>
2017-06-05 13:40:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-var cart=[];
+var cart =[];
+
+function getCart(){
+  return cart
+}
 
 function setCart(newCart) {
   cart = newCart;
@@ -16,52 +20,47 @@ function total() {
   return t
 }
 
-function getCart(){
-  return cart
-}
 
 function addToCart(item){
-var price=Math.floor(Math.random() * 101);
-cart.push({[item]:price});
-console.log(item + " has been added to your cart.");
-return cart
+  var price = Math.floor(Math.random() * 101)
+  cart.push({[item]: price})
+  console.log(`${item} has been added to your cart.`)
+  return getCart()
 }
 
-
-function viewCart(){
-var itemsInCart= "";
-  if(cart.length<1){
-    console.log("Your shopping cart is empty.");
-}
-  else{
-      for (var i in cart){
-        var itemAndPrice= (`${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}, `)
-        itemsInCart+= itemAndPrice; //add to items in cart before the loop stars again
-}
-}
-console.log("In your cart, you have " + itemsInCart.slice(0,-2) +".");
-}
-
-
-function removeFromCart(removeitem){
-
-for (var i in cart){
-    if(cart[i].hasOwnProperty(removeitem) == true){
-      cart.splice(i, 1)
-        return cart
+function viewCart() {
+  if (!cart.length) {
+    console.log("Your shopping cart is empty.")
+  } else {
+    var items = [];
+    for (var i = 0; i < cart.length; i++) {
+      for (var item in cart[i]) {
+        items.push(item + " at $" + cart[i][item])
+      }
+    }
+    console.log("In your cart, you have " + items.join(", ") + ".");
   }
 }
-console.log("That item is not in your cart.");
 
+function removeFromCart(item){
+  for (let i=0; i < (cart.length); i++){
+  if (Object.keys(cart[i]).includes(item)==true){
+      cart.splice(i,1)
+      //     The second parameter of splice is the
+      //     number of elements to remove.
+      //     Note that splice modifies the array in place and returns
+      //     a new array containing the elements that have been removed.
+      return cart
   }
+}
+      console.log("That item is not in your cart.")
+}
 
-  function placeOrder(cardNumber){
-    if( cardNumber == null || undefined){
+function placeOrder(cardNumber){
+  if (cardNumber==null){
     console.log("We don't have a credit card on file for you to place your order.")
+  }else{
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
   }
-  else{
-    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
-  cart.splice(0)//empty cart
-  }
-
+  cart=[]
 }
