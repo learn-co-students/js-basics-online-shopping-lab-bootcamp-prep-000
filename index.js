f43285a78b733940599dfb6421106767fece970d
Sam Crowder <samecrowder@gmail.com>
2017-06-07 23:26:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var cart;
+
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +15,17 @@ function total() {
   }
 
   return t
+}
+
+function getCart()
+{
+  return cart;
+}
+
+function addToCart(item)
+{
+  var price = Math.floor((Math.random() * 100));
+  cart.push({item:price});
+  console.log(`${item} has been added to your cart.`);
+  return cart;
 }
