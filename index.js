be42da1734bc481057816adb419f2941d54b3596
David Goodman <davegoodman20@gmail.com>
2017-06-11 13:45:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-var cart;
+ var cart = [];
+
+function getCart(){
+  return cart;
+}
+
+function addToCart(item){
+   var itemPrice = Math.floor(Math.random() * 100)
+   cart.push({item: item.price})
+   console.log(`${item} has been added to your cart.`)
+   return cart
+}
+
+function viewCart() {
+   if(cart.length === 0) {
+     console.log('Your shopping cart is empty.')
+   }
+   else {
+     var newCart = []
+     for (var i = 0; i < cart.length; i++) {
+       for (var item in cart[i]) {
+         newCart.push(`${item} at $${cart[i][item]}`)
+       }
+     }
+     var joinNewCart = newCart.join(', ')
+     console.log("In your cart, you have " + joinNewCart + ".")
+   }
+ }
 
 function setCart(newCart) {
   cart = newCart;
