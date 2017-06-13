ba7af2db84473e2d2fef585a9f7bfdc13abed22c
David Goodman <davegoodman20@gmail.com>
2017-06-13 03:41:23 +0000

    Done.

--- index.js
+++ index.js
@@ -45,8 +45,10 @@ function placeOrder(cardNumber){
   if (!cardNumber) {
     console.log("We don't have a credit card on file for you to place your order.")
   }
+  else{
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
     cart = []
+  }
 }
 
 function setCart(newCart) {
