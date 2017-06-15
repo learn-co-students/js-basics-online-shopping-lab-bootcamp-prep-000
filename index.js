85ed4af8915407cbe044b018631609c6236ca69e
Cole Shapiro <colesayershapiro@gmail.com>
2017-06-15 22:45:28 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,69 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var itemPrice = Math.floor(Math.random() * 100) + 1;
+ cart.push({[item]:itemPrice});
+ console.log(`${item} has been added to your cart.`);
+ return cart
+
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+ console.log('Your shopping cart is empty.');
+} else if (cart.length <= 2) {
+  var newArray = [];
+  for (var i = 0; i < cart.length; i++){
+    var item = Object.keys(cart[i])[0];
+    var itemPrice = cart[i][item];
+    newArray.push(`${item} at $${itemPrice}`)
+  }
+  console.log(`In your cart, you have ${newArray.join(" and ")}.`);
+} else {
+  var newArray = [];
+  for (var i = 0; i < cart.length; i++){
+    var item = Object.keys(cart[i])[0];
+    var itemPrice = cart[i][item];
+    newArray.push(`${item} at $${itemPrice}`)
+  }
+  var last = newArray.pop();
+    console.log(`In your cart, you have ${newArray.join(", ")}, and ${last}.`);
+}
 }
 
 function total() {
-  // write your code here
+  var sum = 0
+  for (var i = 0; i < cart.length; i++) {
+    var item = Object.keys(cart[i])[0];
+    var itemPrice = (cart[i][item]);
+    sum += parseInt(itemPrice);
+  }
+  return sum;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var newCart = [];
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      cart.splice(i, 1);
+      newCart.push(item);
+      i--;
+    }
+  }
+  if (newCart.length === 0){
+    console.log('That item is not in your cart.')
+  }
+  return cart;
 }
 
+
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined){
+    console.log("Sorry, we don't have a credit card on file for you.");
+  } else {
+    var cartTotal = total();
+    console.log(`Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`)
+    cart = [];
+}
+return cart;
 }
