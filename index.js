c86e4174fd407b392c3b993bd7f16a2ff57c84b2
Tor Magnus Michaelsen <tormic1992@gmail.com>
2017-07-09 08:19:52 +0000

    Done.

--- index.js
+++ index.js
@@ -8,14 +8,33 @@ function setCart(c) {
   cart = c;
   return cart;
 }
-
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
 function addToCart(item) {
- // write your code here
+var itemName = item
+var itemPrice = getRandomIntInclusive(1,100)
+var item = new Object();
+item[itemName] = itemPrice;
+cart.push(item)
+console.log(`${itemName} has been added to your cart.`)
+return cart
 }
 
 function viewCart() {
-  // write your code here
-}
+  if (getCart().length === 0) {
+    console.log("Your shopping cart is empty.")
+  } else if (getCart().length === 1) {
+    console.log(`In your cart, you have
+      ${cart[0].itemName} at
+      $${cart[0].itemPrice}.`)
+    }
+  }
+
+
+
 
 function total() {
   // write your code here
