837b8c588b849ae3fca7324597efa195dbb58060
satya maddali <abhignyamaddali@gmail.com>
2017-07-15 18:12:43 +0000

    Done.

--- index.js
+++ index.js
@@ -11,10 +11,30 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var itemName=item;
+ var itemPrice = Math.floor((Math.random() * 100) +1);
+ var obj= {[itemName] : itemPrice};
+ cart.push(obj);
+ console.log(`${itemName} has been added to your cart.`);
+ return(cart);
 }
 
 function viewCart() {
   // write your code here
+  var string="In your cart, you have";
+if(cart.length===0)
+{
+  console.log("Your shopping cart is empty.");
+}
+else {
+  for(var i=0; i<cart.length;i++)
+  {
+  return(`${string} ${cart[i]} at ${cart[i]},`);
+}
+
+
+}
+
 }
 
 function total() {
