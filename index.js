5fc778e61d6a57e1db08cbc02065b583633b81e0
Jerry Noel <noel.jerry94@gmail.com>
2017-07-18 03:12:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
-var cart = [];
+var cart = {};
 
-function getCart() {
- return cart;
+function addToCart(name){
+var holder = Math.random() * 100;
+holder = Math.floor(holder);
+console.log(name + " has been added to your cart.");
+cart[name] = holder;
+return cart;
 }
 
-function setCart(c) {
-  cart = c;
-  return cart;
-}
-
-function addToCart(item) {
- // write your code here
-}
-
-function viewCart() {
-  // write your code here
-}
-
-function total() {
-  // write your code here
-}
-
-function removeFromCart(item) {
-  // write your code here
-}
-
-function placeOrder(cardNumber) {
-  // write your code here
+function viewCart(){
+  for (i in cart){
+       return    "In your cart, you have " + cart[i] + " at $" + i + " , " + cart[i] + " at $" +  i + ", " +  "and " +  cart[i] + " at " + $49 + ".";
+  }
 }
