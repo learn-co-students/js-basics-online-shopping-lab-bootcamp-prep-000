79eb475bd78555f993a58fb9dc2c8b5849e1d9f0
zarah s <leechte@gmail.com>
2017-07-20 00:06:01 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,62 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ cart.push({[item] : Math.floor(Math.random()*100)})
+ console.log(`${item} has been added to your cart.`)
+ return cart
 }
 
 function viewCart() {
-  // write your code here
-}
+  var cartItems = ["In your cart, you have "]
 
+   if (cart.length === 0) {
+     console.log("Your shopping cart is empty.")
+
+    } else if (cart.length === 1) {
+     cartItems.push(`${Object.keys(cart[0])[0]} at $${cart[0][Object.keys(cart[0])[0]]}.`)
+
+    } else if (cart.length === 2) {
+      cartItems.push(`${Object.keys(cart[0])[0]} at $${cart[0][Object.keys(cart[0])[0]]} `+
+      `and ${Object.keys(cart[1])[0]} at $${cart[1][Object.keys(cart[1])[0]]}.`)
+
+    } else if (cart.length > 2) {
+      for (var i = 0; i < cart.length-1; i++) {
+        cartItems.push(`${Object.keys(cart[i])[0]} at $${cart[i][Object.keys(cart[i])[0]]}, `);
+      };
+      cartItems.push(`and ${Object.keys(cart[cart.length-1])[0]} at ` +
+    `$${cart[cart.length-1][Object.keys(cart[cart.length-1])[0]]}.`);
+    };
+
+   console.log(cartItems.join(""));
+
+return cartItems.join("");
+}
 function total() {
-  // write your code here
+ var total = 0
+ for(let i = 0; i < cart.length; i++) {
+   var item_name = Object.keys(cart[i])
+   var price = parseInt(cart[i][item_name])
+   total += price
+ }
+ return total
 }
 
 function removeFromCart(item) {
-  // write your code here
+ for (let i = 0; i < cart.length; i++) {
+   if(cart[i].hasOwnProperty(item)){
+     cart.splice(i,1);
+     return cart;
+   }
+ }
+ console.log("That item is not in your cart.")
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber) {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+      cart.splice(0);
+  } else {
+    console.log("Sorry, we don't have a credit card on file for you.");
+  }
+return cart
 }
