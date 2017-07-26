d0b3883b85b4a64a9912c7cf172a1c3bbb1f528b
Mamadou Sand <shoudou@outlook.com>
2017-07-26 01:48:44 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var newItem = {}
+ newItem[item]= Math.floor((Math.random() * 100) + 1)
+ cart.push(newItem)
+ console.log(item+ "has been added to your cart.")
+ return cart
 }
 
 function viewCart() {
