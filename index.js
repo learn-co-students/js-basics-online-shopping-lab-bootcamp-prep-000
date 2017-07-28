98666b5426a7cf6682fbf41847ca0a11222fae1d
Kevin Kim <kevikim33@gmail.com>
2017-07-28 00:48:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var cart = [];
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
 function getCart() {
- return cart;
+  return cart;
 }
 
 function setCart(c) {
@@ -9,22 +15,67 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+  cart.push({[itemName] : getRandomInt(1, 100)});
+  console.log(`${itemName} has been added to your cart.`)
+  return cart;
 }
 
+//In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.
 function viewCart() {
-  // write your code here
+  const itemsAndPrice = [];
+  for (let i = 0; i < cart.length; i++) {
+    for (var itemName in cart[i])
+    itemsAndPrice.push(`${itemName} at $${cart[i][itemName]}`);
+  }
+  if (!cart.length) {
+    return console.log(`Your shopping cart is empty.`);
+  } else if (cart.length === 1) {
+    return console.log(`In your cart, you have ${itemsAndPrice}.`);
+  } else if (cart.length === 2) {
+    return console.log(`In your cart, you have ${itemsAndPrice.join(' and ')}.`);
+  } else if (cart.length > 2) {
+    return console.log(`In your cart, you have ${itemsAndPrice.slice(0, itemsAndPrice.length - 1).join(', ')}, and ${itemsAndPrice.slice(-1).join('')}.`);
+  }
 }
 
 function total() {
-  // write your code here
+  var total = 0;
+  for (var i = 0; i < cart.length; i++) {
+    for (var itemName in cart[i]) {
+      total+= cart[i][itemName];
+    }
+  }
+  return total;
 }
 
-function removeFromCart(item) {
-  // write your code here
+function removeFromCart(itemName) {
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(itemName)) {
+      cart.splice(i,1);
+      //delete cart[i][itemName];
+      return cart;
+    }
+  }
+  console.log('That item is not in your cart.');
+  return cart;
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined) {
+    console.log("Sorry, we don't have a credit card on file for you.");
+  } else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    cart = [];
+  }
 }
+
+addToCart('bananas');
+addToCart('pancake batter');
+addToCart('eggs');
+console.log(cart);
+
+//console.log(viewCart());
+//console.log(total());
+
+//placeOrder();
