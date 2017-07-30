8a3366c280957e50bd6d4b72f44049559c59230b
Will Middlebrook <will.d.middlebrook@gmail.com>
2017-07-30 03:41:15 +0000

    Done.

--- index.js
+++ index.js
@@ -9,21 +9,71 @@ function setCart(c) {
   return cart;
 }
 
+
 function addToCart(item) {
- // write your code here
+  var random = Math.floor((Math.random() * 100) + 1);
+  var newobj = {};
+  newobj[item]= random;
+  cart.push (newobj);
+  console.log(`${item} has been added to your cart.`);
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  for (var i = 0; i < cart.length; i++) {
+    var key = Object.keys(cart[i])[0] //still slightly confused about the formating of [0]
+    if (cart.length === 1) {
+      console.log(`In your cart, you have ${key} at $${cart[i][key]}.`);
+    }
+  }
+for (var i = 0; i < [cart.length -1]; i++) {
+  var last = cart.length-1
+  var lastkey = Object.keys(cart[last])
+  var key2 = Object.keys(cart[i])[0] //still slightly confused about the formating of [0]
+  if (cart.length === 2) {
+    console.log(`In your cart, you have ${key2} at $${cart[i][key2]} and ${lastkey} at $${cart[last][lastkey]}.`);
+  }
 }
 
-function total() {
-  // write your code here
+for (var i = 0; i < [cart.length -1]; i++) {
+  var last = cart.length-1
+  var lastkey = Object.keys(cart[last])
+  var key2 = Object.keys(cart[i])[0]
+  var statement = ('you have' ${key2} ' at ' {cart[i][key2])})
+  if (cart.length > 2) {
+    console.log(`In your cart, ${statement}, and ${lastkey} at $${cart[last][lastkey]}.`);
+  }
+}
+  if (cart.length===0) {
+  console.log (`Your shopping cart is empty.`)
+}
 }
 
-function removeFromCart(item) {
-  // write your code here
-}
+// function total() {
+// var zero = 0
+// for (var i = 0; i < cart.length; i++) {
+//   var keys = Object.keys(cart[i])
+//   return zero += cart[i][keys];
+// }
+// }
+
+// function removeFromCart(item) {
+//   var 0 ="true"
+//   var 1 ="false"
+//   var booleanarray=[]
+//   var boozero = 0
+//   for (var i = 0; i < cart.length; i++) {
+//     booleanarray.push(cart[i].hasOwnProperty(item))
+//     if ((boozero += booleanarray[i]) === 0) {
+//       console.log (`That item is not in your cart.`)
+//       return cart;
+//     }
+//     else {
+//       delete cart.item;
+//       return cart;
+//     }
+//   }
+// }
 
 function placeOrder(cardNumber) {
   // write your code here
