ffeecb53432493a8ce05fddf0abc8bc7922e5df2
Katy Eng <katye610@gmail.com>
2017-07-31 13:45:03 +0000

    Done.

--- index.js
+++ index.js
@@ -10,8 +10,18 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
-}
+ // Price is a random integer between 1 and 100
+   var itemPrice = Math.floor((Math.random() * 99) + 1)
+ // Use item string to create new object representing item. Should have one key-pair value, itemName: price
+   var cart = new Object({})
+ // As more items are added, the object should expand
+   cart[item] = [itemPrice]
+ // Upon successful addition, print ``<itemName> has been added to your cart.`
+   console.log(`${item} has been added to your cart.`);
+ // Return updated cart to console
+   return cart
+ }
+
 
 function viewCart() {
   // write your code here
