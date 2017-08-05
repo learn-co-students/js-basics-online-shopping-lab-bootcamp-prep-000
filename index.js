5edb78594b7fa9ef926bdc3c0a628975fb3f9101
Araba Taylor <araba.taylor20@gmail.com>
2017-08-05 03:54:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var cart = [];
 
 function getCart() {
- return cart;
+  return cart;
 }
 
 function setCart(c) {
@@ -9,22 +9,68 @@ function setCart(c) {
   return cart;
 }
 
+
 function addToCart(item) {
- // write your code here
+  var itemPrice = Math.floor(Math.random()*10)
+  var itemName = item
+  item = {[itemName]: itemPrice};
+  cart.push(item);
+  console.log(`${itemName} has been added to your cart.`);
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length===0) {
+    console.log("Your shopping cart is empty.")
+  }
+  else {
+    var sentence = `In your cart, you have `
+    for(var i=0; i < cart.length; i++){
+      var itemsAndPrice = cart[i]     //a single object in the cart {socks: 42}
+      var itemName = Object.keys(itemsAndPrice)[0]
+      var itemPrice = itemsAndPrice[itemName]
+      if ((i+1 < cart.length|| cart.length===1) && cart.length!==2){
+        sentence += `${itemName} at $${itemPrice}, `}
+      else if (i+1 < cart.length && cart.length===2) {
+          sentence += `${itemName} at $${itemPrice} `
+      }
+      else {
+          sentence += `and ${itemName} at $${itemPrice}  `
+      }
+    }
+
+    sentence = sentence.slice(0, sentence.length - 2) + "."
+    console.log(sentence)
+  }
+
 }
+
 
 function total() {
-  // write your code here
+  var cartTotal = 0
+  for (var i = 0; i < cart.length; i++) {
+    var itemsAndPrice = cart[i]     //a single object in the cart {socks: 42}
+    var itemName = Object.keys(itemsAndPrice)[0]
+    var itemPrice = itemsAndPrice[itemName]
+    cartTotal = cartTotal + itemPrice
+  }
+  return cartTotal
 }
 
-function removeFromCart(item) {
-  // write your code here
-}
 
-function placeOrder(cardNumber) {
-  // write your code here
+function removeFromCart(itemName) {
+  for (var i = 0; i < cart.length; i++) {
+    console.log(cart[i]);
+  if (Object.keys(cart[i])[0] === itemName){
+      cart.splice(i, 1);
+      return cart;
+    }
+    else {
+      console.log("That item is not in your cart");
+      return cart;
+    }
+  }
+}
+function placeOrder(cardNumber){
+
 }
