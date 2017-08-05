1d2072c203b530082ad0a2c397441f5f931b9098
debjohnson33 <debjohnson_11@yahoo.com>
2017-08-04 21:32:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var cart;
+var cart = [];
+var cartContents = {};
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +15,38 @@ function total() {
   }
 
   return t
+}
+
+function getCart(){
+  return cart
+}
+
+var min = 0;
+var max = 100;
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function addToCart(item){
+  var price = getRandomIntInclusive(0, 100);
+  cartContents = {[item]: price}
+  cart[cart.length] = cartContents;
+  console.log(`${cart[cart.length - 1]["item"]} has been added to your cart.`);
+
+  return cart;
+}
+
+
+function removeFromCart( item ) {
+  for(var i = 0; i < cart.length; i++) {
+    if( cart[i].hasOwnProperty(item) ) {
+     cart.splice(i, 1);
+     return cart
+    } else {
+        console.log( "That item is not in your cart.");
+    }
+  }
 }
