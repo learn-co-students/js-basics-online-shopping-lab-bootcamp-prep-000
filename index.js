a991da85ae754fdf9ae42993931d33912fee89fa
jbingham223 <jbingham223@gmail.com>
2017-08-14 04:47:50 +0000

    Done.

--- index.js
+++ index.js
@@ -9,22 +9,77 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+  var itemPrice = parseInt(Math.random() * 100)
+  cart.push({ [itemName]: itemPrice })
+  console.log(`${itemName} has been added to your cart.`)
+  return cart
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length == 0) {
+    console.log("Your shopping cart is empty.")
+  } else if (cart.length == 1) {
+      var itemName = Object.keys(cart[0])
+      var itemPrice = cart[0][itemName]
+      console.log(`In your cart, you have ${itemName} at $${itemPrice}.`)
+  } else if (cart.length == 2) {
+    var itemName = Object.keys(cart[0])
+    var itemPrice = cart[0][itemName]
+    var itemN2 = Object.keys(cart[1])
+    var itemP2 = cart[1][itemN2]
+    console.log(`In your cart, you have ${itemName} at $${itemPrice} and ${itemN2} at $${itemP2}.`)
+  } else {
+      var itemName = Object.keys(cart[0])
+      var itemPrice = cart[0][itemName]
+      var string = `In your cart, you have ${itemName} at $${itemPrice}, `
+      for (var i = 1; i < cart.length - 1; i++) {
+        itemName = Object.keys(cart[i])
+        itemPrice = cart[i][itemName]
+        string += `${itemName} at $${itemPrice}, `
+      }
+      var i = cart.length - 1
+      itemName = Object.keys(cart[i])
+      itemPrice = cart[i][itemName]
+      console.log(`${string}and ${itemName} at $${itemPrice}.`)
+  }
 }
 
 function total() {
-  // write your code here
+  let total = 0
+  for (var i = 0; i < cart.length; i++) {
+    let itemPrice = cart[i][Object.keys(cart[i])]
+    total += itemPrice
+  }
+  return total
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var count = 0
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      if (i === 0) {
+        cart.shift()
+      } else if (i === cart.length - 1) {
+        cart.pop()
+      } else {
+        let pre = cart.slice(0, i)
+        let post = cart.slice(i + 1, cart.length)
+        cart = pre.concat(post)
+      }
+      count += 1
+    }
+  }
+  if (count === 0) {
+    console.log("That item is not in your cart.")
+  }
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber > 9999999) {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    cart = []
+  } else {
+    console.log("Sorry, we don't have a credit card on file for you.")
+  }
 }
