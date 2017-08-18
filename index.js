56f3fe9f93ef74c6795e78334d0a6b4e438a3286
Jeremy Scharf <jvscharf@gmail.com>
2017-08-18 23:41:10 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,76 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = parseInt(Math.random() * 100 + 1)
+  var newObject = {[item]: price}
+  cart.push(newObject)
+  console.log(item + " has been added to your cart.")
+  return cart
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.")
+  }
+  else {
+    var string = "In your cart, you have "
+    var array = []
+    for (var i = 0; i < cart.length; i++) {
+      array[i] = Object.keys(cart[i])[0] + " at $" + cart[i][Object.keys(cart[i])[0]]
+    }
+    if (cart.length === 1) {
+      string = string + array[0] + "."
+      console.log(string)
+    }
+    else if (cart.length === 2) {
+      string = string + array[0] + " and " + array[1] + "."
+      console.log(string)
+    }
+    else {
+      for (var i = 0; i < cart.length - 1; i++) {
+        string = string + array[i] + ", "
+      }
+      string = string + "and " + array[cart.length - 1] + "."
+      console.log(string)
+    }
+  }
 }
 
 function total() {
-  // write your code here
+  var sum = 0
+  for (var i = 0; i < cart.length; i++) {
+    sum = sum + parseInt(cart[i][Object.keys(cart[i])[0]])
+  }
+  return sum
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (var i = 0; i < cart.length; i++) {
+    if(cart[i].hasOwnProperty(item)) {
+      cart.splice(i,1)
+      return cart
+    }
+  }
+  console.log("That item is not in your cart.")
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined) {
+    console.log("Sorry, we don't have a credit card on file for you.")
+    return 0
+  }
+  else {
+    var sum = 0
+    for (var i = 0; i < cart.length; i++) {
+      sum = sum + cart[i][Object.keys(cart[i])[0]]
+    }
+    cart = []
+    console.log("Your total cost is $" + sum + ", which will be charged to the card " + cardNumber + ".")
+    return 0
+  }
 }
+
+addToCart('apples');
+addToCart('oranges')
+addToCart('pears')
+placeOrder(555);
