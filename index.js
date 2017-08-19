f19e964cbc1b9af73451aa13264fe4ea04ff3cdc
Ellen Keal <ellenkeal@gmail.com>
2017-08-19 03:47:31 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,79 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  cart[cart.length] = new Object({[item]:Math.floor(Math.random()*100 + 1)})
+  var string = "";
+  console.log("" + item + " has been added to your cart.")
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  var string = "";
+  if (cart.length === 0) {
+    string = "Your shopping cart is empty.";
+  }
+  else {
+    string = 'In your cart, you have ';
+  }
+  if (cart.length === 1) {
+    for (var imet in cart[0]) {
+      string += "" + imet +  " at \$" + cart[0][imet] + ".";
+    }
+
+  }
+  else if (cart.length === 2) {
+    for (var imet in cart[0]) {
+      string += "" + imet + " at \$" + cart[0][imet] + ' and ';
+    }
+    for (var imet in cart[1]) {
+      string += "" + imet +  " at \$" + cart[1][imet] + ".";
+    }
+  }
+  else if (cart.length > 2) {
+    for (var i = 0; i < cart.length-1; i++) {
+      for (var imet in cart[i]) {
+        string += "" + imet + " at \$" + cart[i][imet] + ', ';
+      }
+    }
+    for (var imet in cart[cart.length-1]) {
+      string += "and " + imet +  " at \$" + cart[cart.length-1][imet] + ".";
+    }
+  }
+  console.log(string);
 }
 
 function total() {
-  // write your code here
+  var sum = 0;
+  for (var i = 0; i < cart.length; i++) {
+    for (var j in cart[i]) {
+      sum += cart[i][j];
+    }
+  }
+  return sum;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var inCart = false;
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty([item])) {
+      inCart = true;
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+  if (!inCart) {
+    console.log("That item is not in your cart.");
+  }
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (typeof cardNumber === 'undefined') {
+    console.log("Sorry, we don't have a credit card on file for you.")
+  }
+  else {
+    var val = "Your total cost is \$" + total() +
+    ", which will be charged to the card " + cardNumber + ".";
+    cart = [];
+    console.log(val);
+  }
 }
