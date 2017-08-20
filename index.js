7208e73a4a7d0e4f92ffdb1efb575bf7a855189c
rjcolmed <rjcolmed@gmail.com>
2017-08-20 15:15:24 +0000

    Done.

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ function placeOrder(cardNumber) {
       if (cardNumber === undefined) {
           console.log('Sorry, we don\'t have a credit card on file for you.')
        } else {
-         console.log(`Your total cost is $${total}, which will be charged to the card ${cardNumber}.`);
+         console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
          cart = [];
        }
 
