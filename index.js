8819342c3523d6adf2a3f001dbb49a19eba889b5
Devon Kincaid <devon.kincaid@gmail.com>
2017-08-26 02:30:30 +0000

    Done.

--- index.js
+++ index.js
@@ -10,39 +10,84 @@ function setCart(c) {
 }
 
 function addToCart(item) {
+
 cart.push({[item]: Math.floor(Math.random() * 100)});
+
 console.log(`${item} has been added to your cart.`)
+
 return cart;
 
 }
 
 function viewCart() {
-if (cart.length === 0){
-  console.log("Your shopping cart is empty.")
-} for (let i = 0; i < cart.length; i++){
-  var list = [];
-  list.push(cart[i])
+const l = cart.length;
 
+if (l === 0){
+  return console.log("Your shopping cart is empty.");
+}
+let itemsAndPrices = [];
+
+for (let i = 0; i < l; i++){
+  let itemAndPrice = cart[i];
+  let item = Object.keys(itemAndPrice)[0];
+  let price = itemAndPrice[item]
+
+  itemsAndPrices.push(`${item} at \$${price}`)
 }
 
-return list
+switch(itemsAndPrices.length){
+  case 1:
+    break;
+  case 2:
+    itemsAndPrices = itemsAndPrices.join(" and ");
+    break;
+  default:
+    itemsAndPrices[l-1] = "and ".concat(itemsAndPrices[l-1]);
+    itemsAndPrices = itemsAndPrices.join(", ");
+}
+
+console.log(`In your cart, you have ${itemsAndPrices}.`);
+
 }
 
 
 
 
 function total() {
-var total = 0
-for (let i = 0; i < cart.length; i++){
-  total += `${cart.item[i]}`;
+let t = 0
+
+for (let i = 0, l = cart.length; i < l; i++){
+  for (let item in cart[i]){
+    t += cart[i][item];
+  }
 }
-return total;
+return t;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  let itemInCart = false;
+
+  for (let i = 0, l = cart.length; i < l; i++){
+    if (cart[i].hasOwnProperty(item)){
+      itemInCart = true;
+      cart = cart.slice(0, i).concat(cart.slice(i + 1));
+      l--;
+    }
+  }
+  if (!itemInCart){
+    console.log("That item is not in your cart.")
+  }
+  return cart;
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
-}
+
+  if (!cardNumber) {
+    console.log("Sorry, we don't have a credit card on file for you.");
+   return false;
+  }
+
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+
+  cart = [];
+ }
