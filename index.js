308803aed55d833dea11eedb15e03f12f9934d4d
github username <github email address>
2017-08-28 17:21:17 -0400

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,59 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  let price = Math.floor(Math.random()*100) + 1;
+  let newItem = {};
+  newItem[item] = price;
+  cart.push(newItem);
+  console.log(`${item} has been added to your cart.`);
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.")
+  } else if (cart.length === 1) {
+    console.log(`In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])}.`)
+  } else if (cart.length === 2) {
+    console.log(`In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])} and ${Object.keys(cart[1])} at $${Object.values(cart[1])}.`)
+  } else {
+    let cartContents = "In your cart, you have ";
+    for (var i = 0; i < cart.length; i++) {
+      let item = Object.keys(cart[i]);
+      let price = Object.values(cart[i]);
+      if (i === cart.length-1){
+        cartContents += `and ${item} at $${price}.`;
+      } else {
+        cartContents += `${item} at $${price}, `;
+      }
+    }
+    console.log(cartContents);
+  }
 }
 
 function total() {
-  // write your code here
+  let currentTotal = 0;
+  for (var i = 0; i < cart.length; i++) {
+    currentTotal += parseInt(Object.values(cart[i]));
+  }
+  return currentTotal;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+  console.log("That item is not in your cart.");
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined) {
+    console.log("Sorry, we don't have a credit card on file for you.")
+  } else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+  }
+  cart = [];
 }
