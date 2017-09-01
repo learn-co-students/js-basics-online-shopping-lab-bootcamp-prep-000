8effdd80a9d5bdb400059c26bd00b742b6680a9c
Alexander David <adavid459@gmail.com>
2017-09-01 14:47:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var cart = [];
 
+function randomPrice() {
+  return Math.floor((Math.random() * 100) + 1)
+}
+
 function getCart() {
  return cart;
 }
@@ -10,11 +14,37 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var itemArray = {}
+  itemArray[item] = randomPrice()
+  cart.push(itemArray)
+  console.log(`${item} has been added to your cart.`)
+  return cart
 }
 
+var cartArray = []
 function viewCart() {
-  // write your code here
+  var l = cart.length;
+  if (l === 0) {
+    console.log("Your shopping cart is empty.")
+
+  } if (l === 1) {
+    var first = `${Object.keys(cart[0])[0]} at $${cart[0][Object.keys(cart[0])]}.`
+    console.log(`In your cart, you have ${first}`)
+
+  } if (l === 2) {
+    var first = `${Object.keys(cart[0])[0]} at $${cart[0][Object.keys(cart[0])]}`
+    var second = `${Object.keys(cart[1])[0]} at $${cart[1][Object.keys(cart[1])]}`
+    console.log(`In your cart, you have ${first} and ${second}.`)
+
+  } if (l > 2) {
+    for (let i = 0; l - 1 > i; i++) {
+      var itemString = `${Object.keys(cart[i])[0]} at $${cart[i][Object.keys(cart[i])]}`
+      cartArray.push(itemString)
+    }
+    var last = `${Object.keys(cart[l - 1])[0]} at $${cart[l - 1][Object.keys(cart[l - 1])]}.`
+    var cartString = cartArray.join(", ")
+    console.log(`In your cart, you have ${cartString} and ${last}`)
+  }
 }
 
 function total() {
