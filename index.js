b6624e677faf71d81c4cd6dbbcee219e7b908ee7
Chris Snyder <christopher.snyder.m+flatiron@gmail.com>
2017-09-03 14:18:00 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var itemObject = { [itemName]: Math.floor(Math.random()*(100-1)+1)};
+ cart.push(itemObject);
+ console.log(`${item} has been added to your cart.`);
+ return cart;
 }
 
 function viewCart() {
