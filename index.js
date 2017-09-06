46496a43081d3e8ba663b87474c3ce5fbeeaa7bf
Eric Frommelt <ericfrommelt@gmail.com>
2017-09-06 15:19:37 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,20 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  function getRandomPrice(min, max) {
+    return parseInt(Math.random() * (max-min) + min);
+  }
+  cart.push({ [item]: getRandomPrice(1, 100) });
+  console.log(`${item} has been added to your cart.`);
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  for(var i = 0; i < cart.length; i++) {
+    if (cart.length === 1) {
+      console.log("In your cart, you have " + cart[0][0] + "at $" + cart[0][1]);
+    }
+  }
 }
 
 function total() {
