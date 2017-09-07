84d1cee9678d380d61af1394ffcfbfde4d581825
Adil Asif <asifadilanis@gmail.com>
2017-09-07 05:28:53 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,71 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var itemPrice = Math.floor((Math.random() * 100) + 1);
+  var obj = {[item]: itemPrice};
+  cart.push(obj);
+    console.log(`${item} has been added to your cart.`);
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  const length = cart.length;
+  if(length === 0) {
+    console.log("Your shopping cart is empty.");
+  } else {
+    var msg = "In your cart, you have ";
+    for(var i = 0; i < length; i++) {
+      for(var list in cart[i]) {
+        msg = msg + `${list} at $${cart[i][list]}`;
+        if(cart.length === 2) {
+          if(i !== cart.length - 1) {
+            msg = msg + " and ";
+          } else {
+            msg = msg + ".";
+          }
+        } else if(cart.length > 2 || cart.length === 1) {
+          if (i !== cart.length - 2 && i !== cart.length - 1) {
+            msg = msg + ", ";
+          } else if(i === cart.length - 2 ) {
+            msg = msg + ", and ";
+          } else {
+            msg = msg + ".";
+          }
+        }
+      }
+    }
+    console.log(msg);
+  }
 }
 
 function total() {
-  // write your code here
+  var subTotal = 0;
+  for(var i = 0; i < cart.length; i++) {
+    for(var list in cart[i]) {
+      subTotal += cart[i][list];
+    }
+  }
+  return subTotal;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var itemBool = false;
+  for(var i = 0; i < cart.length; i++) {
+    if(cart[i].hasOwnProperty(item)) {
+    cart = cart.slice(0, i).concat(cart.slice(i + 1));
+    itemBool = true;
+    }
+  }
+  if(!itemBool) {
+    console.log("That item is not in your cart.");
+  }
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(cardNumber === undefined || typeof cardNumber !== "number") {
+    console.log("Sorry, we don't have a credit card on file for you.")
+  } else {
+    console.log(`Your total cost is \$${total()}, which will be charged to the card ${cardNumber}.`)
+    cart = [];
+  }
 }
