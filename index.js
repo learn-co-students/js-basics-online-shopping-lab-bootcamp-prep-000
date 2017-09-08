3be3c85c38309d44653fdb8227ac541adb7864e0
Yuliya Yasenetska <jasenetska@gmail.com>
2017-09-08 00:15:11 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,59 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
-}
+ cart.push(new Object({[item]: Math.floor(Math.random()*100+1)}));
+ console.log(`${item} has been added to your cart.`);
+ return cart;
+ }
 
-function viewCart() {
-  // write your code here
-}
+ function viewCart() {
+   if (cart.length > 0) {
+     var statement = [];
+     for (var i = 0; i < cart.length; i++) {
+       statement.push(`${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}`);
+     }
+   }else {
+       console.log("Your shopping cart is empty.");
+       return
+     }
+   if (statement.length === 1) {
+        console.log(`In your cart, you have ${statement}.`);
+      }else if (statement.length === 2){
+         statement[statement.length - 1] = `and ${statement[statement.length - 1]}`;
+         console.log(`In your cart, you have ${statement.join(" ")}.`);
+       }else {
+         statement[statement.length - 1] = `and ${statement[statement.length - 1]}`;
+         console.log(`In your cart, you have ${statement.join(", ")}.`);
+       }
+         return
+       }
 
 function total() {
-  // write your code here
+   var cost = 0;
+  for(var i = 0; i < cart.length; i++) {
+    var key = Object.keys(cart[i]);
+    cost = cost + cart[i][key];
+  }
+  return cost;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+  console.log(`That item is not in your cart.`);
+    return cart;
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber) {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+      cart.splice(0);
+  }else {
+    console.log("Sorry, we don\'t have a credit card on file for you.");
+  }
+  return cart;
 }
