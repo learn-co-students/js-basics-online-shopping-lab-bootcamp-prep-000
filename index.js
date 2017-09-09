96520392ee1e06b28e11215032fc43ec2d821d6b
Greg Driza <gdriza@uarts.edu>
2017-09-09 21:09:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ function removeFromCart(item)
 
 function placeOrder(cardNumber)
 {
-  if(cardNumber === null)
+  if(cardNumber > 0)
   {
     console.log("Sorry, we don't have a credit card on file for you.");
   }
