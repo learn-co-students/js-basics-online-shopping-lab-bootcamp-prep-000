ea2e6dc9f669d7339a8df238abb0b67f4057cd97
Greg Driza <gdriza@uarts.edu>
2017-09-09 19:17:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -100,11 +100,11 @@ function removeFromCart(item)
       console.log("That item is not in your cart.");
       return cart;
 
-    }else
-    {
-      var newArray = cart.splice(i, 1);
-      return newArray;
-    }
+    // }else
+    // {
+    //   var newArray = cart.splice(i, 1);
+    //   return newArray;
+    // }
 
   }
 }
