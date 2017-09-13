e7f44a8ae0014116c88d32d2c9522b74ca08bf4e
T <theresa.hindersinn@gmail.com>
2017-09-13 01:04:34 +0000

    Done.

--- index.js
+++ index.js
@@ -10,10 +10,24 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var itemPrice = Math.floor((Math.random()*100)+1);
+  cart.push({[item]: itemPrice});
+  /* I can't seem to get the price to be recognized as an interger*/
+ console.log(`${item} has been added to your cart.`);
+ return cart;
 }
 
 function viewCart() {
+  var fullCart = [];
+  if (cart.length < 1){
+    console.log("Your shopping cart is empty.");
+    return cart;
+  }else {
+    for(var x = 0; cart.length > x; x++){
+      fullCart.push(cart[x] + " at $" + x.price);
+    }
+    return "In your cart, you have" + fullCart + "."
+  }
   // write your code here
 }
 
