f866421b90c283052e8efccfd4e1c23bd448a9bb
Jennifer Stewart <jlgregg1@gmail.com>
2017-09-17 03:42:23 +0000

    Done.

--- index.js
+++ index.js
@@ -9,22 +9,47 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+  var itemPrice = Math.floor(Math.random() *100)+1
+  var item = {[itemName]: itemPrice}
+  cart.push(item)
+  console.log(`${itemName} has been added to your cart.`)
+  return cart
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.")
+  }
+  else {
+    var name = Object.keys(cart)
+    for (let i=0; i < name.length; i++) {
+      var item = name[i]
+      var price = cart.item
+      console.log(`In your cart, you have ${item} at $${price}.`)
+    }
+
+    // var price =
+    // console.log(`In your cart, you have ${name} at $${price}.`)
+  }
+  // else {
+  //   var inCart= "In your cart you have "
+  //   for (let i=0; i < cart.length; i++) {
+  //     inCart = inCart + `${name} at $${price}`
+  //   }
+  //   inCart= inCart + `and ${name} at $${price}.`
+  //   return inCart
+  //   }
 }
 
-function total() {
-  // write your code here
-}
-
-function removeFromCart(item) {
-  // write your code here
-}
-
-function placeOrder(cardNumber) {
-  // write your code here
-}
+// function total() {
+//   // write your code here
+// }
+//
+// function removeFromCart(item) {
+//   // write your code here
+// }
+//
+// function placeOrder(cardNumber) {
+//   // write your code here
+// }
