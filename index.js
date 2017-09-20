e6eb51cafa592871b4eae9560eac27678a97960f
Man Leung <lepusleung@gmail.com>
2017-09-20 20:05:59 +0000

    Done.

--- index.js
+++ index.js
@@ -10,15 +10,39 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * 100);
+    cart.push({[item] : price});
+    console.log(item +' has been added to your cart.');
+    return cart;
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+      console.log("Your shopping cart is empty.");
+    } else {
+      var newArr = [];
+
+      for (var i = 0; i < cart.length; i++) {
+        var price = Object.keys(cart[i]);
+
+        for (var price in cart[i]) {
+          newArr.push(`${Object.keys(cart[i])} at $${cart[i][price]}`);
+        }
+      }
+      return console.log("In your cart, you have " + newArr.join(", ") + ".");
 }
 
 function total() {
-  // write your code here
+  let ttl = 0;
+
+  for (var i = 0; i < cart.length; i++) {
+
+    for (var item in cart[i]) {
+      ttl += cart[i][item];
+    }
+  }
+
+  return ttl;
 }
 
 function removeFromCart(item) {
@@ -26,5 +50,10 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+    if (cardNumber === undefined) {
+      console.log('We don\'t have a credit card on file for you to place your order.');
+    }
+    else {
+      console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+}
 }
