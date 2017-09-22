0a120d80639198d74a4946ecbbb832d2bcd1250a
T <theresa.hindersinn@gmail.com>
2017-09-22 15:32:46 +0000

    Done.

--- index.js
+++ index.js
@@ -12,33 +12,69 @@ function setCart(c) {
 function addToCart(item) {
   var itemPrice = Math.floor((Math.random()*100)+1);
   cart.push({[item]: itemPrice});
-  /* I can't seem to get the price to be recognized as an interger*/
  console.log(`${item} has been added to your cart.`);
  return cart;
 }
 
 function viewCart() {
   var fullCart = [];
-  if (cart.length < 1){
-    console.log("Your shopping cart is empty.");
-    return cart;
-  }else {
-    for(var x = 0; cart.length > x; x++){
-      fullCart.push(cart[x] + " at $" + x.price);
+  if(cart.length === 0){
+    console.log('Your shopping cart is empty.');
+  } else{
+    for(var x = 0; x<cart.length; x++){
+      var object = cart[x];
+      var itemName = Object.keys(object);
+      var itemPrice = object[itemName];
+        if(cart.length === 1){
+          console.log('In your cart, you have ' + itemName + ' at $' + itemPrice + '.');
+        }
+        if ((cart.length - 1 === x)){
+          fullCart.push(`and ${itemName} at $${itemPrice}`);
+        }
+        else {
+          fullCart.push(`${itemName} at $${itemPrice}`);
+        }
     }
-    return "In your cart, you have" + fullCart + "."
+    if (cart.length === 2){
+      console.log(`In your cart, you have ${fullCart.join(' ')}.`)
+}
+  else{
+    console.log(`In your cart, you have ${fullCart.join(', ')}.`)
   }
-  // write your code here
+  }
+
 }
 
 function total() {
-  // write your code here
+  var total = 0
+  for(var a = 0; cart.length>a; a++){
+    var object = cart[a];
+    var itemName = Object.keys(object);
+    var itemPrice = object[itemName];
+    total = total + itemPrice;
+  }
+  return total;
 }
 
 function removeFromCart(item) {
-  // write your code here
-}
+    for(var f = 0; f < cart.length; f++){
+      if(cart[f].hasOwnProperty(item)){
+        cart.splice(f,1);
+        return viewCart();
+      }
+    }
+    console.log('That item is not in your cart.');
+    return viewCart();
+  }
+
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(Number.isInteger(cardNumber)){
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    cart.splice(0)
+
+  }
+  else{
+    console.log("Sorry, we don't have a credit card on file for you.")
+  }
 }
