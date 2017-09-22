ba3e8bbf7108ac6787738112f404096b33fbbef6
Jenn Cacace <jenn.cacace@gmail.com>
2017-09-22 02:46:34 +0000

    Done.

--- index.js
+++ index.js
@@ -24,56 +24,70 @@ function viewCart() {
   var key
 
   if(cart.length === 0){
-    console.log("Your shopping cart is empty.")
+    console.log("Your shopping cart is empty.");
   } else if (cart.length === 1){
-    firstItem  = cart[0]
-    firstKey  = Object.keys(firstItem)[0]
-    console.log(`In your cart, you have ${firstKey} at $${firstItem[firstKey]}.`)
+    firstItem  = cart[0];
+    firstKey  = Object.keys(firstItem)[0];
+    console.log(`In your cart, you have ${firstKey} at $${firstItem[firstKey]}.`);
   } else if (cart.length === 2){
-    firstItem  = cart[0]
-    firstKey  = Object.keys(firstItem)[0]
-    secondItem = cart[1]
-    secondKey = Object.keys(secondItem)[0]
+    firstItem  = cart[0];
+    firstKey  = Object.keys(firstItem)[0];
+    secondItem = cart[1];
+    secondKey = Object.keys(secondItem)[0];
     console.log( `In your cart, you have ${firstKey} at $${firstItem[firstKey]} and ${secondKey} at $${secondItem[secondKey]}.`);
   } else {
-    var longList = "In your cart, you have "
+    var longList = "In your cart, you have ";
     for(var i=0; i < cart.length-1; i++){
-      item = cart[i]
-      key = Object.keys(item)[0]
+      item = cart[i];
+      key = Object.keys(item)[0];
       longList = longList + `${key} at $${item[key]}, `;
     }
-    item = cart[cart.length-1]
-    key = Object.keys(item)[0]
-    longList = longList + `and ${key} at $${item[key]}.`
-    console.log(longList)
+    item = cart[cart.length-1];
+    key = Object.keys(item)[0];
+    longList = longList + `and ${key} at $${item[key]}.`;
+    console.log(longList);
   }
 }
 
 
 function total() {
-var item
-var key
+var item;
+var key;
 var sum = 0
   for(var i=0; i < cart.length; i++) {
-    item = cart[i]
-    key = Object.keys(item)[0]
-    sum = sum + item[key]
+    item = cart[i];
+    key = Object.keys(item)[0];
+    sum = sum + item[key];
   }
-  return sum
+  return sum;
 }
 
 function removeFromCart(item) {
-  for (var i = 0; i < cart.length; i++){
-    if (Object.keys(cart[i])[0] === item) {
-      return cart.shift()
-    }
-  } else {
-    return "The item is not in your cart."
+var itemInCart = false;
+   for (var i = 0; i < cart.length; i++){
+     if (cart[i].hasOwnProperty(item)) {
+        itemInCart = true;
+        var inspectedItems = cart.slice(0, i);
+        var remainingItems = cart.slice(i + 1);
+        cart = inspectedItems.concat(remainingItems);
+     }
+   }
+   if (!itemInCart) {
+    console.log ("That item is not in your cart.")
   }
+  return cart
 }
 
+
 function placeOrder(cardNumber) {
-  // write your code here
+  if (!cardNumber) {
+    console.log("Sorry, we don't have a credit card on file for you.");
+    return false;
+  }
+
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+
+  cart = [];
 }
 
 //addToCart("banana")
