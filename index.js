d9e63ab024604b5279417d05efb60ee19f3774b2
Jenn Cacace <jenn.cacace@gmail.com>
2017-09-22 00:24:05 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,73 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ cart.push({[item]: Math.ceil(Math.random() * 100)})
+ console.log(`${item} has been added to your cart.`);
+ return cart;
 }
 
 function viewCart() {
-  // write your code here
+  var firstItem
+  var firstKey
+  var secondItem
+  var secondKey
+  var item
+  var key
+
+  if(cart.length === 0){
+    console.log("Your shopping cart is empty.")
+  } else if (cart.length === 1){
+    firstItem  = cart[0]
+    firstKey  = Object.keys(firstItem)[0]
+    console.log(`In your cart, you have ${firstKey} at $${firstItem[firstKey]}.`)
+  } else if (cart.length === 2){
+    firstItem  = cart[0]
+    firstKey  = Object.keys(firstItem)[0]
+    secondItem = cart[1]
+    secondKey = Object.keys(secondItem)[0]
+    console.log( `In your cart, you have ${firstKey} at $${firstItem[firstKey]} and ${secondKey} at $${secondItem[secondKey]}.`);
+  } else {
+    var longList = "In your cart, you have "
+    for(var i=0; i < cart.length-1; i++){
+      item = cart[i]
+      key = Object.keys(item)[0]
+      longList = longList + `${key} at $${item[key]}, `;
+    }
+    item = cart[cart.length-1]
+    key = Object.keys(item)[0]
+    longList = longList + `and ${key} at $${item[key]}.`
+    console.log(longList)
+  }
 }
 
+
 function total() {
-  // write your code here
+var item
+var key
+var sum = 0
+  for(var i=0; i < cart.length; i++) {
+    item = cart[i]
+    key = Object.keys(item)[0]
+    sum = sum + item[key]
+  }
+  return sum
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (var i = 0; i < cart.length; i++){
+    if (cart[i] === item) {
+      return cart.shift()
+    }
+  } else {
+    return "The item is not in your cart."
+  }
 }
 
 function placeOrder(cardNumber) {
   // write your code here
 }
+
+//addToCart("banana")
+//addToCart("apple")
+//addToCart("muffins")
+viewCart()
