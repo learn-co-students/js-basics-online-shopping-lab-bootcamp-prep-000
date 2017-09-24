4d6736a1e6334f2a5afac505c21dd3748bf0085c
Jessica Greene <jessica0greene@gmail.com>
2017-09-24 15:51:51 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,58 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  cart.push(Object.assign({}, {[item]: Math.floor((Math.random() * 100) + 1)}))
+  console.log(item + " has been added to your cart.")
+  return cart
 }
 
+
 function viewCart() {
-  // write your code here
+  if ( cart.length === 0){
+    console.log("Your shopping cart is empty.")
+  } else if(cart.length === 1) {
+      console.log("In your cart, you have " + Object.keys(cart[0]) + " at $" + cart[0][Object.keys(cart[0])] + ".")
+  } else if(cart.length === 2) {
+      console.log("In your cart, you have " + Object.keys(cart[0]) + " at $" + cart[0][Object.keys(cart[0])] + " and " + Object.keys(cart[1]) + " at $" + cart[1][Object.keys(cart[1])] +  ".")
+  } else {
+    var cartList = [];
+    var a = parseInt(cart.length-1)
+    for (let i = 0; i < cart.length-1; i++){
+      cartList.push(" " + Object.keys(cart[i]) + " at $" + cart[i][Object.keys(cart[i])])
+    }
+    //console.log(cartList)
+    console.log("In your cart, you have" + cartList + ", and " + Object.keys(cart[a]) + " at $" + cart[a][Object.keys(cart[a])] + ".")
+
+  }
 }
 
 function total() {
-  // write your code here
+  var currentTotal  = 0;
+  for (let i = 0; i < cart.length; i++) {
+    currentTotal += parseInt(cart[i][Object.keys(cart[i])])
+  }
+  return currentTotal;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for ( let i = 0; i < cart.length; i++) {
+    if (String(Object.keys(cart[i])) === item) {
+      cart.splice(i,1)
+      return cart
+    }
+  }
+    console.log("That item is not in your cart.")
+    return cart
 }
 
-function placeOrder(cardNumber) {
-  // write your code here
+
+function placeOrder(cardNumber){
+  if (cardNumber === undefined) {
+    console.log('Sorry, we don\'t have a credit card on file for you.');
+  } else {
+    var cost = total();
+    console.log("Your total cost is $" + cost + ", which will be charged to the card " + cardNumber + ".");
+  }
+  cart = [];
+  return cart
 }
