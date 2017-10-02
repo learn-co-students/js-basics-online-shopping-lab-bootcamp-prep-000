539485aff7f9747d7ba4a29ad13130bc9aac9289
Zoe Robertson <zoe.robertson.m@gmail.com>
2017-10-02 04:43:36 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,56 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
-}
+  cart.push({[item]:Math.floor(Math.random() * 100) + 1});
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+ }
 
 function viewCart() {
-  // write your code here
-}
+  var itemsAndPrices = []
+  var cartL = cart.length
+  for (let i = 0; i < cartL; i++) {
+    let itemAndPrice = cart[i]
+    let item = Object.keys(itemAndPrice)[0];
+    let price = itemAndPrice[item];
+    itemsAndPrices.push(`${item} at \$${price}`);
+  } let greeting = 'In your cart, you have '
+  if (cartL === 0) {console.log('Your shopping cart is empty.')}
+  else if (cartL === 1) {console.log(`${greeting}${itemsAndPrices[0]}.`)}
+  else if (cartL === 2) {console.log(`${greeting}${itemsAndPrices[0]} and ${itemsAndPrices[1]}.`)}
+  else {
+    var itemsAndPricesAnd = []
+    for (let i = 0; i < cartL; i++) {
+      if (i === cartL - 1) {itemsAndPricesAnd.push(`and ${itemsAndPrices[i]}`)}
+      else { itemsAndPricesAnd.push(`${itemsAndPrices[i]}`)}
+    }
+    console.log(`${greeting}${itemsAndPricesAnd.join(', ')}\.`)
+    }
+
+  }
+
+
+
 
 function total() {
-  // write your code here
+  var pr = [];
+  var cartL = cart.length;
+  for (let i = 0; i < cartL; i++) {
+    let itemAndPrice = cart[i];
+    let item = Object.keys(itemAndPrice)[0];
+    let price = itemAndPrice[item];
+    pr.push(price);
+} return pr.reduce(function(accumulator, currentValue) {return accumulator + currentValue})
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var cartL = cart.length;
+  for (let i = 0; i < cartL; i++) {
+    if (cart[i].hasOwnProperty(item) === true) {cart.splice(i, 1); return cart}
+  } console.log('That item is not in your cart.')
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined) {console.log('Sorry, we don\'t have a credit card on file for you.')}
+  else {console.log(`Your total cost is \$${total()}, which will be charged to the card ${cardNumber}.`); cart = []}
 }
