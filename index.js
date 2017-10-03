0e366dc91e538bd925d975f6643a4446ad1800a2
Emma Konrad <emma.konrad92@gmail.com>
2017-10-03 23:51:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,42 +1,92 @@
 var cart = [];
 
-  function getCart() {
+function getCart() {
+ return cart;
+}
+
+function setCart(c) {
+  cart = c;
   return cart;
+}
+
+function addToCart(item) {
+  const price = Math.floor(Math.random() * 100) + 1;
+
+  cart.push({ [item]: price });
+
+  console.log(`${item} has been added to your cart.`);
+
+  return cart;
+}
+
+function viewCart() {
+  const l = cart.length;
+
+  if (!l) {
+    return console.log("Your shopping cart is empty.");
   }
 
-   function setCart(newCart) {
-     cart = newCart;
-   }
+  let itemsAndPrices = [];
 
-  function addToCart(item) {
-  var price = Math.floor(Math.random() * 100) + 1;
-  var item_object = {};
-  item_object[item] = price
-  cart.push(item_object)
-  console.log(item + " has been added to your cart.");
-  return cart
+  for (let i = 0; i < l; i++) {
+    let itemAndPrice = cart[i];
+    let item = Object.keys(itemAndPrice)[0];
+    let price = itemAndPrice[item];
+
+    itemsAndPrices.push(`${item} at \$${price}`);
   }
 
-  function viewCart() {
-    if (!cart.length) {
-      console.log("Your shopping cart is empty.")
+  switch(itemsAndPrices.length) {
+    case 1:
+      break;
+    case 2:
+      itemsAndPrices = itemsAndPrices.join(" and ");
+      break;
+    default:
+      itemsAndPrices[l-1] = "and ".concat(itemsAndPrices[l-1]);
+      itemsAndPrices = itemsAndPrices.join(", ");
+  }
+
+  console.log(`In your cart, you have ${itemsAndPrices}.`);
+}
+
+function total() {
+  let t = 0;
+
+  for (let i = 0, l = cart.length; i < l; i++) {
+    for (let item in cart[i]) {
+      t += cart[i][item];
     }
+  }
 
-    var itemsAndPrices = [];
+  return t;
+}
 
-    for (var i = 0, l = cart.length; i < l; i++) {
-      var itemAndPrice = cart[i];
-      var item = Object.keys(itemAndPrice)[0];
-      var price = itemAndPrice[item];
+function removeFromCart(item) {
+  let itemInCart = false;
 
-      if (i === 1) {
-        itemsAndPrices.push(`${item} at $${price}.`);
-      }
-      else if (i === 2) {
-        itemsAndPrices.push(`${item} at $${price} and`);
-      }
-      else {
-        itemsAndPrices.push(`${item} at $${price},`);
-      }
+  for (let i = 0, l = cart.length; i < l; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      itemInCart = true;
+      cart = cart.slice(0, i).concat(cart.slice(i + 1));
+      l--;
     }
   }
+
+  if (!itemInCart) {
+    console.log("That item is not in your cart.");
+  }
+
+  return cart;
+}
+
+function placeOrder(cardNumber) {
+  if (!cardNumber) {
+    console.log("Sorry, we don't have a credit card on file for you.");
+    return false;
+  }
+
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+
+  cart = [];
+}
