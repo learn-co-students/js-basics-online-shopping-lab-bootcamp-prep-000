2eafb3e679c8595d6110c5a836040eeaa661a778
tahseen447 <tahseenfatima1986@gmail.com>
2017-10-04 04:40:44 +0000

    Done.

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
- var price = Math.floor(Math.random() * (100 - 1))
+ var min = Math.ceil(1)
+ var max = Math.floor(100)
+ var price = Math.floor(Math.random() * (max - min + 1)) + min
 cart.push(Object.assign({},  { [item]:price }) )
 console.log(item + " has been added to your cart.")
 return cart;
@@ -25,11 +27,12 @@ function viewCart() {
     return
   }
   var str = "In your cart, you have "
-  for(var i = 0; i <cart.length; i++)
+//  for(var i = 0; i <cart.length; i++)
+if(cart.length===1)
   {
-    var itemName = Object.keys(cart[i])[0]
-    var itemPrice = cart[i][itemName]
-    str = str + "${itemName} at $${itemPrice}."
+    var itemName = Object.keys(cart[0])[0]
+    var itemPrice = cart[0][itemName]
+    str += itemName+" at $"+ itemPrice +"."
   }
 }
 
@@ -49,16 +52,16 @@ function removeFromCart(item) {
   // write your code here
   for(var i =0; i<cart.length; i++)
   {
-    for(var key in cart[i])
+    var object = cart[i]
+    var firstItem = Object.keys(object)[0]
+    if(object.hasOwnProperty(item))
     {
-    if(key.hasOwnProperty(item))
+      cart.splice(i, 1)
+    }
+    else
     {
-      cart= cart.splice(i, 1)
+      console.log("That item is not in your cart.")
     }
-    else {
-      alert("That item is not in your cart.")
-    }
-  }
 }
 return cart;
 }
