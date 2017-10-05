a55928a2502d05d10f382403a1681092beb067b1
Nancy Eckenthal <nancy.eckenthal@gmail.com>
2017-10-05 04:12:58 +0000

    Done.

--- index.js
+++ index.js
@@ -10,15 +10,68 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+    var obj = new Object()
+    obj[item] = Math.floor(Math.random() * (100 - 1) + 1)
+    cart = [...cart, obj];
+    console.log(`${item} has been added to your cart.`)
+    return cart
 }
+
+//from call below
+function viewCart() {
+  let i = 0;
+  if (cart.length === 0) {
+      console.log('Your shopping cart is empty.')
+  } else {
+    var new_cart = []
+      for (let i = 0; i < cart.length; i++) {
+          var key = Object.keys(cart[i])[0]
+          new_cart = [...new_cart, `${key} at $${cart[i][key]}`]
+          if (cart.length === 1) {
+              console.log(`In your cart, you have ${new_cart}.`)
+          }
+          else if (cart.length === 2) {
+              console.log(`In your cart, you have ${new_cart.join(' and ')}.`)
+          }
+          else if (cart.length >= 3) {
+              console.log(`In your cart, you have ${new_cart.pop().join(', ')} and ${new_cart[new_cart.length - 1]}.`)
+          }
+        }
+    }
+};
 
 function viewCart() {
-  // write your code here
-}
+  let i = 0;
+  if (cart.length === 0) {
+      console.log('Your shopping cart is empty.')
+  } else {
+      for (let i = 0; i <=cart.length; i++) {
+          var new_cart = []
+          new_cart = [...new_cart, `${Object.keys(cart)[i]} at $${cart[Object.keys(cart)[i]]}`]
+          if (cart.length === 1) {
+              console.log(`In your cart, you have ${new_cart}.`)
+          }
+          else if (cart.length === 2) {
+              console.log(`In your cart, you have ${new_cart}.join(' and ').`)
+          }
+          else if (cart.length >= 3) {
+              console.log(`In your cart, you have ${new_cart.pop()}.join(', ') and ${new_cart[new_cart.length - 1]}.`)
+          }
+        }
+    }
+};
+
 
 function total() {
-  // write your code here
+  var total = 0
+  for (let i = 0; i <= cart.length; i++) {
+    //   prices = [...prices, cart[Object.keys(cart)[i]]]
+    total += Number(cart[Object.keys(cart)[i]])
+  }
+  // for (let p = 0; p < prices.length; p++) {
+  //     total += Number(Object.keys(prices)[p])
+  // }
+  return total
 }
 
 function removeFromCart(item) {
@@ -27,4 +80,5 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+
 }
