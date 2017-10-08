d2a806507fe32df3f98ba7db5a76a79fdae697c4
Nicole Campbell <nscampbell303@gmail.com>
2017-10-08 00:09:15 +0000

    Done.

--- index.js
+++ index.js
@@ -30,7 +30,12 @@ function viewCart() { //[{apples: 2}, {socks: 5}]
     return console.log('Your shopping cart is empty.')
   }
   if(cart.length > 2) {
-    console.log(`In your cart, you have ${newArray.join(", ")}.`)
+    //we only want to get up to the last element of newArray, all of that stuff we join with (", ")
+    //then we join the last element of newArray with (" and ")
+    //we can do this by using .slice to get the parts of the array we need
+    //newArray.slice(0, newArray.length-1) <~ this part we .join with (", ")
+    //then, after that, we join the last part of newArray (newArray[newArray.length-1]) with (" and ")
+    console.log(`In your cart, you have ${newArray.slice(0, newArray.length - 1).join(", ")}, and ${newArray.slice(-1)}.`)
   } else if (cart.length === 2) {
       console.log(`In your cart, you have ${newArray.join(" and ")}.`)
   } else if (cart.length === 1) {
