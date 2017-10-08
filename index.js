dc3cb97f3d0a1df458378f075e495c9479f0ccf1
Marvin Clerge <clergemarvin@gmail.com>
2017-10-08 22:50:15 +0000

    Done.

--- index.js
+++ index.js
@@ -16,7 +16,40 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  // write your code here
+  //create base message
+  var message = 'In your cart, you have ';
+
+  // If cart is empty display that message
+  if(cart.length === 0){
+    message = 'Your shopping cart is empty.';
+  }
+
+  //If its not empty begin process
+  else {
+
+    //for loop to go through entire array
+    for (let i = 0; i < cart.length; i++) {
+
+      var price = Object.values(cart[i]);
+
+      //If there is only 1 item proform this
+      if(cart.length === 1){
+        message += `${Object.keys(cart[i])} at ${ price }.`;
+      }
+      else if(i === cart.length-1){
+        message += `and ${ Object.keys(cart[i]) } is priced at $${ Object.values(cart[i]) }.`;
+      }
+      else if(i === 0){
+        message += `${ Object.keys(cart[i]) } is priced at $${ Object.values(cart[i]) }, `;
+      }
+      else{
+        message += `and ${ Object.keys(cart[i]) } is priced at $${ Object.values(cart[i]) }, `;
+      }
+
+    }
+  }
+
+  console.log(message);
 }
 
 function total() {
