8d93e9299d1cebcbe8d4c69263b5941f806c0d37
Nancy Eckenthal <nancy.eckenthal@gmail.com>
2017-10-10 03:45:15 +0000

    Done.

--- index.js
+++ index.js
@@ -27,58 +27,36 @@ function viewCart() {
       for (let i = 0; i < cart.length; i++) {
           var key = Object.keys(cart[i])[0]
           new_cart = [...new_cart, `${key} at $${cart[i][key]}`]
-          if (cart.length === 1) {
-              console.log(`In your cart, you have ${new_cart}.`)
-          }
-          else if (cart.length === 2) {
-              console.log(`In your cart, you have ${new_cart.join(' and ')}.`)
-          }
-          else if (cart.length >= 3) {
-              console.log(`In your cart, you have ${new_cart.pop().join(', ')} and ${new_cart[new_cart.length - 1]}.`)
-          }
-        }
-    }
-};
-
-function viewCart() {
-  let i = 0;
-  if (cart.length === 0) {
-      console.log('Your shopping cart is empty.')
-  } else {
-      for (let i = 0; i <=cart.length; i++) {
-          var new_cart = []
-          new_cart = [...new_cart, `${Object.keys(cart)[i]} at $${cart[Object.keys(cart)[i]]}`]
-          if (cart.length === 1) {
-              console.log(`In your cart, you have ${new_cart}.`)
-          }
-          else if (cart.length === 2) {
-              console.log(`In your cart, you have ${new_cart}.join(' and ').`)
-          }
-          else if (cart.length >= 3) {
-              console.log(`In your cart, you have ${new_cart.pop()}.join(', ') and ${new_cart[new_cart.length - 1]}.`)
-          }
-        }
-    }
-};
-
-
-function total() {
-  var total = 0
-  for (let i = 0; i <= cart.length; i++) {
-    //   prices = [...prices, cart[Object.keys(cart)[i]]]
-    total += Number(cart[Object.keys(cart)[i]])
+      }
   }
-  // for (let p = 0; p < prices.length; p++) {
-  //     total += Number(Object.keys(prices)[p])
-  // }
-  return total
-}
+  if (cart.length === 1) {
+      console.log(`In your cart, you have ${new_cart}.`)
+  } else if (cart.length === 2) {
+      console.log(`In your cart, you have ${new_cart.join(' and ')}.`)
+  } else if (cart.length >= 3) {
+      var last = new_cart.pop()
+      console.log(`In your cart, you have ${new_cart.join(', ')}, and ${last}.`)
+  }
+};
 
-function removeFromCart(item) {
-  // write your code here
-}
 
-function placeOrder(cardNumber) {
-  // write your code here
-
-}
+// function total() {
+//   var total = 0
+//   for (let i = 0; i <= cart.length; i++) {
+//     //   prices = [...prices, cart[Object.keys(cart)[i]]]
+//     total += Number(cart[Object.keys(cart)[i]])
+//   }
+//   // for (let p = 0; p < prices.length; p++) {
+//   //     total += Number(Object.keys(prices)[p])
+//   // }
+//   return total
+// }
+//
+// function removeFromCart(item) {
+//   // write your code here
+// }
+//
+// function placeOrder(cardNumber) {
+//   // write your code here
+//
+// }
