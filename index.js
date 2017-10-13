f4df63c2224872c548101bf6a95079d49acf1571
Nancy Eckenthal <nancy.eckenthal@gmail.com>
2017-10-13 03:10:22 +0000

    Done.

--- index.js
+++ index.js
@@ -71,7 +71,12 @@ function removeFromCart(item) {
 }
 // }
 //
-// function placeOrder(cardNumber) {
-//   // write your code here
-//
-// }
+function placeOrder(cardNumber) {
+    var c = cardNumber
+    if (c === None) {
+      console.log(`Sorry, we don't have a credit card on file for you.`)
+    } else {
+      console.log(`Your total cost is $${total()}, which will be charged to ${c}.`)
+      cart.length = 0
+    }
+  }
