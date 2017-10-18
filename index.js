e1fa4598e52c75d1bfb90dc6f45673182e2fcb16
Shumei Huang <rose1264@gmail.com>
2017-10-18 20:06:31 +0000

    Done.

--- index.js
+++ index.js
@@ -11,20 +11,74 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var price = Math.floor(Math.random()*100)+1;
+ var itemObject = {};
+ itemObject[item]=price
+ cart.push(itemObject)
+ console.log(item + " has been added to your cart.");
+ return cart
 }
 
 function viewCart() {
   // write your code here
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.");
+  } else if(cart.length < 3){
+    var items = [];
+    for (var i = 0; i < 2; i++) {
+      for (var item in cart[i]) {
+        items.push(item + " at $" + cart[i][item])
+      }
+    }
+    console.log("In your cart, you have " + items.join(" and ") + ".");
+  } else {
+    var items = [];
+    var n = cart.length-1;
+    for (var i = 0; i < n; i++) {
+      for (var item in cart[i]) {
+        items.push(item + " at $" + cart[i][item])
+      }
+    }
+    var lastItemKey = Object.keys(cart[n])
+    var lastItem = `${lastItemKey} at $${cart[n][lastItemKey]}`;
+    console.log("In your cart, you have " + items.join(", ") + ", and " + lastItem +".");
+  }
+
 }
 
 function total() {
   // write your code here
+  var total = 0;
+  for(var i=0;i<cart.length;i++){
+    for (var item in cart[i]){
+      total += cart[i][item]
+    }
+  }
+  return total
 }
 
+
 function removeFromCart(item) {
-  // write your code here
-}
+   var itemInCart = false
+   for(var i = 0; i < cart.length; i++){
+         if (cart[i].hasOwnProperty(item)) {
+       itemInCart = true;
+       cart.splice(i, 1);
+     }
+   }
+
+   if (!itemInCart) {
+     console.log("That item is not in your cart.");
+   }
+   return cart
+ }
 
 function placeOrder(cardNumber) {
   // write your code here
+  if(!cardNumber){
+    console.log("Sorry, we don't have a credit card on file for you.")
+  } else{
+    console.log (`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+  }
+  cart=[]
 }
