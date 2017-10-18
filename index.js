ce07b7e2fe096a03a6a9765b4fc5a26d5331fc8f
Samiur Rashid <samrashid@gmail.com>
2017-10-18 17:45:32 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,77 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  cart.push({[item]: Math.floor(Math.random()*100)+1});
+  console.log(`${item} has been added to your cart.`);
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  if(cart.length == 0) {
+    console.log("Your shopping cart is empty.");
+  }
+
+  else if (cart.length == 1 || cart.length == 2) {
+    var items = [];
+      for (var i = 0; i < cart.length; i++) {
+        for (var item in cart[i]) {
+          items.push(item + " at $" + cart[i][item])
+      }
+      console.log("In your cart, you have " + items.join(" and ") + ".");
+    }
+  }
+
+  else {
+    var items = [];
+    for (var i = 0; i < cart.length; i++) {
+      for (var item in cart[i]) {
+        if (i == cart.length-1) {
+          items.push("and " + item + " at $" + cart[i][item])
+        }
+
+        else {
+        items.push(item + " at $" + cart[i][item])
+      }
+    }
+
+  }
+  console.log("In your cart, you have " + items.join(", ") + ".");
+}
 }
 
 function total() {
-  // write your code here
+  var sum = 0;
+  for (var i = 0; i < cart.length; i++) {
+    for (var item in cart[i]) {
+      sum += cart[i][item];
+    }
+  }
+  return sum;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var cartItem = false;
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      cart.splice(i, 1);
+      cartItem = true;
+    }
+  }
+
+  if (cartItem == false) {
+    console.log("That item is not in your cart.")
+  }
+
+  return cart;
+
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(cardNumber == undefined) {
+    console.log("Sorry, we don't have a credit card on file for you.")
+  }
+  else {
+    console.log("Your total cost is $" + total() + ", which will be charged to the card " + cardNumber + ".")
+    cart.length = 0;
+  }
 }
