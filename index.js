41a4aac17d92e0379d2d4a4efe1a1bf49d992e6a
Nick Casciola <s0948644@monmouth.edu>
2017-10-18 22:28:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 var cart = [];
 
 function getCart() {
-  return cart;
+ return cart;
 }
 
 function setCart(c) {
   cart = c;
+  return cart;
 }
 
 function addToCart(item) {
-  const price = Math.floor(Math.random() * 100);
+  const price = Math.floor(Math.random() * 100) + 1;
 
   cart.push({ [item]: price });
 
@@ -25,7 +26,7 @@ function viewCart() {
     return console.log("Your shopping cart is empty.");
   }
 
-  const itemsAndPrices = [];
+  let itemsAndPrices = [];
 
   for (let i = 0; i < l; i++) {
     let itemAndPrice = cart[i];
@@ -35,17 +36,40 @@ function viewCart() {
     itemsAndPrices.push(`${item} at \$${price}`);
   }
 
-  console.log(`In your cart, you have ${itemsAndPrices.join(', ')}.`);
+  switch(itemsAndPrices.length) {
+    case 1:
+      break;
+    case 2:
+      itemsAndPrices = itemsAndPrices.join(" and ");
+      break;
+    default:
+      itemsAndPrices[l-1] = "and ".concat(itemsAndPrices[l-1]);
+      itemsAndPrices = itemsAndPrices.join(", ");
+  }
+
+  console.log(`In your cart, you have ${itemsAndPrices}.`);
+}
+
+function total() {
+  let t = 0;
+
+  for (let i = 0, l = cart.length; i < l; i++) {
+    for (let item in cart[i]) {
+      t += cart[i][item];
+    }
+  }
+
+  return t;
 }
 
 function removeFromCart(item) {
   let itemInCart = false;
-  let i = 0;
 
-  for (i; i < cart.length; i++) {
+  for (let i = 0, l = cart.length; i < l; i++) {
     if (cart[i].hasOwnProperty(item)) {
       itemInCart = true;
       cart = cart.slice(0, i).concat(cart.slice(i + 1));
+      l--;
     }
   }
 
@@ -58,22 +82,11 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (!cardNumber) {
-    return console.log("Sorry, we don't have a credit card on file for you.");
+    console.log("Sorry, we don't have a credit card on file for you.");
+    return false;
   }
 
   console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
 
   cart = [];
-}
-
-function total() {
-  let t = 0;
-
-  for (var i = 0, l = cart.length; i < l; i++) {
-    for (var item in cart[i]) {
-      t += cart[i][item];
-    }
-  }
-
-  return t;
 }
