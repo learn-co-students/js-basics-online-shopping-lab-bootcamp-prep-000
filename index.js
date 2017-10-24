488ea9b15b9c18ed17418edea34cda276cbca12a
Małgorzata Klisińska <klis.mal@gmail.com>
2017-10-24 09:42:19 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,65 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var obj = {}
+  obj[item] = Math.floor(Math.random() * 100 + 1)
+  cart.push(obj)
+  console.log(`${item} has been added to your cart.`)
+  return cart
 }
 
 function viewCart() {
-  // write your code here
+  if(!cart.length){
+    console.log("Your shopping cart is empty.")
+  }
+  else if(cart.length === 1){
+    var i = 0
+    var ret = `In your cart, you have ${ Object.keys(cart[i])[0] } at $${ cart[0][Object.keys(cart[i])[0]] }.`
+    console.log(ret)
+  }
+  else if(cart.length ===2){
+    console.log( `In your cart, you have ${ Object.keys(cart[0])[0] } at $${ cart[0][Object.keys(cart[0])[0]] } and ${ Object.keys(cart[1])[0] } at $${ cart[1][Object.keys(cart[1])[0]] }.` )
+  }
+  else {
+    var ret = `In your cart, you have ${ Object.keys(cart[0])[0] } at $${ cart[0][Object.keys(cart[0])[0]] }`
+    var i = 1
+    while(i<cart.length-1){
+      ret += `, ${ Object.keys(cart[i])[0] } at $${ cart[i][Object.keys(cart[i])[0]] }`
+      i++
+    }
+    ret += `, and ${ Object.keys(cart[i])[0] } at $${ cart[i][Object.keys(cart[i])[0]] }.`
+    console.log(ret)
+  }
 }
 
 function total() {
-  // write your code here
+  var sum = 0
+  for( var i = 0; i<cart.length; i++){
+    sum += cart[i][Object.keys(cart[i])[0]]
+  }
+  return sum
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var b = 0
+  for(var i=0; i<cart.length; i++){
+    if(Object.keys(cart[i])[0] === item ){
+      cart.splice(i, 1)
+      b=1
+    }
+  }
+  if(!b){
+    console.log("That item is not in your cart.")
+  }
+  return cart
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(!cardNumber){
+    console.log("Sorry, we don\'t have a credit card on file for you.")
+  }
+  else{
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    cart = []
+  }
 }
