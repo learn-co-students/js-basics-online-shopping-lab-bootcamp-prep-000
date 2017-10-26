e68ab6bac91f1c70406e1cb83bd78fe9b9293cef
Patrick McClernan <patmccler@gmail.com>
2017-10-26 22:05:53 +0000

    Done.

--- index.js
+++ index.js
@@ -9,22 +9,91 @@ function setCart(c) {
   return cart;
 }
 
+function getRandomPrice()
+{
+  return Math.floor(Math.random() * (99) + 1)
+}
+
 function addToCart(item) {
- // write your code here
+  var item_obj = { [item]: getRandomPrice()}
+  cart.push(item_obj)
+  console.log(`${item} has been added to your cart.`)
+  return cart
+}
+
+function punctuationNeeded(i,length)
+{
+
+  if(length === 2 && i === 0)
+  {
+    return " "
+  }
+  else
+  {
+    return i < length - 1 ? ", " : "."
+  }
 }
 
 function viewCart() {
-  // write your code here
+  if(cart.length > 0) {
+    var cart_items_list = "In your cart, you have "
+    for(let i = 0; i < cart.length; i++) {
+      var item = cart[i]
+      var cart_item_key = Object.keys(item)[0]
+      if(i === cart.length - 1 && cart.length > 1) {
+        cart_items_list += 'and '
+      }
+      cart_items_list += `${cart_item_key} at $${item[cart_item_key]}${punctuationNeeded(i,cart.length)}`
+    }
+    console.log(cart_items_list)
+  }
+  else {
+    console.log("Your shopping cart is empty.")
+  }
 }
 
 function total() {
-  // write your code here
+  var cartPrice = 0
+  for(let i = 0; i < cart.length; i++) {
+    var item = cart[i]
+    var cart_item_key = Object.keys(item)[0]
+
+
+    cartPrice += item[cart_item_key]
+  }
+  return cartPrice
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for(let i = 0; i < cart.length; i++) {
+    var thisItem = cart[i]
+    thisItem = Object.keys(thisItem)[0]
+    if(thisItem === item) {
+      cart.splice(i,1)
+      return cart
+    }
+  }
+  console.log("That item is not in your cart.")
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(!cardNumber) {
+    console.log("Sorry, we don't have a credit card on file for you.")
+  }
+  else {
+      console.log(`Your total cost is $${calcTotal()}, which will be charged to the card ${cardNumber}.`)
+      cart = []
+  }
+}
+
+function calcTotal() {
+  var total = 0;
+  for(let i = 0; i < cart.length; i++) {
+    var thisItem = cart[i]
+    var thisItemKey = Object.keys(thisItem)[0]
+    var thisItemCost = parseInt(thisItem[thisItemKey])
+
+    total += thisItemCost
+  }
+  return total
 }
