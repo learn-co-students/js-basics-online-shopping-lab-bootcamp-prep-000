0c16a6972e8e52d56c92ba48c0abdd125a3f5e6b
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-10-30 03:16:31 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,74 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+var price= Math.floor(Math.random()*100+1);
+var itemCart= {};
+itemCart[item]= price;
+cart.push(itemCart);
+console.log(`${item} has been added to your cart.`);
+return cart;
 }
 
 function viewCart() {
-  // write your code here
+  var itemArray=[];
+  var output= "In your cart, you have ";
+  var keyArray= Object.keys(cart);
+
+  if(cart.length===0){
+    console.log('Your shopping cart is empty.');
+  } else {
+    for (var i =0;i<cart.length;i++){
+
+      itemArray.push(Object.keys(cart[i]) + " at $" + cart[i][Object.keys(cart[i])[0]]) ;
+         console.log(itemArray);
+    }
+    if (itemArray.length===1){
+      console.log(output + itemArray + '.');
+
+    } else if (itemArray.length===2){
+      console.log(output + itemArray.join(' and ')+'.');
+
+    } else {
+      var end='';
+      for(var j=0;j<itemArray.length;j++){
+
+        if (j<itemArray.length-1){
+          end += itemArray[j] + ", ";
+        } else {end += "and " + itemArray[j]+".";}
+      }
+    }console.log (output + end);
+  }
 }
 
 function total() {
-  // write your code here
+  var sum=0;
+  for (var i =0;i<cart.length;i++){
+  sum+= cart[i][Object.keys(cart[i])[0]] ;
+} return sum;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var inCart=false;
+
+  for(var i =0;i<cart.length;i++){
+    if (cart[i].hasOwnProperty(item)){
+      cart.splice(i,1);
+      inCart=true;
+    }
+  }
+
+  if (inCart===false){
+    console.log('That item is not in your cart.');
+  }
+  console.log(cart);
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  var localTotal=total();
+
+  if (!cardNumber){
+    console.log("Sorry, we don't have a credit card on file for you.");
+  } else {
+    console.log(`Your total cost is $${localTotal}, which will be charged to the card ${cardNumber}.`);
+  } cart=[];
 }
