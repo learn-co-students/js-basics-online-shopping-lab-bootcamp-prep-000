c609204670af21fa8920df626f98b5796347adbc
Helen Liutongco <bellpickle@gmail.com>
2017-10-30 05:30:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var cart = [];
 
 function getCart() {
  return cart;
@@ -6,25 +5,73 @@ function getCart() {
 
 function setCart(c) {
   cart = c;
+  cartItems = [];
+  cartPrice = [];
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+var cartItems=[];
+var cartPrice=[];
+var cart=[];
+
+function addToCart(items) {
+  var num=Math.random()*100;
+  var itemPrice=Math.floor(num);
+  cartItems.push(items);
+  cartPrice.push(itemPrice);
+  var newObject={ [items]:itemPrice };
+  cart.push(newObject);
+  console.log(`${items} has been added to your cart.`);
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+var array=[];
+if(cart<=[]) {
+  console.log("Your shopping cart is empty.");
+}
+  for(var i=0;i<cart.length;i++) {
+    array.push(` ${cartItems[i]} at $${cartPrice[i]}`);
+  }
+  if(cart.length===1) {
+    console.log(`In your cart, you have${array}.`);
+  }
+  else if(cart.length===2) {
+    console.log(`In your cart, you have${array[0]} and${array[1]}.`);
+  }
+  else {
+    console.log(`In your cart, you have${array.slice(0,-1)}, and${array.slice(-1)}.`);
+  }
 }
 
-function total() {
-  // write your code here
+  function total() {
+  var totalPrice=0;
+  for(var i=0;i<cart.length;i++) {
+    totalPrice+=cart[i][cartItems[i]];
+  }
+  return totalPrice;
 }
 
-function removeFromCart(item) {
-  // write your code here
+function removeFromCart(itemName) {
+  for(var i=0;i<cart.length;i++) {
+    if(cart[i].hasOwnProperty(itemName)) {
+      cart.splice(i,1);
+      return cart;
+    }
+  }
+  console.log("That item is not in your cart.");
+  return cart;
 }
+
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(!cardNumber) {
+    console.log("Sorry, we don't have a credit card on file for you.");
+  }
+  else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    for(var i=cart.length;i>0;i--) {
+      cart.pop();
+    }
+  }
 }
