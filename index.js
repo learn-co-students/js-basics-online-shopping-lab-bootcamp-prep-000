45a42dfef7d95515069fb9963080551bdcd6e2d4
Andrew McDermitt <andrew.mcdermitt@doitbest.com>
2017-11-01 17:55:36 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,80 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * 100+1);
+  var newItem = {
+    [item]: price
+  }
+  cart.push(newItem);
+  console.log(item + " has been added to your cart.")
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  var message = "In your cart, you have ";
+  var cartLength = cart.length - 1;
+
+  if (cart.length == 0){
+    message = "Your shopping cart is empty.";
+  } else{
+    for (var i = 0; i <= cartLength; i++){
+      var obj = cart[i];
+      var itemName = Object.keys(obj);
+      var price = obj[itemName];
+      //var price = obj.hasOwnProperty(itemName);
+      if (i == 0){
+        message += itemName + " at $" + price;
+      }else if (i == cartLength){
+        if (i==1){
+          message += " and " + itemName + " at $" + price;
+        }else{
+          message += ", and " + itemName + " at $" + price;
+        }
+      }else
+      {
+        message += ", " + itemName + " at $" + price;
+      }
+    }
+    message += ".";
+  }
+  console.log(message);
 }
 
 function total() {
-  // write your code here
+  var finalPrice = 0;
+  var cartLength = cart.length - 1;
+
+  for (var i = 0; i <= cartLength; i++){
+    var obj = cart[i];
+    var itemName = Object.keys(obj);
+    var price = obj[itemName];
+
+    finalPrice += price;
+  }
+  return finalPrice;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var cartLength = cart.length - 1;
+
+  for (var i = 0; i <= cartLength; i++){
+    var obj = cart[i];
+    var itemName = Object.keys(obj);
+
+    if (item == itemName){
+      cart.splice(i,1);
+      return cart;
+    }
+  }
+  console.log("That item is not in your cart.")
+  return cart;
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber == "" || cardNumber == null){
+    console.log("Sorry, we don't have a credit card on file for you.");
+  } else{
+    console.log("Your total cost is $" + total() + ", which will be charged to the card " + cardNumber + ".");
+    cart = [];
+  }
 }
