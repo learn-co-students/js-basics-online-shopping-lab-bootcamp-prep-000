c4ece03c08cdee5c7d7b6bf1b5cd1341ad4deba0
Kevin M <shoron@gmail.com>
2017-11-13 04:34:36 +0000

    Done.

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ function addToCart(item) {
 
 function viewCart() {
     var key1 = Object.keys(cart[0]);
-    var value1 = Object.values(cart[0]);
+    var value1 = cart[0][key1];
     var key2 = Object.keys(cart[1]);
-    var value2 = Object.values(cart[1]);
+    var value2 = cart[1][key2];
   if (cart.length === 0){console.log('Your shopping cart is empty.')}
   else if (cart.length === 1) {
     console.log(`In your cart, you have ${key1} at $${value1}.`)}
@@ -31,12 +31,12 @@ function viewCart() {
     for (var i = 0; i < cart.length-2; i++){          //-2 because we need to leave space for last item
       var objectx = cart[i]
       var itemx = Object.keys(cart[i])
-      var pricex = Object.values(cart[i])
+      var pricex = cart[i][itemx]
       var itemsandprices = []
       itemsandprices.push(itemx +' at $'+ pricex)
       var sentence = itemsandprices.join(' ,')
       var lastkey = Object.keys(cart[cart.length-1])
-      var lastvalue = Object.values(cart[cart.length-1])
+      var lastvalue = cart[cart.length-1][lastkey]
      console.log(`In your Cart, you have ${sentence} and ${lastkey} at $${lastvalue} `)
      }
   }
