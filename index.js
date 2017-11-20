981ade80b0415de3c7678870a5c1f78b6360d809
Eric Rogers <eric.rog17@gmail.com>
2017-11-20 01:08:25 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,47 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var itemPrice = Math.floor(Math.random() * 100)
+  var newItem = {[item]: itemPrice};
+  cart.push(newItem)
+  console.log(item + ' has been added to your cart')
+  return cart
 }
 
 function viewCart() {
-  // write your code here
+  var returnStatement = 'In your cart, you have '
+  var itemObj = {}
+  var itemArray = []
+  var item = undefined
+  var itemPrice = undefined
+  if (cart.length === 0) {
+    console.log('Your shopping cart is empty.')
+  }else if(cart.length === 1) {
+    for(let i=0; i<cart.length; i++) {
+      itemObj = cart[i]
+      itemArray = Object.keys(itemObj)
+      item = itemArray[0]
+      itemPrice = itemObj[item]
+      returnStatement = `in your cart, you have ${item} at $${itemPrice}`
+      return returnStatement
+    }
+    //console.log(returnStatement)
+  }else if(cart.length === 2) {
+    for(let i=0; i<cart.length; i++) {
+      itemObj = cart[i]
+      itemArray = Object.keys(itemObj)
+      item = itemArray[0]
+      itemPrice = itemObj[item]
+      if(i === 1) {
+        returnStatement = returnStatement + ` and ${item} at $${itemPrice}`
+        return returnStatement
+      }
+      returnStatement = returnStatement + `${item} at $${itemPrice}`
+    }
+
+  }else if(cart.length > 3) {
+
+  }
 }
 
 function total() {
