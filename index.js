a2f72a3a307bdb2e93ed6a8cbbbce0a1b3a6a7db
Travis Lavery <travisalavery@gmail.com>
2017-11-21 04:33:09 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,28 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random()*100)
+  var itemObj = {}
+  itemObj[item] = price
+ cart = [...cart, itemObj]
+ console.log(`${item} has been added to your cart.`)
+ return cart
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length<1) {
+    console.log('Your shopping cart is empty.')
+  } else if (cart.length === 1) {
+    var itemHere = cart[0]
+    console.log(`In your cart, you have ${Object.keys()} at $${itemHere[0].price}.`)
+  } else {
+    var statement = "In your cart, you have "
+    for(var i = 0; i<cart.length; i++){
+      var nextItem = `${cart[i]} at $${cart[i].item}`
+      statement += nextItem
+  }
+  }
+  
 }
 
 function total() {
