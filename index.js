a73a4e0c1ce29cc9bfb92859107e7c53245bf72a
Jacob Denbeaux <jdenbeaux19@wooster.edu>
2017-11-25 08:15:38 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,57 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  cart.push({[item]: Math.floor(Math.random()*100)+1})
+  console.log(`${item} has been added to your cart.`)
+  return cart
 }
 
 function viewCart() {
-  // write your code here
+  let itemsWithCosts = cart.map(item => {
+    return `${Object.keys(item)[0]} at $${item[Object.keys(item)[0]]}`
+  })
+  let lastItemAndCost = itemsWithCosts.pop()
+  let commaSeparatedItemsWithCosts = itemsWithCosts.join(", ")
+  let cartStatement = ""
+  if (cart.length > 2) {
+    commaSeparatedItemsWithCosts = commaSeparatedItemsWithCosts + ","
+    cartStatement = "In your cart, you have " + commaSeparatedItemsWithCosts + " and " + lastItemAndCost + "."
+  }
+  else if (cart.length == 2) {
+    cartStatement = "In your cart, you have " + commaSeparatedItemsWithCosts + " and " + lastItemAndCost + "."
+  }
+  if (cart.length == 1) {
+    cartStatement = "In your cart, you have " + lastItemAndCost + "."
+  }
+  else if (cart.length == 0) {
+    cartStatement = "Your shopping cart is empty."
+  }
+  console.log(cartStatement)
 }
 
 function total() {
-  // write your code here
+  const costs = cart.map(item => {return item[Object.keys(item)[0]]})
+  return costs.reduce((i,j) => i + j)
 }
 
 function removeFromCart(item) {
-  // write your code here
+  const itemNames = cart.map(item => {return Object.keys(item)[0]})
+  const locInCart = itemNames.indexOf(item)
+  if (locInCart < 0) {
+    return cart
+  }
+  else {
+    cart.splice(locInCart, 1)
+    return cart
+  }
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (!cardNumber) {
+    console.log("Sorry, we don\'t have a credit card on file for you.")
+  }
+  else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    cart = []
+  }
 }
