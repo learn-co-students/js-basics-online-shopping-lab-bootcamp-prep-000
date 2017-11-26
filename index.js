2be8bfa9908677825023ff95b14711e39481b22e
AAM77 <adeel.a.mhd@gmail.com>
2017-11-26 16:33:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var cart = [];
 
 function getCart() {
- return cart;
+  return cart;
 }
 
 function setCart(c) {
@@ -10,10 +10,28 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var itemPrice = Math.floor(Math.random()*100);
+  var itemObject = {
+    [item]:itemPrice
+  };
+  cart.push(itemObject);
+  console.log(`${item} has been added to your cart.`)
+  return (cart);
 }
 
 function viewCart() {
+  if (cart.length === 0) {
+    return ("Your shopping cart is empty.");
+  }
+  else {
+    var cartList = "In your cart, you have ";
+    for (itemCount = 0; itemCount < cart.length; itemCount++) {
+      if (cart.length === 1) {
+        cartList = cartList + `Object.` at ${cart[itemCount][0]}`
+        return (`In your cart, you have `)
+      }
+    }
+  }
   // write your code here
 }
 
