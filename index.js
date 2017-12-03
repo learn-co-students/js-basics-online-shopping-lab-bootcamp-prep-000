316693374eb0f6fcdb1bb7b21e55b3ff27336d76
Angela <wang.angela90@gmail.com>
2017-12-03 18:13:07 +0000

    Done.

--- index.js
+++ index.js
@@ -10,25 +10,36 @@ function setCart(c) {
 }
 
 function addToCart(itemName) {
-  var itemPrice = Math.floor(Math.random() * (100-1))+ 1;//randomly generate integer from 1 to 100.
-  cart.push({itemName: itemPrice}); // pushing the object to the cart
-    console.log (`${itemName} has been added to your cart.`)
-  return(cart);
+  var itemPrice = Math.floor(Math.random() * 100) + 1;//randomly generate integer from 1 to 100.
+  var item = {};//empty variable for item
+  item[itemName]= itemPrice; // using bracket notation to set key value pair
+  cart.push(item); // pushing the object to the cart
+  console.log (`${itemName} has been added to your cart.`);
+  return cart;
 }
 
 function viewCart() {
-  if (cart === 0){
-    return "Your shopping cart is empty."
+    if (cart.length === 0) {
+      console.log('Your shopping cart is empty.');
+    }
+    else if (cart.length === 1) {
+      var firstObject = cart[0];
+      for (var item in firstObject){
+        var price = firstObject[item];
+        console.log(`In your cart, you have ${item} at $${price}.`);
+      }
+    }
   }
-  for (i = 0; i < cart.length; i++){
-  console.log (`In your cart, you have ${itemName} at ${itemPrice}.`);
- }
-}
 
-var TotalValue = itemPrice;
-var sum = 0;
-function total () {
-  for (i = 0; i < cart.length; i++){ //iterate through our itemPrice array
-  sum += TotalValue[i];
- }
-}
+
+// var TotalValue = itemPrice;
+// var sum = 0;
+// function total () {
+//   for (i = 0; i < cart.length; i++){ //iterate through our itemPrice array
+//   sum += TotalValue[i];
+//  }
+// }
+
+// function removeFromCart (removedItem){
+//
+// }
