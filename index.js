d45263c27f20516e6ad27694bac42086c15c5526
Tim Yamaguchi <timothy.yamaguchi@comcast.net>
2017-12-07 17:36:42 +0000

    Done.

--- index.js
+++ index.js
@@ -11,10 +11,35 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var price = Math.floor(Math.random() * 101)
+ cart.push({[item] : price})
+ console.log(`${item} has been added to your cart.`)
+ return cart
 }
 
 function viewCart() {
   // write your code here
+  if (!cart.length) {
+    return console.log("Your shopping cart is empty.")
+  }
+  var itemPrice = []
+  for (let i = 0, l = cart.length; i < l; i++) {
+    var itemPrices = cart[i]
+    var item = Object.keys(itemPrices)[0]
+    var price = itemPrices[item]
+    itemPrice.push(`${item} at \$${price}`)
+  }
+  switch(itemPrices.length) {
+    case 1:
+      break;
+    case 2:
+      itemPrice = itemPrice.join(" and ");
+      break;
+    default:
+      itemPrice[cart.length-1] = "and ".concat(itemPrice[cart.length-1]);
+      itemPrice = itemPrice.join(", ");
+  }
+  console.log(`In your cart, you have ${itemPrices}`)
 }
 
 function total() {
