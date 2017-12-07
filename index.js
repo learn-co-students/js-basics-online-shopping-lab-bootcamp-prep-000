9edda5c4734e9b2131bb6c72708bd1697ceab3ea
Allison Tzadok <allisontzadok@gmail.com>
2017-12-07 20:52:05 +0000

    Done.

--- index.js
+++ index.js
@@ -71,11 +71,11 @@ function removeFromCart(item) {
 }  
 
 function placeOrder(cardNumber) {
-  if (cardNumber === null){
+  if (cardNumber === undefined){
     console.log("Sorry, we don't have a credit card on file for you.");
     
   }else{
-  console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`);
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
   cart = []
   }
 }
