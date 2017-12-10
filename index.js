7b923092034fe6e5d17c13fb8955c738bc3578f5
Michael Carroll <carrollm2@gmail.com>
2017-12-10 16:11:21 +0000

    Done.

--- index.js
+++ index.js
@@ -33,48 +33,83 @@ return cart
 function viewCart() {
   // write your code here
 
-  var statement = "In your cart, you have "
-
-
-  if(cart.length === 1){
-    statement += `${cart[0]['itemName']} at $${cart[0]['itemPrice']}.`
-    return console.log(statement)
-  }
-
-
-  if (cart.length === 2){
-    statement += `${cart[0]['itemName']} at $${cart[0]['itemPrice']} and ${cart[1]['itemName']} at $${cart[1]['itemPrice']}.`
-    return console.log(statement)
-
-  }
-
-  if (cart.length > 2){
-    for (var i = 0; i < cart.length; i++){
-      if (i < cart.length - 1){
-        statement += `${cart[i]['itemName']} at $${cart[i]['itemPrice']}, `
-      } else {
-        statement += `and ${cart[i]['itemName']} at $${cart[i]['itemPrice']}.`
-      }
-    }
-    return console.log(statement)
-  }
-
   if (cart.length === 0){
     return console.log("Your shopping cart is empty.")
 
-  }
 
+  } else {
+
+    var statement = "In your cart, you have "
+
+    for (var i = 0; i < cart.length; i++){
+      var obj = {}
+      obj = cart[i]
+
+
+      statement += `${Object.keys(obj)} at $${Object.values(obj)}`
+
+      if (i === cart.length - 1){
+        statement += `.`
+      } else if (i === cart.length - 2) {
+        if (cart.length <= 2){
+          statement += ` and `
+        } else {
+          statement += `, and `
+        }
+      } else {
+        statement += `, `
+      }
+
+    }
+    return console.log(statement)
+  }
 
 }
 
 function total() {
   // write your code here
+  var sum = 0.0
+  for (var i = 0; i < cart.length; i++){
+    console.log(Object.values(cart[i]))
+    sum += parseFloat(Object.values(cart[i]))
+  }
+
+  return sum
+
 }
 
 function removeFromCart(item) {
   // write your code here
+  var updatedCart = []
+
+  for (var i = 0; i < cart.length; i++){
+    if(!cart[i].hasOwnProperty(item)){
+      updatedCart.push(cart[i])
+    }
+  }
+
+  if (cart.length !== updatedCart.length){
+    cart = updatedCart
+    return cart
+  } else {
+    console.log("That item is not in your cart.")
+    return cart
+  }
+
 }
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (cardNumber) {
+    var totalOrder = total()
+
+    console.log(totalOrder, "totalOrder")
+    console.log(`Your total cost is $${totalOrder}, which will be charged to the card ${cardNumber}.`)
+    cart = []
+    return cart
+  } else {
+    return console.log("Sorry, we don\'t have a credit card on file for you.")
+
+  }
+
 }
