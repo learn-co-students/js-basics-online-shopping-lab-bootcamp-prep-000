46a33c87d40c30d4b9772853f69038e7bc984eec
Peter Coughlin <coughlin1966@gmail.com>
2017-12-11 17:25:32 +0000

    Done.

--- index.js
+++ index.js
@@ -21,27 +21,12 @@ addToCart(eggs)
 
 
 function viewCart() {
-var newObject = []
-var price1 = Math.floor(Math.random() * 100)
-var price2 = Math.floor(Math.random() * 100)
-var price3 = Math.floor(Math.random() * 100)
+if (cart.length === 0) {
+  consolelog ('Your shopping cart is empty.')
+}
 
-  if (cart.length == 0) {
-    console.log('Your shopping cart is empty.')
-    for (i=1; i<cart.length; i++) {
-if (cart.length = 1) {
-console.log (`In your cart, you have bananas at \$ ${price1}.`)
-else if (cart.lenght = 2) {
-  console.log (`In your cart, you have bananas at \$ ${price1} and pancake batter at \$ ${price2}.`)
+
 }
-else (if cart.length = 3) {
-  console.log (`In you cart, you have bananas at \$ ${price1}, and pancake batter at \$ ${price2}, and eggs at \$ ${price3}.`)
-}
-}
-    }
-  }
-}
-viewCart()
 
 viewCart()
 
