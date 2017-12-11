03a1d951975eee0ced85dbf67e7cd0e9ae1cb68e
Peter Coughlin <coughlin1966@gmail.com>
2017-12-11 18:36:04 +0000

    Done.

--- index.js
+++ index.js
@@ -21,15 +21,33 @@ addToCart(eggs)
 
 
 function viewCart() {
-var cartItem = []
-var price = Math.floor(Math.random() * 100)
-if (cart.length === 0) {
-  console.log ('Your shopping cart is empty.')
-}
-else { for (i = 0; i < cart.length; i++)
-cartItem.push (`${cart[i]} at ${price}.`)}
-console.log (`In you cart, you currently have ${cartItem}`)
-}
+//var cartItem = []
+//var price = Math.floor(Math.random() * 100)
+//if (cart.length === 0) {
+//console.log ('Your shopping cart is empty.')
+//}
+//for (var i = 0; i < cart.length; i++)
+
+var cart = getCart()
+  if(cart.length === 0)
+    return console.log("Your shopping cart is empty.")
+
+  var itemprice_str = []
+  for(var i = 0; i < cart.length; i++){
+    var key = Object.keys(cart[i])[0]
+    itemprice_str.push(`${key} at $${cart[i][key]}`)
+  }
+  var msg = "In your cart, you have "
+  if(cart.length === 1)
+    msg += itemprice_str + "."
+  else if(cart.length === 2){
+    msg += itemprice_str.join(" and ") + "."
+  }
+  else if(cart.length > 2){
+    itemprice_str[cart.length - 1 ]= "and ".concat(itemprice_str[cart.length - 1]).concat(".")
+    msg += itemprice_str.join(", ")
+  }
+  return console.log(msg)
 
 viewCart()
 
