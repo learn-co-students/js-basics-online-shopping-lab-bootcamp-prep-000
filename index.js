c7963d809d74d64e6181283e49bb84cadaa0a819
Peter Coughlin <coughlin1966@gmail.com>
2017-12-11 23:26:22 +0000

    Done.

--- index.js
+++ index.js
@@ -18,38 +18,30 @@ console.log(`${item} has been added to your cart.`)
 return cart
 }
 addToCart(eggs)
-
-
 function viewCart() {
-//var cartItem = []
-//var price = Math.floor(Math.random() * 100)
-//if (cart.length === 0) {
-//console.log ('Your shopping cart is empty.')
-//}
-//for (var i = 0; i < cart.length; i++)
 
-var cart = getCart()
-  if(cart.length === 0)
-    return console.log("Your shopping cart is empty.")
+//var cart = getCart()
+  if(cart.length === 0) console.log("Your shopping cart is empty.")
 
-  var itemprice_str = []
+  var itemPrice = []
   for(var i = 0; i < cart.length; i++){
     var key = Object.keys(cart[i])[0]
-    itemprice_str.push(`${key} at $${cart[i][key]}`)
+    itemPrice.push(`${key} at $${cart[i][key]}`)
   }
-  var msg = "In your cart, you have "
-  if(cart.length === 1)
-    msg += itemprice_str + "."
-  else if(cart.length === 2){
-    msg += itemprice_str.join(" and ") + "."
-  }
-  else if(cart.length > 2){
-    itemprice_str[cart.length - 1 ]= "and ".concat(itemprice_str[cart.length - 1]).concat(".")
-    msg += itemprice_str.join(", ")
-  }
-  return console.log(msg)
 
-viewCart()
+  var msg = "In your cart, you have"
+  if(cart.length === 1) {
+    // msg += itemPrice + "."
+    msg = `${msg} ${itemPrice}.`
+  } else if(cart.length === 2){
+    msg += itemPrice.join(" and ") + "."
+  } else if(cart.length > 2) {
+    itemPrice[cart.length - 1 ]= "and ".concat(itemPrice[cart.length - 1]).concat(".")
+    msg += itemPrice.join(", ")
+  }
+
+  console.log(msg)
+}
 
 function total() {
   // write your code here
