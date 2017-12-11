828c7d21b545f488ba6d737ac24b1a26efd18100
Peter Coughlin <coughlin1966@gmail.com>
2017-12-11 17:31:04 +0000

    Done.

--- index.js
+++ index.js
@@ -21,11 +21,14 @@ addToCart(eggs)
 
 
 function viewCart() {
+var cartItem = []
+var price = Math.floor(Math.random() * 100)
 if (cart.length === 0) {
-  consolelog ('Your shopping cart is empty.')
+  console.log ('Your shopping cart is empty.')
 }
-
-
+else { for (i = 0; i < cart.length; i++)
+cartItem.push (`${cart[i]} at ${price}.`)}
+console.log (`In you cart, you currently have ${cartItem}`)
 }
 
 viewCart()
