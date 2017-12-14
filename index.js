b1e3e1ffba7418f6702912d51cb4d1f3d921ff06
Zach Vary <zach.vary@gmail.com>
2017-12-14 16:47:20 +0000

    Done.

--- index.js
+++ index.js
@@ -9,20 +9,42 @@ function setCart(c) {
   return cart;
 }
 
+
+
 function addToCart(item) {
- // write your code here
+  var itemPrice = Math.floor(Math.random() * 100 + 1)
+  cart.push({[item]: itemPrice});
+  console.log(`${item} has been added to your cart.`)
+  return cart
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    console.log('Your shopping cart is empty.');
+  } else {
+    var cartList = [];
+    for (var i = 0; i < cart.length; i++) {
+      var item = Object.keys(cart[i]);
+      var prices = cart[i][item];
+      cartList.push(`${item} at $${prices}`);
+    }
+    var message = 'In your cart, you have ';
+    for (var i = 0; i < cartList.length; i++) {
+      message += cartList[i];
+    }
+} console.log(message +'.');
 }
 
 function total() {
-  // write your code here
 }
 
 function removeFromCart(item) {
-  // write your code here
+    for (var i = 0; i < cart.length; i++) {
+        if (cart.hasOwnProperty(item)) {
+            return cart.splice(i,1)
+        } else  return cart
+        console.log("That item is not in your cart");
+    }
 }
 
 function placeOrder(cardNumber) {
