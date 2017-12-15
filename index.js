1a514f8d08976cd8924bf9d7a87c6b4841778398
Peter Coughlin <coughlin1966@gmail.com>
2017-12-15 22:08:55 +0000

    Done.

--- index.js
+++ index.js
@@ -19,64 +19,47 @@ return cart
 }
 addToCart(eggs)
 
+
 function viewCart() {
   getCart();
-  var cartArray = [];
-  //console.log(cartArray);
-  var keyArray = [];
-  if(Object.keys(getCart()).length > 0){
-  for(var i = 0; i < Object.keys(getCart()).length; i++){
-    cartArray.push(Object.keys(getCart())[i]);
-  }
-}
-  //for(var a = 0; a < cartArray.length; a++){
-    //keyArray.push(Object.keys(getCart()[a]))
-  //}
-  if(keyArray.length < 1){
-    console.log("Your shopping cart is empty.");
-  }
-  if(keyArray.length === 1){
-    console.log(`In your cart, you have ${keyArray[0]} at $${getCart()[0][keyArray[0]]}.`)
-  }
-  if(keyArray.length === 2){
-      var contents2 = "In your cart, you have";
-        for(var p = 0; p < keyArray.length - 1; p++){
-          contents2 += ` ${keyArray[p]} at $${getCart()[p][keyArray[p]]}`;
-        } for(var q = keyArray.length-1; q < keyArray.length; q++){
-            contents2 += ` and ${keyArray[q]} at $${getCart()[p][keyArray[q]]}.`;
-            console.log(contents2);
-        }
-      }
-  if(keyArray.length > 2){
-      var contents = "In your cart, you have";
-        for(var i = 0; i < keyArray.length - 1; i++){
-          contents += ` ${keyArray[i]} at $${getCart()[i][keyArray[i]]},`;
-        } for(var j = keyArray.length-1; j < keyArray.length; j++){
-            contents += ` and ${keyArray[j]} at $${getCart()[i][keyArray[j]]}.`;
-        }
-        console.log(contents);
-  }
+
+const msg = 'In your cart, you have ';
+var keyItem = [];
+var valueItem = [];
+var rtString = "";
+
+if (cart.length == 0) {
+  console.log('Your shopping cart is empty.')
 }
 
-/*function viewCart() {
-getCart()
+for (var i = 0; i < cart.length; i++) {
 
-var cartItems = []
-var price = []
-var msg = 'In your cart, you have'
-
-if (cart.length === 0) {
-  console.log ('Your shopping cart is empty.')
+}
 }
 
-for (var i = 1; i < cart.length; i++) {
-  if (i === 1) {
-    console.log(`${msg} ${cart}.`)
+
+/*
+function viewCart() {
+  const l = cart.length
+
+  if (!l) {
+    return console.log("Your shopping cart is empty.")
   }
+
+  const itemsAndPrices = []
+
+  for (let i = 0; i < l; i++) {
+    let itemAndPrice = cart[i]
+    let item = Object.keys(itemAndPrice)[0]
+    let price = itemAndPrice[item]
+
+    itemsAndPrices.push(`${item} at \$${price}`)
+  }
+
+  console.log(`In your cart, you have ${itemsAndPrices.join(', ')}.`)
 }
 
-}
-*/
+
 function total() {
   // write your code here
 }
