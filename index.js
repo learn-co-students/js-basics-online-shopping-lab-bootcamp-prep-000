753c5c490baed2c0d4e2a13149b8f6ffbb7c983b
jencekade <gaephuoc@gmail.com>
2017-12-17 09:54:01 +0000

    Done.

--- index.js
+++ index.js
@@ -11,10 +11,26 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ let newOject = {};
+ let value = Math.floor(Math.random()*101);
+ newOject[item] = value;
+ cart.push(newOject);
+ console.log(`${item} has been added to your cart.`);
+ getCart();
 }
 
 function viewCart() {
   // write your code here
+  if (cart.length ==0) {
+    console.log(`Your shopping cart is empty.`);
+  }
+  let stringer = "";
+  for (let index = 0;index < cart.length-1 ;index++ ){
+    stringer = stringer + "you have "+cart[index].item+" at $"+cart[index].value;
+  }
+  stringer=stringer.toString();
+  console.log(`In your cart, ${stringer} and ${cart[cart.length-1].item} at $${cart[cart.length-1].vlaue}.);
+
 }
 
 function total() {
