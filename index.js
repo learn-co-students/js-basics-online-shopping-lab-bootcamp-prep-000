b0b245456a15229de9f1837963ef3ada1b27f74b
jencekade <gaephuoc@gmail.com>
2017-12-17 09:59:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var cart = [];
 
 function getCart() {
- return cart;
+  return cart;
 }
 
 function setCart(c) {
@@ -10,26 +10,26 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
- let newOject = {};
- let value = Math.floor(Math.random()*101);
- newOject[item] = value;
- cart.push(newOject);
- console.log(`${item} has been added to your cart.`);
- getCart();
+  // write your code here
+  let newOject = {};
+  let value = Math.floor(Math.random() * 101);
+  newOject[item] = value;
+  cart.push(newOject);
+  console.log(`${item} has been added to your cart.`);
+  getCart();
 }
 
 function viewCart() {
   // write your code here
-  if (cart.length ==0) {
+  if (cart.length === 0) {
     console.log(`Your shopping cart is empty.`);
   }
   let stringer = "";
-  for (let index = 0;index < cart.length-1 ;index++ ){
-    stringer = stringer + "you have "+cart[index].item+" at $"+cart[index].value;
+  for (let index = 0; index < cart.length - 1; index++) {
+    stringer = stringer + "you have " + cart[index].key + " at $" + cart[index].value;
   }
-  stringer=stringer.toString();
-  console.log(`In your cart, ${stringer} and ${cart[cart.length-1].item} at $${cart[cart.length-1].vlaue}.);
+  stringer = stringer.toString();
+  console.log(`In your cart, ${stringer} and ${cart[cart.length-1].key} at $${cart[cart.length-1].value}.`);
 
 }
 
