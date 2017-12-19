ba17a2e8d17217587dca0f7715245dd4774cf81c
William Spangler <bspangs@hotmail.com>
2017-12-19 21:19:36 +0000

    Done.

--- index.js
+++ index.js
@@ -9,8 +9,30 @@ function setCart(c) {
   return cart;
 }
 
+function genPrice(min=1, max=100) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+  //Will insert 1 and 100 when call upon in add to cart
+}
+
+
 function addToCart(item) {
  // write your code here
+function addToCart(item) {
+ // write your code here
+var price = genPrice(1, 100);
+var addition = { [item]: [price] };
+//code for adding onto the array below
+cart.push(addition);
+//code 
+console.log(`${item} has been added to your cart`)
+return cart  
+}
+
+
+//console.log   //  "<itemName> has been added to your cart."   Do I use ${item} instead?
+return cart;  
 }
 
 function viewCart() {
