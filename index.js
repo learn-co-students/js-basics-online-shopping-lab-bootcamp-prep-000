d819df6c8890608b2418b068872de204985e077d
jencekade <gaephuoc@gmail.com>
2017-12-20 08:48:24 +0000

    Done.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function addToCart(item) {
   newOject[item] = value;
   cart.push(newOject);
   console.log(`${item} has been added to your cart.`);
-  getCart();
+  return cart;
 }
 
 function viewCart() {
@@ -24,13 +24,23 @@ function viewCart() {
   if (cart.length === 0) {
     console.log(`Your shopping cart is empty.`);
   }
-  let stringer = "";
+  let itemAndPrice = [];
   for (let index = 0; index < cart.length - 1; index++) {
-    stringer = stringer + "you have " + cart[index].key + " at $" + cart[index].value;
+    for (const x in cart[index]) {  //bananas at $17
+      itemAndPrice.push(`${x} at $${cart[index].x}`);
+    }
   }
-  stringer = stringer.toString();
-  console.log(`In your cart, ${stringer} and ${cart[cart.length-1].key} at $${cart[cart.length-1].value}.`);
-
+  if(itemAndPrice.length ==1){
+    console.log(`In your cart, you have ${itemAndPrice[0]}.`);
+  } else if (itemAndPrice.length == 2){
+    console.log(`In your cart, you have ${itemAndPrice[0]} and ${itemNamendPrice[1]}.`)
+  } else {
+    let stringer = "";
+    for (let newIndex = 0; newIndex < itemAndPrice.length-1; newIndex++){
+      stringer =  stringer + itemAndPrice[newIndex] + ", "
+    }
+    console.log(`In your cart, you have ${stringer}and ${itemAndPrice[itemAndPrice.length-1]}.`);
+  }
 }
 
 function total() {
