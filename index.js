911cc8e43b3ebc42fa29dffc08e6386973a3a523
Sean Oughton <seanoughton@gmail.com>
2017-12-21 20:29:01 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,99 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var itemName = item 
+  var itemPrice = Math.random() * (100 - 1) + 1;
+  itemPrice = Math.floor(itemPrice)
+  var cartItem = new Object({ [itemName]: itemPrice })
+  cart.push(cartItem)
+  console.log(`${itemName} has been added to your cart.`)
+  return cart
+
 }
 
 function viewCart() {
-  // write your code here
+  var returnString = `In your cart, you have `
+  var itemString =``
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.")
+  } 
+  else if(cart.length === 1) {
+    var itemName = Object.keys(cart[0])
+    var itemPrice = Object.values(cart[0])
+    returnString = returnString + `${itemName} at $${itemPrice}.`
+    console.log(returnString)
+  }
+  else if(cart.length === 2) {
+    var i = 0
+    while (i<cart.length){
+      itemName = Object.keys(cart[i])
+      itemPrice = Object.values(cart[i])
+      itemString = itemString + `${itemName} at $${itemPrice}`
+      i += 1
+      if (i<cart.length){
+        itemString =  itemString + ' and '
+      }
+    }
+    returnString = returnString + itemString + `.`
+    console.log(returnString)
+  }
+  else{
+    itemString = ``
+    i = 0
+    while (i<cart.length){
+      itemName = Object.keys(cart[i])
+      itemPrice = Object.values(cart[i])
+      if (i === cart.length-1){
+        itemString =  itemString + `and ${itemName} at $${itemPrice}.`
+      } else {
+        itemString = itemString + `${itemName} at $${itemPrice}, `
+      }
+      i += 1
+
+    }
+    returnString = returnString + itemString
+    console.log(returnString)
+    
+  }
+
+  
 }
 
 function total() {
-  // write your code here
+  var i = 0
+  var currentTotal = 0
+  while (i <cart.length){
+    var itemPrice = parseInt(Object.values(cart[i]))
+    currentTotal = currentTotal + itemPrice
+    i += 1
+  }
+  return currentTotal
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var i = 0
+  while (i<cart.length){
+    var test = cart[i].hasOwnProperty(item);
+    if(test === true){
+      cart.splice(i,1)
+      return cart
+    } 
+    i += 1
+  }
+  console.log('That item is not in your cart.')
+  return cart
+
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined){
+  console.log("Sorry, we don't have a credit card on file for you.")
+  } 
+  else {
+    total(cart)
+    console.log(`Your total cost is $${total(cart)}, which will be charged to the card ${cardNumber}.`)
+    cart = []
+    return cart
+  }
 }
+
