2bc9d0f4e621cfd20c5142621f5bd8733b5eb698
Emily Garrison <emily.e.garrison@gmail.com>
2017-12-23 02:42:10 +0000

    Done.

--- index.js
+++ index.js
@@ -49,7 +49,6 @@ function addToCart(item) {
 
 function viewCart() {
    var listOfContents = `In your cart, you have ` + Object.keys(cart[0]) + ` at $` + Object.values(cart[0]);
-   
   if (cart) {
     console.log("Your shopping cart is empty.");
   }
@@ -69,23 +68,7 @@ function viewCart() {
       console.log(listOfContents);
     }
   } 
-  else if (cart.length === 1) {
-        listOfContents = `${listOfContents}` + `.`;
-        console.log(listOfContents);
-    }
-  else if (cart.length === 2) {
-      listOfContents = `${listOfContents}` + ` and ` + Object.keys(cart[1]) + ` at $` + Object.values(cart[1]) + `.`;
-      console.log(listOfContents);
-    }
-  else {
-      for (var i = 1; i < cart.length - 1; i++) {
-        listOfContents += Object.keys(cart[i]) + " at $" + Object.values(cart[i]) + ", ";
-      }
-      listOfContents = `${listOfContents}` + ` and ` + Object.keys(cart[-1]) + ` at $` + Object.values(cart[-1]) + `.`;
-      console.log(listOfContents);
-    }
-  } 
-
+ 
   
 function total() {
 var prices = [];
