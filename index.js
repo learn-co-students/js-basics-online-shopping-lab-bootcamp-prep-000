50b773c3f93aa29dcde46e750e9d24f1b311421f
winter mutt <sejongsan@hotmail.com>
2017-12-28 21:22:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,62 +1,63 @@
 var cart = [];
-function setCart(newCart) {
-  cart = newCart;
+
+function getCart() {
+ return cart;
 }
 
-function total() {
-  let t = 0
-
-  for (var i = 0, l = cart.length; i < l; i++) {
-    for (var item in cart[i]) {
-      t += cart[i][item]
-    }
-  }
-
-  return t
-}
-
-function getCart(){
+function setCart(c) {
+  cart = c;
   return cart;
 }
 
-function addToCart(item){
-  var price = Math.floor(Math.random() * 100);
-  cart.push({[item]: price});
-  console.log(`${item} has been added to your cart.`);
-  return cart;
+function addToCart(item) {
+  var price = Math.floor(Math.random() + 1);
+ cart.push({[item]: price});
+ console.log(`${item} has been added to your cart.`);
+ return cart;
 }
 
-function viewCart(){
+function viewCart() {
   if(cart.length === 0){
-    console.log('Your shopping cart is empty.');
+    console.log("Your shopping cart is empty.");
   }
   else{
     var items = [];
-    for (var i = 0; i < cart.length; i++){
-        for(var item in cart[i]){
-      items.push(`${item} at $${cart[i][item]}`);
+    for(var i = 0; i < cart.length; i++){
+      for (var item in cart[i]){
+        items.push(`${item} at $${cart[i][item]}`);
       }
     }
-    console.log("In your cart, you have " + items.join(', ') + ".");
+    console.log(`In your cart, you have ${items.join(', ')}.`);
   }
 }
 
-function removeFromCart(item){
-  var unwantedItem = false;
-  for(item in cart){
-    var i = cart.indexOf(item);
-    if(cart.hasOwnProperty(item)){
-      unwantedItem = true;
-      cart.splice(i, 1);
+function total() {
+  let total = 0;
+  for(var i = 0; i < cart.length; i++){
+    for(var item in cart[i]){
+      total += cart[i][item];
     }
   }
-    if(!unwantedItem){
+  return total;
+}
+
+function removeFromCart(item) {
+  var unwanted = false;
+    for(item in cart){
+      var i = cart.indexOf(item);
+      if(cart.hasOwnProperty(item)){
+        unwanted = true;
+        cart.splice(i,1);
+      }
+    }
+    if(!unwanted){
       console.log("That item is not in your cart.");
+      return cart;
     }
   return cart;
 }
 
-function placeOrder(cardNumber){
+function placeOrder(cardNumber) {
   if(!cardNumber){
     console.log("We don't have a credit card on file for you to place your order.");
   }
