f4ef268d98ebdab3d131d0e8bdb8034a7048e483
Shirley Zhang <shirleyz9402@yahoo.com>
2017-12-30 01:30:53 +0000

    Done.

--- index.js
+++ index.js
@@ -22,20 +22,31 @@ function viewCart() {
   // write your code here
    var text = "In your cart, you have"
   if (cart[0] === undefined) {
-  console.log(`Your shopping cart is empty.`)}
-  if (cart.length === 1) {
-  console.log(`${text} ${Object.keys(cart[0])} at $${Object.values(cart[0])}.` )}
-  if (cart.length === 2) {
-  console.log(`${text} ${Object.keys(cart[0])} at $${Object.values(cart[0])} and ${Object.keys(cart[1])} at $${Object.values(cart[1])}.`)}
-  else for (let i = 0; i < cart.length; i++) {
-    var sentence = []
-    if (i === cart.length) {
-      sentence.push(`and ${Object.keys(cart[i])} at $${Object.values(cart[i])}.`)}
-    else {
-      sentence.push(`${Object.keys(cart[i])} at $${Object.values(cart[i])}.`)
-    console.log (`${text} ${sentence.join(', ')}.`)}
-  }
   
+  console.log(`Your shopping cart is empty.`)}
+  
+  if (cart.length === 1) {
+  
+  console.log(`${text} ${Object.keys(cart[0])} at $${Object.values(cart[0])}.` )}
+  
+  if (cart.length === 2) {
+  
+  console.log(`${text} ${Object.keys(cart[0])} at $${Object.values(cart[0])} and ${Object.keys(cart[1])} at $${Object.values(cart[1])}.`)}
+  
+  else { 
+    for (let i = 0; i < cart.length; i++) {
+      var sentence = []
+  
+  
+      if (i === cart.length) {
+        sentence.push(`and ${Object.keys(cart[i])} at $${Object.values(cart[i])}`)}
+  
+      else {
+        sentence.push(`${Object.keys(cart[i])} at $${Object.values(cart[i])}`)
+      console.log (`${text} ${sentence.join(', ')}.`)}
+  
+      }
+    }
 }
 
 function total() {
