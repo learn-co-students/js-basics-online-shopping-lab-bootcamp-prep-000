2e6751a0561d7e0d051317dcd3d3e33baf89f55a
Luke Conley <luke.sovern.conley@gmail.com>
2017-12-31 21:38:54 +0000

    Done.

--- index.js
+++ index.js
@@ -8,17 +8,63 @@ function setCart(c) {
   cart = c;
   return cart;
 }
-
 function addToCart(item) {
- // write your code here
+  var itemPrice = Math.floor(Math.random() * 101) + 1;
+  var newItem = { [item]:itemPrice };
+  cart.push(newItem);
+  console.log(`${item} has been added to your cart.`)
+  return cart
 }
 
 function viewCart() {
-  // write your code here
+var currentCart = [];
+
+//var itemPrices = cart.values();
+if (cart.length === 0) {
+  return console.log('Your shopping cart is empty.');
 }
 
+if (cart.length>0) {
+  for (let i = 0; i < cart.length; i++){
+    var itemNames = Object.keys(cart[i]);
+    var price = cart[i][itemNames[0]]
+      currentCart.push(`${itemNames} at $${price}`)
+    }
+  } //for loop final bracket
+var finalStatement = '';
+  switch (cart.length) {
+
+    case 1:
+    finalStatement = currentCart.join();
+    break;
+
+    case 2:
+
+    finalStatement = currentCart.join(' and ');
+    break;
+
+    default:
+    var x = currentCart.length-2;
+    var y = currentCart.length-1;
+    debugger
+    currentCart[y] = `and ${currentCart[y]}`;
+    finalStatement = currentCart.join(', ');
+  }
+  console.log(`In your cart, you have ${finalStatement}.`);
+} //if statement final bracket
+
+
+
 function total() {
-  // write your code here
+  var sum = [];
+    for (let i = 0; i < cart.length; i++){
+      var itemNames = Object.keys(cart[i]);
+      var price = cart[i][itemNames[0]]
+      sum.push(`${price}`);
+    var total = sum.reduce();
+    return total;
+  }
+
 }
 
 function removeFromCart(item) {
