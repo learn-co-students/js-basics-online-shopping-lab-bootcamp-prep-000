d28d78e25b0e050ecb4b6c1a7756e1786cdb4158
Monica Tamaru Jones <mntamaru@gmail.com>
2018-01-03 17:29:51 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,79 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var price = Math.floor(Math.random() * 99) + 1
+ var newItem = { [item] : price}
+ console.log (item + " has been added to your cart.")
+ cart.push(newItem)
+ return cart
 }
 
 function viewCart() {
-  // write your code here
+  var keyNames = []
+  var valueNames = []
+  var str = "In your cart, you have "
+  
+  for (let i=0; i < cart.length; i++) {  
+    keyNames[i] = Object.keys(cart[i])
+    valueNames[i] = Object.values(cart[i])
+  }
+
+  if (cart.length === 0) {
+    console.log ("Your shopping cart is empty.")
+  }
+  
+  else if (cart.length === 1) {
+    str += keyNames[0] + " at $" + valueNames[0] + "."
+  }
+  
+  else if (cart.length === 2) {
+    str += keyNames[0] + " at $" + valueNames[0] + " and " + keyNames[1] + " at $" + valueNames[1] + "."
+  }
+  
+  else {
+    for(let i=0; i<keyNames.length-1; i++) {
+      str += keyNames[i] + " at $" + valueNames[i] + ", "
+    }
+    str += "and " + (keyNames[keyNames.length-1]) + " at $" + valueNames[valueNames.length-1] + "."
+  }
+  
+  console.log (str)
 }
 
 function total() {
-  // write your code here
+  var valueNames = []
+  var sum = 0
+  
+  for (let i=0; i < cart.length; i++) {  
+    valueNames[i] = parseInt(Object.values(cart[i]))
+    sum += valueNames[i]
+  }
+  return sum
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var inCart = false
+
+  for (let i=0; i<cart.length; i++) {
+    if(String(Object.keys(cart[i])) === item) {
+      inCart = true
+      cart.splice(i,1)
+    }
+  }
+  if(inCart === false) {
+    console.log("That item is not in your cart.")
+  }
+   return cart
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined) {
+    console.log("Sorry, we don't have a credit card on file for you.")
+ 
+  }
+  else{
+    var cost = total()
+    console.log("Your total cost is $" + cost + ", which will be charged to the card " + cardNumber + ".")
+    cart = []
+  }
 }
