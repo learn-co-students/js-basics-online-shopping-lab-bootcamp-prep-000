735f1e1a958b5cd5c7f732f4c4856b1693cb5d74
Katrina Brinson <kbrinso@gmail.com>
2018-01-05 22:49:00 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,44 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var price = Math.floor(Math.random() * 100) + 1; 
+ var obj = Object.assign({[item]: price});
+ cart.push(obj);
+ console.log(`${item} has been added to your cart.`);
+ return cart ;
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length == 0){
+    return console.log("Your shopping cart is empty.");
+  } else {
+    for(var i = 0; i < cart.length; i++){
+      var li = "In your cart, you have ";
+      var currItem = cart[i];
+      var itemName = Object.getOwnPropertyNames(currItem);
+      var itemPrice = currItem.itemName;
+      if (cart.length >= 3){
+        //3 plus items
+          if(i + 1 == cart.length){ 
+            li = `${li}and ${itemName} at $${itemPrice}.`;
+            return console.log(li);
+          } else {
+            li = `${li} ${itemName} at $${itemPrice}, `
+          }
+      } else if (cart.length == 2){
+        var nextItem = cart[i + 1];
+        var nitemName = Object.getOwnPropertyNames(nextItem);
+        var nitemPrice = nextItem.nitemName;
+        li = `${li} ${itemName} at $${itemPrice} and ${nitemName} at $${nitemPrice}.`;
+        return console.log(li);
+        //2 items in cart
+      } else if(cart.length == 1){ 
+        li = `${li}${itemName} at $${itemPrice}.`;
+        return console.log(li);
+        //1 item in cart
+      }
+    }
+  }
 }
 
 function total() {
