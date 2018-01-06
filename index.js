b62ea60985020df380206590ba04f3a5fdb06131
Tim Hillhouse <thillhouse2@gmail.com>
2018-01-06 20:38:43 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor((Math.random() * 100) + 1);
+ cart.push(item, price);
 }
 
 function viewCart() {
