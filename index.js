ab10d069efba0f2ecf2818f756fba7a3cc890574
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-09 16:58:15 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,60 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var price = Math.floor(Math.random() * 100) + 1;
+ cart.push({ [item]: price});
+ console.log(`${item} has been added to your cart.`);
+ return cart;
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length > 0) {
+    var cartContents = [];
+    for (let i = 0; i < cart.length; i++) {
+      cartContents.push(` ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}`);
+    }
+    if (cart.length == 1) {
+      console.log(`In your cart, you have${cartContents[0]}.`); 
+    } else if (cart.length == 2) {
+      console.log(`In your cart, you have${cartContents[0]} and${cartContents[1]}.`); 
+    } else if (cart.length > 2) {
+      var allButLast = [];
+      for (let i = 0; i < cart.length - 1; i++) {
+        allButLast.push(cartContents[i]);
+      }
+      var allButLastString = allButLast.join();
+      console.log(`In your cart, you have${allButLastString}, and${cartContents[cart.length - 1]}.`);
+    }
+  } else {
+    console.log('Your shopping cart is empty.');
+  }
+  
 }
 
 function total() {
-  // write your code here
+  var totalPrice = 0;
+  for (let i = 0; i < cart.length; i++) {
+    totalPrice = totalPrice + cart[i][Object.keys(cart[i])];
+  }
+  return totalPrice
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      cart.splice(cart.indexOf(cart[i]), 1);
+      return cart;
+    }
+  }
+  console.log("That item is not in your cart.");
+  return cart;
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber) {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    cart = [];
+  } else {
+    console.log("Sorry, we don't have a credit card on file for you.")
+  }
 }
