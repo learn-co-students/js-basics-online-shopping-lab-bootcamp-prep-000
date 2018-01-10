af55bc8c1cabf826086f11eac0403b0931fa0f7e
James Overbay <orangetoque13@gmail.com>
2018-01-10 07:07:49 +0000

    Done.

--- index.js
+++ index.js
@@ -58,9 +58,23 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
-}
+  for (let i = 0; i<cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      cart.splice(i, 1)
+    }
+  }
+    console.log("That item is not in your cart.")
+    return cart
+  }
+  
+ 
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined) {
+  console.log("Sorry, we don't have a credit card on file for you.")
+  }
+  else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+  }
+  cart.length = 0
 }
