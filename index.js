bfdf72b3d50e6f03d5015c960c2c79782ff367e1
James Overbay <orangetoque13@gmail.com>
2018-01-10 04:51:57 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,34 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  function price() {
+  Math.ceil(1);
+  Math.floor(100);
+  return Math.floor(Math.random() * (100 - 1 + 1)) + 1
+  }
+ cart.push({ [item]: price() })
+ console.log(item + " has been added to your cart.")
+ return cart
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.")
+  }
+  var cartList = []
+  for (let i = 0; i<cart.length; i++){
+    let groceryAndPrices = cart[i]
+    let grocery = Object.keys(groceryAndPrices)
+    let price = groceryAndPrices[grocery]
+    cartList.push(`${grocery} at $${price}`)
+  }
+  if (cart.length === 1) {
+  console.log(`In your cart, you have ${cartList}.`)
+  }
+  if (cart.length === 2) {
+    console.log(`In your cart, you have ${cartList[0]} and ${cartList[1]}.`)
+  }
+// if (cart.length > 2) 
 }
 
 function total() {
