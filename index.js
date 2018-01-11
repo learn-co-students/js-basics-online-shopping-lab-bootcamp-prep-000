2195c59b8abbcfd69dc103802c9e67f06ceaeb5a
Andrew McIntosh <andrewdavidmcintosh@gmail.com>
2018-01-11 20:14:04 +0000

    Done.

--- index.js
+++ index.js
@@ -61,8 +61,7 @@ function placeOrder(cardNumber) {
    console.log("Sorry, we don\'t have a credit card on file for you.");
  } else {
    console.log("Your total cost is ${total()}, which will be charged to the card ${cardNumber}.");
-   var card = '0';
-   card.length = 0;
+   cart.length = 0;
    return cart;
  }
 }
