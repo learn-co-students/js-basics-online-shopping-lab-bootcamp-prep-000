ed7cc495bd5444596d4856f0e72cb4f495bbad3a
Andrew McIntosh <andrewdavidmcintosh@gmail.com>
2018-01-11 18:28:58 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,29 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var price = Math.floor(Math.random()*99);
+ cart.push(new Object({[item]: price}));
+ console.log('${item} has been added to your cart.' );
+ return cart;
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) { 
+  console.log('Your shopping cart is empty');
+} else {
+  var cartItems = "In your cart, you have";
+  for(var i = 0, l = cart.length; i < l; i++);{
+    for(var list in cart[i]){
+      cartItems+=`${list} at $${cart[i][list]}`;
+        if(i!==cart.length-1){
+           cartItems+=", ";
+               } else {
+               cartItems+=".";
+                }
+              }
+            }
+        console.log(cartItems);
+      }
 }
 
 function total() {
