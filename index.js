2cb9b0f446729bb834a67b59e1479f75e3cf4c45
Krista Dotzenrod <kdotzenrod517@gmail.com>
2018-01-12 19:51:07 +0000

    Done.

--- index.js
+++ index.js
@@ -19,28 +19,60 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  var inCart = "In your cart, you have "
-  var sentence = []
   if (cart.length === 0) {
-  return console.log("Your shopping cart is empty.")
-} else {
+      return console.log("Your shopping cart is empty.");
+    }
 
-  Object.keys(cart).forEach(function(key) {
-          return console.log(key, cart[key]);
-    });
+    var returnStatement = "In your cart, you have";
 
-  }
-}
+    if (cart.length === 1) {
+      returnStatement += ` ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}.`;
+
+    } else if (cart.length === 2) {
+      returnStatement += ` ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]} and ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}.`;
+    } else {
+      for (var i = 0; i < cart.length; i ++) {
+         if (i === (cart.length - 1) ) {
+           returnStatement += ` and ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}.`;
+         } else {
+          returnStatement += ` ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]},`;
+         }
+      }
+    }
+    return console.log(returnStatement);
+ }
 
 
 function total() {
-  // write your code here
+  var total = 0
+  for (let i = 0; i < cart.length; i++) {
+    total += parseInt(`${cart[i][Object.keys(cart[i])]}`);
+  }
+  return total;
 }
 
-function removeFromCart(item) {
 
+function removeFromCart(item) {
+    if(cart.length > 0){
+      for(let i = 0; i < cart.length; i++){
+          if(cart[i].hasOwnProperty(item)){
+            cart.splice(i, 1);
+            return cart;
+          }
+      }} else {
+        console.log('That item is not in your cart.')
+      }
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
-}
+  if (!cardNumber) {
+console.log("Sorry, we don't have a credit card on file for you.");
+   }
+
+    else {
+      console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    }
+      cart = [];
+      return cart;
+
+    }
