00e116bd6186e20295c2c53e01db82a27ea0f932
Andrew McIntosh <andrewdavidmcintosh@gmail.com>
2018-01-12 17:40:49 +0000

    Done.

--- index.js
+++ index.js
@@ -10,10 +10,11 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- var price = Math.floor(Math.random()*99);
- cart.push(new Object({[item]: price}));
- console.log(item + 'has been added to your cart.');
- return cart;
+  var itemName = item;
+  const itemPrice = Math.floor(Math.random() * 100 + 1);
+  cart.push(item);
+  console.log(itemName + " has been added to your cart.");
+  return cart;
 }
 
 
