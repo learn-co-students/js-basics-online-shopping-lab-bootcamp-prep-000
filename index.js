6eb95898cd1c0e771272cd6d2d350c36ce2f31ce
kendocode <git@kendocoding.com>
2018-01-15 01:10:15 -0600

    Test view cart and get cart

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var cart = [];
 
-function getCart() {
- return cart;
+function randomPrice() {
+  return Math.floor((Math.random() * 100) + 1);
+}
+
+
+function getCart(item) {
+  cart.push({[item]: [randomPrice()]});
+  console.log(`${item} has been added to the cart.`);
+  return cart;
 }
 
 function setCart(c) {
@@ -14,7 +21,26 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  // write your code here
+  var message = "In your cart, you have "
+  var products = []
+  var prices = []
+  
+  for (let item of cart) {
+    products.push(Object.keys(item)[0]);
+    prices.push(Object.values(item)[0]);
+  }
+  
+  switch (products.length) {
+  case 1:
+    return message += `${products[0]} at $${prices[0]}.`;
+  case 2:
+    return message += `${products[0]} at $${prices[0]} and ${products[1]} at $${prices[1]}.`;
+  default:
+    for(let i = 0; i < products.length - 1; i++) {
+      message += `${products[i]} at $${prices[i]}, `
+    }
+    return message += `and ${products[products.length]} at $${prices[products.length]}.`
+  }
 }
 
 function total() {
