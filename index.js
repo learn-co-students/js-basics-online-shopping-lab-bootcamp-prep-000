1dd1b2e0aa8bccb0010f588f3291505096ba462c
Jabulani Msimango <msimango.jabulani@moov.life>
2018-01-17 14:42:49 +0000

    Done.

--- index.js
+++ index.js
@@ -10,15 +10,38 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  cart[item] = Math.floor(Math.random() * 100) + 1
+  console.log(`${item} has been added to your cart.`)
+  return cart
 }
 
 function viewCart() {
-  // write your code here
+  var message = "In your cart, you have "
+  for (var key in cart) {
+    message += `${key} at $${cart[key]} and `
+  }
+  /*
+  for (let i = 0; i < cart.length; i++) {
+    if (cart.length == 0)
+      return "Your shopping cart is empty."
+    if (i < cart.length - 1)
+      message += `${cart[i]} at $${cart[i]} and `
+    else
+      message += `${cart[i]} at $${cart[i]}.`
+  }*/
+  return message
 }
 
 function total() {
-  // write your code here
+  var total = 0
+  for (var key in cart) {
+    total += cart[key]
+  }
+  /*
+  for (i = 0; i < cart.length; i++) {
+    total += cart[i]
+  }*/
+  return total
 }
 
 function removeFromCart(item) {
