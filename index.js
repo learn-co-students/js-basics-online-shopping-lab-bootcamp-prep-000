289cd8e93a4aa8b8702c6ec969f82de980bd5732
SpencerWhitehead7@gmail.com <spencerwhitehead7@gmail.com>
2018-01-19 04:07:20 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var newitem = {[item]:Math.floor(Math.random()*100)}
+ cart.push(newitem)
+ console.log(`${item} has been added to your cart.`)
+ return cart
 }
 
 function viewCart() {
