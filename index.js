7c958888834bdf9d35e477c6eb4feabb18d9140f
Jessie Chen <jessiechen0513@gmail.com>
2018-01-20 02:46:21 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,28 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var itemObject = {};
+ var itemPrice = Math.floor(Math.random() * 101);
+ itemObject[item] = itemPrice;
+ cart.push(itemObject);
+ console.log (item + " has been added to your cart.");
+ return cart;
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length > 0){
+    while (cart.length !== 0){
+      if (cart.length == 1){
+        console.log ("In your cart, you have " + Object.keys(cart)[0] + " at $" + cart[0] +".");
+      }else if (cart.length == 2){
+        console.log ("In your cart, you have " + Object.keys(cart)[0] + " at $" + cart[0] + " and " + Object.keys(cart)[1] + " at $" + cart[1] + ".");
+      }else if (cart.length == 3){
+        console.log("In your cart, you have" + Object.keys(cart)[0] + " at $" + cart[0] + " ," + Object.keys(cart)[1] + " at $" + cart[1] + " , and " + Object.keys(cart)[2] + " at $" + cart[2] + ".");
+      }
+    }
+  }else {
+    console.log("Your shopping cart is empty.");
+  }
 }
 
 function total() {
