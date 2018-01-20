49b0455b1dbeafae665e7015d7d9d88007b4ab6f
Jo Christian Sandvik <jochristian.sandvik@gmail.com>
2018-01-20 16:21:07 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var itemPrice = Math.floor(Math.random()*100);
+  cart.push({item: itemPrice});
+  return cart;
 }
 
 function viewCart() {
