70fe76b5d06bdf89e654b89b452263548e957d8d
David Clark <dlc.clark@gmail.com>
2018-01-21 20:02:54 +0000

    Done.

--- index.js
+++ index.js
@@ -10,12 +10,40 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+// write your code here
+  var itemPrice = Math.floor((Math.random() * 100) + 1);
+  var itemObj = {[item]: itemPrice};
+  cart.push(itemObj);
+  console.log(`${item} has been added to your cart.`)
+  return cart;
 }
 
 function viewCart() {
   // write your code here
+  if (cart.length === 0){
+    console.log('Your shopping cart is empty.')
+  } else if (cart.length === 1){
+    var oneItem = `In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])}.`
+    console.log(oneItem)
+  } else if (cart.length === 2){
+    var twoItems = `In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])} and ${Object.keys(cart[1])} at $${Object.values(cart[1])}.`
+    console.log(twoItems)
+  } else {
+    var cartData = []
+    for (var i = 0; i < cart.length-1; i++){
+      cartData.push(`${Object.keys(cart[i])} at $${Object.values(cart[i])}`)
+
+    }
+    var threePlusItems = `In your cart, you have ${cartData.join(', ')} and ${Object.keys(cart[cart.length-1])} at $${Object.values(cart[cart.length-1])}.`
+  }
+  console.log(threePlusItems)
 }
+
+addToCart("apples");
+addToCart("oranges");
+addToCart("bananas");
+
+viewCart()
 
 function total() {
   // write your code here
