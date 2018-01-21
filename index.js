ae9eae487745f4415013e3ef6371d97f988fc6d8
Jessie Chen <jessiechen0513@gmail.com>
2018-01-21 04:27:11 +0000

    Done.

--- index.js
+++ index.js
@@ -19,19 +19,20 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  if (cart.length > 0){
-    while (cart.length !== 0){
-      if (cart.length == 1){
-        console.log ("In your cart, you have " + Object.keys(cart)[0] + " at $" + cart[0] +".");
-      }else if (cart.length == 2){
-        console.log ("In your cart, you have " + Object.keys(cart)[0] + " at $" + cart[0] + " and " + Object.keys(cart)[1] + " at $" + cart[1] + ".");
-      }else if (cart.length == 3){
-        console.log("In your cart, you have" + Object.keys(cart)[0] + " at $" + cart[0] + " ," + Object.keys(cart)[1] + " at $" + cart[1] + " , and " + Object.keys(cart)[2] + " at $" + cart[2] + ".");
+  if (cart.length === 0){
+      console.log("Your shopping cart is empty.");
+    }else {
+    const itemsAndPrices = [];
+    const l = cart.length;
+      for (let i = 0; i< 0; i++){
+        let itemAndPrice = cart[i];
+        let item = Object.keys(itemAndPrice)[0];
+        let price = itemAndPrice[item];
+        itemsAndPrices.push(`${item} at \$${price}`);
       }
+    console.log(`In your cart, you have ${itemsAndPrices.join(', ')}.`);
     }
-  }else {
-    console.log("Your shopping cart is empty.");
-  }
+
 }
 
 function total() {
