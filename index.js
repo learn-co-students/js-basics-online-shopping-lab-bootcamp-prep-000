567d879efe5f3d92ee5c0e271e450931be20fa26
Aspen James <aspenjames@tqca.org>
2018-01-22 20:10:38 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,53 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random()*100)
+  var itemObj = { [item]: price }
+  cart.push(itemObj)
+  console.log(`${item} has been added to your cart.`)
+  return cart
 }
 
-function viewCart() {
-  // write your code here
+/*function viewCart() {
+  var message = "";
+  var keys = [];
+  var values = [];
+  for(var obj in cart){
+    keys.push(Object.keys(cart[obj]));
+    values.push(cart[obj]);
+    }
+  if(cart.length === 0){
+    console.log("Your shopping cart is empty.")
+  }else if(cart.length === 1){
+    console.log(`In your cart, you have ${keys[0]} at $${values[0]}.`)
+  }else{
+    for(let i=0;i<cart.length-1;i++){
+      message = message + `${keys[i]} at $${values[i]}, `
+    }
+    message = message + `and ${keys[cart.length-1]} at $${values[cart.length-1]}.`
+    console.log(`In your cart, you have ${message}.`)
+  }
+}*/
+
+function viewCart(){
+  var message = "";
+  var keys = [];
+  var values = [];
+  for(var obj in cart){
+    keys.push(Object.keys(cart[obj]));
+    values.push(cart[obj]);
+  }
+  if(cart.length > 1){
+    for(let i=0;i<cart.length;i++){
+      message += `${keys[i]} at $${values[i]}, `;
+    }
+    message += `and ${keys[(cart.length)-1]} at $${values[(cart.length)-1]}.`
+    console.log(`In your cart, you have ${message}`)
+  }else if(cart.length === 1){
+    console.log(`In your cart, you have ${keys[0]} at $${values[0]}.`)
+  }else{
+    console.log("Your shopping cart is empty.")
+  }
 }
 
 function total() {
