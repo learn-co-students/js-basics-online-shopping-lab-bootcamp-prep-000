f0505b67ba8666f53563feb144fa0b48345df73a
Greg Stimson <stimsong@yahoo.com>
2018-01-23 22:40:07 +0000

    Done.

--- index.js
+++ index.js
@@ -20,6 +20,27 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
+  var wholeSentence = "In your cart, you have ";
+  if(!cart.length) {
+    return console.log("Your shopping cart is empty.");
+  } else if(cart.length === 1) {       // 1 item cart
+      wholeSentence += `${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}.`;
+  } else if(cart.length === 2) {     // 2 item cart
+      wholeSentence += `${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]} and ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}.`;
+  } else { 
+    for( var i = 0; i<cart.length; i++ ) {
+      var filler;
+      if(i === cart.length - 2) {
+        filler = ", and ";
+      } else if( i === cart.length - 1) {
+        filler = ".";
+      } else {
+        filler = ", ";
+      }
+    wholeSentence += `${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}` + filler;
+    }
+  }
+  return console.log(wholeSentence);
 }
 
 function total() {
