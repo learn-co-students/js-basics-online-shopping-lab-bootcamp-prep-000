da69e70f7d881709f4d93f90bf110eb7ba5f61de
Betsy Sallee <ensallee@gmail.com>
2018-01-25 17:44:25 +0000

    Done.

--- index.js
+++ index.js
@@ -33,20 +33,41 @@ console.log(`In your cart, you have ${newArray.join(" and ")}.`);
  newArray[newArray.length-1]="and ".concat(newArray[newArray.length-1]);
  console.log(`In your cart, you have ${newArray.join(", ")}.`);
 }
-     
-  } else {
+ } else {
   console.log("Your shopping cart is empty.");
 }
 }
 
 function total() {
-//write your code here
+let prices=[];
+let sum=0;
+
+for (let i=0; i<cart.length; i++){
+  sum+=(Object.values(cart[i])[0]);
+}
+//prices.reduce(function(a, value) {a + value;
+//  return accumulator
+//})
+return sum;
 }
 
 function removeFromCart(item) {
-  // write your code here
-}
+  for (let i=0; i<cart.length; i++){
+  if (cart[i].hasOwnProperty([item])){
+   cart.splice(i, 1);
+   return cart;
+    }
+  }
+    console.log("That item is not in your cart.");
+    return cart;
+  }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(cardNumber){
+      console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+      cart.splice(0, cart.length);
+  }
+  else{
+    console.log("Sorry, we don't have a credit card on file for you.");
+  }
 }
