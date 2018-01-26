23a29a08b50406363fbeafc79b99e457e3c208de
rosie w <rosie.wilt@gmail.com>
2018-01-26 00:53:46 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,19 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var item = {}
+  for (var itemN = 0; itemN < item.length; i++) {Object.assign({itemName:"itemPrice"})
+  Math.floor(Math.random(`${itemPrice}`))
+  console.log(`${itemName} has been added to your cart.`)}
+  return item
 }
 
-function viewCart() {
-  // write your code here
+function viewCart(){
+if  (cart==0) {console.log(`Your shopping cart is empty`) }
+else {
+for (var i=0; i< cart.length; i++) {return(`In your cart, you have ${itemName}, at ${itemPrice}.`)}
+}
+
 }
 
 function total() {
