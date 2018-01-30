a30d1082e1b3d0e235557d805f45a7506b6c017c
Jeewon Kim <jki00129@gmail.com>
2018-01-30 23:11:35 +0000

    Done.

--- index.js
+++ index.js
@@ -11,10 +11,21 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var itemPrice = Math.floor(Math.random() * 100) + 1;
+ var obj = {[item]: itemPrice};
+ cart.push(obj)
+  console.log(item + " has been added to your cart.");
+ return cart
 }
 
 function viewCart() {
   // write your code here
+  if(cart.length === 0){
+  console.log ("Your shopping cart is empty.")
+} 
+  for (let i = 0; i < items.length; i++)
+  cart.push(`In your cart ${item} at ${itemPrice[i]}`);
+  return cart
 }
 
 function total() {
