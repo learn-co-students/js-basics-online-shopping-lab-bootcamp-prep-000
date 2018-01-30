4e20bf011aa18bc1e70ef6eb5de247ab73f61dc7
Jeewon Kim <jki00129@gmail.com>
2018-01-30 23:15:09 +0000

    Done.

--- index.js
+++ index.js
@@ -28,14 +28,47 @@ function viewCart() {
   return cart
 }
 
-function total() {
+ function total() {
   // write your code here
-}
+  let t = 0;
+
+  for (let i = 0, l = cart.length; i < l; i++) {
+    for (let item in cart[i]) {
+      t += cart[i][item];
+    }
+  }
+
+  return t;
+ }
 
 function removeFromCart(item) {
   // write your code here
-}
+ let itemInCart = false;
 
-function placeOrder(cardNumber) {
+  for (let i = 0, l = cart.length; i < l; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      itemInCart = true;
+      cart = cart.slice(0, i).concat(cart.slice(i + 1));
+      l--;
+    }
+  }
+
+  if (!itemInCart) {
+    console.log("That item is not in your cart.");
+  }
+
+  return cart;
+ }
+
+
+ function placeOrder(cardNumber) {
   // write your code here
-}
+  if (!cardNumber) {
+    console.log("Sorry, we don't have a credit card on file for you.");
+    return false;
+  }
+
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+
+  cart = [];
+ }
