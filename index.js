a1dd83bbbe310589de9fcafe2512181468442b73
Landen Danyluk <landendanyluk@gmail.com>
2018-02-04 02:12:53 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,28 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ cart[item] = Math.floor(Math.random()*100);
+ console.log(`${item} has been added to your cart.`)
 }
 
+// In your cart you have
+// something at $2
+// (and ... / ,something at $3)
+
 function viewCart() {
-  // write your code here
+  var response
+  if (cart.length > 0) {
+    response = `In your cart, you have ${Object.keys(cart)[0]} at ${Object.values(cart)[0]}`
+
+      for (var i = 1; i < cart.length - 1; i++) {
+        response = response + `, ${Object.keys(cart)[i]} at ${Object.values(cart)[i]}`
+      }
+      response = response + `and ${Object.keys(cart)[cart.length + 1]} at ${Object.values(cart)[cart.length + 1]}`
+
+  } else {
+    response = "Your shopping cart is empty.";
+  }
+  return response;
 }
 
 function total() {
