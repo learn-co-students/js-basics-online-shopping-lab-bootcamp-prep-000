911813bb5c58f4e03b3d62e658c74fcfd7207cf7
Brandon Cassidy <brandoncassidy11@yahoo.com>
2018-02-07 23:02:23 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var cart = {};
+ var randomNum = Math.floor(Math.random() * 101) + 1;
+ cart[item] = randomNum;
+ return cart
 }
 
 function viewCart() {
