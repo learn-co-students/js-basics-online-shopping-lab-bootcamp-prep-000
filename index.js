4475423830a3f8e25ecc73f2bfc123a1454d27e7
combskyle@gmail.com <combskyle@gmail.com>
2018-02-08 14:39:58 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,68 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var newItem = {[item]: (Math.floor(Math.random() * Math.floor(100)))}
+ cart.push(newItem);
+ console.log(`${item} has been added to your cart.`);
+ return cart;
 }
 
 function viewCart() {
-  // write your code here
+  var sentence = "In your cart, you have "
+  var newCart = [];
+  for (var i = 0; i < cart.length; i++) {
+    var item = Object.keys(cart[i])
+    var price = cart[i][item];
+    newCart.push(`${item} at $${price}`);
+  }
+  
+  if (newCart.length === 0) {
+    console.log(`Your shopping cart is empty.`)
+  } else if (newCart.length === 1) {
+    console.log(sentence + `${newCart[0]}.`)
+  } else if (newCart.length === 2) {
+    console.log(sentence + `${newCart[0]} and ${newCart[1]}.`)
+  } else if (newCart.length > 2) {
+    for (var x = 0; x < (newCart.length - 1); x++) {
+      sentence +=`${newCart[x]}, `
+    }
+    var last = newCart.length - 1;
+    console.log(sentence +  `and ${newCart[last]}.`)
+  }
 }
 
+
 function total() {
-  // write your code here
+  var cartTotal = 0;
+  for (var i = 0; i < cart.length; i++) {
+    var item = Object.keys(cart[i])
+    var price = cart[i][item];
+    cartTotal += price;
+  }
+  return cartTotal;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var removed = [];
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      removed = cart.splice(i,1);
+    } 
+  }
+  if (removed.length === 0) {
+      console.log (`That item is not in your cart.`);
+    } else {
+      return cart;
+    }
 }
 
+
 function placeOrder(cardNumber) {
-  // write your code here
+  var cartTotal = total();
+  if (cardNumber) {
+    console.log(`Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`)
+    cart = [];
+  } else {
+    console.log(`Sorry, we don't have a credit card on file for you.`)
+  }
 }
