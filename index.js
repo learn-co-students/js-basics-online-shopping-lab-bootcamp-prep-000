163e40abb1785c5d08244d308baf78dc3c434bf1
Troy Lawrence <redirect@gmail.com>
2018-02-09 18:42:45 +0000

    Done.

--- index.js
+++ index.js
@@ -9,16 +9,38 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+  var itemPrice = Math.floor((Math.random() * 100) + 1);
+  var item = { [itemName]: itemPrice };
+ cart.push(item);
+ console.log(`${itemName} has been added to your cart.`);
+ return cart;
 }
 
 function viewCart() {
-  // write your code here
+  let text = [];
+  let items = [];
+  let prices = [];
+
+  if (cart.length===0) {
+       console.log(`Your shopping cart is empty.`);
+      } else if (cart.length===1) {
+        console.log(`In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])}.`);
+      } else if (cart.length===2) {
+          console.log(`In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])} and ${Object.keys(cart[1])} at $${Object.values(cart[1])}.`);
+      } else {
+        for (let i = 0; i < cart.length; i++) {
+            items.push(Object.keys(cart[i]));
+            prices.push(Object.values(cart[i]));
+            text.push(`${items[i]} at $${prices[i]}`);
+          }
+        console.log(`In your cart, you have ${text.join(', ')}.`);
+      }
+      return cart;
 }
 
 function total() {
-  // write your code here
+  return cart.values;
 }
 
 function removeFromCart(item) {
