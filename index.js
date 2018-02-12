ae670803c03f9b32d34555dc7198137a9b0fd724
Jeffrey Damaso <jeffreydamaso340@gmail.com>
2018-02-12 19:07:04 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,16 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  
+ var price = Math.floor(Math.random() * 100);
+ cart.push({[item] : price});
+ console.log(`${item} has been added to your cart.`);
+ return cart;
 }
 
 function viewCart() {
-  // write your code here
+
+
 }
 
 function total() {
