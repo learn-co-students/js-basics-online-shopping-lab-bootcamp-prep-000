35162f6d4d9fe4baceff72fd07439fc5372b3d84
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-02-13 04:45:46 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,70 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ let newObj = {[item]: Math.floor(Math.random() * 100)};
+ getCart().push(newObj);
+ console.log(`${item} has been added to your cart.`);
+ return cart;
 }
 
 function viewCart() {
-  // write your code here
+  var cart = getCart();
+  var cartContents = "In your cart, you have "
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.");
+    return;
+  }
+  for(var i = 0; i <cart.length; i++) {
+    var item = cart[i];
+    var itemName = Object.keys(item)[0];
+    cartContents += `${itemName} at $${item[itemName]}`;
+    if (i === cart.length - 1) {
+      cartContents += '.';
+    } else if (cart.length === 2) {
+        cartContents += ' and '
+    } else {
+      if (i === cart.length - 2) {
+        cartContents += ', and '
+      } else {
+        cartContents += ', ';
+      }
+    }
+  }
+   console.log(cartContents);
 }
 
 function total() {
-  // write your code here
+  var total = 0;
+  for (var i = 0; i < cart.length; i++) {
+    total += Object.values(cart[i])[0];
+  }
+  return total;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var matchingItem;
+  var matchingItemIndex;
+  for (var i = 0; i < cart.length; i++) {
+    var itemKey = Object.keys(cart[i])[0];
+    if (itemKey === item) {
+      matchingItem = cart[i];
+      matchingItemIndex = i;
+    }
+  }
+  if (matchingItem === undefined) {
+    console.log("That item is not in your cart.");
+    return;
+  }
+  cart.splice(matchingItemIndex, 1);
+  return cart;
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined) {
+    console.log("Sorry, we don\'t have a credit card on file for you.");
+  } else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    cart = [];
+  }
+  
 }
