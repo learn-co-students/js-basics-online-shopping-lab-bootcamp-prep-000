853157ad0981cc6b33d158d06640fb81a1181bb4
Pitta <mailez4all@gmail.com>
2018-02-17 06:48:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var cart = [];
+var cart = [ ];
+
 
 function getCart() {
  return cart;
@@ -9,20 +10,93 @@ function setCart(c) {
   return cart;
 }
 
+setCart();
+
 function addToCart(item) {
- // write your code here
+
+  var price = Math.floor(Math.random() * 100) + 1;
+
+  cart.push({[item[i]] : price});
+
+  console.log(`${item[i]} has been added to your cart.`)
+  return cart;
+
 }
+
 
 function viewCart() {
-  // write your code here
+
+   if (Object.keys(cart).length === 0) {
+
+      console.log("Your shopping cart is empty.")
+
+    } else if (Object.keys(cart).length === 1 ) {
+
+      console.log(`In your cart, you have ${Object.keys(cart)[0]}
+      at $${Object.values(cart)[0]}. `)
+
+      return (`In your cart, you have ${Object.keys(cart)[0]}
+      at $${Object.values(cart)[0]}. `)
+
+    } else if (Object.keys(cart).length === 2 ) {
+
+      console.log("In your cart, you have " + Object.keys(cart)[0] + " at $" +
+      Object.values(cart)[0] + " and " + Object.keys(cart)[1] + " at $" +
+      Object.values(cart)[1] + "." )
+
+      return ("In your cart, you have " + Object.keys(cart)[0] + " at $" +
+      Object.values(cart)[0] + " and " + Object.keys(cart)[1] + " at $" +
+      Object.values(cart)[1] + "." )
+
+  } else {
+
+     var items = [];
+
+     for (var i = 0; i < Object.keys(cart).length; i++) {
+
+     items.push(Object.keys(cart[i]) + " at $"  + Object.values(cart[i]) )
+     // lastItem.push()
+
+     }
+     console.log("In your cart, you have " + items.join(", ") + "and");
+     return items;
+   }
+
 }
+
+
+
 
 function total() {
-  // write your code here
+
+var sum = 0;
+  for (var price in cart) {
+    if ( cart.hasOwnProperty(price) ) {
+
+    sum += parseFloat( cart[price] );
+
+  }
+}
+  return sum;
 }
 
+var total = sum(cart);
+
+
+
 function removeFromCart(item) {
-  // write your code here
+
+delete cart.watermelon;
+return cart;
+
+if (cart !== item) {
+
+  console.log("this item in not in ou cart")
+    return ("this item in not in ou cart")
+}
+
+
+
 }
 
 function placeOrder(cardNumber) {
