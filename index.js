dfb2a95423a16c9c699aefce8e63b2e2dad4d60d
Riyaad Azad <azad.riyaad@gmail.com>
2018-02-18 19:54:21 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,21 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ // TODO write your code here
 }
 
 function viewCart() {
-  // write your code here
+  // TODO write your code here
 }
 
 function total() {
-  // write your code here
+  // TODO write your code here
 }
 
 function removeFromCart(item) {
-  // write your code here
+  // TODO write your code here
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  // TODO write your code here
 }
