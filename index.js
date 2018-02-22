8854bf8d433749d0545d77f61c0e54a163ff7a03
LaneMiller <chris.lane.miller@gmail.com>
2018-02-22 21:41:07 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,39 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var itemPrice = Math.floor(Math.random() * 100);
+  
+  var newItem = {[item] : itemPrice};
+  cart.push(newItem);
+  
+  console.log(`${item} has been added to your cart.`);
+  console.log(cart);
+  return cart;
 }
 
-function viewCart() {
-  // write your code here
+function viewCart(cart) {
+  var output = [];
+  var keys = Object.keys(cart);
+  
+  for (let i = 0; i < keys.length; i++) {
+    if (kays.length === 1) {
+      output.push( `${ keys[i] } at $${ cart[keys] [i] }`);
+    
+    } else if (i < keys.length) {
+      output.push( `${ keys[i] } at $${ cart[keys] [i] }`);
+      
+    } else if (i === keys.length - 1) {
+      output.push( `and ${ keys[i] } at $${ cart[keys] [i] }`);
+    }
+    
+    // "i at i, i+ at i+, and i++ at i++.
+    
+    console.log(cart);
+    if (length !== 0) {
+      return `In your cart, you have ${output}.`;
+    }
+  }
+  return `Your shopping cart is empty.`;
 }
 
 function total() {
