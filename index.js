f5b2693b211814410285730a4150d01bf95dee78
LaneMiller <chris.lane.miller@gmail.com>
2018-02-22 21:43:08 +0000

    Done.

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function viewCart(cart) {
   var keys = Object.keys(cart);
   
   for (let i = 0; i < keys.length; i++) {
-    if (kays.length === 1) {
+    if (keys.length === 1) {
       output.push( `${ keys[i] } at $${ cart[keys] [i] }`);
     
     } else if (i < keys.length) {
