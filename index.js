a78e4f781d4752d7218b0bb5afe1fb277db4f0cf
Sabine Emden <emdensabine@gmail.com>
2018-02-24 22:08:52 +0000

    Done.

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ function addToCart(item) {
   var newItem = {[item]: price};
   cart.push(newItem);
   console.log(`${item} has been added to your cart.`);
-  console.print(`${item} has been added to your cart.`);
   return cart;
 }  
 
@@ -22,7 +21,8 @@ function viewCart() {
   if (cart.length === 0) {
     console.log('Your shopping cart is empty.');
   } else if (cart.length === 1) {
-    console.log(`In your cart, you have ??? at $???.`);
+    var items = Object.getOwnPropertyNames(cart);
+    console.log(`In your cart, you have ${items[0]} at ${cart[0]}.`);
   }
 }
 
