bde75e49ffa00b605d6cad08236cece3b609eb51
LaneMiller <chris.lane.miller@gmail.com>
2018-02-24 01:01:35 +0000

    Done.

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ function setCart(c) {
 function addToCart(item) {
   var itemPrice = Math.floor(Math.random() * 100);
   
-  var newItem = { [item] : itemPrice };
-  cart.push(newItem);
+  cart.push({ [item] : itemPrice });
   
   console.log(`${item} has been added to your cart.`);
   return cart;
