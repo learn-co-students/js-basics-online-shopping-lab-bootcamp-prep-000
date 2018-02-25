eee02278b479a94c1b0bc2f9baea165264050d62
Everett <everett.bowline@gmail.com>
2018-02-25 22:48:04 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,82 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor((Math.random()*100)+1)
+ var obj = {[item]: price}
+ cart.push(obj)
+ console.log(`${item} has been added to your cart.`)
+ return cart
 }
 
 function viewCart() {
-  // write your code here
+  let l = cart.length
+  
+  if (l === 0) {
+    console.log("Your shopping cart is empty.");
+    return
+  }
+  
+    let itemList = []
+  
+      for(let i = 0; i < l; i++){
+        let items = cart[i]
+        let value = Object.keys(items)
+        let price = items[value]
+      
+          itemList.push(`${value} at $${price}`)
+          
+        }
+      if (itemList.length === 1) {
+      
+      } else if (itemList.length === 2) {
+        
+          itemList = itemList.join(" and ")
+      
+        }
+        else if (itemList.length > 2) {
+           itemList[l-1] = "and ".concat(itemList[l-1])
+           itemList = itemList.join(", ")
+        }
+        
+      console.log(`In your cart, you have ${itemList}.`)
 }
 
 function total() {
-  // write your code here
+  let l = cart.length
+  var cartTotal = 0
+  for(let i = 0; i < l; i++) {
+      let items = cart[i]
+      let value = Object.keys(items)
+      let price = items[value]
+      
+     cartTotal += price
+  }
+  return cartTotal
 }
 
 function removeFromCart(item) {
-  // write your code here
+  let newItem = false
+  var l = cart.length
+  for(let i = 0; i < l; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      newItem = true
+      cart = cart.slice(0, i).concat(cart.slice(i + 1))
+      l--
+    }
+  }
+  if (newItem !== null) {
+      console.log("That item is not in your cart.")
+    }
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (!cardNumber) {
+    console.log("Sorry, we don't have a credit card on file for you.")
+    return false
+  }
+
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    
+  cart = []
+  
 }
