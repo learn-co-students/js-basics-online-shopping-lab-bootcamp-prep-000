0fbb6eee39aa5edec5df04761e4dcd7047b095ca
Kelsey Fecho <kfecho30@gmail.com>
2018-02-25 20:07:06 +0000

    Done.

--- index.js
+++ index.js
@@ -21,16 +21,16 @@ function viewCart(){
   if(cart.length === 0){
     msg = 'Your shopping cart is empty.';
   } else if(cart.length === 1){
-    msg = 'In your cart, you have '+ Object.keys(cart[0])+' at $'+ Object.values(cart[0])+'.';
+    msg = `In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])}.`;
   } else if(cart.length === 2){
-    msg = 'In your cart, you have '+ Object.keys(cart[0])+' at $'+Object.values(cart[0])+' and '+ Object.keys(cart[1])+' at $'+Object.values(cart[1])+'.';
+    msg = `In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])} and ${Object.keys(cart[1])} at $${Object.values(cart[1])}.`;
   } else {
-    msg = 'In your cart, you have ';
+    msg = `In your cart, you have `;
     for(var item in cart){
       if(cart[item] === cart[cart.length-1]){
-        msg += Object.keys(cart[item]) +' at $' +Object.values(cart[item])+'.';
+        msg += `${Object.keys(cart[item])} at $${Object.values(cart[item])}.`;
       } else {
-      msg += Object.keys(cart[item])+' at $'+Object.values(cart[item])+', ';
+      msg += `${Object.keys(cart[item])} at $${Object.values(cart[item])}, `;
       }
     }
   }
