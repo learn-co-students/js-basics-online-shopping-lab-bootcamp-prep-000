1e25e85c3acb7930d2d440614db259a5a0a87186
Jullian Cockerell <jullian.cockerell@yahoo.com>
2018-03-01 22:16:13 +0000

    Done.

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
-}
+var object = {"item": Math.random()};
+cart.push(object);}
 
 function viewCart() {
   // write your code here
