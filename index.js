662bf5b6510ca5568612e7948ff4914574ae7cf3
fdterr <fdterr@gmail.com>
2018-03-06 04:55:21 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,75 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var itemPrice = parseInt(Math.random()*100);
+  var newObject = {[item]: itemPrice};
+  cart.push(newObject);
+  console.log(`${item} has been added to your cart.`)
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  if(!cart.length) {
+    console.log("Your shopping cart is empty.");
+  } else {
+    var placeholder = [];
+    for(var i = 0; i < cart.length; i++) {
+      var temp = cart[i];
+      placeholder[i] = `${Object.keys(cart[i])} at \$${temp[Object.keys(cart[i])]}`;
+    }
+    if(cart.length === 1) {
+      console.log(`In your cart, you have ${placeholder}.`);
+    } else if(cart.length === 2) {
+      placeholder = placeholder.join(' and ');
+      console.log(`In your cart, you have ${placeholder}.`);
+    } else {
+      placeholder[placeholder.length - 1] = `and ${placeholder[placeholder.length-1]}`;
+      placeholder = placeholder.join(', ');
+      console.log(`In your cart, you have ${placeholder}.`);
+    }
+  }
 }
 
 function total() {
-  // write your code here
+  var sumTotal = 0;
+  if(!cart.length) {
+    console.log("Your cart is empty.");
+  } else {
+    for(var i = 0; i < cart.length; i++) {
+      var temp = cart[i];
+      var itemPrice = temp[Object.keys(cart[i])];
+      sumTotal += itemPrice;
+    }
+  }
+  return sumTotal;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for(var i = 0; i < cart.length; i++) {
+    if(cart[i].hasOwnProperty(item)) {
+      cart.splice(i,1);
+      return cart;
+    }
+  }
+  console.log("That item is not in your cart.");
+  return cart;
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(!cardNumber) {
+    console.log("Sorry, we don't have a credit card on file for you.");
+  } else {
+    var totalPrice = total();
+    console.log(`Your total cost is \$${totalPrice}, which will be charged to the card ${cardNumber}.`)
+    cart.splice(0,cart.length);
+    console.log(cart);
+  }
 }
+
+addToCart("apples");
+addToCart("bannanas");
+addToCart("peaches");
+// viewCart();
+// total();
+removeFromCart("apples");
+placeOrder("4147256841262597");
