dcef44d0e595fac0bdf906f0e2704d1e265e3ffa
Murat Caliskan <muratcnjid@hotmail.com>
2018-03-10 05:36:56 +0000

    Done.

--- index.js
+++ index.js
@@ -50,14 +50,25 @@ return totalCost;
 }
 
 function removeFromCart(item) {
-  for (var i = 0; i < cart.length; i++){
-    if (Object.keys(cart[i]) === item);{
-      cart = [...cart.slice(0,i), ...cart.slice(i-1)];
+  for (var i = 0; i <cart.length; i++) {
+    if (cart[i].hasOwnProperty(item)) {
+      cart.splice(i, 1);
       return cart;
-    }
+    } 
   }
+  console.log('That item is not in your cart.');
+  return cart;
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+    if (cardNumber) {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    for (var i = cart.length; i >= 0; i--) {
+      cart.pop();
+    } 
+  } 
+  else {
+    console.log("Sorry, we don't have a credit card on file for you.");
+  }
+  return cart;
 }
