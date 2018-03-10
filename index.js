8283ebcc1f01f59fc354df69f4e267cc5a0d8d4f
Cara O'Shea <oshea.cara@gmail.com>
2018-03-10 23:15:29 +0000

    Done.

--- index.js
+++ index.js
@@ -10,12 +10,40 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * 100);
+  cart.push(new Object({[item]: price}));
+  
+  console.log(`${item} has been added to your cart.`);
+  return cart;
 }
 
+
 function viewCart() {
-  // write your code here
-}
+  if (cart.length == 0){
+    console.log("Your shopping cart is empty.");
+  }
+  var array = [];
+  for (let i=0; i < cart.length; i++){
+  
+  var cartItem = Object.keys(cart[i])[0];
+  var itemPrice = cart[i][cartItem];
+  
+  array.push(`${cartItem} at $${itemPrice}`);
+    
+  if (cart.length == 1){
+    console.log(`In your cart, you have ${array}.`);
+  } else if (cart.length == 2){
+    console.log(`In your cart, you have ${array.join(" and ")}.`)
+  } else {
+      console.log("hello")
+      var newArray = array.slice(0, -1)
+      console.log(newArray);
+      console.log(`In your cart, you have ${newArray.join(", ")}, and ${array[length-1]}.`)
+      }
+    }
+  }
+
+
 
 function total() {
   // write your code here
