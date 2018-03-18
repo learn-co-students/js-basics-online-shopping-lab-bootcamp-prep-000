431fca7a88ac8daa301c3608b51a1451e7f43bae
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-03-18 19:28:46 +0000

    Done.

--- index.js
+++ index.js
@@ -20,29 +20,49 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
-  let message = 'In your cart, you have ';
-  if(cart.length === 0) {
-    console.log('Your shopping cart is empty.');
-  } else if (cart.length === 1) {
-    message += `${cart[0].itemName}`
-  }
-  
-  // for(let i=0; i < cart.length; i++){
-  //   if(i != (cart.length - 1)) {
-  //     message += `${cart[i].itemName} at $ ${cart[i].itemPrice},`;
+  // if(getCart().length === 0) {
+  //   let message = 'Your shopping cart is empty.';
+  //   console.log('Your shopping cart is empty.');
+  // } 
+  // else {
+  //   if(getCart().length === 1) {
+  //     console.log(`In your cart, you have ${Object.keys(getCart()[0])[0]} at $${getCart()[0][Object.keys(getCart()[0])[0]]}.`);
+  //   } else if(getCart().length === 2) {
+  //     console.log(`In your cart, you have ${Object.keys(getCart()[0])[0]} at $${getCart()[0][Object.keys(getCart()[0])[0]]}, and ${Object.keys(getCart()[1])[0]} at $${getCart()[1][Object.keys(getCart()[1])[0]]}.`);
   //   } else {
-  //     message += ` and ${cart[i].itemName} at $ ${cart[i].itemPrice}.`;
+  //     let message = `In your cart, you have `;
+  //       for(let i=0; i < getCart().length; i++) {
+  //         if(i != (getCart().length - 1)) {
+  //         message += `${Object.keys(getCart()[i])[0]} at $${getCart()[i][Object.keys(getCart()[i])[0]]},`;
+  //       } else {
+  //         message += ` and ${Object.keys(getCart()[i])[0]} at $${getCart()[i][Object.keys(getCart()[i])[0]]}.`;
+  //       }
+  //     }
+  //     console.log(message);
   //   }
   // }
-  
-  console.log(message);
+  if(getCart().length === 0) {
+    console.log('Your shopping cart is empty.');
+  } else if(getCart().length === 1) {
+    console.log(`In your cart, you have ${Object.keys(getCart()[0])[0]} at $${getCart()[0][Object.keys(getCart()[0])[0]]}.`);
+  } else {
+    let message = `In your cart, you have `;
+    for(let i = 0; i < getCart().length; i++) {
+          if(i != (getCart().length - 1)) {
+          message += `${Object.keys(getCart()[i])[0]} at $${getCart()[i][Object.keys(getCart()[i])[0]]},`;
+        } else {
+          message += ` and ${Object.keys(getCart()[i])[0]} at $${getCart()[i][Object.keys(getCart()[i])[0]]}.`;
+        }
+      }
+      console.log(message);
+  }
 }
 
 function total() {
   // write your code here
   let somme = 0;
   for(let i = 0; i < cart.length; i++) {
-    somme += cart[i].itemPrice;
+    somme += cart[i][Object.keys(cart[i])[0]];
   }
   return somme;
 }
@@ -53,4 +73,11 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if(!isNaN(parseInt(cardNumber, 10))) {
+    cart.length = 0;
+    let number = total();
+    return `Your total cost is $${number}, which will be charged to the card ${cardNumber}.`
+  } else {
+    return `Sorry, we don't have a credit card on file for you.`;
+  }
 }
