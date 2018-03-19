2de1bbe0627e2937eda22499aa50eca31dab3ae9
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-03-19 11:45:24 +0000

    Done.

--- index.js
+++ index.js
@@ -20,18 +20,33 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
+  if(getCart().length === 0) {
+    let message = 'Your shopping cart is empty.';
+    console.log('Your shopping cart is empty.');
+  } 
+  else if(getCart().length === 1) {
+      console.log(`In your cart, you have ${Object.keys(getCart()[0])[0]} at $${getCart()[0][Object.keys(getCart()[0])[0]]}.`);
+  } else if(getCart().length === 2) {
+      return `In your cart, you have ${Object.keys(getCart()[0])[0]} at $${getCart()[0][Object.keys(getCart()[0])[0]]}, and ${Object.keys(getCart()[1])[0]} at $${getCart()[1][Object.keys(getCart()[1])[0]]}.`;
+  } else {
+      let message = `In your cart, you have `;
+        for(let i=0; i < getCart().length; i++) {
+          if(i != (getCart().length - 1)) {
+          message += `${Object.keys(getCart()[i])[0]} at $${getCart()[i][Object.keys(getCart()[i])[0]]},`;
+        } else {
+          message += ` and ${Object.keys(getCart()[i])[0]} at $${getCart()[i][Object.keys(getCart()[i])[0]]}.`;
+        }
+      }
+      return message;
+}
+  
   // if(getCart().length === 0) {
-  //   let message = 'Your shopping cart is empty.';
   //   console.log('Your shopping cart is empty.');
-  // } 
-  // else {
-  //   if(getCart().length === 1) {
-  //     console.log(`In your cart, you have ${Object.keys(getCart()[0])[0]} at $${getCart()[0][Object.keys(getCart()[0])[0]]}.`);
-  //   } else if(getCart().length === 2) {
-  //     console.log(`In your cart, you have ${Object.keys(getCart()[0])[0]} at $${getCart()[0][Object.keys(getCart()[0])[0]]}, and ${Object.keys(getCart()[1])[0]} at $${getCart()[1][Object.keys(getCart()[1])[0]]}.`);
-  //   } else {
-  //     let message = `In your cart, you have `;
-  //       for(let i=0; i < getCart().length; i++) {
+  // } else if(getCart().length === 1) {
+  //   console.log(`In your cart, you have ${Object.keys(getCart()[0])[0]} at $${getCart()[0][Object.keys(getCart()[0])[0]]}.`);
+  // } else {
+  //   let message = `In your cart, you have `;
+  //   for(let i = 0; i < getCart().length; i++) {
   //         if(i != (getCart().length - 1)) {
   //         message += `${Object.keys(getCart()[i])[0]} at $${getCart()[i][Object.keys(getCart()[i])[0]]},`;
   //       } else {
@@ -39,23 +54,7 @@ function viewCart() {
   //       }
   //     }
   //     console.log(message);
-  //   }
   // }
-  if(getCart().length === 0) {
-    console.log('Your shopping cart is empty.');
-  } else if(getCart().length === 1) {
-    console.log(`In your cart, you have ${Object.keys(getCart()[0])[0]} at $${getCart()[0][Object.keys(getCart()[0])[0]]}.`);
-  } else {
-    let message = `In your cart, you have `;
-    for(let i = 0; i < getCart().length; i++) {
-          if(i != (getCart().length - 1)) {
-          message += `${Object.keys(getCart()[i])[0]} at $${getCart()[i][Object.keys(getCart()[i])[0]]},`;
-        } else {
-          message += ` and ${Object.keys(getCart()[i])[0]} at $${getCart()[i][Object.keys(getCart()[i])[0]]}.`;
-        }
-      }
-      console.log(message);
-  }
 }
 
 function total() {
@@ -68,16 +67,20 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for(let i = 0; i < getCart().length; i++) {
+    if(getCart()[i].hasOwnProperty(item)) {
+      getCart().splice(1,1);
+      return getCart();
+    }
+  }
+  console.log('That item is not in your cart.');
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
-  if(!isNaN(parseInt(cardNumber, 10))) {
-    cart.length = 0;
-    let number = total();
-    return `Your total cost is $${number}, which will be charged to the card ${cardNumber}.`
+  if(cardNumber !== undefined) {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    getCart().length = 0;
   } else {
-    return `Sorry, we don't have a credit card on file for you.`;
+    console.log(`Sorry, we don't have a credit card on file for you.`);
   }
 }
