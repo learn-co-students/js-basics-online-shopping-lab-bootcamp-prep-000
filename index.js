7e66d1c1ee25634717cfe89e989c11b77e0c82d5
Houston Green <greenhouston@gmail.com>
2018-03-26 00:50:49 +0000

    Done.

--- index.js
+++ index.js
@@ -9,8 +9,24 @@ function setCart(c) {
   return cart;
 }
 
+// function addToCart(item) {
+//   cart.push(item);
+//   for (let i = 0; i < cart.length; i++) {
+//     cart[i] = {
+//       itemName: cart[i],
+//       itemPrice: Math.floor(Math.random() * (100 - 1) ) + 1,
+//     }
+//   }
+//   return `${item} has been added to your cart.`
+// }
+
 function addToCart(item) {
- // write your code here
+  cart.push(item);
+  cart = cart.map(list => ({
+    itemName: list,
+    itemPrice: Math.floor(Math.random() * (100 - 1) ) + 1,
+  }));
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
