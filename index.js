f54a01e98e4c38b8955622edf86d604af832d24a
Brittany Tinnin <bntinnin7@gmail.com>
2018-03-27 23:42:40 +0000

    Done.

--- index.js
+++ index.js
@@ -18,18 +18,18 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  let cartItems = [];
+  let cartItems = []; // []
   if (cart.length === 0) {
     return "Your shopping cart is empty.";
   } else if (cart.length === 1) {
-    return `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`;
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
   } else if (cart.length === 2) {
-    return  `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice} and ${getCart()[1].itemName} at $${getCart()[1].itemPrice}.`;
+    return  `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice} and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
   } else {
     for (var i = 0; i < cart.length; i++) {
-      cartItems.push(` ${cart[i].itemName}.`);
+      cartItems.push(`${cart[i].itemName} at $${cart[i].itemPrice}`);
     }
-    return `In your cart, you have${cartItems}, ${cartItems} and ${cartItems}.`;
+    return `In your cart, you have ` + cartItems.join(', ') + `.`;
   }
 }
 
