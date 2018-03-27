e98c0d44e4e18abce105ffeea61429b705de3ec7
Christopher <christopherccm@msn.com>
2018-03-27 16:33:17 +0000

    Done.

--- index.js
+++ index.js
@@ -11,15 +11,27 @@ function setCart(c) {
 
 function addToCart(itemName) {
     cart[itemName] = Math.floor(Math.random() * (Math.floor(100) - Math.ceil(1)) + Math.ceil(1));
+    debugger;
     return `${itemName} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+//  if (!cart.length) {
+//    return "Your shopping cart is empty."
+//  }
+  const itemNamesAndPrices = []
+  for (var i = 0; i < cart.length; i++) {
+    //itemNamesAndPrices.push(`In your cart, you have ${itemName.join(', ')}`)
+    itemNamesAndPrices.push(`${cart[i]}`);
+    console.log(cart[i]);
+  }
+  return itemNamesAndPrices;
 }
 
 function total() {
-  // write your code here
+  return cart.reduce( function(a, b){
+        return a + b[prop];
+    }, 0);
 }
 
 function removeFromCart(item) {
