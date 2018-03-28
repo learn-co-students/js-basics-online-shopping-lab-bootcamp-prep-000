b64c77abc6de3dc7ad69847e6e2ef62b573ea1d1
Christopher <christopherccm@msn.com>
2018-03-28 23:36:27 +0000

    Done.

--- index.js
+++ index.js
@@ -9,23 +9,42 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(itemName) {
-    cart[itemName] = Math.floor(Math.random() * (Math.floor(100) - Math.ceil(1)) + Math.ceil(1));
-    debugger;
-    return `${itemName} has been added to your cart.`
+function itemConstructor(name) {
+  return {
+    itemName: name,
+    itemPrice: Math.floor(Math.random() * (Math.floor(100) - Math.ceil(1)) + Math.ceil(1)),
+  }
+}
+
+function addToCart(nameOfItem) {
+  // var newItem = itemConstructor(nameOfItem); Alternate solution, using variable
+  // console.log(newItem);                      Alternate solution, using variable
+  cart.push(itemConstructor(nameOfItem));
+  //  return `${newItem.itemName} has been added to your cart.` Alternate solution, using variable
+  return `${nameOfItem} has been added to your cart.`
 }
 
 function viewCart() {
-//  if (!cart.length) {
-//    return "Your shopping cart is empty."
-//  }
-  const itemNamesAndPrices = []
-  for (var i = 0; i < cart.length; i++) {
-    //itemNamesAndPrices.push(`In your cart, you have ${itemName.join(', ')}`)
-    itemNamesAndPrices.push(`${cart[i]}`);
-    console.log(cart[i]);
+  if (!cart.length) {
+    return "Your shopping cart is empty."
   }
-  return itemNamesAndPrices;
+
+  var statement1 = "In your cart, you have"
+  var statement2 = "."
+
+  if (cart.length === 1) {
+    console.log(`In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`);
+  } else if (cart.length === 2) {
+    console.log(`In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}]`);
+  } else {
+    var listOfItems = "";
+      for (var i = 0; i < cart.length -1; i++) {
+        listOfItems += `${cart[i].itemName}, at $${cart[i].itemPrice}, `
+      }
+        listOfItems += `and ${cart[cart.length -1].itemName} at $${cart[cart.length -1].itemPrice}`
+
+    return `In your cart, you have ${listOfItems}.`
+  }
 }
 
 function total() {
