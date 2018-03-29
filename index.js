4ea954a8b7d517dea6b50caf65c888d24948e72c
Christopher <christopherccm@msn.com>
2018-03-29 20:59:34 +0000

    Done.

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ function itemConstructor(name) {
 function addToCart(nameOfItem) {
   // var newItem = itemConstructor(nameOfItem); Alternate solution, using variable
   // console.log(newItem);                      Alternate solution, using variable
-  cart.push(itemConstructor(nameOfItem));
+  //cart.push(itemConstructor(nameOfItem));
+  cart.push({itemName: nameOfItem, itemPrice: Math.floor(Math.random() * (Math.floor(100) - Math.ceil(1)) + Math.ceil(1))});
   //  return `${newItem.itemName} has been added to your cart.` Alternate solution, using variable
   return `${nameOfItem} has been added to your cart.`
 }
@@ -29,17 +30,17 @@ function viewCart() {
     return "Your shopping cart is empty."
   }
 
-  var statement1 = "In your cart, you have"
-  var statement2 = "."
+  //var statement1 = "In your cart, you have"
+  //var statement2 = "."
 
   if (cart.length === 1) {
-    console.log(`In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`);
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
   } else if (cart.length === 2) {
-    console.log(`In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}]`);
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
   } else {
     var listOfItems = "";
       for (var i = 0; i < cart.length -1; i++) {
-        listOfItems += `${cart[i].itemName}, at $${cart[i].itemPrice}, `
+        listOfItems += `${cart[i].itemName} at $${cart[i].itemPrice}, `
       }
         listOfItems += `and ${cart[cart.length -1].itemName} at $${cart[cart.length -1].itemPrice}`
 
@@ -47,9 +48,17 @@ function viewCart() {
   }
 }
 
+function test() {
+  var bagOfPrices = 0;
+
+  for (var i = 0; i < cart.length; i++) {
+    bagOfPrices += `${cart[i].itemPrice}`;
+  }
+}
+
 function total() {
-  return cart.reduce( function(a, b){
-        return a + b[prop];
+  return cart.reduce( function(accumulator, item) {
+        return accumulator + item.itemPrice;
     }, 0);
 }
 
