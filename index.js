634e81292e0482d6ec754dd6f7cc87389f9974c0
Christopher <christopherccm@msn.com>
2018-03-30 18:17:11 +0000

    Done.

--- index.js
+++ index.js
@@ -79,6 +79,7 @@ function placeOrder(cardNumber) {
   if (cardNumber == null) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
+    //var total = total();
     //return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`;
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
     //cart.length = 0;
