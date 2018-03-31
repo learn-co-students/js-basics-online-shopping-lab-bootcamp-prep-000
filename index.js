98d9fa879793a18ec31300c86a25b87884c52f65
Brittany Tinnin <bntinnin7@gmail.com>
2018-03-31 03:47:43 +0000

    Done.

--- index.js
+++ index.js
@@ -24,16 +24,20 @@ addToCart('book')
 addToCart('radio')
 
 function viewCart() {
+cartItems = [];
 var string = "In your cart, you have ";
   if (cart.length === 0) {
     return "Your shopping cart is empty.";
-  }
-  
-  if (cart.length === 1) {
-    return string + `${cart[0].itemName} at $${cart[0].itemPrice}.`;
+  } else if (cart.length === 1) {
+     return string + `${cart[0].itemName} at $${cart[0].itemPrice}.`;
   } else if (cart.length === 2) {
     return string + `${cart[0].itemName} at $${cart[0].itemPrice} and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
+  } else {
+    for (var i = 2; i < cart.length; i++) {
+      cartItems.push(`${cart[i].itemName} at $${cart[i].itemPrice}`);
   }
+    return string + cartItems.join(", ") + `and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`;
+  } 
 }
 
 function total() {
