1d56dfb4992a46bd6fdd50cedf9f23cbe337de20
Brittany Tinnin <bntinnin7@gmail.com>
2018-03-31 02:05:59 +0000

    Done.

--- index.js
+++ index.js
@@ -17,24 +17,22 @@ function addToCart(item) {
  
 }
 
+addToCart('shoes')
+addToCart('table')
+addToCart('walker')
+addToCart('book')
+addToCart('radio')
+
 function viewCart() {
-var string = "In your cart, you have";
+var string = "In your cart, you have ";
   if (cart.length === 0) {
     return "Your shopping cart is empty.";
-  } else if (cart.length === 1) {
-    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
+  }
+  
+  if (cart.length === 1) {
+    return string + `${cart[0].itemName} at $${cart[0].itemPrice}.`;
   } else if (cart.length === 2) {
-    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice} and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
-  } else {
-    for (var i = 0; i < cart.length; i++) {
-      var text;
-      if (i === cart.length - 2) {
-        text = ", and ";
-      } else {
-        text = ", ";
-      }
-      return string + `${cart[i].itemName} at $${cart[i].itemPrice}` + text + `${i === cart.length - 1} .`
-  } 
+    return string + `${cart[0].itemName} at $${cart[0].itemPrice} and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
   }
 }
 
