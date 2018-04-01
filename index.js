832cdf495cb1d227d7360015263ffad0314bffe6
wangfanqoo@126.com Wang <wangfanqoo@126.com>
2018-04-01 16:23:29 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,28 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  for(let i =0 ; i < cart.length; i++){
+    if (cart[i].itemName === item ) {
+      return (`${item} has been added to your cart`);
+    }
+  }
+  var a = {}
+  a.itemName = item 
+  a.itemPrice = Math.floor(Math.random()*100 +1)
+  cart.push(a)
+  return cart
 }
 
 function viewCart() {
-  // write your code here
+ if (!cart.length){
+   return "Your shopping cart is empty."
+ } 
+   for(let i = 0; i<cart.length;i++) {
+     var cart_itemName = []
+     var cart_itemPrice = []
+     cart_item.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)
+   }
+   return `In your cart, you have ${cart_item.join(', and')}.`
 }
 
 function total() {
