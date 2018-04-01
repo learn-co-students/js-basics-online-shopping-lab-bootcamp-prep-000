29b38bdb9e781a94a681c36a7d62514465e09a59
Brittany Tinnin <bntinnin7@gmail.com>
2018-04-01 13:29:29 +0000

    Done.

--- index.js
+++ index.js
@@ -48,7 +48,11 @@ var string = "In your cart, you have ";
 }
 
 function total() {
-  // write your code here
+  var totalCost = 0;
+  for (var i = 0; i < cart.length; i++) {
+  totalCost = totalCost + cart[i].itemPrice;
+ }
+return totalCost;
 }
 
 function removeFromCart(item) {
