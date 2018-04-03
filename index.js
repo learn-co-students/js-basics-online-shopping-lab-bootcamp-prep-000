332e6dc3c0604d20c3e414190474854644a1bfca
Brittany Tinnin <bntinnin7@gmail.com>
2018-04-03 16:23:55 +0000

    Done.

--- index.js
+++ index.js
@@ -66,5 +66,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (arguments[0] === undefined) {
+    return "Sorry, we don't have a credit card on file for you.";
+  } else {
+    var sumToCharge = total();
+    setCart([]);
+    return `Your total cost is $${sumToCharge}, which will be charged to the card ${cardNumber}.`;
+  }
 }
