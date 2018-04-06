db5e12fd142d708ac74bbab068b389a88656a58f
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-04-06 03:39:45 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,17 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  cart.push({ itemName: "newItem", itemPrice: newItemPrice })
+  var itemName = ( [] )
+  var itemPrice = Math.floor((Math.random() * 100) + 1)
+  return `${itemName} has been been added to your cart`
 }
 
 function viewCart() {
-  // write your code here
+  var newCart = []
+  if (cart.length === 0) {
+    return `Your shopping cart is empty.`
+  }
 }
 
 function total() {
