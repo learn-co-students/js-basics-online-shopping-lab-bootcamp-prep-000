bbc87c49d7da49955204bf0e6543b5d56fd6c9bd
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-04-09 02:31:45 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,82 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var newcart = { itemName: `${item}`, itemPrice: Math.floor(Math.random() * Math.floor(100))} 
+ cart.push(newcart)
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+ var cartstr = `In your cart, you have `
+ var i = 0
+ if (cart.length === 0){
+    return "Your shopping cart is empty."
+  }
+  else if (cart.length == 1){
+    var cartobj1 = cart[0]
+     var cartname1 = cartobj1.itemName
+     var cartprice1 = cartobj1.itemPrice
+    return `In your cart, you have ${cartname1} at $${cartprice1}.`
+  }
+  else if (cart.length == 2){
+    var cartobj2 = cart[1]
+    var cartname2 = cartobj2.itemName
+    var cartprice2 = cartobj2.itemPrice
+    var cartobj11 = cart[0]
+    var cartname11 = cartobj11.itemName
+    var cartprice11 = cartobj11.itemPrice
+    return `In your cart, you have ${cartname11} at $${cartprice11}, and ${cartname2} at $${cartprice2}.`
+  }
+    else{
+      
+      while(i<cart.length - 1){
+        
+        var cartobjx = cart[i]
+        var cartnamex = cartobjx.itemName
+        var cartpricex = cartobjx.itemPrice
+        i++
+        cartstr += `${cartnamex} at $${cartpricex}, `
+      }
+      var finalobj = cart[cart.length - 1]
+      var finalname = finalobj.itemName
+      var finalprice = finalobj.itemPrice
+    cartstr += `and ${finalname} at $${finalprice}.`
+      return cartstr 
+    }
 }
 
+
 function total() {
-  // write your code here
+ var sum = 0
+ for  (var i = 0;i<cart.length;i++){
+   var object = cart[i]
+   var objectprice = object.itemPrice
+   sum += objectprice
+    }
+  return sum
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (var i = 0;i<cart.length;i++){
+    var object = cart[i]
+    var objectname = object.itemName
+    if (item === objectname){
+    cart.splice(i,1)
+      
+      }
+      
+        
+    
+    }
+    return `That item is not in your cart.`
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  var new_total = total()
+  if (cardNumber === undefined){
+    return `Sorry, we don't have a credit card on file for you.`
+  }
+  cart.length = 0
+    return `Your total cost is $${new_total}, which will be charged to the card ${cardNumber}.`
+  
 }
