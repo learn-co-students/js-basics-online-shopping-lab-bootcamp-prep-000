42dc8720f6b0592a6fb6d5f574b207e92891844c
Rob LeCheminant <lecheminant@gmail.com>
2018-04-11 20:10:31 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var price = Math.floor(Math.random() * 100);
+ cart.push(item);
+ return (`${item} has been added to your cart.`);
 }
 
 function viewCart() {
