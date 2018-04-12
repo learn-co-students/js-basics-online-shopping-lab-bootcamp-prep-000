cc22d45bfa24215e18de4346ff3c5af3120f55c0
Enecas <hypercolorm@gmail.com>
2018-04-12 04:59:03 +0000

    Done.

--- index.js
+++ index.js
@@ -12,13 +12,14 @@ function setCart(c) {
 function addToCart(item) {
   //potentially convert this to a function
   var price = Math.floor(Math.random() * 100) + 1;
-  cart.push({itemName: `${item}`, itemPrice: parseInt(`${price}`)});
+  cart.push({itemName: item, itemPrice: price});
   return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  //loop over all items in cart
-  
+  for (let i = 0; i < cart.length; i++) {
+    
+  }
 }
 //return all contents as a single coherent statement
 //"In your cart, you have itemName at itemPrice, itemName2 at itemPrice2, and itemName3 at itemPrice3."
@@ -27,22 +28,27 @@ function viewCart() {
 function total() {
   var totalValue = 0;
   for (let i = 0; i < cart.length; i++) {
-    totalValue = totalValue + cart[i].itemPrice;
+  totalValue = totalValue + cart[i].itemPrice;
   }
   return totalValue;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].itemName === item) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+  return `That item is not in your cart.`;
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined) {
+    return `Sorry, we don't have a credit card on file for you.`;
+  } else {
+    var subTotal = total();
+    cart = [];
+    return `Your total cost is $${subTotal}, which will be charged to the card ${cardNumber}.`;
+  }
 }
-
-
-// cart = [ 
-//   { itemName:"bananas", itemPrice: 17 },
-//   { itemName:"pancake batter",itemPrice: 5 },
-//   { itemName:"eggs", itemPrice: 49 }
-// ]
