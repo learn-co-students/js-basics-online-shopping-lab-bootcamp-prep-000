79d0311c82678a69bf99e2659e9714bc51d9fb70
Catherine Lee <lee.catherine77@gmail.com>
2018-04-14 22:57:42 +0000

    Done.

--- index.js
+++ index.js
@@ -11,10 +11,17 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ cart.push(Object.assign({}, {itemName:item, itemPrice:Math.random()* (100 - 1) + 1}))
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
+  if(cart.length === 0) {
+    return 'Your shopping cart is empty.'
+  } else if(cart.length === 1) {
+    return `In your cart, you have ${cart[0]} at `
+  }
 }
 
 function total() {
