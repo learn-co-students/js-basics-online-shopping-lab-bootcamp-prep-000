36c55e4ae8d246c6a5a0e2ae7125c6ba3a2de641
Ifeoluwa Abimbola <david.abimbola@aol.com>
2018-04-15 21:25:55 +0000

    Done.

--- index.js
+++ index.js
@@ -11,10 +11,24 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ cart.push({[item]:
+ Math.floor(Math.random()*100)});
+  console.log('${item} has been added to your cart.');
+  return cart;
 }
 
 function viewCart() {
   // write your code here
+if (cart.length === 0) {
+  return console.log("Your shopping cart is empty.");
+}
+ var string = "In your cart, you have";
+
+if (cart.length === 1){
+  string += `$Object.keys(cart[0])} at $$ {cart[0][Object,keys(cart[0])]}.`
+} else if (cart.length === 2) {
+  string += `${Object.keys(cart[0])} at $$
+}
 }
 
 function total() {
