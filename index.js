4464e74e365c1f63e34aa0417e133a6e1b7c9c4d
Catherine Lee <lee.catherine77@gmail.com>
2018-04-16 13:49:07 +0000

    Done.

--- index.js
+++ index.js
@@ -20,7 +20,14 @@ function viewCart() {
   if(cart.length === 0) {
     return 'Your shopping cart is empty.'
   } else if(cart.length === 1) {
-    return `In your cart, you have ${cart[0]} at `
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  } else if(cart.length === 2) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
+  } else {
+    var longCart = []
+    for(var i = 0; i < cart.length; i++) {
+      
+    }
   }
 }
 
