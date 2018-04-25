2b96f37809e1dce6d5957d2775b5b5255b50c881
Sharmaine Requerme <srqrme@gmail.com>
2018-04-25 17:12:20 +0000

    Done.

--- index.js
+++ index.js
@@ -40,7 +40,11 @@ function viewCart() {
 
 function total() {
   // write your code here
-
+  var totalPrice = 0
+  for (var i = 0; i < cart.length; i++) {
+    totalPrice += cart[i].itemPrice
+  }
+  return totalPrice
 }
 
 function removeFromCart(item) {
