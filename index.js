4631900b46055f92c0646dea38d0b0a05ba96a38
Sharmaine Requerme <srqrme@gmail.com>
2018-04-26 02:06:25 +0000

    Done.

--- index.js
+++ index.js
@@ -56,3 +56,16 @@ function removeFromCart(item) {
   }
   return `That item is not in your cart.`
 }
+
+function placeOrder(cardNumber) {
+  // write your code here
+  if (!cardNumber) {
+    return `Sorry, we don't have a credit card on file for you.`
+  }
+
+  else if (cardNumber) {
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    cart.pop()
+  }
+  placeOrder()
+}
