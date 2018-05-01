7e4fbe8a2b4458d9476b715371cc5d6f0e20b459
NGAN PHAM <nganvpham@gmail.com>
2018-05-01 00:02:55 +0000

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function setCart(c) {
   return cart;
 }
 
+
 function addToCart(item) {
   var price = Math.floor(Math.random()*100);
   var newItem = {
@@ -19,8 +20,26 @@ function addToCart(item) {
   return `${newItem.itemName} has been added to your cart.`;
 }
 
+
 function viewCart() {
-  // write your code here
+  var array = [];
+
+  for ( let i = 0; i < cart.length; i++) {
+      array.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)
+  }
+  if (cart.length === 0) {
+    return `Your shopping cart is empty.`;}
+
+  if (cart.length === 1){
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
+  }
+
+  if (cart.length ===2){
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
+  }else{
+    let lastItem = array.pop();
+    return `In your cart, you have ${array.join(', ')}, and ${lastItem}.`
+  }
 }
 
 function total() {
