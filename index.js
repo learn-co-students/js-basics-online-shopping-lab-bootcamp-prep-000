6f89e65d3b046b0e8c3062ce46cc875a92162b2a
NGAN PHAM <nganvpham@gmail.com>
2018-05-02 23:56:08 +0000

    Done.

--- index.js
+++ index.js
@@ -43,11 +43,21 @@ function viewCart() {
 }
 
 function total() {
-  // write your code here
+  let totalCost = 0;
+  for (let i = 0; i < cart.length; i++){
+    totalCost += cart[i].itemPrice;
+  }
+  return totalCost;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (let i = 0; i < cart.length; i++){
+    if (cart[i].itemName === item){
+      cart.splice(i,1);
+      return cart;
+    }
+  }
+  return "That item is not in your cart.";
 }
 
 function placeOrder(cardNumber) {
