62610cc84fac5ec7167a2de25dfe79936d91a915
NGAN PHAM <nganvpham@gmail.com>
2018-05-03 01:09:44 +0000

    Done.

--- index.js
+++ index.js
@@ -61,5 +61,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber){
+    let totalCost = total();
+    cart =[];
+    return`Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`;
+  } else {
+    return "Sorry, we don't have a credit card on file for you.";
+  }
 }
