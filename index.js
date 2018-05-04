a7263ef0e557f97fca3a22c20fad96673adbea9c
jmoypace <jmoypace@gmail.com>
2018-05-04 14:36:32 +0000

    Done.

--- index.js
+++ index.js
@@ -73,8 +73,8 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  var holder;
   var totalnum=total()
+  var cctrue;
    var ccnum=Math.floor(Math.random() * 100000000);
    ccnum=ccnum.toString()
   if(cardNumber===undefined){
@@ -84,12 +84,13 @@ function placeOrder(cardNumber) {
   console.log("Sorry, we don't have a credit card on file for you.")
 }
 else if(cardNumber.length===ccnum.length){
-  holder=`Your total cost is $${totalnum}, which will be charged to the card ${cardNumber}.`
+  cctrue=true;
 }
  for(var k=0;k<cart.length;k++){
  cart.pop()
  }
- console.log(holder) ;
+if(cctrue===true){ 
+  return `Your total cost is$${totalnum}, which will be charged to the card "+cardNumber.`}
 }
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
