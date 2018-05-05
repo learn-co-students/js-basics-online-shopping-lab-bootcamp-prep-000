ecd47d544d88d556caa21a58c39b8613847d0aca
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-05 15:46:55 +0000

    Done.

--- index.js
+++ index.js
@@ -65,13 +65,23 @@ function removeFromCart(item) {
   {
     if(item === cart[i].itemName)
     {
-      cart.splice((i - 1), 1);
+      cart.splice(i, 1);
       return cart;
     }
-    
   }
+  return "That item is not in your cart.";
 }
 
 function placeOrder(cardNumber) {
   // write your code here
+  if(cardNumber === undefined)
+  {
+    return "Sorry, we don't have a credit card on file for you.";
+  }
+  else
+  {
+    var finalPrice = total();
+    cart = [];
+    return `Your total cost is $${finalPrice}, which will be charged to the card ${cardNumber}.`;
+  }
 }
