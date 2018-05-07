b1a6415475598f9b8f7e111f4cd41d165a2d92c2
Shaun Rezac <shaun.rezac@gmail.com>
2018-05-07 15:08:01 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  // write your code here
+  var price = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+  var itemObj = {itemName: [item], itemPrice: price };
+  cart.push(itemObj);
+  return [item] + ' has been added to your cart.';
 }
 
 function viewCart() {
