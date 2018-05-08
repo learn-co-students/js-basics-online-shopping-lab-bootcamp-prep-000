55985ae00f0f5ac12956205379a109ae348a1586
Rob LeCheminant <lecheminant@gmail.com>
2018-05-08 16:19:28 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item, itemName, itemPrice) {
- var price = Math.floor(Math.random() * (100-1) + 1);
+ var price = Math.floor(Math.random() * ((100 - 1) +1) +1);
  var newItem = {itemName: `${item}`, itemPrice: `${price}`};
  cart.push(newItem);
  
