486651487ec0c042cc620cdaef69442b67d34f74
Houston Green <greenhouston@gmail.com>
2018-05-10 02:52:42 +0000

    Done.

--- index.js
+++ index.js
@@ -9,17 +9,6 @@ function setCart(c) {
   return cart;
 }
 
-// function addToCart(item) {
-//   cart.push(item);
-//   for (let i = 0; i < cart.length; i++) {
-//     cart[i] = {
-//       itemName: cart[i],
-//       itemPrice: Math.floor(Math.random() * (100 - 1)) + 1,
-//     }
-//   }
-//   return `${item} has been added to your cart.`
-// }
-
 function addToCart(item) {
   cart.push(item);
   cart = cart.map(list => ({
@@ -27,30 +16,35 @@ function addToCart(item) {
     itemPrice: Math.floor(Math.random() * (100 - 1)) + 1,
   }));
   return `${item} has been added to your cart.`
-}
+};
 
 function viewCart() {
-  let message = "In your cart, you have";
-  let cartLength = cart.length;
+  let message = "In your cart, you have ";
+  const cartLength = cart.length;
+
   if (cartLength === 0) {
     return "Your shopping cart is empty."
-  } else if (cartLength === 1) {
-    cart.map(list => (
-      message = `${message} ${list.itemName} at $${list.itemPrice}.`
-    ));
-  } else if (cartLength === 2) {
-    let cartArr = cart.map(list => list);
-    let i = 0;
-    let cartItem = cartArr.splice(i, 1);
-    return message = `${message} ${cartItem.itemName} at $${cartItem.itemPrice},
-    and ${cartItem.itemName} at $${cartItem.itemPrice}.`
-  }
+  };
+
+  cart.map((obj, index) => {
+    const item = obj.itemName;
+    const price = obj.itemPrice;
+    const itemPricePair = `${item} at $${price}`;
+
+    if (cartLength === 1) {
+      message += `${itemPricePair}.`
+    } else {
+      message += index === cartLength-1 ? `and ${itemPricePair}.` : `${itemPricePair}, `
+    };
+  });
   return message;
-}
+};
 
 function total() {
-  // write your code here
-}
+  let prices = cart.map(obj => obj.itemPrice);
+  const reducer = (sum, value) => sum + value;
+  return prices.reduce(reducer);
+};
 
 function removeFromCart(item) {
   // write your code here
