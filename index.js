4639e7713688d20f4ca50a45c40a3af6a7c3a200
caida.ding@baruchmail.cuny.edu <caida.ding@baruchmail.cuny.edu>
2018-05-13 14:36:28 +0000

    Done.

--- index.js
+++ index.js
@@ -9,12 +9,28 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(name) {
+ var obj = {};
+ obj[itemName] = name;
+ obj[itemPrice] = Math.floor(Math.random() * 101);
+ cart.push(obj);
+ obj = {};
+ return `<${name}> has been added to your cart. `;
 }
 
 function viewCart() {
-  // write your code here
+  let str = '';
+  if(cart.length === 0) return 'Your shopping cart is empty.';
+  else if(cart.length === 1) return `In your cart, you have ${cart[0][itemName]} at $${cart[0][itemPrice]}.`;
+  else if(cart.length >1){
+    str += `In your cart, you have ${cart[0][itemName]} at $${cart[0][itemPrice]},`
+  for(i=1;i<cart.length;i++){
+    str += ` ${cart[i][itemName]} at $${cart[i][itemPrice]},`
+  }
+  str.split(' ').splice(-4, 0, 'and');
+  str.splice(-1, 1, '.')
+  }
+  return str;
 }
 
 function total() {
