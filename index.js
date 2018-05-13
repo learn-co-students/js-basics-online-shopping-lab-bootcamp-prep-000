3d31e1379445f1e803bf2902a3f3b0e37a1a3a34
Samantha brake <sammy.brake@gmail.com>
2018-05-13 16:28:01 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,26 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var itemName = item ; 
+  var itemPrice = Math.floor(Math.random() *100) ; 
+ var object = {[itemName]: itemPrice} ; 
+  cart.push(object) ; 
+  console.log(`${itemName} has been added to your cart.`);
+  return cart;
+ 
 }
 
+
+
 function viewCart() {
-  // write your code here
+ 
+ var i = cart.length;
+  for (i === cart.length; i < cart.length; i ++);
+     if ( i === 0 ) {
+     return console.log("Your shopping cart is empty.") ; 
+   } else if (i === 1) {
+     return console.log(`In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}`);
+   }
 }
 
 function total() {
