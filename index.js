8158463d86d8721fc0fe3e10acfa553ffcf43c32
ericlimjoco@gmail.com Limjoco <ericlimjoco@gmail.com>
2018-05-16 11:56:24 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,66 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ cart.push({
+   itemName: item,
+   itemPrice: Math.floor(Math.random()*100)
+ })
+ 
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  if(cart.length === 0) return 'Your shopping cart is empty.'
+  var res = 'In your cart, you have '
+  
+  for(var i=0; i<cart.length; i++){
+    res += `${cart[i].itemName} at $${cart[i].itemPrice}${i === (cart.length - 1) ? '.':', '}`
+    
+    if(i === cart.length - 2) res += 'and '
+  }
+  return res
 }
 
 function total() {
-  // write your code here
+  var tot = 0;
+  cart.forEach((item) => tot += item.itemPrice)
+  return tot
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for(var i=0; i<cart.length; i++){
+    if(cart[i].itemName === item){
+      cart.splice(i,1)
+      return cart
+    }
+  }
+  return 'That item is not in your cart.'
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(cardNumber === undefined) return "Sorry, we don't have a credit card on file for you."
+  
+  var res = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  cart = []
+  return res
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
