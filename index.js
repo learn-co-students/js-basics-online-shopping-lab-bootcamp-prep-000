7179646034ce59af82bc6b4726f96a0e984f9843
Ryan Namgung <heon.namgung@richmond.edu>
2018-05-20 18:59:00 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,32 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var items = new Object({itemName: item,
+  itemPrice: Math.floor(Math.random()*100)+1
+  });
+  
+  cart.push(items)
+  
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  if(cart.length === 0)
+  {
+    return 'Your shopping cart is empty'
+  }
+  
+  var string = (`In your cart, you have `)
+  for(let i = 0; i < cart.length; (i+2))
+  {
+    if(i === cart.length - 2){
+      string += (`and ${cart[i]} at $${cart[i+1]}`)
+    }else{
+      string += (`${cart[i]} at $${cart[i+1]}`)
+    }
+  }
+  string += (`.`)
+  return string
 }
 
 function total() {
