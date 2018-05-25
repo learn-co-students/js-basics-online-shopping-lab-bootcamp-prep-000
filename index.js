e71b3562d12b29391cb462d5b794f55e5b72dcda
Esayas Gebre <dave183278@gmail.com>
2018-05-25 00:06:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var cart = [];
+var string ="In your cart, you have "
 
 function getCart() 
 {
@@ -13,9 +14,14 @@ function addToCart(item)
  var object= additem
  additem.itemName=item;
  additem.itemPrice= Math.floor((Math.random() * 100) + 1);
+
  cart.push(additem);
  
  return  `${additem.itemName} has been added to your cart.`
+ cart.push(item);
+ 
+ return  additem
+
 }
 
 function setCart(c) {
@@ -25,8 +31,24 @@ function setCart(c) {
 
 
 
-function viewCart() {
-  // write your code here
+ 
+
+function viewCart()
+{
+  var i
+    if(cart.length<1)
+    {
+      return "Your shopping cart is empty."
+    }
+  
+  for (i=0;i<cart.length;i++)
+  {
+    
+    string=string+`${getCart()[i].itemName} at $${getCart()[i].itemPrice}`
+    
+    
+  }
+  return string +"."
 }
 
 function total() {
