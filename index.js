cba6f270eacf2ca32ddd1b504ca144b78a1decd5
e-barr <ebarrero04@gmail.com>
2018-05-30 01:36:28 +0000

    Done.

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function removeFromCart(item) {
 
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
-      cart = cart.splice(i, 1)
+      cart.splice(i, 1)
       return cart
     }
   }
