b67b02b565c4070ad271987c2074b8c831e5caf4
e-barr <ebarrero04@gmail.com>
2018-05-30 01:41:14 +0000

    Done.

--- index.js
+++ index.js
@@ -65,5 +65,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber == "") {
+    return 'Sorry, we don\'t have a credit card on file for you.'
+  }
 }
