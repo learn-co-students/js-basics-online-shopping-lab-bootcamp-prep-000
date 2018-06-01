a4d11146a8c3084ee587ed851d384ece790ef6e9
Addie Brunk <mlbrunk23@gmail.com>
2018-06-01 19:35:20 +0000

    Done.

--- index.js
+++ index.js
@@ -10,16 +10,39 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random()*100)+1;
+  //var itemName = '';
+    cart.push(new Object({['itemName']: item, ['itemPrice']: price}))
+    console.log(cart);
+    return `${item} has been added to your cart.`
+
 }
 
 function viewCart() {
-  // write your code here
+  var viewList = 'In your cart, you have ';
+  if (cart.length === 0) return `Your shopping cart is empty.`
+  else {
+    for(var i = 0; i < cart.length; i++){
+      ('itemName' in cart[i])
+      viewList += `${cart[i]['itemName']} at $${cart[i]['itemPrice']}`
+        if (i === cart.length-1) viewList += '.';
+        else if (i === cart.length-2) viewList += ', and ';
+        else viewList += ', ';;
+    }
+  }
+  return viewList;
 }
 
 function total() {
-  // write your code here
+  let totalAmt = 0;
+  for(let i = 0; i <cart.length - 1; i++){
+    let current = cart['itemPrice'[i]];
+    totalAmt += current;
+  }
+  return totalAmt;
 }
+
+
 
 function removeFromCart(item) {
   // write your code here
