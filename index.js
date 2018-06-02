b92b9eb5cdf25d529f1cfaa70a466809e81b15a1
Brad McCollum <btmcco8@gmail.com>
2018-06-02 18:31:23 +0000

    Done.

--- index.js
+++ index.js
@@ -11,10 +11,22 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var itemName = item;
+ var price = Math.floor(Math.random() * 100 + 1);
+ var newItem = { itemName: item, itemPrice: price };
+ cart.push(newItem);
+ return `${itemName} has been added to your cart.`;
 }
 
 function viewCart() {
   // write your code here
+  if ( cart.length === 0){
+    return `Your shopping cart is empty.`;
+  }else{
+    for (var i=0; i < 0; i++) {
+      
+    }
+  }
 }
 
 function total() {
