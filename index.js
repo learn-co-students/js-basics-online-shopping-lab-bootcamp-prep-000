dd94fc7e25ad1e1a9d3c5e2f202f191d8ca5a7b4
Edward Mixon <edwardchase@gatech.edu>
2018-06-03 18:02:24 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,75 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * 100);
+  var obj = {'itemName': `${item}`, 'itemPrice': price};
+  cart.push(obj);
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  var message = [];
+    if (cart.length === 0) {
+    return 'Your shopping cart is empty.'
+  } else {
+  for (let i = 0; i < cart.length; i++) {
+  if (i===0) {
+    message[i] = `In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}`;
+    if (cart.length < 2) {
+    message[i] = `${message[i]}.`;
+    }
+  }
+  else if (i < cart.length-1) {
+    message[i] = (`${cart[i].itemName} at $${cart[i].itemPrice}`)
+  }
+  else if (i < cart.length) {
+    message[i] = (`and ${cart[i].itemName} at $${cart[i].itemPrice}.`)
+  }
+  }
+}
+return message.join(', ');
 }
 
 function total() {
-  // write your code here
+  var sum = 0;
+  for (let i = 0; i < cart.length; i++) {
+    sum += cart[i].itemPrice;
+  }
+  return sum;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  cart = getCart();
+  var lim = cart.length;
+  var count = 0;
+  var have = 0;
+  while (lim > 0) {
+  var string = `${cart[count].itemName}`;
+    if (string ===`${item}`) {
+    cart = cart.slice(0,count).concat(cart.slice(count+1))
+    count = count;
+    lim -= 1;
+    have = 1;
+    }
+    else {
+      count += 1;
+      lim -= 1;
+    }
+  }
+      if (have === 0) {
+      return 'That item is not in your cart.'
+      }
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+   var cart = getCart();
+   var cost = total();
+   cart = cart.splice(0,cart.length);
+  if (arguments.length === 0) {
+  return 'Sorry, we don\'t have a credit card on file for you.';
+  }
+    else {
+      return `Your total cost is $${cost}, which will be charged to the card ${cardNumber}.`;
+    }
+    return cart;
 }
