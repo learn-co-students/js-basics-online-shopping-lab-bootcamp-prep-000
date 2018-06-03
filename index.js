f7f24c97427eb9e3ce1f0531b496ff9bfc93c928
Rodolfo Guzman <rodolfoguzman147@gmail.com>
2018-06-03 06:22:57 +0000

    Done.

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ return `${itemName} has been added to your cart.`;
 }
 
 function viewCart() {
-  let l=cart.length;
+  var l=cart.length;
     if(l==1){
       return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
     }else if(l==2){
@@ -32,48 +32,35 @@ function viewCart() {
     }
 }
 
-//Below Error: Expected [ 'In your cart, you have orange at $32', 'pear at $74', 'and quince at $78.' ] to equal 'In your cart, you have orange at $32, pear at $74, and quince at $78.'
-/*
-function viewCart() {
-  var l=cart.length;
-    if(l==1){
-      return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
-    }else if(l==2){
-      return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
-    }else if(l>=3){
-    var viewCartArray=[];
-      viewCartArray.push(`In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}`)
-      for (var i=1; i<(l-1); i++){      
-      viewCartArray.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)
-      }
-      viewCartArray.push(`and ${cart[l-1].itemName} at $${cart[l-1].itemPrice}.`)
-      return viewCartArray;
-    }else{
-    return `Your shopping cart is empty.`
-    }
-}
-*/
+
 
 function total(){
-  let b=cart.length
+  var b=cart.length
   let i=0;
   let sum= cart[i].itemPrice
     for(i=1; i<b; i++){
-    sum= sum + cart[i].itemPrice;
+    sum = sum + cart[i].itemPrice;
     }
   return sum;
 }
 
-function removeFromCart(thing){
+function removeFromCart(valuables){
   let h=cart.length
   for (let q=0; q<h; q++) {
-    if(thing==cart[q].itemName) {
+    if(valuables==cart[q].itemName) {
     cart.splice(q,1)
-    }
-  }return cart;
+    return cart;
+    }  
+  }return `That item is not in your cart.`
 }
 
-
 function placeOrder(cardNumber) {
-  // write your code here
+  let w = cart.length;
+  if(cardNumber !== null && cart.length > 0 ){
+   let calculated_total = total();
+   cart = [];
+   return `Your total cost is $${calculated_total}, which will be charged to the card ${cardNumber}.`;
+  }else{ 
+    return `Sorry, we don't have a credit card on file for you.`
+  }
 }
