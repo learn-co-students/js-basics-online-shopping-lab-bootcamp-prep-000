2711ee888e7103a0fca42c702f0d20c0e829874a
Sterling Moss <mtsilvertm@gmail.com>
2018-06-06 14:00:58 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,17 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var items = { itemName: [], itemPrice: [], };
+  var price = (Math.random() * 100 + 1);
+  var itemPrice = price;
+  
+  Object.assign({}, items, { itemName: [item] }, itemPrice, { itemPrice: [price] });
+ 
+  items;
+  
+  [items, ...cart];
+  return [...cart, items], '"${itemname} has been added to your cart"';
+  
 }
 
 function viewCart() {
