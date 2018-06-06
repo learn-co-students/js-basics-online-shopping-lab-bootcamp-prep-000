c85d276314fe94e087209edb49e780c4a2392553
Daniel O'Brien <dannyk85.dob@gmail.com>
2018-06-06 17:02:20 +0000

    Done.

--- index.js
+++ index.js
@@ -36,15 +36,93 @@ The price of each item should be a randomly-generated integer between 1 and 100.
 */
 
 function addToCart(item) {
- // write your code here
+  // creat a new obj from the string and give it a price
+  var newObj = { itemName: item, itemPrice: getRandomInt() }
+  
+  // add the item to the cart
+  cart.push(newObj)
+  
+  // let the customer know what went into the cart
+  return `${newObj.itemName} has been added to your cart.`
 }
+
+/*
+The viewCart() function does not accept any arguments. It should loop over every item in 
+your cart, returning the contents as one long, coherent statement in this format: 
+In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.
+If the cart is empty, the function should instead return Your shopping cart is empty.
+Note: Pay close attention to the syntax above. The returned statement should be a 
+single sentence that begins with In your cart, you have, terminates in a period, and 
+can assume the following shapes according to how many items the cart contains:
+1 item — In your cart, you have bananas at $17.
+2 items — In your cart, you have bananas at $17, and pancake batter at $5.
+3+ items — In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.
+*/
 
 function viewCart() {
-  // write your code here
+  // check if cart contains items
+  if(cart.length > 0)
+  {
+    // the cart contains items
+    // variable for placing the 'and'
+    var andPlacement = cart.length
+    
+    // start retString off
+    var retString = "In your cart, you have "
+    
+    // loop through the cart's items
+    for(let i = 0; i < cart.length; i++)
+    {
+      // decrement the andPlacement
+      andPlacement--
+      
+      // add the item's itemName and itemPrice
+      retString += `${cart[i].itemName} at $${cart[i].itemPrice}`
+      
+      // check for comma or 'and'
+      if(andPlacement > 1)
+      {
+        // add a comma
+        retString += ","
+      }
+      else
+      {
+        // or else its the last one and needs 'and'
+        retString += " and "
+      }
+    }
+    
+  }
+  else
+  {
+    // the cart is empty
+    return "Your shopping cart is empty."
+  }
 }
 
+/*
+The total() function accepts no arguments, iterates through the cart array, 
+and returns the current total value of the items in the cart.
+*/
+
 function total() {
-  // write your code here
+  // check if there are items in the cart:
+  if(cart.length > 0)
+  {
+    // create var to store total
+    var total = 0
+    // check the cart prices
+    for(let i = 0; i < cart.length; i++)
+    {
+      // tally it up!
+      total += cart[i].itemPrice
+    }
+    
+    // return the total
+    return total
+  }
+  
+  return total
 }
 
 function removeFromCart(item) {
@@ -53,4 +131,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  
+  // final step, empty the cart
+  cart = []
 }
