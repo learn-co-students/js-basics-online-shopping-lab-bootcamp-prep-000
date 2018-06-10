fa0c57f05e13a87e86789e05fc6c5364d440913b
Mark Noonan <markthomasnoonan@gmail.com>
2018-06-10 18:51:45 +0000

    Done.

--- index.js
+++ index.js
@@ -11,20 +11,53 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ cart.push({itemName: item, itemPrice: randomPrice()})
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
+  if (!cart.length) {
+    return "Your shopping cart is empty."
+  }
+  
+  let descriptionOfItems = cart.map(item => `${item.itemName} at $${item.itemPrice}`)
+  if (cart.length > 1) {
+    const removedLastItemDescription = `, and ${descriptionOfItems.pop()}`
+    descriptionOfItems = descriptionOfItems.join(', ') + removedLastItemDescription
+  }
+  const descriptionOfCart = `In your cart, you have ${descriptionOfItems}.`
+  return descriptionOfCart
+  
 }
 
 function total() {
   // write your code here
+  return cart.reduce((accumulator, item) => {
+    return accumulator + item.itemPrice
+  }, 0)
 }
 
 function removeFromCart(item) {
   // write your code here
+  const indexToRemove = cart.findIndex(product => product.itemName === item)
+  if (indexToRemove > 0){
+    cart.splice(indexToRemove, 1)    
+  } else {
+    return "That item is not in your cart."
+  }
 }
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (!cardNumber) {
+    return "Sorry, we don't have a credit card on file for you."
+  }
+  const message = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  cart = []
+  return message
+}
+
+function randomPrice() {
+  return Math.floor((Math.random() * 100) + 1)
 }
