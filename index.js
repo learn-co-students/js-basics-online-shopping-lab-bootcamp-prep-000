4a2547883ac45862db9fdece2181e053b96b5a05
Gavin Soko <sokolofg@gmail.com>
2018-06-11 21:03:56 +0000

    Done.

--- index.js
+++ index.js
@@ -8,12 +8,28 @@ function setCart(c) {
   cart = c;
   return cart;
 }
+function addToCart(itemName) {
+    cart.push({itemName, itemPrice: (Math.floor(Math.random() * 100))});
+    return `${itemName} has been added to your cart.`
+ }
 
-function addToCart(item) {
- // write your code here
-}
+//
+
 
 function viewCart() {
+  for (var i = 0; i > cart.length; i++) {
+    if (cart.length === 0) {
+      return `Your shopping cart is empty`;
+      } else if (cart.length === 1) {
+        return `In your cart, you have ${itemName[i]} at ${itemPrice[i]}.`;
+      } else if (cart.length === 2) {
+        return `In your cart, you have ${cart[i].itemName} at ${cart[i].itemPrice}, and ${itemName[i]} at ${cart[i].itemPrice}.`;
+      } else {
+        var man = cart.legnth - 1;
+        return `In your cart you have ${itemName[i]} at ${cart[i].itemPrice},${itemName[i]} at ${cart[i].itemPrice}, and ${cart[man].itemName at cart[man].itemPrice}`;
+      }
+      
+    }
   // write your code here
 }
 
