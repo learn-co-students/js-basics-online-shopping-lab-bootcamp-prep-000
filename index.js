b72bc06996d25915a256b78fd4d9ddb6149c50fb
gmat191@gmail.com <gmat191@gmail.com>
2018-06-12 23:59:05 +0000

    Done.

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
+function addToCart(itemName) {
  // write your code here
+ 
 }
 
 function viewCart() {
@@ -19,12 +20,31 @@ function viewCart() {
 
 function total() {
   // write your code here
+  var sum = 0;
+  for (var i=0; i<cart.length; i++) {
+    sum += cart[i][Object.keys(cart[i])];
+  }
+  return sum;
 }
 
 function removeFromCart(item) {
   // write your code here
+  for (var i = 0; i < cart.length; ++i) {
+    if (item == cart[i].itemName) {
+      cart.splice(i, 1)
+      return cart
+    }
+    }
+  return "That item is not in your cart."  
 }
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (cardNumber === undefined) {
+    return console.log("Sorry, we don't have a credit card on file for you.")
+  }
+
+  return console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`)
+  cart = [];
+  return cart
 }
