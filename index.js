dce9e276b1f21befca2058950309ada1fc7178de
Andres Manco <andresmanco@gmail.com>
2018-06-13 02:30:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var cart = [];
 
+
 function getCart() {
  return cart;
 }
@@ -9,12 +10,36 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+ // write your code hereaaa
+
+ cart.push({itemName: itemName, itemPrice:Math.floor((Math.random() *100) +1)})
+ return `${itemName} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
+  var arr = [];
+  var backUp = cart;
+  if(cart.length>0){
+    for(var i = 0; i<cart.length; i++) {
+      if(i===0 && cart.length===1){
+        arr.push(`In your cart, you have ${backUp.shift().itemName} at ${backUp.shift().itemPrice}.`)
+      }
+      else if(i===0 && cart.length>1){
+        arr.push(`In your cart, you have ${backUp.shift().itemName} at ${backUp.shift().itemPrice},`)
+      }
+      else if(i===cart.length - 1){
+        arr.push(`${backUp.shift().itemName} at ${backUp.shift().itemPrice}.`)
+      }
+      else{
+        arr.push(`${itemName} at ${itemPrice},`)
+      }
+    }
+  }
+  else {
+    return "Your shopping cart is empty."
+  }
 }
 
 function total() {
