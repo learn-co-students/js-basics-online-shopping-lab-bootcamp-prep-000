49c5564c81ec439b67c56db3ad7fe83dddc0c01e
Andres Manco <andresmanco@gmail.com>
2018-06-13 06:19:42 +0000

    Done.

--- index.js
+++ index.js
@@ -55,10 +55,23 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
-
+  var backUp = cart;
+  var c = 0;
+  for(var i = 0; i < backUp.length; i++){
+    if(item === cart[i].itemName){
+      return cart.splice(i, 1)
+    }
+  }
+    return 'That item is not in your cart.';
 }
 
 function placeOrder(cardNumber) {
   // write your code here
+  if(cardNumber === undefined){
+    return 'Sorry, we don\'t have a credit card on file for you.'
+  }
+  var t = total();
+  cart = [];
+  return `Your total cost is $${t}, which will be charged to the card ${cardNumber}.`
 }
 
