08be82a806db138a469cf310d6d3c6d62b136afc
robert.zacchia@gmail.com <robert.zacchia@gmail.com>
2018-06-14 19:50:39 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+var price = Math.ceil(Math.random() * 100);
+cart.push({item: price})
+console.log (`${item} has been added to your cart.`)
+return cart;
 }
 
 function viewCart() {
