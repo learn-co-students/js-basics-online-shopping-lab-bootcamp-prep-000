d199c2030d054ded93ab1eb8e654153bc822866b
Umyippa <sywhitehouse@gmail.com>
2018-06-25 19:13:38 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,31 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var item = generateCartItem(item)
+  getCart().push(item)
+  return `${item.itemName} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  return getCart().length === 0 ? "Your shopping cart is empty." : generateCartDescription()
 }
 
 function total() {
-  // write your code here
+  var sum = sumUpPrices()
+  return sum
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var itemToRemove = searchCartForItemToRemove(itemName)
+  return itemToRemove ? removeItemFromCart(itemToRemove) : notifyUserThereIsNoItemToRemove()
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (arguments[0] == undefined) {
+    return "Sorry, we don't have a credit card on file for you."
+  } else {
+    var sumToCharge = total()
+    setCart([])
+    return `Your total cost is $${sumToCharge}, which will be charged to the card ${cardNumber}.`
+  }
 }
