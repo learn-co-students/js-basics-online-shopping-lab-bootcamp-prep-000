68626922d3880667f7427b3e34f12eb0a5336b8f
Nikki M <nrmonegain@gmail.com>
2018-06-25 16:57:34 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ cart.itemName = "${item}"
+ cart.itemPrice = Math.floor(Math.random()*100)
+ 
+ return "${item} has been added to your cart."
 }
 
 function viewCart() {
