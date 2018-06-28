2dbbe6676408a748f7d903c5e664649ab9dcb326
Padraig McDonald <pseamus1212@gmail.com>
2018-06-28 12:38:30 +0000

    Done.

--- index.js
+++ index.js
@@ -40,15 +40,21 @@ function viewCart() {
 }
 
 function total() {
-var cartTotal = []
-for (i = 0; i < cart.length -1; i++) {
+var cartTotal = 0
+for (var i = 0; i < cart.length; i++) {
 cartTotal += cart[i].itemPrice
 }
-return cartTotal.reduce(reducer)
+return cartTotal;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (var i = 0; i < cart.length; i++) {
+    if (item === `${cart[i].itemName}`) {
+      cart.splice(i, 1)
+      return cart
+    }
+  }
+  return "That item is not in your cart."
 }
 
 function placeOrder(cardNumber) {
