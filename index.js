b1622ab9bfdf2864ec833fd43b0ed254ce7d6b08
Padraig McDonald <pseamus1212@gmail.com>
2018-06-28 12:59:58 +0000

    Done.

--- index.js
+++ index.js
@@ -58,5 +58,12 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber) {
+    var successful = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    cart = [];
+    return successful
+  }
+  else {
+    return 'Sorry, we don\'t have a credit card on file for you.'
+  }
 }
