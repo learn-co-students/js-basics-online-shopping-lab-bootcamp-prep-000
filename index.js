f601125d476322f89de298586c0c49ed78006675
Beerscheba C <designergo@aol.com>
2018-06-29 21:20:30 +0000

    Done.

--- index.js
+++ index.js
@@ -11,20 +11,58 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
-}
+
+ var price= (Math.random() * 100)
+
+ var newObject={itemName:item, itemPrice:price};
+ 
+newObject[item]={price}
+
+ cart.push(newObject)
+  return `${item} has been added to your cart.`;
+  
+ 
 
 function viewCart() {
-  // write your code here
+if( cart.length === 0){
+  
+  return 'Your shopping cart is empty.';
 }
+
+
+else  if (cart.length === 1){
+
+  var printOneItem = `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  
+  
+return printOneItem;}
+
+
+	 else if (cart.length === 2)
+	 
+	 {var printTwoItem = `In your cart, you have
+	 ${cart[0].itemName},  at $${cart[0].itemPrice}, and  ${cart[1].itemName} at $${cart[1].itemPrice}.`
+	   
+	 return printTwoItem;}    
+	  
+	 
+
+  
+
 
 function total() {
-  // write your code here
-}
-
+  
+  
+  
+}    
 function removeFromCart(item) {
-  // write your code here
-}
+  
+ 
+} 
+  
+  
+
 
 function placeOrder(cardNumber) {
-  // write your code here
+  
 }
