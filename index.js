7f139951fc944add9fac19edc38032d488c49983
Glen Simpson <gsimpson@airmaster.com.au>
2018-07-01 06:42:35 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,69 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var product = {};
+  var cost = Math.floor((Math.random() * 100))
+  product.itemName = item;
+  product.itemPrice = cost;
+  cart.push(product);
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  var s1 = "Your shopping cart is empty.";
+  var s2 = "In your cart, you have ";
+  if(cart.length > 0){
+    if(cart.length > 1) {
+        for(var i = 0; i < cart.length;i++){
+          if(i === cart.length - 1) {
+            s2 += `and ${cart[i].itemName} at $${cart[i].itemPrice}.`;
+          }
+          else {
+            s2 += `${cart[i].itemName} at $${cart[i].itemPrice}, `;
+          }
+        }
+    }
+    else {
+      s2 += `${cart[0].itemName} at $${cart[0].itemPrice}.`;
+    }
+    
+    return s2;
+  }
+  else {
+    return s1;
+  }
 }
 
 function total() {
-  // write your code here
+  var tot = 0;
+  for(var i = 0; i < cart.length; i++) {
+    tot += cart[i].itemPrice;
+  }
+  return tot;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var flag = false;
+  var updCart = [];
+  for(var i = 0;i<cart.length; i++) {
+    if(cart[i].itemName === item) {
+      flag = true;
+      cart.splice((i) , 1)
+    }
+  }
+  if(flag === true) {
+    return cart;
+  }
+  else {
+    return `That item is not in your cart.`;
+  }
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  var tot = total();
+  cart = [];
+  if(cardNumber > 0) {
+    return `Your total cost is $${tot}, which will be charged to the card ${cardNumber}.`;
+  }
+  return `Sorry, we don't have a credit card on file for you.`;
 }
