15dd5b20a5a2a98e4f07921f0b26b48718d23662
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-06-30 20:07:18 -0400

    Implement addToCart function

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  cart.push({
+    itemName: item,
+    itemPrice: Math.ceil(Math.random() * 100)
+  });
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
