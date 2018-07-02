1060ab48f4db8001e4e364c8c7106396d0276291
Jhenna Foronda <jfcaldetera@gmail.com>
2018-07-02 21:46:48 +0000

    Done.

--- index.js
+++ index.js
@@ -9,22 +9,77 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(item)
+{
+  cart.push({ itemName: item, itemPrice: Math.floor(Math.random() * 100) });
+
+  return `${item} has been added to your cart.`;
 }
 
-function viewCart() {
-  // write your code here
+function viewCart()
+{
+  if (cart.length == 0)
+  {
+    return 'Your shopping cart is empty.'
+  }
+  else {
+    var line = ''
+    var i = 1
+    var size = cart.length
+    for (var key in cart)
+    {
+      if (i == size && size > 1)
+      {
+        line += `and ${cart[key].itemName} at $${cart[key].itemPrice}, `
+      }
+      else {
+        line += `${cart[key].itemName} at $${cart[key].itemPrice}, `
+      }
+      i++
+    }
+
+    line = line.slice(0, -2)
+
+    return ('In your cart, you have ' + line + '.')
+  }
+
 }
 
 function total() {
-  // write your code here
+  var total = 0;
+  for (var key in cart)
+  {
+    total += cart[key].itemPrice
+  }
+  return total
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for(var i = 0, l = cart.length; i < l; i++)
+  {
+    for(var list in cart[i])
+    {
+      if(item === list)
+      {
+        cart.splice(i,1)
+        return cart
+      }
+    }
+  }
+  return 'That item is not in your cart.'
 }
 
-function placeOrder(cardNumber) {
-  // write your code here
+function placeOrder(cardNumber)
+{
+  if (cardNumber === undefined)
+  {
+    return 'Sorry, we don\'t have a credit card on file for you.'
+  }
+  else {
+    {
+      var result = total()
+      cart = []
+      return `Your total cost is $${result}, which will be charged to the card ${cardNumber}.`
+    }
+  }
 }
