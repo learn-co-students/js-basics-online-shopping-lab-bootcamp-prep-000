cc0bc1b90eb7eafae5152bb4a2a45c9a6bc8d73c
Tristan Brown <tgbrown@stanford.edu>
2018-07-05 21:31:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,33 +1,61 @@
-var cart = [];
-
-function getCart() {
- return cart;
-}
-
-function setCart(c) {
-  cart = c;
-  return cart;
-}
-
-function addToCart (item) {
-var price = Math.floor(Math.random() * 100+1);
-    cart.push({ [item]: price});
-    console.log(`${item} has been added to your cart.`);
-    return cart;
+var cart = []
+ 	 
+ function setCart(newCart) {	 
+   cart = newCart;	 
+ }	 
+ 
+ function getCart() {
+  return cart
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.");
+ }
+  let statementArray = []
+  let i = 0, l = cart.length
+  while(i<l) {
+  var item = Object.keys(cart[i])[0]
+  var price = cart[i][item]
+  statementArray.push(item +" at $" + price);
+  i++
+  }
+  console.log(`In your cart, you have ${statementArray.join(', ')}.`)
 }
 
-function total() {
-  // write your code here
+
+function addToCart(item) {
+  var price = Math.round(Math.random()*100);
+  cart.push({[item]:price})
+  console.log(`${item} has been added to your cart.`)
+return getCart()
 }
 
-function removeFromCart(item) {
-  // write your code here
-}
+ function total() {
+   let t = 0	
+   return t 
+ }
+ 
+ function removeFromCart(item) {
+  var inCart = false
+  for (var n = 0, l = cart.length; n<l; n++) {
+    if (cart[n].hasOwnProperty(item)) {
+      var inCart = true;
+      cart.shift(item)
+    }
+  }
+  if (!inCart) {
+    console.log("That item is not in your cart.")
+  }
+  return cart
+  }
 
-function placeOrder(cardNumber) {
-  // write your code here
-}
+  function placeOrder(creditCard) {
+    if (creditCard === undefined) {
+      console.log("We don't have a credit card on file for you to place your order.");
+    }
+    if (creditCard !== undefined) {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${creditCard}.`)
+    }
+    cart = []
+  }
