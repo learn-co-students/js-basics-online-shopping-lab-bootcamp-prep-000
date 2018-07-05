f987b000a48bf18398533c8966c85d7894cc00e4
Carie Pigeon <cariepigeon81@gmail.com>
2018-07-05 13:37:09 +0000

    Done.

--- index.js
+++ index.js
@@ -47,7 +47,11 @@ function viewCart() {
 }
 
 function total() {
-  // write your code here
+  var sum = 0;
+  for (var i = 0; i < cart.length; i++) {
+    sum += parseInt(cart[i]['itemPrice']);
+  }
+  return sum;
 }
 
 function removeFromCart(item) {
