eb048c54595c434ffd75ce9f810067dc99fd2da2
jenniferlong8 <jlong8@bu.edu>
2018-07-05 22:06:17 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,32 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var random_price = Math.floor(Math.random() * 10) + 1;
+  cart.push({['itemName']: item, ['itemPrice']: random_price })
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  var cartItem1 = [];
+  var cartItem2 = [];
+  let i = 0;
+  while (i < cart.length) {
+    cartItem1.push(` ` + cart[i].itemName + ` at $` + cart[i].itemPrice);
+    i++;
+  }
+    if (cart.length === 0) {
+      return "Your shopping cart is empty."
+  } else if (cart.length === 1) {
+    return (`In your cart, you have` + cartItem1 + `.`);
+  } else {
+    var lastItem = cart[cart.length-1]
+    let i = 0;
+    while (i < cart.length-1) {
+      cartItem2.push(` ` + cart[i].itemName + ` at $` + cart[i].itemPrice + `, and ` + lastItem].itemName + ` at $` + [lastItem].itemPrice);
+      i++;
+    }
+      return (`In your cart, you have` + cartItem2 + `.`)
+  }
 }
 
 function total() {
