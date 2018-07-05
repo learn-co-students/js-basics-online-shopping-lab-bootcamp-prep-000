4bf061cfa5ef6a3f227a4f67bcca96433c61f13e
Tristan Brown <tgbrown@stanford.edu>
2018-07-05 01:46:17 +0000

    Done.

--- index.js
+++ index.js
@@ -9,8 +9,11 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart (item) {
+ var price = Math.floor(Math.random() * 100);
+  cart.push({[item]: price});
+  console.log(${item} +' has been added to your cart.');
+return cart;
 }
 
 function viewCart() {
