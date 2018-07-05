67c401367d74fb219387dbfff4731f201983873e
Carie Pigeon <cariepigeon81@gmail.com>
2018-07-05 14:49:54 +0000

    Done.

--- index.js
+++ index.js
@@ -59,7 +59,15 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for(var i = 0; i < cart.length; i++) {
+    if ( item === cart[i]['itemName']) {
+      cart.splice(cart[i], 1);
+      return cart;
+    }
+    else {
+      return 'That item is not in your cart.';
+    }
+  }
 }
 
 function placeOrder(cardNumber) {
