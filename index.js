c9e4af52b9235742c6bd81ccad606b0548e347d4
jenniferlong8 <jlong8@bu.edu>
2018-07-06 00:11:17 +0000

    Done.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
   var random_price = Math.floor(Math.random() * 10) + 1;
-  cart.push({['itemName']: item, ['itemPrice']: random_price })
+  cart.push({['itemName']: item, ['itemPrice']: random_price})
   return `${item} has been added to your cart.`
 }
 
@@ -20,15 +20,17 @@ function viewCart() {
   var lastItem = cart[cart.length-1]
   let i = 0;
   while (i < (cart.length-1)) {
-    cartItem1.push(cart[i].itemName + ` at $` + cart[i].itemPrice +', ');
+    cartItem1.push(cart[i].itemName + ` at $` + cart[i].itemPrice +` `);
     i++;
     }
   if (cart.length === 0) {
-    return "Your shopping cart is empty."
+    return "Your shopping cart is empty.";
   } else if (cart.length === 1) {
     return (`In your cart, you have ` + cart[0].itemName +  ` at $` + cart[0].itemPrice + `.`);
+  } else if (cart.length === 2) {
+    return (`In your cart, you have ` + cart[0].itemName + ` at $` + cart[0].itemPrice + `, and ` + cart[1].itemName + ` at $` + cart[1].itemPrice + `.`);
   } else {
-    return (`In your cart, you have ` + cartItem1 + ` and ` + (lastItem.itemName) + ` at $` + (lastItem.itemPrice) + `.`);
+    return (`In your cart, you have ` + cartItem1 + `, and ` + (lastItem.itemName) + ` at $` + (lastItem.itemPrice) + `.`);
   }
 }
 
--- test/index-test.js
+++ test/index-test.js
@@ -88,7 +88,6 @@ describe("viewCart()", function() {
     addToCart("orange");
     addToCart("pear");
     addToCart("quince");
-
     ;
 
     expect(viewCart()).toEqual(
@@ -146,7 +145,7 @@ describe("removeFromCart()", function() {
 
 describe("placeOrder()", function() {
   it("doesn't place the order if a credit card number is not provided", function() {
-    
+
     expect(placeOrder()).toEqual("Sorry, we don't have a credit card on file for you.");
   });
 
@@ -156,7 +155,7 @@ describe("placeOrder()", function() {
     const cartTotal = total();
     const cardNumber = Math.floor(Math.random() * 100000000);
 
-    
+
 
     expect(placeOrder(cardNumber)).toEqual(`Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`);
   });
