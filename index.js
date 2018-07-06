5e4fe2b7466d44411756af93fdcdafb5c91964f9
Viktor Ivanov <sessgreem@gmail.com>
2018-07-06 02:55:47 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,62 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var items = {'itemName': item,
+ 'itemPrice':Math.round(Math.random()*100 + 1)
+ };
+ getCart().push(items);
+   return `${items.itemName} has been added to your cart.`
+
 }
 
 function viewCart() {
-  // write your code here
+ if(cart.length !== 0){
+  var cartDescription = 'In your cart, you have '
+  if ( cart.length >= 1 ) {
+    cartDescription += `${cart[0].itemName} at $${cart[0].itemPrice}`
+  }
+  if ( cart.length >= 2 ) {
+    var middleCartItemsDescription = ''
+    for (var i=1; i<cart.length -1; i++) {
+      middleCartItemsDescription += `, ${cart[i].itemName} at $${cart[i].itemPrice}`
+    }
+    cartDescription += `${middleCartItemsDescription}, and ${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}`
+  }
+    return `${cartDescription}.`
+ }else{
+   return 'Your shopping cart is empty.'
+ }
 }
 
 function total() {
-  // write your code here
+var total = 0;
+ for(let i =0; i<cart.length;i++){
+ total += cart[i].itemPrice;
+ } 
+ return total;
 }
-
-function removeFromCart(item) {
-  // write your code here
-}
-
 function placeOrder(cardNumber) {
-  // write your code here
+ if (cardNumber !== undefined){
+var Total = total();
+cart = [];
+  return `Your total cost is $${Total}, which will be charged to the card ${cardNumber}.`;
+}else{
+  return "Sorry, we don't have a credit card on file for you.";
+}
+}
+
+function searchCartForItemToRemove(itemName) {
+  var searchResult
+  for (var i=0; i<getCart().length; i++) {
+    if (getCart()[i].itemName === itemName) {searchResult = getCart()[i]}
+  }
+  return searchResult
+}
+function removeItemFromCart(itemToRemove) {
+  var indexOfItemToRemove = cart.indexOf(itemToRemove)
+  getCart().splice(indexOfItemToRemove,1)
+}
+function removeFromCart(itemName) {
+  var itemToRemove = searchCartForItemToRemove(itemName)
+  return itemToRemove ? removeItemFromCart(itemToRemove): 'That item is not in your cart.'
 }
