b5fc89c5f49c9761fdf8ad5786239852341c6e40
Benjamin Lee <nycxbl@hotmail.com>
2018-07-07 04:15:44 +0000

    Done.

--- index.js
+++ index.js
@@ -10,35 +10,60 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  var price = Math.floor(1 + 100*Math.random());
-  var obj = {[item]: price};
-  cart.push(obj);
+  var price = Math.ceil(Math.random()*100);
+  cart.push({itemName:item, itemPrice: price});
   return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  if (cart.length===0){
+  var print = "In your cart, you have ";
+  if (getCart().length===0) {
     return "Your shopping cart is empty.";
-  } else {
-   for (let i = 0; i < cart.length; i++){
-     console.log("In your cart, you have " + cart[i].itemName + " at $" + cart[i].price);
+  } 
+  else {
+   for (let i = 0; i < getCart().length; i++) {
+     print = print + getCart()[i].itemName + " at $" + getCart()[i].itemPrice;
+     if (i === getCart().length-1){
+       print = print + ".";
+     } 
+     if (i < getCart().length-1) {
+       print = print +", ";
+       if (i === getCart().length-2){
+         print = print + "and ";
+        }
+      }
     }
   }
+  return print;
 }
 
 function total() {
   var total = 0;
- for (let i = 0; i < cart.length; i++){
-  total = total + cart[i].price;
- }
+  for (let i = 0; i < getCart().length; i++){
+    total = total +getCart()[i].itemPrice;
+  }
  return total;
-  
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var removeIndex;
+  for (let i = 0; i < getCart().length; i++) {
+    if(item === getCart()[i].itemName) {
+      removeIndex = i;
+      getCart().splice(removeIndex,1);
+      break;
+    }
+  }    
+  return 'That item is not in your cart.';
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(cardNumber >0) {
+    var value = total();
+    setCart([]);
+    return `Your total cost is $${value}, which will be charged to the card ${cardNumber}.`;
+  }
+  else {
+    return 'Sorry, we don\'t have a credit card on file for you.';
+  }
 }
