2589ba00c3580b42c04f7019b6bb1548938abff1
katy@karstaway.com <katy@karstaway.com>
2018-07-07 09:14:47 +0000

    Done.

--- index.js
+++ index.js
@@ -10,16 +10,52 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ item = {
+   itemName:item, 
+    itemPrice: Math.floor(Math.random() * 100) + 1,
+  }
+       
+
+  cart.push (item)
+
+  return `${item.itemName} has been added to your cart.`
+
 }
 
 function viewCart() {
-  // write your code here
+  
+  if (cart.length === 0) {
+    return `Your shopping cart is empty.`
+  } else {
+    
+    var cartItems = [];
+    for(var i = 0; i < cart.length - 1; i++){
+      
+       cartItems.push(cart[i].itemName)
+       if (cartItem.length ===1)
+       return `In your cart, you have ${cart[i].item} at $17.`
+       
+    }
+   
+ /* }
+}
+  
+  if (cart.length === 1) 
+   return (`In your cart, you have ${cart.itemName}  at $${cart.price}`  )
+  
 }
 
 function total() {
-  // write your code here
+  var sum = 0
+for (var i = 0; i< cart.length; i++) {
+  sum += cart.price[i];
+  return sum;
+  */
 }
+
+}
+
+
 
 function removeFromCart(item) {
   // write your code here
