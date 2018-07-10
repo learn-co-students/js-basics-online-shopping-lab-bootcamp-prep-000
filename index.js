dcb8c732f4c524ad37eb30e50eb407391a7626fb
Bluebird Vine <zindzi.thompson@gmail.com>
2018-07-10 16:34:42 +0000

    Done.

--- index.js
+++ index.js
@@ -11,11 +11,20 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
-}
+ cart.push({itemName: item, itemPrice: Math.floor(Math.random()*100 + 1)})
+ return `${item} has been added to your cart.`}
+
 
 function viewCart() {
   // write your code here
-}
+  for (var i = 0; i < cart.length; i++)
+  keys=[Object.keys(cart)]
+  price=[Object.values(cart)]
+   i++;
+   if (cart.length === 0) {return "Your shopping cart is empty."}
+   else {return `In your cart, you have ${keys[i]} at ${price[i]}.`} 
+  }
+  
 
 function total() {
   // write your code here
