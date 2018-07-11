5215fe7d93c5e0f86c01f3410bd75507fe2874a2
Bluebird Vine <zindzi.thompson@gmail.com>
2018-07-11 12:59:20 +0000

    Done.

--- index.js
+++ index.js
@@ -17,15 +17,17 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
+   if (cart.length === 0) {
+     return "Your shopping cart is empty."}
+  
   for (var i = 0; i < cart.length; i++)
   /*
   var string=Object.values(cart[i])
   var product=string[0]
-  var price=string[1] */
+  var price=string[1] 
+  why does the if statment ===0 not work when placed within for loop?*/
   
-   if (cart.length===0) {
-     return "Your shopping cart is empty."}
-   else if (cart.length === 1) {
+  if (cart.length === 1) {
      return `In your cart, you have ${Object.values(cart[i])[0]} at $${Object.values(cart[i])[1]}.`} 
    else if (cart.length===2){
        return `In your cart, you have ${Object.values(cart[i])[0]} at $${Object.values(cart[i])[1]}, and ${Object.values(cart[i + 1])[0]} at $${Object.values(cart[i + 1])[1]}.`}
