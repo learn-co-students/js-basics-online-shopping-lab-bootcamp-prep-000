b8e478838bda7173897933c965cbff629095d3b3
Juan Elias Castillo <eliasiscoding@gmail.com>
2018-07-14 19:28:00 +0000

    Done.

--- index.js
+++ index.js
@@ -56,11 +56,9 @@ function removeFromCart(item) {
     if(cart[i].itemName === item) {
        cart.splice(i,1)
        return cart;
-    
-  } else {
-      return 'That item is not in your cart.'
     }
-  }
+    
+  } return 'That item is not in your cart.'
 }
 
 function placeOrder(creditcardnumber) {
