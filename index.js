884e556a69699b9e1602c4625db650e8bfb6d44d
Juan Elias Castillo <eliasiscoding@gmail.com>
2018-07-14 19:01:20 +0000

    Done.

--- index.js
+++ index.js
@@ -54,7 +54,8 @@ function removeFromCart(item) {
  for (let i = 0; i < cart.length; i++){
     
     if(cart[i].itemName === item) {
-      return cart.splice(i,1)
+       cart.splice(i,1)
+       return cart;
     
   } else {
       return 'That item is not in your cart.'
