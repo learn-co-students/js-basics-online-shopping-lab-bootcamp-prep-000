927af9ae13be841be16aa2ddc73cbeb18501765f
Travis Tokuyama <travistokuyama@gmail.com>
2018-07-18 14:19:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var cart = [];
+var cart = []
+
 
 function getCart() {
  return cart;
@@ -10,21 +11,63 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var itemObject = { itemName: item, itemPrice: Math.floor((Math.random() * 100) + 1)}
+ cart.push(itemObject)
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  const cartList = []
+  var threeOrMore = 'In your cart, you have '
+  if (!cart.length) { //empty cart
+    return 'Your shopping cart is empty.'
+    
+  } else if (cart.length === 2) { //two item cart
+    cartList.push(`${cart[0].itemName} at $${cart[0].itemPrice}`)
+    cartList.push(`${cart[1].itemName} at $${cart[1].itemPrice}`)
+    return `In your cart, you have ${cartList.join(', and ')}.`
+    
+  } else if (cart.length === 1) { //one item cart
+    cartList.push(`${cart[0].itemName} at $${cart[0].itemPrice}`)
+    return `In your cart, you have ${cartList.join(', and ')}.`    
+    
+  } else { for (var i = 0; i < cart.length; i++) { //three or more items
+    cartList.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)
+    if (i===cart.length - 1) {
+      threeOrMore += `and ${cart[i].itemName} at $${cart[i].itemPrice}.`
+    } else {
+      threeOrMore += `${cart[i].itemName} at $${cart[i].itemPrice}, `
+    }
+  }
+  return threeOrMore
+  }
 }
 
 function total() {
-  // write your code here
+  var totalPrice = 0
+   for (var i = 0; i < cart.length; i++) {
+     totalPrice += parseInt(cart[i].itemPrice, 10)
+   }
+   return totalPrice
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].itemName === item) {
+      cart.splice(i, 1)
+      return cart
+    } 
+  }
+  return 'That item is not in your cart.'  
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (arguments[0] === undefined) {
+    return "Sorry, we don't have a credit card on file for you."
+  } else {
+    var totalPrice = total(cart)
+    cart = []
+    return `Your total cost is $${totalPrice}, which will be charged to the card ${cardNumber}.`
+  }
 }
+
