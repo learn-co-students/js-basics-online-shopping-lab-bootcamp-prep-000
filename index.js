56d300b01097d7c7f6954ef7ccebecb503e24423
karen S <buzz.kyt@gmail.com>
2018-07-20 18:54:17 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,37 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+    var newItem = {};
+    newItem['itemName'] = item;
+    newItem['itemPrice'] = Math.floor(Math.random() * 100);
+  
+    cart.push(newItem);
+    
+    return `${newItem.itemName} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  var itemList = [];
+  if (cart.length === 1) {
+    for (let i = 0; i < cart.length; i++){
+  itemList = [...itemList,` ${cart[i].itemName} at $${cart[i].itemPrice}`];
+    } return `In your cart, you have ${itemList[itemList.length-1]}.`;
+
+    
+  } else if (cart.length === 2) {
+    for (let i = 0; i < cart.length; i++){
+  itemList = [...itemList,` ${cart[i].itemName} at $${cart[i].itemPrice}`];
+    } return `In your cart, you have ${itemList.slice(0,itemList.length-1)}, and ${itemList[itemList.length-1]}.`;
+
+    
+  } else if (cart.length > 2) {
+    for (let i = 0; i < cart.length; i++){
+  itemList = [...itemList,` ${cart[i].itemName} at $${cart[i].itemPrice}`];
+    } return `In your cart, you have ${itemList.slice(0)}, ${itemList.slice(1,itemList.length-1)}, and ${itemList[itemList.length-1]}.`;
+
+    
+  } else {
+  return "Your shopping cart is empty.";
 }
 
 function total() {
