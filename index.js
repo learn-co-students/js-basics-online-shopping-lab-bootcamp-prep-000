38be0218b6705d33fef7e847fe327d41f3688d9b
Fogang Rodriq <fogangrodriq@gmail.com>
2018-07-22 22:09:14 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,43 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var newItem = {};
+  var price = Math.floor(Math.random() * 100) + 1;
+  newItem.itemName = item;
+  newItem.itemPrice = price;
+  cart.push(newItem);
+  return `${newItem.itemName} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+   var defMsg = `In your cart, you have`;
+  var adMsg = `.`;
+  var its = [];
+  var i = 0
+  if(cart.length === 0){
+    return `Your shopping cart is empty.`;
+  }
+  else if (cart.length === 1){
+ 
+  for( i=0; i<cart.length; i++){
+    its.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)
+  }
+  return `${defMsg} ${its}${adMsg}`
+}
+else if (cart.length === 2){
+   adMsg = `, and`;
+  for( i=0; i<cart.length; i++){
+    its.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)
+  }
+  return `${defMsg} ${its.join(', and ')}.`
+}
+else if (cart.length === 3){
+   adMsg = `, and`;
+  for( i=0; i<cart.length; i++){
+    its.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)
+  }
+  return `${defMsg} ${its[0]}, ${its[1]}, and ${its[2]}.`
+}
 }
 
 function total() {
@@ -28,3 +60,6 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
 }
+
+
+ 
