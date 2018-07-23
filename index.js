c011ce7870c012a1dc452c2c1e2aeb78c50f8b7e
Fogang Rodriq <fogangrodriq@gmail.com>
2018-07-23 03:54:21 +0000

    Done.

--- index.js
+++ index.js
@@ -53,29 +53,36 @@ function total() {
   var tp = 0
   for(var i=0; i<cart.length; i++){
    tp += cart[i].itemPrice;
-    
   }
   return tp;
 }
 
 function removeFromCart(item) {
   var i = 0;
+  var mm =[];
   for (i=0; i<cart.length; i++){
    if(cart[i].itemName == item){
      cart.splice(i, 1);
-     return cart;
-   }
-   else{
-     return `That item is not in your cart.`;
-   }
-   
+    return cart;
+   } 
   }
- 
+  return `That item is not in your cart.`;
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(!cardNumber){
+    return `Sorry, we don't have a credit card on file for you.`;
+  }
+  else{
+    var pri = total()
+    var ends = cart.length;
+    cart.splice(0, ends)
+    return `Your total cost is $${pri}, which will be charged to the card ${cardNumber}.`
+  }
 }
+
+
+
 
 
  
