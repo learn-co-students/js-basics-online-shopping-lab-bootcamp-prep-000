c37ac43230fa560dc31ac197ffac687158e28328
cd <coraldarling@outlook.com>
2018-07-25 01:09:09 +0000

    Done.

--- index.js
+++ index.js
@@ -3,19 +3,39 @@ var cart = [];
 function getCart() {
  return cart;
 }
+console.log(getCart())
 
 function setCart(c) {
   cart = c;
   return cart;
 }
+console.log(setCart())
 
 function addToCart(item) {
- // write your code here
+  var p = Math.floor(Math.random() * 100 + 1);
+  cart.push({itemName: item, itemPrice: p});
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
-}
+  getCart();
+  setCart();
+  var arr = [];
+  for (let i = 0; i < cart.length; i++) {
+    arr.push(`${(cart[i].itemName)} at ${(cart[i].itemPrice)}`);
+    return arr;
+    }
+    if (arr.length = 0) {
+      return 'Your shopping cart is empty.';
+    }  else if (arr.length = 1) {
+        return `In your cart, you have ${(arr[0])}.`;
+    }   else if (arr.length = 2) {
+          return `In your cart, you have ${(arr[0])}, and ${(arr[1])}.`;
+    }     else if (arr.length > 3) {
+            return `In your cart, your have ${arr.slice(0, cart.length - 1)}, and ${arr.slice(-1)}.`
+    }
+  }
+
 
 function total() {
   // write your code here
