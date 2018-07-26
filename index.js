3c3eda31b136235a565d05beb02896c4f169845d
Charles Kim <cwkim3@gmail.com>
2018-07-26 22:53:46 +0000

    Done.

--- index.js
+++ index.js
@@ -56,7 +56,8 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   }
   else {
+    var cartTotal = total();
     cart = [];
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    return `Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`;
   }
 }
