f8a6480020da7b6db9f78ed523cb68eeb08bf202
Taylor J Young <tjy14@scarletmail.rutgers.edu>
2018-07-26 01:26:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var cart = {cereal: 'Frosted Flakes', poultry: 'Raw Chicken', coldcuts: 'Turkey Breast', fruit:'Strawberries'};
+var cart = []
+//{cereal: 'Frosted Flakes', poultry: 'Raw Chicken', coldcuts: 'Turkey Breast', fruit:'Strawberries'};
 
 function getCart() {
  return cart;
@@ -21,25 +22,38 @@ function addToCart(itemName) {
 }
 
 
-Object.size = function(obj) {
-    var size = 0, key;
-    for (key in obj) {
-        if (obj.hasOwnProperty(key)) size++;
-    }
-    return size;
-};
-
-// Get the size of an object
-var size = Object.size(cart);
 
 function viewCart() {
-  
-  for (i=1, i<csize, i++){
-    return "In your cart, you have " + 
-    
+
+if (cart.length < 1){
+  return "Your shopping cart is empty."
+}
+
+else if (cart.length <2){
+   return ("In your cart, you have "+ cart[0].itemName + " at $" + cart[0].itemPrice + ".")
   }
-  return "In your cart, you have"
-}}
+  
+else if (cart.length <3){
+  
+   return ("In your cart, you have "+ cart[0].itemName + " at $" + cart[0].itemPrice + ", and " + cart[1].itemName + " at $" + cart[1].itemPrice + ".") 
+}
+else { 
+var lastItem = cart[cart.length-1].itemName
+var lastPrice = cart[cart.length-1].itemPrice
+var x;
+var firstSent = ("In your cart, you have "+ cart[0].itemName + " at $" + cart[0].itemPrice + ", " + cart[1].itemName + " at $" + cart[1].itemPrice + ", ")
+
+var lastSent = (+ " and " +lastItem +" at $" +lastPrice +"."  )
+
+//NEXT TRY TO FIGURE OUT WHY CART[X].ITEMNAME DOES NOT WORK
+for (x=2; x< cart.length -1;x++){
+  var midSent = cart[x].itemName + " at $" + cart[x].itemPrice + " ,"
+  return midSent
+}
+return (firstSent + midSent + lastSent)
+}
+}
+
 
 function total() {
   // write your code here
