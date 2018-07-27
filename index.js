78c6aea4fec9b3d7b4fd08f07108fe7f70f405ef
cd <coraldarling@outlook.com>
2018-07-27 00:45:50 +0000

    Done.

--- index.js
+++ index.js
@@ -3,50 +3,66 @@ var cart = [];
 function getCart() {
  return cart;
 }
-console.log(getCart())
 
 function setCart(c) {
   cart = c;
   return cart;
 }
-console.log(setCart())
 
 function addToCart(item) {
   var p = Math.floor(Math.random() * 100 + 1);
   cart.push({itemName: item, itemPrice: p});
-  return `${item} has been added to your cart.`
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  debugger;
   var cartOrder = 'In your cart, you have ';
   if (cart.length === 0) {
     return 'Your shopping cart is empty.';
   } else {
     if(cart.length >= 1) {
-        cartOrder += `${cart[0].itemName} at $${cart[0].itemPrice}.`;
+        cartOrder += `${cart[0].itemName} at $${cart[0].itemPrice}`;
       }
     if(cart.length >= 2) {
-      var items = ""
+      var items = "";
       for (let i = 1; i < cart.length - 1; i++) {
-        items += `${(cart[i].itemName)} at $${(cart[i].itemPrice)}`);
+        items += `, ${cart[i].itemName} at $${cart[i].itemPrice}`;
       }
-      cartOrder += `${items}, and ${cart[cart.length - 1].itemName} at ${cart[cart.length - 1].itemPrice}.`
+      cartOrder += `${items}, and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}`;
     }
     }
-    return cartOrder
+    return `${cartOrder}.`;
   }
 
 
-
 function total() {
-  // write your code here
+var cartTotal = 0;
+var	prices = [];
+for (var i = 0; i < cart.length; i++) {
+  prices.push(cart[i].itemPrice);
+}
+for (let i = 0; i < cart.length; i++) {
+	cartTotal += prices[i];
+}
+   return(cartTotal);
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].itemName === item) {
+      cart.splice(i, 1);
+    getCart();
+    }
+  }
+      return ('That item is not in your cart.');
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+    if (cardNumber > 0) {
+      var amount = total()
+    cart.shift();
+    return `Your total cost is $${amount}, which will be charged to the card ${cardNumber}.`;
+    } else { 
+      return 'Sorry, we don\'t have a credit card on file for you.';
+}
 }
