9d965277f884dc6b0dbf80b0180956ee9356cf30
Samthedriver <amanuelabraham0202@gmail.com>
2018-07-27 17:38:06 +0000

    Done.

--- index.js
+++ index.js
@@ -69,8 +69,20 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
+  for (var i = 0; i < cart.length; i++) {
+    if (item == cart[i].itemName) {
+      cart.splice(i,1)
+      return cart
+    }
+  }
+  return "That item is not in your cart."
 }
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (cardNumber == undefined || cardNumber.length == 0)
+    return "Sorry, we don't have a credit card on file for you."
+  var tot = total();
+  cart = [];
+  return "Your total cost is $" + tot + ", which will be charged to the card " + cardNumber + "."
 }
