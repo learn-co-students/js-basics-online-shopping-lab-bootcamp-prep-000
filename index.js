0bc69e1b8a5af93101e255772d67fe1da4a1aca4
Hilary Sturges <hilarysturges@gmail.com>
2018-08-07 17:50:58 +0000

    Done.

--- index.js
+++ index.js
@@ -11,10 +11,45 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ let price = Math.floor(Math.random()*100)
+ cart.push({["itemName"]:item, ["itemPrice"]:price})
+ return `${item} has been added to your cart.`
 }
 
-function viewCart() {
+/* function viewCart() {
   // write your code here
+  if (!cart.length) {
+    return `Your shopping cart is empty.`
+  } else {
+    var sentence = "In your cart, you have ";
+    var items = "";
+    for (let i=0; i < cart.length+1; i++) {
+      items += cart[i]['itemName']+" at ";
+      items += "$"+cart[i]['itemPrice']+",";
+      var fullSentence = sentence + items;
+      return fullSentence.slice(0,fullSentence.length-1)+".";
+    }
+  }
+} */
+
+function viewCart () {
+  // write your code here
+    if (!cart.length) {
+    return `Your shopping cart is empty.`
+  } else {
+    listOfCart();
+  }
+  
+}
+
+function listOfCart() {
+  var sentence = "In your cart, you have ";
+  if (cart.length >= 1) {
+    for (let i=0, l=cart.length; i < l; i++) {
+    sentence += `${cart[i]['itemName'} at ${cart[i]['itemPrice']}`
+    }
+  }
+  return sentence;
 }
 
 function total() {
@@ -28,3 +63,8 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
 }
+
+
+
+testArray =[{["name"]: "test1", ["price"]: "test2"}];
+console.log(testArray[0]['name']);
