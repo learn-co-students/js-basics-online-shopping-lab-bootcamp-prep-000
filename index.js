38630d26c50dce41f1b0b0d8cd678325e5d0256e
Hilary Sturges <hilarysturges@gmail.com>
2018-08-07 19:48:59 +0000

    Done.

--- index.js
+++ index.js
@@ -48,21 +48,7 @@ function viewCart () {
   return fullSentence;
 }
 
-function listOfCart() {
-  var cartDescription = 'In your cart, you have '
-  if ( getCart().length >= 1 ) {
-    cartDescription += `${getCart()[0].itemName} at $${getCart()[0].itemPrice}`
-  }
-  if ( getCart().length >= 2 ) {
-    var middleCartItemsDescription = ''
-    for (var i=1; i<getCart().length -1; i++) {
-      middleCartItemsDescription += `, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`
-    }
-    cartDescription += `${middleCartItemsDescription}, and ${getCart()[getCart().length-1].itemName} at $${getCart()[getCart().length-1].itemPrice}`
-  }
-
-  return `${cartDescription}.`
-}
+//I give up!
 
 function total() {
   // write your code here
