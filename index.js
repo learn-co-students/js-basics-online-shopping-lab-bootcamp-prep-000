e2279c91a3d04cef2697fdd13c6b3c179524f566
Renan Góes <renangoes12@gmail.com>
2018-08-08 19:17:45 +0000

    Done.

--- index.js
+++ index.js
@@ -9,20 +9,43 @@ function setCart(c) {
   return cart;
 }
 function addToCart(item) {
-  var newObj = {};
-  var itemName = `itemName:"${item},`;
-  var itemPrice =` itemPrice: ${Math.floor(100*Math.random())}`;
-  newObj[itemName] = itemPrice;
-  cart.push(newObj);
+  var newItem = {
+    itemName: item,
+    itemPrice: Math.floor(100*Math.random())
+  };
+  cart.push(newItem);
   return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0){
+    return `Your shopping cart is empty.`
+  }
+  else if (cart.length === 1){
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  }
+  else if (cart.length === 2){
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
+  }
+  else {
+    var newVar = `In your cart, you have `;
+    var countdown = cart.length;
+    
+  for (var i = 1; i < countdown; i++) {
+    newVar = newVar + `${cart[i-1].itemName} at $${cart[i-1].itemPrice}, `;
+  }
+  newVar = newVar + `and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`
+  return `${newVar}`;
+  }
 }
 
 function total() {
-  // write your code here
+  var newVar = 0;
+  var countdown = cart.length;
+  for (var i = 0; i < countdown; i++){
+    newVar = newVar + cart[i].itemPrice;
+  }
+  return newVar;
 }
 
 function removeFromCart(item) {
@@ -32,6 +55,3 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
 }
-
-console.log(addToCart('bananas'))
-console.log(cart)
