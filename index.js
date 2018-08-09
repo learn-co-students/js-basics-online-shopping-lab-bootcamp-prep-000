c97eba89688b242c4c71988444aad33b53285e1f
Noelle Rolwes <nrolwes@gmail.com>
2018-08-09 22:39:44 +0000

    Done.

--- index.js
+++ index.js
@@ -42,13 +42,34 @@ function viewCart() {
 }
 
 function total() {
-  // write your code here
+  var totalCost = 0;
+  
+  for (var i = 0; i< cart.length; i++){
+    totalCost += cart[i].itemPrice;
+  }
+  return totalCost;
 }
+
 
 function removeFromCart(item) {
-  // write your code here
+ for (var i = 0; i < cart.length; i++) {
+  if (cart[i].itemName === item) {
+    cart.splice(i,1);
+    return cart;
+  }
+} 
+return `That item is not in your cart.`;
 }
 
+
 function placeOrder(cardNumber) {
-  // write your code here
+
+  if (!cardNumber){
+    return `Sorry, we don't have a credit card on file for you.`;
+  } else {
+    var chargeReply = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    cart = [];
+    return chargeReply;
+  }
 }
+
