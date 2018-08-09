63ac4ed906f7e4f8288f4cb5742566a3e34eb8fb
Noelle Rolwes <nrolwes@gmail.com>
2018-08-09 17:15:56 +0000

    Done.

--- index.js
+++ index.js
@@ -9,12 +9,36 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(name) {
+  var price = Math.floor(Math.random()*100);
+  
+  var itemObj = {
+    itemName: name, 
+    itemPrice: price
+  }
+  cart.push(itemObj);
+  return `${name} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+   var nameAndPrice = [];
+    
+    if (cart.length === 0){
+      return `Your shopping cart is empty.`;
+      
+    } else if (cart.length === 1) {
+     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
+     
+    } else if (cart.length === 2) {
+      return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
+      
+   } else if (cart.length>3){
+      for (var i = 0 ; i < cart.length; i++){
+        nameAndPrice.push(`${cart[i].itemName} at $${cart[i].itemPrice},`) 
+      }
+      
+      return `In your cart, you have ${nameAndPrice} and ${nameAndPrice.slice(-1)}.`
+   }
 }
 
 function total() {
