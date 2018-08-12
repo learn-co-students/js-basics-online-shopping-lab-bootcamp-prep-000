3a94442694b090d31455531df3109b305bbde760
Ashley  Zaki <ahnahn.un@gmail.com>
2018-08-12 23:11:13 +0000

    Done.

--- index.js
+++ index.js
@@ -56,12 +56,48 @@ function total() {
 
 
 function removeFromCart(item) {
-  var cItem;
-  cart.slice(item);
-  return cart;
-  
+  for (let i = 0; i< cart.length; i++){
+    if (cart[i].itemName == item){
+      cart.splice(i,1)
+      return cart;
+    }
+  }
+  return "That item is not in your cart.";
 }
+  
+
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (!cardNumber){
+    return `Sorry, we don't have a credit card on file for you.`
+  }
+  else{
+    var totalOrder = total();
+    cart = [];
+    return `Your total cost is $${totalOrder}, which will be charged to the card ${cardNumber}.`
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
