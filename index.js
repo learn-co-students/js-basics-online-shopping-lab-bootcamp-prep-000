d2ed5623fcac0b549f3c10413c2be4697af27d5f
highdevilxx@hotmail.com Khaled <highdevilxx@hotmail.com>
2018-08-13 18:41:31 +0000

    Done.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function setCart(c) {
 function addToCart(item)
 {
   var price = Math.floor((Math.random()*100)+1);
-  cart.push({ itemName: item,itemprice:price });
+  cart.push({ itemName: item,itemPrice:price });
    return (`${item} has been added to your cart.`);
 }
 
