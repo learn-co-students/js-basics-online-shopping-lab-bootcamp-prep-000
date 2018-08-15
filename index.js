f2f510559cdd00622c72960ded5bc8e51707ce4e
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-08-15 23:52:33 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,74 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
-}
+ let price = Math.floor(Math.random() * 100);
+ var itemInventory = {itemName: item, itemPrice: price};
+ cart.push(itemInventory);
+ return `${item} has been added to your cart.`;
+ }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0){
+    return "Your shopping cart is empty.";
+  } else if (cart.length === 1){
+    var oneItem = cart[0];
+    return `In your cart, you have ${oneItem.itemName} at $${oneItem.itemPrice}.`;
+  } else if (cart.length > 1){
+    var lastItem = cart[cart.length - 1];
+    var itemsList = [];
+    for (var i = 0; i <= cart.length - 2; i++){
+      var multipleItems = cart[i];
+      itemsList.push(` ${multipleItems.itemName} at $${multipleItems.itemPrice}`);
+      }
+      return `In your cart, you have${itemsList.toString()}, and ${lastItem.itemName} at $${lastItem.itemPrice}.`;
+  }
 }
 
 function total() {
-  // write your code here
+   var allPrices = [];
+   for (var i = 0; i <= cart.length -1; i++){
+    var eachPrice = cart[i];
+    allPrices.push(eachPrice['itemPrice']);
+    allPrices;
+  }
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return allPrices.reduce(reducer);
 }
 
 function removeFromCart(item) {
-  // write your code here
+  function getItems(cart){
+    var i = 0;
+    var justItems = [];
+    while (i <= cart.length -1){
+      var eachItem = cart[i];
+      justItems.push(eachItem.itemName);
+      i++;
+    } return justItems;
+  }
+  if ((getItems(cart)).includes(item) === false){
+    return "That item is not in your cart.";
+  } else if ((getItems(cart)).includes(item) === true){
+    var newCart = [];
+    for (var i = 0; i <= cart.length - 1; i++){
+      var eachItem = cart[i];
+    if (eachItem.itemName === item){
+      cart.splice(i, 1);
+      newCart.push(cart);
+      return newCart;
+  }
+} 
 }
+}
+ 
 
-function placeOrder(cardNumber) {
-  // write your code here
+function placeOrder(cardNumber){
+  var checkForNumber = Boolean(cardNumber);
+  if (checkForNumber === false){
+    return "Sorry, we don't have a credit card on file for you.";
+  } else if (checkForNumber === true){
+    var bill = [];
+    bill.push(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    cart.length = 0;
+    return bill[0];
+  }
 }
