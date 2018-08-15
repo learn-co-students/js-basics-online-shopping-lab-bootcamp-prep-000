43bdd5ca6a546c5616bb4cd511274780f9e69a99
Arlisha2019 <arlisha_hayles@yahoo.com>
2018-08-15 01:40:36 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,21 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
-}
+  let price = Math.floor(Math.random() * (100 - 1));
+  cart.push({itemName:[item], itemPrice: price});
+return `${item} has been added to your cart.`;
+} 
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    return "Your shopping cart is empty.";
+  } else {
+    let string = 'In your cart, you have'
+    for (var i = 0; i < cart.length; i++) {
+      string += `${cart[i][itemName]} at $${cart[i][itemPrice]}.`
+    }
+   return string;
+  }
 }
 
 function total() {
