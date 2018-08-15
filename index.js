241c0a15d43fb47b73309542d18c5482261b5345
livingshine@gmail.com <livingshine@gmail.com>
2018-08-15 06:19:56 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,24 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * 101) +1;
+  var obj = {};
+  obj.itemName = item;
+  obj.itemPrice = price;
+  cart.push(obj);
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length===0)
+  return "Your shopping cart is empty.";
+  
+  else{
+  var newArray = cart.slice(0);
+  for (var i=0; i<newArray.length; i++){
+      newArray[i] 
+    }
+  }
 }
 
 function total() {
