70cdbc7abc61a2f4954c406e1403137025e8925c
skylerwphillipstx@gmail.com Phillips <skylerwphillipstx@gmail.com>
2018-08-16 20:42:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var cart = [];
 
 function getCart() {
@@ -10,21 +11,62 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var obj = {}
+  obj['itemName'] = item
+  obj['itemPrice']= Math.floor((Math.random()*100)+1)
+  getCart().push(obj)
+  return item + " has been added to your cart."
 }
 
 function viewCart() {
-  // write your code here
+  getCart()
+
+  if (cart.length === 1) {
+    return `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`
+  } else if (cart.length === 2 ) {
+    return `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}, and ${getCart()[1].itemName} at $${getCart()[1].itemPrice}.`
+    
+  } else if (cart.length >= 3) {
+    var array = [`In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}`] 
+    for (let i = 1; i < cart.length - 1 ; i++) {
+    array.push(` ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`)
+    array.push(` and ${getCart()[cart.length -1].itemName} at $${getCart()[cart.length - 1].itemPrice}.`)
+    }
+    return array.toString()
+   
+  } else if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  }
 }
 
 function total() {
+  var sum = 0
+  for (let i = 0; i < getCart().length; i ++) {
+    sum += getCart()[i].itemPrice
+  }
+  return sum 
+}
+
+
+
+function placeOrder(cardNumber) {
+  if (cardNumber > 0) {
+    var string = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  getCart().length = 0
+  return string 
+  } else {
+    return "Sorry, we don't have a credit card on file for you."
+  }
   // write your code here
 }
 
 function removeFromCart(item) {
-  // write your code here
-}
-
-function placeOrder(cardNumber) {
-  // write your code here
+ for (let i = 0; i < getCart().length; i++) {
+   if (getCart()[i].itemName === item) {
+     getCart().splice(i, 1) 
+     return getCart()
+   } else {
+   }
+ }
+ return "That item is not in your cart."
 }
