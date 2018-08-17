c7ecf58df183f1e60b5bffbe60f592d648334874
Arlisha2019 <arlisha_hayles@yahoo.com>
2018-08-17 01:38:16 +0000

    Done.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
   let price = Math.floor(Math.random() * (100 - 1));
-  cart.push({itemName:[item], itemPrice: price});
+  cart.push({itemName: item, itemPrice: price});
 return `${item} has been added to your cart.`;
 } 
 
