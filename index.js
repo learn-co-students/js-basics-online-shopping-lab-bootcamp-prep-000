5fd2b7b49be5021dfcfca416e5cf4ab3ac588a34
Mark Ehler <ehlerm42@gmail.com>
2018-08-21 21:07:48 +0000

    Done.

--- index.js
+++ index.js
@@ -20,23 +20,24 @@ function addToCart (item) {
 }
 
 
-addToCart('glove')
-addToCart('ball')
-addToCart('skis')
-addToCart('shoes')
+addToCart('glove');
+addToCart('ball');
+addToCart('skis');
+addToCart('shoes');
 
 
 function viewCart() {
   var cartItems = [];
   if (cart.length === 0) {
-    return 'Your shopping cart is empty.'
-} else {
-  for (let i = 0; i < cart.length - 1; i++){
-    cartItems.push(`${cart[i].itemName} at $${cart[i].itemPrice}, `)}}
- cartItems.push(`and ${cart[-1].itemName} at $${cart[-1].itemPrice}.`)
-     return `In your cart, you have ${cartItems}.` 
-
-
+    return 'Your shopping cart is empty.'}
+  else if (cart.length === 1){
+    cartItems.push(` ${cart[0].itemName} at $${cart[0].itemPrice}`)} 
+  else {
+  for (let i = 1; i < cart.length - 1; i++){
+    cartItems.push(` ${cart[i].itemName} at $${cart[i].itemPrice}`)}
+    
+ cartItems.push(` and ${cart[cart.length -1].itemName} at $${cart[cart.length -1].itemPrice}`);}
+ return `In your cart, you have${cartItems}.`     
 }
 
 viewCart(cart);
@@ -44,36 +45,37 @@ viewCart(cart);
 
 function total() {
   var pricePerItemInCart = [];
-  var totalPrice = 0;
   for(var i = 0; i < cart.length; i++){
-    var itemPrice = cart[i][Object.keys(cart[i])]
-    pricePerItemInCart.push(itemPrice)}
-    
-    for (var j =0; j < pricePerItemInCart.length; j++)
-    {
-      totalPrice = pricePerItemInCart[j]+=totalPrice;
-    }
-    return totalPrice
+    pricePerItemInCart += cart[i].itemPrice
   }
-  
+  return pricePerItemInCart
+}
+    
 total(cart);
 
-
+function removeFromCart(item) {
+  for(let i = 0; i<cart.length; i++){
+    if(cart[i].item == item){
+      cart.splice(i, 1)
+      return cart
+    } else {
+      return "That item is not in your cart."
+    }
+  }
+}
 
 removeFromCart('skis');
   
   
 function placeOrder(cardNumber) {
-  if(cardNumber) 
-  {
-    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+  if(cardNumber)  {
     for (var i = cart.length; i>= 0; i--)
     { cart.pop();} 
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   }
   else 
   {
-    console.log('Sorry, we don\'t have a credit card on file for you.')
-    return cart;
+    return 'Sorry, we don\'t have a credit card on file for you.'
   }
 }
 
