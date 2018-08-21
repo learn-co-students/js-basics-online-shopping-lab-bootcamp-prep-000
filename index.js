dd3993f6440e654a525cafd008bf4929d80b04de
Ava Barron <mztriz@gmail.com>
2018-08-21 21:04:29 +0000

    Done.

--- index.js
+++ index.js
@@ -26,15 +26,11 @@ function viewCart() {
   if (getCart().length === 0){
     return "Your shopping cart is empty."
   } 
-  
   var sentance = "In your cart, you have"
-  
   if (getCart().length === 1){
     return `${sentance} ${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`
   }
-  
   var middleSentances = ''
-    
   if (getCart().length >= 2){
     for (let i = 0; i < getCart().length; i++){
       if (i === getCart().length - 1){
@@ -59,15 +55,17 @@ function removeFromCart(item) {
   if (getCart().indexOf(item) === -1){
     return "That item is not in your cart."
   }
-  else {
-    let index = getCart().indexOf(item)
-    getCart().splice(index, 1)
-    return cart
-  }
+  setCart(getCart().splice(getCart().indexOf(item), 1))
+  return getCart()
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined){
+    return "Sorry, we don't have a credit card on file for you."
+  }
+  let sentance = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  setCart("")
+  return sentance
 }
 
 
