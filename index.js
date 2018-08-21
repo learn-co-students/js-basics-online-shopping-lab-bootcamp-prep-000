25be6afe74cf10f66bdac131325451dda104baec
Ava Barron <mztriz@gmail.com>
2018-08-21 19:13:19 +0000

    Done.

--- index.js
+++ index.js
@@ -9,22 +9,58 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(item){
+  let newItem = getItemAndPrice(item)
+  getCart().push(newItem)
+  return `${newItem.itemName} has been added to your cart.`
 }
 
+function getItemAndPrice(item){
+  return {
+    'itemName':item,
+    'itemPrice':Math.floor(Math.random() * 101)
+  }
+}
+  
 function viewCart() {
-  // write your code here
+  if (getCart().length === 0){
+    return "Your shopping cart is empty."
+  } 
+  
+  var sentance = "In your cart, you have"
+  
+  if (getCart().length === 1){
+    return `${sentance} ${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`
+  }
+  
+  var middleSentances =''
+    
+  if (getCart().length >= 2){
+    for (let i = 0; i < getCart().length; i++){
+      if (i === getCart().length - 1){
+        middleSentances += `${middleSentances}, and ${getCart()[i].itemName} at $${getCart()[i].itemPrice}.`
+      }
+      else {
+        middleSentances +=`${middleSentances}, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`
+      }
+    }
+  }
+  return `${sentance} ${middleSentances}`
 }
 
 function total() {
-  // write your code here
+  let total = 0
+  for (let i = 0; i < getCart().length; i++) {
+    total = total + getCart()[i].itemPrice
+  }
+  return total
 }
 
 function removeFromCart(item) {
-  // write your code here
+  delete getCart()[item].itemName
 }
 
 function placeOrder(cardNumber) {
   // write your code here
 }
+
