168fc3b3eca9fb3a836d6e69d3981a0b0147b85b
Mark Ehler <ehlerm42@gmail.com>
2018-08-22 00:01:05 +0000

    Done.

--- index.js
+++ index.js
@@ -27,59 +27,60 @@ addToCart('shoes');
 
 
 function viewCart() {
+  
   var cartItems = [];
-  if (cart.length === 0) {
+  if (cart.length === 0 ) {
     return 'Your shopping cart is empty.'}
+  
   else if (cart.length === 1){
     cartItems.push(` ${cart[0].itemName} at $${cart[0].itemPrice}`)} 
+  
   else {
-  for (let i = 1; i < cart.length - 1; i++){
+  for (let i = 0; i < getCart().length - 1; i++){
     cartItems.push(` ${cart[i].itemName} at $${cart[i].itemPrice}`)}
     
- cartItems.push(` and ${cart[cart.length -1].itemName} at $${cart[cart.length -1].itemPrice}`);}
+ cartItems.push(` and ${getCart()[cart.length -1].itemName} at $${cart[cart.length -1].itemPrice}`);}
  return `In your cart, you have${cartItems}.`     
 }
 
 viewCart(cart);
 
-
 function total() {
-  var pricePerItemInCart = [];
-  for(var i = 0; i < cart.length; i++){
-    pricePerItemInCart += cart[i].itemPrice
+  var pricePerItemInCart = 0
+  for(var i = 0; i < getCart().length; i++){
+    pricePerItemInCart += getCart()[i].itemPrice
   }
   return pricePerItemInCart
 }
     
 total(cart);
 
+
+
 function removeFromCart(item) {
-  for(let i = 0; i<cart.length; i++){
-    if(cart[i].item == item){
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i] == (item)) {
       cart.splice(i, 1)
       return cart
-    } else {
-      return "That item is not in your cart."
-    }
+    } 
   }
+  return 'That item is not in your cart.'
 }
-
-removeFromCart('skis');
   
+
   
 function placeOrder(cardNumber) {
   if(cardNumber)  {
-    for (var i = cart.length; i>= 0; i--)
-    { cart.pop();} 
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+      for (var i = getCart().length; i > 0; i--){ 
+        cart.pop();
+        i--;
+      }
   }
   else 
   {
     return 'Sorry, we don\'t have a credit card on file for you.'
   }
 }
-
-placeOrder();
-
 
 
