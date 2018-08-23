674b391798e43600be475ac37a958be26124385f
Joon Jang <joon.jang95@gmail.com>
2018-08-23 20:41:14 +0000

    Done.

--- index.js
+++ index.js
@@ -67,8 +67,8 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
     return `Sorry, we don't have a credit card on file for you.`
 } else {
-  var cashOut = total()
+  var buy = total()
   cart.splice(0, cart.length);
-  return `Your total cost is $${cashOut}, which will be charged to the card ${cardNumber}.`
+  return `Your total cost is $${buy}, which will be charged to the card ${cardNumber}.`
   }
 }
