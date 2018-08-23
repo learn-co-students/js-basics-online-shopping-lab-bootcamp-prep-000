7997075a7607f6f580a3f3da40874a6f4fc74b67
adigitalnative@gmail.com <adigitalnative@gmail.com>
2018-08-23 19:16:38 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,78 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var item = generateCartItem(item);
+  cart.push(item)
+  return `${item.itemName} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  if (getCart().length === 0) {
+    
+    return "Your shopping cart is empty."
+    
+  } else if (getCart().length === 1) {
+    
+    return `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`
+  } else {
+    var counter = 0
+    var descs = []
+    while (counter < getCart().length-1) {
+      descs.push(`${getCart()[counter].itemName} at $${getCart()[counter].itemPrice}`)
+      counter++
+    }
+    descs.push(`and ${getCart()[counter].itemName} at $${getCart()[counter].itemPrice}.`)
+    
+    return `In your cart, you have ${descs.join(", ")}`
+  }
 }
 
 function total() {
-  // write your code here
+  var total = 0
+  
+  for (let i=0, l=getCart().length; i<l; i++) {
+    total = total + parseInt(getCart()[i].itemPrice)
+  }
+  
+  return total
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var indexOfItemToRemove = "none"
+  for(let i=0, l=getCart().length;i<l; i++) {
+    if (getCart()[i].itemName == item) {
+      indexOfItemToRemove = i
+    }
+  }
+  
+  if (indexOfItemToRemove === "none") {
+    return "That item is not in your cart."
+  } else {
+    return getCart().splice(indexOfItemToRemove, 1)
+  }
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber) {
+    const totalCost = total();
+    cart = []
+    return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`
+  } else {
+    return "Sorry, we don't have a credit card on file for you."
+  }
+  
+}
+
+
+// private
+
+function getRandomInt(min, max){
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateCartItem(item) {
+  return {
+    itemName: item,
+    itemPrice: getRandomInt(1,100)
+  }
 }
