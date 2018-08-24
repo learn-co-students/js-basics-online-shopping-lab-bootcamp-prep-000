6c52318bb3b6662e892b8f4a511d91334fd08c58
Viam Mercer <viam.mercer@hotmail.com>
2018-08-24 16:40:52 +0000

    Done.

--- index.js
+++ index.js
@@ -11,11 +11,24 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var myObject = new Object();
+ myObject.itemName = item;
+ myObject.itemPrice = Math.floor(Math.random() * (100 - 1)) + 1;
+ cart.push(myObject);
+ return `${myObject.itemName} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
-}
+  if (cart.length === 0) {
+    return `Your shopping cart is empty.`;
+  }
+  else if (cart.length === 1) {
+    return `In your cart, you have ${cart[0].itemName} at $ ${cart[0].itemPrice} `;
+  }
+  else {
+    return `In your cart, you have ${cart} `;
+      }
+  }
 
 function total() {
   // write your code here
