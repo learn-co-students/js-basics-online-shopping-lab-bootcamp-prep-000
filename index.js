df75110124aa4fb135a3669f5c9e89913c071d2d
Cedric David <cedric.david@btopenworld.com>
2018-08-26 14:07:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,39 +1,75 @@
-var cart;
 
+var cart = [];
 
-function setCart(newCart) {
-  cart = newCart;
-}
-
-function total() {
-  let t = 0
-
-  for (var i = 0, l = cart.length; i < l; i++) {
-    for (var item in cart[i]) {
-      t += cart[i][item]
-    }
-  }
-
-  return t
-}
 function getCart() {
+ return cart;
+}
+
+function setCart(c) {
+  cart = c;
   return cart;
 }
 
-function addToCart(item){
-  var price=Math.floor(Math.random()*101);
+function addToCart(name) {
+  var newObj = {};
+  var price = Math.ceil(Math.random() * 99)
+  newObj.itemName = name;
+  newObj.itemPrice = price;
+  cart.push(newObj);
 
-  cart.push({item: price});
-  console.log(item+" has been added to your cart.");
-  return cart;
+  return (`${name} has been added to your cart.`);
 }
-function viewCart(){
-  if(cart.length===0){
-    console.log("Your shopping cart is empty.")
-  }else{
-    var message= "In your cart you have ";
-    for(var i=0; i<cart.length;i++){
-      message=message+cart[i].item+" at $"+cart[i].price;
+
+function viewCart() {
+    var frontMes = "In your cart, you have ";
+    var midMes = "";
+    var endMes;
+    var message;
+    if (cart.length === 0){
+      return ('Your shopping cart is empty.');
+
+    }else{
+    for (var i = 0; i<cart.length-1; i++){
+
+      midMes += `${cart[i].itemName} at $${cart[i].itemPrice}, `
     }
+    endMes = `and ${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}.`
+}
+     if (cart.length === 1){
+      message = frontMes + `${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}.`
+    }else{
+      message = frontMes + midMes + endMes;
+    }
+    return message;
   }
-}console.log(message);
+
+  function total() {
+    var total=0;
+    for (let i = 0; i<cart.length; i++) {
+      total += cart[i].itemPrice;
+    }
+    return total;
+  }
+
+function removeFromCart(item) {
+  var pos = cart.findIndex(i => i.itemName === item);
+
+  if(pos === -1){
+    return "That item is not in your cart.";
+
+  }else{
+
+    cart.splice(pos, 1);
+    return cart;
+  }
+}
+function placeOrder(credNum) {
+  var a = total()
+  if (credNum === undefined) {
+    return "Sorry, we don't have a credit card on file for you."
+  }else{
+cart = [];
+    return `Your total cost is $${a}, which will be charged to the card ${credNum}.`
+
+  }
+}
--- test/root.js
+++ test/root.js
@@ -4,7 +4,6 @@ const babel = require('babel-core');
 const jsdom = require('jsdom');
 const path = require('path');
 
-
 before(function(done) {
   const babelResult = babel.transformFileSync(path.resolve(__dirname, '..', 'index.js'), {
     presets: ['es2015']
