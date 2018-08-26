f7bcb5b3635b17a3b4679f4a684f081c9089dace
Cedric David <cedric.david@btopenworld.com>
2018-08-26 14:11:20 +0000

    Done.

--- index.js
+++ index.js
@@ -21,55 +21,49 @@ function addToCart(name) {
 }
 
 function viewCart() {
-    var frontMes = "In your cart, you have ";
-    var midMes = "";
-    var endMes;
-    var message;
-    if (cart.length === 0){
-      return ('Your shopping cart is empty.');
-
-    }else{
+  var frontMes = "In your cart, you have ";
+  var midMes = "";
+  var endMes;
+  var message;
+  if (cart.length === 0){
+    return ('Your shopping cart is empty.');
+  }else{
     for (var i = 0; i<cart.length-1; i++){
-
       midMes += `${cart[i].itemName} at $${cart[i].itemPrice}, `
     }
     endMes = `and ${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}.`
+  }
+  if (cart.length === 1){
+    message = frontMes + `${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}.`
+  }else{
+    message = frontMes + midMes + endMes;
+  }
+  return message;
 }
-     if (cart.length === 1){
-      message = frontMes + `${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}.`
-    }else{
-      message = frontMes + midMes + endMes;
-    }
-    return message;
-  }
 
-  function total() {
-    var total=0;
-    for (let i = 0; i<cart.length; i++) {
-      total += cart[i].itemPrice;
-    }
-    return total;
-  }
+function total() {
+var total=0;
+for (let i = 0; i<cart.length; i++) {
+total += cart[i].itemPrice;
+}
+return total;
+}
 
 function removeFromCart(item) {
-  var pos = cart.findIndex(i => i.itemName === item);
-
-  if(pos === -1){
-    return "That item is not in your cart.";
-
-  }else{
-
-    cart.splice(pos, 1);
-    return cart;
-  }
+var pos = cart.findIndex(i => i.itemName === item);
+if(pos === -1){
+return "That item is not in your cart.";
+}else{
+cart.splice(pos, 1);
+return cart;
+}
 }
 function placeOrder(credNum) {
-  var a = total()
+  var newTotal = total()
   if (credNum === undefined) {
     return "Sorry, we don't have a credit card on file for you."
   }else{
-cart = [];
-    return `Your total cost is $${a}, which will be charged to the card ${credNum}.`
-
+    cart = [];
+    return `Your total cost is $${newTotal}, which will be charged to the card ${credNum}.`
   }
 }
