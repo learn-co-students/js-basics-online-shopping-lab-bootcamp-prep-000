1c182099f52d069f1782ea2818ef0367e4bd64bc
vibhu mahendru <vibhumahendru@gmail.com>
2018-08-27 18:17:12 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,37 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+var NewItem = {itemName: item, itemPrice:Math.floor(Math.random() * 101) };
+cart.push(NewItem);
+return item + " has been added to your cart.";
 }
 
 function viewCart() {
-  // write your code here
+
+   if (cart.length === 0){
+     return "Your shopping cart is empty."
+     
+   }
+ if(cart.length === 1) {
+      for (var i = 0;i<cart.length;i++){
+     return  `In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}.`
+      }
+  }
+  var array = [];
+  if (cart.length === 2){
+    for(i = 0; i<cart.length;i++){
+      array.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)}
+      return `In your cart, you have ${array[0]}, and ${array[1]}.`
+    }
+    var array2 = [];
+    if (cart.length>2){
+      for (i =0; i<(cart.length - 1);i++){
+        array2.push(`${cart[i].itemName} at $${cart[i].itemPrice}, `)
+      }
+      var x = cart.length
+      return `In your cart, you have ${array2}and ${cart[x].itemName} at ${cart[x].itemPrice}.`
+      
+    }
 }
 
 function total() {
