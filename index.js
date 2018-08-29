7f79c024fcb8d94c85d3809dcd49b7eb8b458454
Annie Eby <eby.annie@gmail.com>
2018-08-29 18:07:38 +0000

    Done.

--- index.js
+++ index.js
@@ -10,12 +10,53 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var fullItem = { 
+    itemName: item,
+    itemPrice:  Math.floor(Math.random() * 100)
+  }
+  cart.push(fullItem)
+  return `${fullItem.itemName} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  var cartContents = [];
+    for (var i = 0; i < cart.length; i++) 
+    if (i === cart.length-1) {
+    cartContents.push(`and ${fullItem.itemName[i]} at $${fullItem.itemPrice[i]}.`)
+    }
+    else {
+    cartContents.push(`${fullItem.itemName[i]} at $${fullItem.itemPrice[i]}, `)
+    }
+    
+    cartContents.push(`and ${fullItem.itemName[i]} at $${fullItem.itemPrice[i]}.`)
+return `In your cart, you have ${cartContents}`
 }
+
+/*
+function viewCart() {
+  var cartContents = [];
+    for (var i = 0; i < cart.length -1; i++) 
+    cartContents.push(`${fullItem.itemName[i]} at $${fullItem.itemPrice[i]}, `)
+    }
+    i = cart.length - 1
+    cartContents.push(`and ${fullItem.itemName[i]} at $${fullItem.itemPrice[i]}.`)
+return `In your cart, you have ${cartContents}`
+}
+
+function viewCart() {
+  i = i+1;
+  do { console.log
+
+var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+}
+ 
+do {
+  console.log("doo-bee-doo-bee-doo");
+  incrementVariable();
+} while (i < 5);
 
 function total() {
   // write your code here
@@ -28,3 +69,4 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
 }
+*/
