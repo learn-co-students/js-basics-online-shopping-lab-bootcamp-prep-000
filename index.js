a745ab8510a6d2bd0c4bab28226c91a3d0f9d3d1
Reza Lotfi <rezaiscool7@aol.com>
2018-08-31 05:45:34 +0000

    Done.

--- index.js
+++ index.js
@@ -43,7 +43,19 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var noMatch = 0
+  for (var i = 0; i < cart.length; i++){
+    if (cart[i].itemName === item){
+      cart.splice(i, 1)
+      return cart
+    }
+    else {
+      noMatch++
+    }
+  }
+  if (noMatch === cart.length){
+    return ('That item is not in your cart.')
+  }
 }
 
 function placeOrder(cardNumber) {
