4f538c6333bb75083cb240a5dd016d9a49e0553c
Suheyl Turac <skbsturac@live.com>
2018-09-02 05:47:13 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,19 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var itemPrice = Math.floor(Math.random()*100)
+ var newItem = {}
+  newItem[itemName]=itemPrice
+cart.push(newItem)
+return (`${itemName} has been added to your cart.`)
 }
 
 function viewCart() {
-  // write your code here
+ let i = 0
+  var printedArr =[]
+  if(cart.length===0){
+    return(`Your shopping cart is empty.`)
+  }
 }
 
 function total() {
@@ -22,9 +30,23 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  let i = 0
+  var printedArr =[]
+  if(cart.length===0){
+    return(`Your shopping cart is empty.`)
+  }
+  else if(cart.length< 3){
+    for (let i= 0; i<cart.length; i++){
+   printedArr.push(`${Object.keys(cart[i])} at $${Object.values(cart[i])}`)
+  }
+  return (`In your cart, you have ${printedArr.join(" and ")}.`)
+  }else{
+    for (let i= 0; i<cart.length-1; i++){
+      printedArr.push(`${Object.keys(cart[i])} at $${Object.values(cart[i])}`)
+    }
+    return(`In your cart, you have ${printedArr.join(", ")}, and ${Object.keys(cart[cart.length-1])} at $${Object.values(cart[cart.length-1])}.`)
+  }
 }
-
 function placeOrder(cardNumber) {
   // write your code here
 }
