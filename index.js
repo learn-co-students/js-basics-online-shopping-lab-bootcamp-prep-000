0c730f5a46c87c19f59cc81144b2f2288278742f
Hank Liu <hanke.liu@gmail.com>
2018-09-06 23:16:55 +0000

    Done.

--- index.js
+++ index.js
@@ -10,8 +10,13 @@ function setCart(c) {
 }
 
 function addToCart(item) {
+<<<<<<< HEAD
  var newItem = {};  // this object contains the item name and price.
  var itemList = []; // this is the arry to contails all the items-price objects.
+=======
+ var newItem = {};  \\ this object contains the item name and price.
+ var itemList = []; \\ this is the arry to contails all the items-price objects.
+>>>>>>> bb963aafada5bb097e2849afbdedfee0f1bf6c17
  newItem.itemName = item;
  newItem.itemPrice = Math.floor(Math.random()*100)
  itemList.pop(newItem);
@@ -22,7 +27,11 @@ function viewCart() {
   if (itemList.length === 0) {
     return 'Your shopping cart is empty.'
   } else {
+<<<<<<< HEAD
       // need to figure out the output format.
+=======
+
+>>>>>>> bb963aafada5bb097e2849afbdedfee0f1bf6c17
   }
 }
 
