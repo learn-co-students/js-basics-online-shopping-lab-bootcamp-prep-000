a4b1579b290152785068387ea1f9ce8f85533aba
Ryan Travis <rpenchenski@gmail.com>
2018-09-06 17:32:09 +0000

    Done.

--- index.js
+++ index.js
@@ -11,10 +11,24 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+  var price = Math.floor(Math.random() * 100) + 1;
+  var itemy = {};
+  itemy["itemName"] = item;
+  itemy["itemPrice"] = price;
+  cart.push(itemy)
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
   // write your code here
+  var blah = []
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  }
+  for (var i=0; i < cart.length; i++) {
+      blah.push(`${cart[i]["itemName"]} at $${cart[i]["itemPrice"]}`)
+    }
+    return `In your cart, you have ${blah.join(", and ")}.`
 }
 
 function total() {
