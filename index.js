08aeb02fa91f9b905820e52c3c1597f51c0b1d41
Alisa McCormick <muntananuchat@gmail.com>
2018-09-07 18:08:53 +0000

    Done.

--- index.js
+++ index.js
@@ -14,27 +14,26 @@ function addToCart(item) {
  var newItem = {
    itemName: item,
    itemPrice: Math.floor(Math.random() * Math.floor(100))
- } 
+ }
  cart.push(newItem)
- return `${item} has been added to your cart.`
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
   if (cart.length === 0) {
-    return "Your shopping cart is empty."
+    return `Your shopping cart is empty.`
   }
-  var summary = `In your cart, you have`
+  var summary = `In your cart, you have `
   for (var i = 0; i < cart.length; i++) {
-    summary += ` ${cart[i].itemName} at $${cart[i].itemPrice}`
+    summary += `${cart[i].itemName} at $${cart[i].itemPrice}`
     if (i < cart.length - 1) {
-      summary += `,`
+      summary += `, `
     }
     if (i === cart.length - 2) {
-      summary += ` and`
+      summary += `and `
     }
   }
-  
   return `${summary}.`
 }
 
@@ -50,25 +49,23 @@ function total() {
 function removeFromCart(item) {
   // write your code here
   var deleteItem = -1
-  for (var i = 0; i < cart.length; i++) {
-    if(cart[i].itemName === item) {
+  for (var i = 0; i < cart.length; i ++) {
+    if (cart[i].itemName === item) {
       deleteItem = i
     }
-  }
+  }  
   if (deleteItem === -1) {
-    return "That item is not in your cart."
-  }
+    return `That item is not in your cart.`
+  } 
   cart.splice(deleteItem, 1)
   return cart
-}
-
-
-function placeOrder(cardNumber) {
-  // write your code here
-  if (!cardNumber) {
-    return "Sorry, we don't have a credit card on file for you."
   }
-  var message = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
-  cart = []
-  return message
-}
+  
+  function placeOrder(cardNumber) {
+    if (!cardNumber) {
+      return `Sorry, we don't have a credit card on file for you.`
+    }
+    var message = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    cart = []
+    return message
+  }
