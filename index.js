5d36128bf9329552a166fb92971c805a3ebae5ac
Hank Liu <hanke.liu@gmail.com>
2018-09-07 04:03:31 +0000

    Done.

--- index.js
+++ index.js
@@ -10,13 +10,10 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-<<<<<<< HEAD
+
  var newItem = {};  // this object contains the item name and price.
  var itemList = []; // this is the arry to contails all the items-price objects.
-=======
- var newItem = {};  \\ this object contains the item name and price.
- var itemList = []; \\ this is the arry to contails all the items-price objects.
->>>>>>> bb963aafada5bb097e2849afbdedfee0f1bf6c17
+
  newItem.itemName = item;
  newItem.itemPrice = Math.floor(Math.random()*100)
  itemList.push(newItem); // it was pop(newItem)
@@ -26,18 +23,19 @@ function addToCart(item) {
 
 function viewCart() {
   if (itemList.length === 0) {
-    return 'Your shopping cart is empty.'
+    return "Your shopping cart is empty."
   } else {
+    allInYourCart = "In your cart, you have "
 
     for (i=0; i < itemList.length - 1; i++) {
-      allInYourCart = "In your cart, you have "
       allInYourCart = allInYourCart + `${itemList[i].itemName}  at ${itemList[i].itemPrice},`;
-
     }
-      allInYourCart = allInYourCart + `and ${itemList[itemList.(length-1).itemName] at ${itemList[itemList.(length-1)].itemPrice} `
+
   }
-  return allInYourCart
+  return allInYourCart + `and ${itemList[itemList.length-1].itemName] at ${itemList[itemList.length-1)].itemPrice} `
+
 }
+
 
 function total() {
   // write your code here
