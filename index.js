35a1e52f8d4910947c2a855a8d1ee1f08bccd281
Hank Liu <hanke.liu@gmail.com>
2018-09-08 02:34:00 +0000

    Done.

--- index.js
+++ index.js
@@ -9,33 +9,38 @@ function setCart(c) {
   return cart;
 }
 
- var newItem = {};  // this object contains the item name and price.
- var itemList = []; // this is the arry to contails all the items-price objects.
 
-function addToCart(item, price) {
+
+ ///////////////////////////create an Objects array.
+
+function addToCart(item) {
+  var newItem = {};  // this object contains the item name and price.
+  var itemList = []; // this is the arry to contails all the items-price objects.
+
   var newItem = {
-    newItem: item,
-    itemPrice: Math.floor(Math.random()*100),
+    itemName: item,
+    itemPrice: Math.floor(Math.random()*100)
   }
   //newItem.itemName = item;
  //newItem.itemPrice = Math.floor(Math.random()*100)
  itemList.push(newItem); // it was pop(newItem)
- return newItem.itemName;
+ return `${newItem.itemName} has been added to your cart.`
 }
 
 
 function viewCart() {
-  allInYourCart = "In your cart, you have ";
+  allItemsInString = "In your cart, you have ";
   if (itemList.length === 0) {
     return "Your shopping cart is empty."
   } else {
 
-    for (i=0; i < itemList.length; i++) {
-      allInYourCart = allInYourCart.concat(`${itemList[i].itemName}  at $${itemList[i].itemPrice},`);
+    for (var i=0; i < itemList.length-1; i++) {
+      allItemsInString.concat(`${itemList[i].itemName}  at $${itemList[i].itemPrice},`);
     }
 
   }
-  return allInYourCart;
+  return allItemsInString.concat(` and ${itemList[itemList.length-1].itemName}  at $${itemList[itemList-1].itemPrice}.`) ;
+
   //allInYourCart.concat( `and ${itemList[itemList.length-1].itemName] at '$'${itemList[itemList.length-1)].itemPrice}) `
 
 }
