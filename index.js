1f71ca813880269b835267f8591f358697c0e73d
vibhu mahendru <vibhumahendru@gmail.com>
2018-09-11 21:10:55 +0000

    Done.

--- index.js
+++ index.js
@@ -9,71 +9,35 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
-var NewItem = {itemName: item, itemPrice:Math.floor(Math.random() * 101) };
-cart.push(NewItem);
-return item + " has been added to your cart.";
-}
-
-function viewCart() {
-
-   if (cart.length === 0){
-     return "Your shopping cart is empty."
-     
-   }
-if(cart.length === 1) {
-      for (var i = 0;i<cart.length;i++){
-     return  `In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}.`
-      }
-  }
-  var array = [];
-  if (cart.length === 2){
-    for(i = 0; i<cart.length;i++){
-      array.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)}
-      return `In your cart, you have ${array[0]}, and ${array[1]}.`
-    }
-    var array2 = [];
-    if (cart.length>2){
-      for (i =0; i<(cart.length - 1);i++){
-        array2.push(` ${cart[i].itemName} at $${cart[i].itemPrice}`)
-      }
-      var x = (cart.length - 1)
-      return `In your cart, you have${array2}, and ${cart[x].itemName} at $${cart[x].itemPrice}.`
-      
-    }
-}
-
-
-function total() {
-  var p = 0
-  for(var i = 0;i<cart.length;i++){
-   p += parseInt(cart[i].itemPrice);
-  }
-  return p 
-}
-
-function removeFromCart(item) {
-  for (var i = 0;i<cart.length;i++){
-    if (cart[i].itemName === item){
-    var newCart = cart.splice(i,1)
-      return cart;
-    }
-   
-    }
-    return "That item is not in your cart."
-  }
-
-
-
-function placeOrder(cardNumber) {
- if(cardNumber){
-   var k = total()
-   cart = []
-   return `Your total cost is $${k}, which will be charged to the card ${cardNumber}.`
+function addToCart(item){
+ 
+   var price = Math.floor(Math.random() * 101); 
+  var itemObj = {itemName:item,itemPrice:price}
   
- }
- else{ return "Sorry, we don't have a credit card on file for you."
-   
- }
-
+  cart.push(itemObj)
+  return `${item} has been added to your cart.`
 }
+
+function viewCart(){
+  if(cart.length === 0){
+    return "Your shopping cart is empty."
+  }
+  
+  if (cart.length === 1){
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  }
+  
+  if (cart.length>1){
+   var x = []
+    for (var i = 0; i<(cart.length - 1);i++){
+    
+    x.push(` ${cart[i].itemName} at $${cart[i].itemPrice}`)
+  }
+  return `In your cart, you have${x}, and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`
+  }
+}
+
+
+
+
+
