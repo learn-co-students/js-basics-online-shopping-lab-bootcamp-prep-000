28bdbdc0e8df9cb0874ede10abcabff84d878b27
Jakob Baumgartner <jakob.baumgartner@hotmail.com>
2018-09-11 21:40:48 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var paket = new Object();
+
+ paket[itemName] = item;
+ paket[itemPrice] = Math.floor(Math.random());
+
+ cart.push(paket);
+
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
