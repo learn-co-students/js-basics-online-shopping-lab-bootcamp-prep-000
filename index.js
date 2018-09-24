79287b6e5bcfe87c8fee69b0a04c9abdcd4cb77b
Connor Finnegan <connorfinnegan16@gmail.com>
2018-09-24 21:05:51 +0000

    Done.

--- index.js
+++ index.js
@@ -11,18 +11,54 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var newItem = Object({
+   itemName: item,
+   itemPrice: Math.floor(Math.random() * 100)
+ });
+ 
+ cart.unshift(newItem);
+ 
+ return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
   // write your code here
+  var arr = [];
+  
+  if (cart.length < 1) {
+    return "Your shopping cart is empty.";
+  }  else {
+  for(var i = 0; i < cart.length; i++) {
+    arr.push(` ${cart[i].itemName} at $${cart[i].itemPrice}` );
+  }
+ }
+ 
+ if (arr.length < 2) {
+   return `In your cart, you have${arr}.`;
+ }
+ else {
+   return `In your cart, you have${arr.slice(0, arr.length - 1)}, and${arr[arr.length - 1]}.`;
+ }
 }
 
 function total() {
   // write your code here
+  var sum = 0;
+  
+  for (var i = 0; i < cart.length; i++) {
+    sum = sum + cart[i].itemPrice;
+  }
+  
+  return sum;
 }
 
 function removeFromCart(item) {
   // write your code here
+  for (i = 0; i < cart.length; i++) {
+    if (cart[i].itemName === item) {
+      
+    }
+  }
 }
 
 function placeOrder(cardNumber) {
