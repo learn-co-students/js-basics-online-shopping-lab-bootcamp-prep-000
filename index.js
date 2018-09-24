8e721b16c2d3fe572f593e8a8e90065eaa7a84a3
Matthew Sherman <msherman0801@gmail.com>
2018-09-24 22:30:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var cart = [];
-
+var object = {
+  itemName: name,
+  itemPrice: price
+}
 function getCart() {
  return cart;
 }
@@ -10,21 +13,56 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var newItem = {
+    itemName: item,
+    itemPrice: test,
+  }
+  cart.push(newItem)
+  return `${newItem.itemName} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  for (var i = 0; i < cart.length; i++) {
+    if (cart.length === 1) {
+      return `In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}.`
+    } else if (cart.length === 2) {
+      return `In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}, and ${cart[i + 1].itemName} at $${cart[i + 1].itemPrice}.`
+    } else if (cart.length === 3) {
+      return `In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}, ${cart[i + 1].itemName} at $${cart[i + 1].itemPrice}, and ${cart[i + 2].itemName} at $${cart[i + 2].itemPrice}.`
+    } else if (cart.length === 0) {
+      return 'Your shopping cart is empty.'
+    }
+  }
+  if (cart.length === 0) {
+    return 'Your shopping cart is empty.'
+  }
 }
 
 function total() {
-  // write your code here
+  var totalValue = 0;
+  for (var i = 0; i < cart.length; i++) {
+    totalValue += cart[i].itemPrice;
+  }
+   return totalValue
 }
 
+
 function removeFromCart(item) {
-  // write your code here
+  for (var i = 0; i<cart.length; i++) {
+    if (cart[i].name === item) {
+      cart.splice(cart[i],1);
+    } else if (cart[i].name !== item) {
+      return 'That item is not in your cart.'
+    }
+  }
+  return cart;
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(cardNumber === undefined) {
+    return `Sorry, we don't have a credit card on file for you.`
+  } else {
+    cart = [];
+    return `Your total cost is $${test}, which will be charged to the card ${cardNumber}.`
+  }
 }
