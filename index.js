c369f2889530a0f6754644ea11b24602ab41adb7
Hamilton Hudson <ehamiltonhudson@gmail.com>
2018-10-03 17:19:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
-var cart = [];
-
-function getCart() {
- return cart;
-}
-
-function setCart(c) {
-  cart = c;
-  return cart;
-}
-
 function addToCart(item) {
- // write your code here
-}
+  var price = Math.floor((Math.random() * 100) + 1 );
+    cart.push(Object.assign({}, { itemName : `${item}`, itemPrice : price } ));
+    return `${item}` + " has been added to your cart."
+  }
 
-function viewCart() {
-  // write your code here
-}
 
-function total() {
-  // write your code here
+function viewCart(itemName, itemPrice) {
+  for (var i = 0; i < cart.length; i++) {
+    if (i < 1) {
+      return("Your shopping cart is empty.")
+  } else {
+    while (i > 0) {
+      return(`In your cart, you have ` + `${itemName[i]}` + ` at ` + `${itemPrice[i]}` + `.`)
+    i++;
+  }
+  }
 }
-
-function removeFromCart(item) {
-  // write your code here
-}
-
-function placeOrder(cardNumber) {
-  // write your code here
 }
