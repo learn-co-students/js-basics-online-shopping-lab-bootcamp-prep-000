c5863e165de0ce16dc304bcebeeb8d9a7e1a40d7
Hamilton Hudson <ehamiltonhudson@gmail.com>
2018-10-04 22:21:14 +0000

    Done.

--- index.js
+++ index.js
@@ -41,7 +41,15 @@ function total() {
   }
 
 function removeFromCart(item) {
-  // write your code here
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].itemName === item) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+    if (cart[item] != item) {
+    return("That item is not in your cart.")
+  }
 }
 
 function placeOrder(cardNumber) {
