8edf71c0debc31862fee6d0969e05cb2595be137
Hamilton Hudson <ehamiltonhudson@gmail.com>
2018-10-05 00:48:47 +0000

    Done.

--- index.js
+++ index.js
@@ -53,5 +53,12 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  var cartTotal = total();
+  if (!cardNumber) {
+    return("Sorry, we don't have a credit card on file for you.");
+  } else if (cardNumber > 0) {
+    cart = [];
+    return("Your total cost is $" + `${cartTotal}` + ", which will be charged to the card " + `${cardNumber}.`);
+    return cart
+  }
 }
