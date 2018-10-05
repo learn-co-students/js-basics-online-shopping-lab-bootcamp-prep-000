537a78bc1e460cfe5cec7b5791555f402abec230
severus889700 <severus889700@gmail.com>
2018-10-05 19:18:47 +0000

    Done.

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ function addToCart(name){
   var dict={"itemName": name, "itemPrice": Math.random()*100};
   cart.push(dict);
   return (name+" has been added to your cart.")
-  return cart;
 }
 
 function total() {
@@ -47,18 +46,20 @@ function placeOrder(cardNumber) {
 }
 
 function viewCart() {
-  var myString = `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}`;
   if(cart.length===0){
-    return "Your shopping cart is empty."}
-  else if(cart.length==1){
-    return myString+`.`}
-  else if(cart.length==2){
-    return cart;
-    
-    // return myString+`, and ${cart[1].itemName} at $${cart[1].itemPrice}.
+    return "Your shopping cart is empty."
+  }else if(cart.length==1){
+    var myString = `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}`;
+    return myString+`.`;
+  }else if(cart.length==2){
+    var myString = `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}`;
+    return myString+`, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
+  }else{
+    var myString = `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}`;
+    for(var i = 1; i<cart.length - 1; i++){
+      myString = myString + `, ${cart[i].itemName} at $${cart[i].itemPrice}`;
+      //return breaking out
     }
-  else{
-    for(var i = 1; i<cart.length-1; i++){
-      myString = myString + `, ${cart[i].itemName} at $${cart[i].itemPrice}`
-      return myString;
-      }}  return myString+", and " + cart[-1].itemName + " at $" +cart[-1].itemPrice+"."}
+    return myString + ", and " + cart[cart.length-1].itemName + " at $" +cart[cart.length-1].itemPrice+"."
+  }  
+}
