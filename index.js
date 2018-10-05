a368846152fd966ab730c6f7c8c53cee56f7cc2b
naizal Edy <pritiadhikari@gmail.com>
2018-10-05 00:11:37 +0000

    Done.

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ var i;
   {
     
     output = `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, ${cart[1].itemName} at $${cart[1].itemPrice},`;
+    
     for (i = 2; i < cart.length; i++) 
     {
       if (i === cart.length - 1)
@@ -69,8 +70,7 @@ var i;
 
 
 
-    
- function total() 
+function total() 
 {
     
  
@@ -93,58 +93,43 @@ var i;
 function removeFromCart(item) {
 
   
- 
-  for (var i = 0; i < cart.length; i++){
-    if (cart[i].hasOwnProperty(item)){
-      
-       cart.splice(i, 1);
-       return cart;
-       
-     
-    }
-  }
-  
-   
-  return `That item is not in your cart.`;
-   
-}
-
-
-function placeOrder(cardNumber) {
-
-  if (cardNumber)
+  if (cart.length === 0) 
   {
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
-    for (var i = cart.length; i>= 0; i--) 
+    return `That item is not in your cart.`
+  } 
+  else 
+  {
+    for (var i = 0; i < cart.length; i++) 
     {
-      cart.pop();
-      
-        return cart;
-     }
-  }
-  return `Sorry, we don't have a credit card on file for you.`
-
-
-  for (i = 0; i < cart.length; i++){
-    if (cart[i].hasOwnProperty(item)){
-      cart.splice([i], 1);
-      return cart;
+      if (cart[i].itemName === item) 
+      {
+        cart.splice(i,1);
+      } 
+      else if (i === cart.length - 1) 
+      {
+        return `That item is not in your cart.`;
+      }
     }
   }
-  console.log(`That item is not in your cart.`);
-  return cart;
+   
 }
+
+
 
 function placeOrder(cardNumber) {
 
-  if (cardNumber){
-    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
-    for (var i = cart.length; i>= 0; i--) {
-      cart.pop();
+  var message;
+  
+  if (cardNumber) 
+  {
+    message = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+  } 
+  else
+  {
+    message = "Sorry, we don\'t have a credit card on file for you.";
   }
-  }
-  console.log(`Sorry, we don't have a credit card on file for you.`);
-  return cart;
+  cart = [];
+  return message;
 
 }
 
