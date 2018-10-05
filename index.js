c356ba1e8857e97e3b784f92520805a08fafe005
naizal Edy <pritiadhikari@gmail.com>
2018-10-05 00:01:06 +0000

    Done.

--- index.js
+++ index.js
@@ -31,24 +31,39 @@ cart.push(itemAndPrice);
 
 function viewCart() {
 
-if (cart.length === 0)
-{
-  return `Your shopping cart is empty.`;
-  
-} 
-  
-else 
-{
-    var items = [];
-    for (var i = 0; i < cart.length; i++) 
+var i;
+  var output;
+  if (cart.length === 0) 
+  {
+    return `Your shopping cart is empty.`;
+  } 
+  else if (cart.length === 1) 
+  {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
+    
+  } 
+  else if (cart.length === 2) 
+  {
+    
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
+  } 
+  else 
+  {
+    
+    output = `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, ${cart[1].itemName} at $${cart[1].itemPrice},`;
+    for (i = 2; i < cart.length; i++) 
     {
-      for (var item in cart[i]) {
-        items.push(item + " at $" + cart[i][item]);
+      if (i === cart.length - 1)
+      {
+        output = output.concat(` and ${cart[i].itemName} at $${cart[i].itemPrice}.`);
+      } 
+      else
+      {
+        output = output.concat(` ${cart[i].itemName} at $${cart[i].itemPrice},`);
       }
-}
-    return `In your cart, you have `+ items.join(", ") + `.`;
     }
-
+    return output;
+  }
 
 }
 
