68d31e86c1f5954ef1909925daecc70e59e0ef4e
t bryant <trbryant1981@gmail.com>
2018-10-07 18:15:34 +0000

    Done.

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ function setCart(c) {
 function addToCart(item) {
  var price = Math.floor(Math.random()*100)
  cart.push({itemName:item , itemPrice:price})
- console.log(`${item} has been added to your cart.`)
+ return`${item} has been added to your cart.`
  return cart
 }
 
 function viewCart() {
   if(cart.length === 0) {
-    console.log ("Your shopping cart is empty.")
+    return"Your shopping cart is empty."
   }
   
   var mycart = [];
@@ -28,13 +28,12 @@ function viewCart() {
     }
   
       if (cart.length === 1 ) {
-          console.log( `In your cart, you have ${mycart[0]}.`)
+          return `In your cart, you have ${mycart[0]}.`
        }  else if (cart.length === 2) {
-            console.log( `In your cart, you have ${mycart[0]}, and
-          ${mycart[1]}.`);     
-       } else if (cart.length > 2) {
+           return `In your cart, you have ${mycart[0]}, and ${mycart[1]}.`
+       }  else if (cart.length > 2) {
             var addeditems = mycart.slice(0, mycart.length - 1)
-            console.log(`In your cart, you have ${addeditems.join(", ")}  and ${mycart.pop()}.`)     
+            return `In your cart, you have ${addeditems.join(", ")}, and ${mycart.pop()}.`
        }
        return cart
 }
@@ -49,21 +48,22 @@ function total() {
 
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
-    if (cart[i].itemName !== item) {
-      console.log ("That item is not in your cart." )
-    } else if (cart[i].itemName === item) {
+     if (cart[i].itemName === item) {
       cart.splice(i, 1)
       return cart
     }
   }
+  return "That item is not in your cart."
 }
 
 function placeOrder(cardNumber) {
   if(cardNumber) {
-    console.log (`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    return`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    }
     cart.length = 0
-    } else {
-      console.log( "Sorry, we don\'t have a credit card on file for you.");
+  
+  if(!cardNumber)
+    return "Sorry, we don\'t have a credit card on file for you."
   }
   return cart
 }
