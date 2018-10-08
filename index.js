bd83154f99307f9fa2823af7b4b487f3c98e1135
Brian <briandsalemi@gmail.com>
2018-10-08 16:35:10 +0000

    Done.

--- index.js
+++ index.js
@@ -15,11 +15,17 @@ function addToCart(name) {
   return(`${name} has been added to your cart.`);
 }
 
- 
 
 function viewCart() {
-  // write your code here
-}
+  if (cart.length === 0) {
+    return ("Your shopping cart is empty."); 
+  } else if (cart.length === 1) {
+    return (`In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`);
+  } else if (cart.length === 2) {
+    return(`In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`);
+  }}
+  
+
 
 function total() {
   // write your code here
