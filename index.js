464f92381cba8360c1cc7713f067b66961c852a4
Brian <briandsalemi@gmail.com>
2018-10-08 16:39:12 +0000

    Done.

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ function viewCart() {
     return (`In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`);
   } else if (cart.length === 2) {
     return(`In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`);
+  } else if (cart.length >= 3) {
+    return(`In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, ${cart[1].itemName} at $${cart[1].itemPrice}, and ${cart[2].itemName} at $${cart[2].itemPrice}.`);
   }}
   
 
