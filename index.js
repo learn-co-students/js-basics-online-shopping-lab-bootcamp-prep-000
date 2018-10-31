9c89bbbd92c3ee66734b08b0bc5a48890d55d7bd
Sarah Anderson <saranderson13@gmail.com>
2018-10-31 20:17:46 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,66 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var newItem = {
+    itemName: item,
+    itemPrice: Math.floor(Math.random() * Math.floor(100))
+  };
+  cart.push(newItem);
+  return `${newItem.itemName} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  let cartDescription = [];
+  let lastIndex = cart.length - 1;
+  let finalItem;
+  if(cart.length === 0) {
+    return "Your shopping cart is empty.";
+  } else if(cart.length === 1){
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
+  } else {
+    for(let i = 0; i < (cart.length - 1); i++) {
+      cartDescription.push(` ${cart[i].itemName} at $${cart[i].itemPrice}`);
+    }
+    for(let i = lastIndex; i < cart.length; i++) {
+      finalItem = `, and ${cart[i].itemName} at $${cart[i].itemPrice}`;
+    }
+    return `In your cart, you have${cartDescription}${finalItem}.`;
+  }
 }
 
 function total() {
-  // write your code here
+  let total = 0;
+  for(let i = 0; i < cart.length; i++) {
+    total = total + cart[i].itemPrice;
+  }
+  return total;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  let itemFound = false;
+  let deleteItemIndex;
+  for(let i = 0; i < cart.length; i++) {
+    if(item === cart[i].itemName) {
+      itemFound = true;
+      deleteItemIndex = i;
+      
+      if(itemFound === true) {
+        cart.splice(deleteItemIndex, 1);
+        return cart;
+      }
+    }
+  }
+  if (itemFound === false) {
+    return "That item is not in your cart.";
+  }
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined) {
+    return "Sorry, we don't have a credit card on file for you.";
+  } else {
+    let finishPurchase = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    cart = [];
+    return finishPurchase;
+  }
 }
