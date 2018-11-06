ffc524089eca98a1272024504925a853597d3537
Cheng Vue <cvu3@ucsc.edu>
2018-11-06 20:42:20 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,74 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var max = 100;
+  var min = 1;
+  var price = Math.floor(Math.random() * (max - min + 1)) + min;
+  var itemObj = {
+    itemName : item,
+    itemPrice : price
+  };
+
+  cart.push(itemObj);
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  var itemStr = '';
+
+  if (cart.length === 0) {
+    return 'Your shopping cart is empty.';
+  } else {
+    for (var i = 0; i < cart.length; i++) {
+      var itemObj = cart[i];
+      var itemName = itemObj.itemName;
+      var itemPrice = itemObj.itemPrice;
+
+      if (cart.length === 1) {
+        itemStr += `${itemName} at $${itemPrice}.`;
+      } else if (i === cart.length - 1) {
+        itemStr += `and ${itemName} at $${itemPrice}.`;
+      } else {
+        itemStr += `${itemName} at $${itemPrice}, `;
+      }
+    }
+  }
+
+  return `In your cart, you have ${itemStr}`;
 }
 
 function total() {
-  // write your code here
+  var totalPrice = null;
+
+  for (var i = 0; i < cart.length; i++) {
+    var itemObj = cart[i];
+
+    totalPrice += itemObj.itemPrice;
+  }
+
+  return totalPrice;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (var i = 0; i < cart.length; i++) {
+    var itemObj = cart[i];
+    var itemInCart = itemObj.itemName;
+
+    if (itemInCart === item) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+
+  return 'That item is not in your cart.';
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined) {
+    return `Sorry, we don\'t have a credit card on file for you.`;
+  } else {
+    var totalCost = total();
+    cart = [];
+    return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`;
+  }
 }
