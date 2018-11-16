8bf41e608471d8c54a26cce960db497c72c70491
Angie Quin <aquinones100@gmail.com>
2018-11-16 12:27:33 +0000

    Done.

--- index.js
+++ index.js
@@ -10,14 +10,46 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var price = Math.floor(100*(Math.random()));
+ var item_object = {[item]: price};
+ cart.push(item_object);
+
+ console.log (`${item} has been added to your cart.`);
 }
+
+addToCart("bananas")
+addToCart("apples")
+addToCart("carrots")
+addToCart("daikon")
 
 function viewCart() {
-  // write your code here
+  var insideCart = [];
+  if (cart.length===0) {
+    console.log("Your shopping cart is empty")
+  } else if(cart.length===1) {
+  var insideCartOne = `In your cart, you have ${Object.key(cart[0])}
+  at $${(cart[0] [Object.keys[0])]}`
+  console.log(insideCartOne)
+} else {
+  for (i=0; i < cart.length-1; i++) {
+    console.log(cart[i]);
+    insideCart.push(`${Object.keys(cart[i])} at $${cart[i] [Object.keys(cart[i])]}`);
+  }
 }
 
+viewCart(cart);
+
+
+
 function total() {
+  var eachItemPrice = [];
+  var totalPrice = 0
+
+  for(var i = 0; i < cart.length; i++) {
+    var itemPrice = cart[i] [Object.keys(cart[i])]
+    eachItemPrice.push(itemPrice)
+
+  }
   // write your code here
 }
 
