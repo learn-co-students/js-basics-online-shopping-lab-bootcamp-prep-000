64394af77a804d9227516092f39a9cfb2c8f985f
Jonathan Koss <jonathan_koss@yahoo.com>
2018-11-17 14:57:08 +0000

    Done.

--- index.js
+++ index.js
@@ -52,11 +52,57 @@ function total() {
 }
 
 
+/*function removeFromCart(item) {
+  for (var i = 0; i < cart.length; i++) {
+    if (!!cart[i]) {
+      cart.splice(i, 1);
+    }
+  }
+  return cart;
+}*/
 
 
-function removeFromCart(item) {
-  // write your code here
+ function removeFromCart(item) {
+  var itemInCart = false;
+  for(var i = 0; i < cart.length; i++){
+    if (item === getCart()[i].itemName) {
+      
+      cart.splice(i, 1);
+    }
+  } 
+  if (!itemInCart) {
+    return "That item is not in your cart.";
+  }
+  return cart;
 }
+
+/*
+describe("removeFromCart()", function() {
+  it("removes the specified item from the cart", function() {
+    addToCart("vanilla");
+    addToCart("watermelon");
+    addToCart("yams");
+
+    removeFromCart("watermelon");
+
+    const firstItemName = getCart()[0].itemName;
+    const secondItemName = getCart()[1].itemName;
+
+    expect(firstItemName).toEqual("vanilla");
+    expect(secondItemName).toEqual("yams");
+
+    removeFromCart("yams");
+
+    expect(getCart().length).toEqual(1);
+  });
+
+  it("alerts you if you're trying to remove an item that isn't in your cart", function() {
+    // Repeat item name from previous test to prevent hard-coding.
+    expect(removeFromCart("yams")).toEqual("That item is not in your cart.");
+  });
+});
+*/
+
 
 function placeOrder(cardNumber) {
   // write your code here
