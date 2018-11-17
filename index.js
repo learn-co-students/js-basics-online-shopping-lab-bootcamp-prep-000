ad42f6b85f14acde3881b62cb80eb85d0ea9953a
dekc89 <dekc89@gmail.com>
2018-11-17 12:43:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var cart = [];
 
 function getCart() {
- return cart;
+  return cart;
 }
 
 function setCart(c) {
@@ -10,21 +10,65 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var obj = {
+    itemName: item,
+    itemPrice: Math.floor(Math.random() * 100)
+  };
+  cart.push(obj);
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  var finalString = 'In your cart, you have ';
+  if (cart.length === 0) {
+    return 'Your shopping cart is empty.';
+  } else {
+    for (let i = 0; i < cart.length - 1; i++) {
+      finalString += `${cart[i].itemName} at $${cart[i].itemPrice}, `;
+    }
+      // finalString += `${(cart.length-1)? 'and ' : ''}${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}.`;
+      if (cart.length - 1 !== 0) {
+        finalString += 'and ';
+      }
+      finalString += `${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}.`;
+  }
+  return finalString;
 }
 
+
 function total() {
-  // write your code here
+  var sum = 0;
+  for (let i = 0; i < cart.length; i++){
+    sum += cart[i]['itemPrice'];
+  }
+  return sum;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  // for (let i = 0; i < cart.length; i++) {
+  //   if (cart[i].itemName === item) {
+  //       cart.splice(i, 1);
+  //   }
+  // }
+  // if (cart.length) {
+  //   return cart;
+  // }
+  // return "That item is not in your cart.";
+  const index = cart.findIndex(x => x.itemName === item);
+    if (index === - 1) {
+    return "That item is not in your cart.";
+  }
+  const temp = cart.splice(index, 1);
+
+  return cart;
 }
 
 function placeOrder(cardNumber) {
+  if (!cardNumber) {
+    return "Sorry, we don't have a credit card on file for you.";
+  }
+  var text = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+  cart.length = 0;
+  return text;
   // write your code here
 }
