90d8caf2b49da4ffc2bdadc784296e7f9ed384a6
FX Wood <fxwood2@gmail.com>
2018-11-21 02:36:24 +0000

    Done.

--- index.js
+++ index.js
@@ -9,22 +9,62 @@ function setCart(c) {
   return cart;
 }
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 function addToCart(item) {
- // write your code here
+  cart.push({"itemName": item, "itemPrice": getRandomInt(100)});
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  let returnString = 'In your cart, you have ';
+  switch(cart.length) {
+    case 0:
+      return "Your shopping cart is empty.";
+      break;
+    case 1:
+      for (let i = 0; i < cart.length; i++) {
+        returnString += `${cart[i].itemName} at $${cart[i].itemPrice}.`;
+      }
+      return returnString;
+      break;
+    default:
+      for (let i = 0; i < cart.length -1; i++) {
+        returnString += `${cart[i].itemName} at $${cart[i].itemPrice}, `;
+      }
+      returnString += `and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`
+      return returnString;
+  }
 }
-
 function total() {
-  // write your code here
+  let price = 0;
+  for (let i = cart.length -1; i >= 0; i--)  {
+    price += cart[i].itemPrice
+  }
+  return price
 }
 
 function removeFromCart(item) {
-  // write your code here
+  let inCart = false
+  for (let i = 0; i < cart.length; i++) {
+    if (item === cart[i].itemName)  {
+      cart.splice(i, 1)
+      return cart
+    } else inCart = false;
+  }
+  if (inCart == false)  {
+    return "That item is not in your cart.";
+  }
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber) {
+    let priceToCharge = total();
+    cart = []
+    return `Your total cost is $${priceToCharge}, which will be charged to the card ${cardNumber}.`
+  } else {
+    return "Sorry, we don\'t have a credit card on file for you."
+  }
 }
