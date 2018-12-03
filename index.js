26df4481f427d55ede8e34f2303a2f0f5863c7f7
Elaina <elainaseki@gmail.com>
2018-12-03 01:39:10 +0000

    Done.

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ function viewCart() {
      }
       else if(cart.length>2){
           var last = array.pop();
-        var middle = array.join(',');
-       output += `${middle} and ${last}.`;
+        var middle = array.join(', ');
+       output += `${middle}, and ${last}.`;
      }
      return`${output}`;
    }
@@ -46,30 +46,28 @@ function total() {
   // write your code here
    var  all = 0;
   for(var i=0; i<cart.length; i++){
-    all += `${(cart[i].itemPrice)}`;
+    all += cart[i].itemPrice;
   }
    return all;
 }
 
 function removeFromCart(item) {
   // write your code here
-   if(cart.hasOwnProperty(item)){
-       for(var i = 0; i<cart.length; i++){
-    cart = cart.slice(0,i).concat(cart.slice(i+1));
-    return cart;
-       }
-   }
-      else{
+  for(var i = 0; i<cart.length; i++){
+   if(cart[i].itemName === item){
+       cart.splice(i,1);
+      return cart;
+     } 
+    }
     return`That item is not in your cart.`;
-     }
-   }
-
+}
+   
 var cardNumber = Math.floor(Math.random()*100000000)+1;
 function placeOrder(cardNumber) {
   // write your code here
   if(cardNumber){
  cart.length =0;
-   return`Your total cost is $${total()}; which will be charged to the card ${cardNumber}.`;
+   return`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   }
   
   else{
