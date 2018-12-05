4e09796f2074f51044b3f4f088ad98c55a6b11dd
Jonathan Koss <jonathan_koss@yahoo.com>
2018-12-05 03:24:41 +0000

    Done.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function setCart(c) {
 function addToCart(item) {
  cart.push({
    itemName: item,
-   itemPrice: Math.floor(Math.random() * 100) + 1
+   itemPrice: Math.floor(Math.random() * 100) + 1 
  });
  return `${item} has been added to your cart.`;
 }
@@ -20,93 +20,72 @@ function addToCart(item) {
 
 
 function viewCart() {
-  if(cart.length === 0){
-    return`Your shopping cart is empty.`;
+   var output = "In your cart, you have ";
+   
+   if (cart.length === 0) {
+    return "Your shopping cart is empty.";
   }
-  
-  else{
-     var array = [];
-     for(var i = 0; i < cart.length; i++){
- array.push(`${cart[i].itemName} at $${cart[i].itemPrice}`);
+  else {
+    var array = [];
+    for(var i = 0; i < cart.length; i++) {
+      array.push(`${getCart()[i].itemName} at $${getCart()[i].itemPrice}`);
      }
-     var output = `In your cart, you have `;
-     if(cart.length === 1){
-       output += `${array[0]}.`;
+     if(cart.length === 1) {
+       output += `${array}.`;
      }
-     else if(cart.length === 2){
+     else if(cart.length === 2) {
        output += `${array[0]}, and ${array[1]}.`;
      }
-      else if(cart.length>2){
-          var last = array.pop();
-        var middle = array.join(', ');
+     else if(cart.length > 2) {
+       var last = array.pop();
+       var middle = array.join(', ');
        output += `${middle}, and ${last}.`;
      }
-     return`${output}`;
-   }
+    }
+    return `${output}`;
 }
+
+/*
+  it("correctly prints a one-item cart", function() {
+    addToCart("lemons");
+    expect(viewCart()).toEqual(`In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`);
+  });
+
+  it("correctly prints a two-item cart", function() {
+    addToCart("mango");
+    addToCart("nuts");
+
+    expect(viewCart()).toEqual(
+      `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}, and ${getCart()[1].itemName} at $${getCart()[1].itemPrice}.`
+    );
+  });
+
+  it("correctly prints a three-or-more-item cart", function() {
+    addToCart("orange");
+    addToCart("pear");
+    addToCart("quince");
+
+    
+
+    expect(viewCart()).toEqual(
+      `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}, ${getCart()[1].itemName} at $${getCart()[1].itemPrice}, and ${getCart()[2].itemName} at $${getCart()[2].itemPrice}.`
+    );
+  });
+});
+*/
 
 
 function total() {
-  var sum = 0;
-  for(let i = 0; i < cart.length; i++) {
-    sum += getCart()[i].itemPrice;
-  }
-  return sum;
+  
 }
 
 
  function removeFromCart(item) {
   var itemNotInCart = true;
-  for(var i = 0; i < cart.length; i++) {
-    if(item === getCart()[i].itemName) {
-      cart.splice(i, 1);
-    }
-  }
-  if(itemNotInCart) {
-    return "That item is not in your cart.";
-  }
-  else{
-    cart.length = 0;
-  }
+  
 }
 
 
 function placeOrder(cardNumber) {
-  const cartTotal = total();
   
-  if(arguments.length === 0) {
-    return "Sorry, we don't have a credit card on file for you.";
-  }
-  else {
-    cart.length = 0; 
-    return `Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`;
-  }
 }
-
-/*
-describe("placeOrder()", function() {
-  it("doesn't place the order if a credit card number is not provided", function() {
-    
-    expect(placeOrder()).toEqual("Sorry, we don't have a credit card on file for you.");
-  });
-
-  it("places an order when a credit card number is provided", function() {
-    addToCart("zucchini");
-
-    const cartTotal = total();
-    const cardNumber = Math.floor(Math.random() * 100000000);
-
-    
-
-    expect(placeOrder(cardNumber)).toEqual(`Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`);
-  });
-
-  it("empties the cart", function() {
-    addToCart("apples");
-
-    placeOrder(12345678);
-
-    expect(getCart()).toEqual([]);
-  });
-});
-*/
