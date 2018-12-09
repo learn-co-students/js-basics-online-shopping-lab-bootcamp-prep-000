85076e05bf058d32562825c50f6eddcd39a8b731
Michael Moravetz <moravemv@gmail.com>
2018-12-09 17:12:45 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Online Shopping
+go slow and be # Online Shopping
 
 ## Objectives
 + Model a shopping cart full of items as an array of objects in JavaScript.
--- index.js
+++ index.js
@@ -10,13 +10,33 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+    var newItem = new Object( { itemName: (item) ,  itemPrice: (Math.floor(Math.random() * 100) + 1 ) } );
+    cart.push(newItem);
+    return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
-}
-
+  if (cart.length === 0 ) {
+      return `Your shopping cart is empty.`;
+  }
+  if (cart.length === 1) {
+    return`In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
+  }
+  
+  if (cart.length === 2) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
+    
+  } else {
+    var newItem = []
+    for (let i = 0; cart.length >0; i++) {
+    newItem.push( `${ cart[i].itemName } at $${ cart[i].itemPrice } and ${ cart[i = cart.length-1].itemName} at $${cart[i = cart.length-1].itemPrice}.`)
+    }
+    return `In your cart, you have ${ newItem.join('') }`;
+  }
+}    
+    
+  
+  
 function total() {
   // write your code here
 }
