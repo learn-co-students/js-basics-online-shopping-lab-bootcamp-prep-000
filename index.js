6d1f4d06e0675ab10b8ab522f0094062cd13ed90
artificial-engine-5731 <icefire587@gmail.com>
2018-12-11 06:58:26 +0000

    Done.

--- index.js
+++ index.js
@@ -10,15 +10,36 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  cart.push({'itemName':item, 'itemPrice':Math.floor(Math.random()*100)})
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  if(!cart.length){
+    return 'Your shopping cart is empty.'
+  } else if ( cart.length === 1){
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  }
+  let cartArray = []  
+  for ( let i = 0; i < cart.length; i++){
+    cartArray.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)
+    }
+  let returnLine = 'In your cart, you have '
+  for ( let j = 0; j < cartArray.length; j++){
+    if ( j === cartArray.length-1){
+      returnLine = returnLine + 'and ' + cartArray[j] + '.'
+      return returnLine
+    }
+    returnLine = returnLine + cartArray[j] + ', '
+  }
+  return returnLine + '.'
 }
 
 function total() {
-  // write your code here
+  let totalValue = 0;
+  for ( let i = 0; i < cart.length; i++){
+    totalValue += cart[i]['itemPrice']
+  }
 }
 
 function removeFromCart(item) {
