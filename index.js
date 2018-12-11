d9995170ff5c0dc0cd5a09e594c383f2943df692
Lerena Holloway <holloway.lerena@gmail.com>
2018-12-11 16:29:48 +0000

    Done.

--- index.js
+++ index.js
@@ -11,10 +11,29 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+  let value = Math.floor(Math.random() * 100)
+  let obj = {'itemName': item, 'itemPrice': value}
+  cart.push(obj)
+  return `${obj['itemName']} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
+  if (cart.length === 0){
+    return "Your shopping cart is empty."
+  }else {
+    let names = []
+    let prices = []
+    let str = '';
+    for(var i = 0; i < cart.length; i++){
+      names.push(cart[i]["itemName"])
+      prices.push(cart[i]["itemPrice"])
+    }
+    for(var j = 0; j<names.length; j++){
+       str +=`${names[j]} at $${prices[j]}, `
+    }
+    return "In your cart, you have " + str  
+  }
 }
 
 function total() {
