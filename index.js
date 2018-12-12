a13d690c242932cf93ce85929641107e47a12ebf
Jonathan Koss <jonathan_koss@yahoo.com>
2018-12-12 04:36:51 +0000

    Done.

--- index.js
+++ index.js
@@ -11,39 +11,45 @@ function setCart(c) {
 
 function addToCart(item) {
   cart.push({
-    itemName: item,
-    itemPrice: Math.floor(Math.random() * 100) + 1
+   itemName: item,
+   itemPrice: Math.floor(Math.random() * 100) + 1
   });
   return `${item} has been added to your cart.`;
 }
 
 
 function viewCart() {
-   if(cart.length === 0) {
-    return "Your shopping cart is empty."; 
-   }
-   else {
-     var array = [];
-     for(var i = 0; i < cart.length; i++) {
-       array.push(`${getCart()[i].itemName} at $${getCart()[i].itemPrice}`);
-     }
-     var output = "In your cart, you have ";
-     if(cart.length === 1){
-       output += `${array[0]}.`;
-     }
-     else if(cart.length === 2){
-       output += `${array[0]}, and ${array[1]}.`; 
-     }
-     else if(cart.length > 2) {
-       var last = array.pop();
-       var middle = array.join(', ');
-       output += `${middle}, and ${last}.`;
-     }
-     return output;
-   }
+  if(cart.length === 0) {
+    return `Your shopping cart is empty.`;
+  } 
+  else{
+    let array = [];
+    let output = `In your cart, you have `;
+    for(let i = 0; i < cart.length; i++) {
+      array.push(`${getCart()[i].itemName} at $${getCart()[i].itemPrice}`);
+    }
+    if(cart.length === 1) {
+      output += `${array[0]}.`;
+    }
+    else if(cart.length === 2) {
+      output += `${array[0]}, and ${array[1]}.`;
+    }
+    else if(cart.length > 2) {
+      let end = array.pop();
+      let middle = array.join(', ');
+      output += `${middle}, and ${end}.`;
+    }
+    return output;
+  }
 }
+
 /*
- it("correctly prints a one-item cart", function() {
+ describe("viewCart()", function() {
+  it("prints 'Your shopping cart is empty.' if the cart is empty", function() {
+    expect(viewCart()).toEqual("Your shopping cart is empty.")
+  });
+
+  it("correctly prints a one-item cart", function() {
     addToCart("lemons");
     expect(viewCart()).toEqual(`In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`);
   });
@@ -68,7 +74,7 @@ function viewCart() {
       `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}, ${getCart()[1].itemName} at $${getCart()[1].itemPrice}, and ${getCart()[2].itemName} at $${getCart()[2].itemPrice}.`
     );
   });
-}); 
+});
 */
 
 
