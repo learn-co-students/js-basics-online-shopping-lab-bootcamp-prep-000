112d26b425fa97478c280a023acb4c6eec27471e
Chad Treloar <cwtreloar@gmail.com>
2018-12-15 19:55:51 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,15 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  let itemName = item;
+  let itemPrice = [];
+  
+  itemPrice.push(Math.floor((Math.random() * 100) + 1));
+  
+  var itemObject = {itemName: itemName, itemPrice: itemPrice};
+  cart.push(itemObject);
+  console.log(`${itemName} has been added to your cart.`);
+  
 }
 
 function viewCart() {
