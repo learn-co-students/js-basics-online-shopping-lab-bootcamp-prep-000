934e155e1c678aa6b107b97206c2729dec67e67e
Chad Treloar <cwtreloar@gmail.com>
2018-12-15 20:09:26 +0000

    Done.

--- index.js
+++ index.js
@@ -9,15 +9,29 @@ function setCart(c) {
   return cart;
 }
 
+/*
+
+Takes the argument, "item" and stores in variable "itemName".
+Push random prices into an empty array.
+
+itemObject has two keys: itemName and itemPrice with value
+of itemName(which holds the value of the "item" argument and
+itemPrice which parses an element in the itemPrice array).
+
+itemObject is pushed to the empty "cart" array and returns
+a message stating that "item" has been added to the cart.
+
+*/
+
 function addToCart(item) {
   let itemName = item;
   let itemPrice = [];
   
   itemPrice.push(Math.floor((Math.random() * 100) + 1));
   
-  var itemObject = {itemName: itemName, itemPrice: itemPrice};
+  var itemObject = {itemName: itemName, itemPrice: parseInt(itemPrice)};
   cart.push(itemObject);
-  console.log(`${itemName} has been added to your cart.`);
+  return `${itemName} has been added to your cart.`;
   
 }
 
