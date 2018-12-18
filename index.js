69441264d780a6eb1208e950e4a899b1567691a3
Samidh Patel <spatel13456@gmail.com>
2018-12-18 21:17:29 +0000

    Done.

--- index.js
+++ index.js
@@ -9,12 +9,23 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+ var price = Math.floor((Math.random()* 100) + 1)
+ var items = {itemName: itemName, itemPrice: price} 
+ cart.push(items)
+ return`${itemName} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  if(cart.length === 0) {
+    return "Your shopping cart is empty."
+  } else {
+    var newitem = []
+    var objectKey = Object.keys(cart[i])
+    for (let i = 0; i< cart.length; i++) {
+      newitem.push(`In your cart, you have " + Object.keys() 
+      }
+    }
 }
 
 function total() {
