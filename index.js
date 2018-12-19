71774b646f0cb1ffa740e1144f4b4d2f0a0f4eb5
Paris Dolfman <parisdolfman@gmail.com>
2018-12-19 20:40:15 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var item = {itemName: item, itemPrice: Math.floor(Math.random() * 101)};
+  cart.push(item)
 }
 
 function viewCart() {
