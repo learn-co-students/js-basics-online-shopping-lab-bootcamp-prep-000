b6aa0b12d388a5528182f90e1a2e0b3425ec8dec
Paris Dolfman <parisdolfman@gmail.com>
2018-12-21 17:29:09 +0000

    Done.

--- index.js
+++ index.js
@@ -18,11 +18,14 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  for (var counter = 0; counter < cart.length; counter++) {
-
-  }
   if (cart.length === 0) {
     return `Your shopping cart is empty.`
+  } else if (cart.length === 1) {
+    return `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`
+  } else if (cart.length === 2) {
+    for (var counter = 0; counter < cart.length; counter++) {
+      var message = `In your cart, you have ${getCart()[counter].itemName} at $${getCart()[counter].itemPrice}.`
+    }
   }
 }
 
