d9f52aa34e068c781040811a86e9ee452bc622d2
Paris Dolfman <parisdolfman@gmail.com>
2018-12-21 18:19:13 +0000

    Done.

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function viewCart() {
     return `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`
   } else if (cart.length === 2) {
     for (var counter = 0; counter < cart.length; counter++) {
-       return `In your cart, you have ${getCart()[counter].itemName} at $${getCart()[counter].itemPrice}, and ${getCart()[getCart().length-1].itemName}`
+       return `In your cart, you have ${getCart()[counter].itemName} at $${getCart()[counter].itemPrice}, and ${getCart()[getCart().length-1].itemName} at $${getCart()[getCart().length-1].itemPrice}.`
     }
   }
 }
