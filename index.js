9d9bd7c0a010d9288d2431bd1f88d0976ce8f7b2
Claire Muller <clairermuller@gmail.com>
2018-12-29 23:30:00 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var object = {
+    itemName: item,
+    itemPrice: Math.floor(Math.random() * 100)
+  };
+  cart.push(object);
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
