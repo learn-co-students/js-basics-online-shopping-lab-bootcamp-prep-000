9e0cd9343530fa6b0ad74e5ec571535d8706aa37
Paris Dolfman <parisdolfman@gmail.com>
2018-12-31 00:15:10 +0000

    Done.

--- index.js
+++ index.js
@@ -58,5 +58,9 @@ function removeFromCart(item) {
    }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber) {
+  return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  } else {
+    return `Sorry, we don't have a credit card on file for you.`
+  }
 }
