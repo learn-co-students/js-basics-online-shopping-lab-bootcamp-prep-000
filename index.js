a3d981926573464d37bc0341333ba4e260c33597
Claire Muller <clairermuller@gmail.com>
2019-01-01 02:37:38 +0000

    Done.

--- index.js
+++ index.js
@@ -19,17 +19,46 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  // write your code here
+
+  if (cart.length === 0) {
+    return `Your shopping cart is empty.`;
+  }
+
+  if (cart.length === 1) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
+  }
+
+  if (cart.length === 2) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
+  }
+
+  if (cart.length >= 3) {
+    let message = "In your cart, you have ";
+
+    for (let i = 0; i < cart.length - 1; i ++) {
+      message += `${cart[i].itemName} at $${cart[i].itemPrice}, `;
+    }
+
+    message += `and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`;
+
+    return message;
+  }
 }
 
 function total() {
-  // write your code here
+  let cartTotal = 0;
+  for (let i = 0; i < cart.length; i++) {
+    cartTotal += cart[i].itemPrice;
+  }
+  return cartTotal;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  if (item) {
+
+  }
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+
 }
