e6c1601b5c7c2b33be9f98ba4a339adf61062e5c
CHRISTOPHER TRUONG <christophertruongm@gmail.com>
2019-01-06 19:26:03 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,78 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var itemObj = {};
+ itemObj['itemName'] = item;
+ itemObj['itemPrice'] = Math.floor((Math.random() * 100) + 1)
+ 
+ cart.push(itemObj);
+ return item + " has been added to your cart.";
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    return "Your shopping cart is empty.";
+  }
+  
+  var finalStr = "In your cart, you have "
+  
+  for (var i = 0; i < cart.length; i++) {
+    var currentObj = cart[i];
+    var currentItem = currentObj['itemName'];
+    var currentPrice = currentObj['itemPrice'];
+    
+    if (cart.length === 1) {
+      finalStr += currentItem + " at " + '$' + currentPrice + '.';
+    }
+    
+    if (cart.length !== 1 && i !== cart.length - 1) {
+      finalStr += currentItem + " at " + '$' + currentPrice + ', '; 
+    }
+    
+    if (cart.length !== 1 && i === cart.length - 1) {
+      finalStr += "and " + currentItem + " at " + '$' + currentPrice + '.';
+    }
+  }
+  
+  return finalStr;
 }
 
 function total() {
-  // write your code here
+  var sum = 0;
+  
+  for (var j = 0; j < cart.length; j++) {
+    var currentObj = cart[j];
+    var currentPrice = currentObj['itemPrice'];
+    
+    sum += currentPrice;
+  }
+  
+  return sum;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  
+  for (var i = 0; i < cart.length; i++) {
+    var currentObj = cart[i];
+    var currentItem = currentObj['itemName'];
+    
+    if (currentItem === item) {
+      cart.splice(i, 1);
+      return;
+    }
+  }
+  
+  return "That item is not in your cart.";
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined) {
+    return "Sorry, we don't have a credit card on file for you.";
+  }
+  
+  var final = total();
+  
+  cart = [];
+  
+  return "Your total cost is " + '$' + final + ", which will be charged to the card " + cardNumber + '.'; 
 }
