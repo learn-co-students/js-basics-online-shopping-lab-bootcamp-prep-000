4e7e361c13c72fd8be94312a5d5ca5305ccaec27
Joan in DC <joanrigdon@gmail.com>
2019-01-14 23:49:29 +0000

    Done.

--- index.js
+++ index.js
@@ -30,17 +30,13 @@ var list = []
     
   } else if (cart.length === 2) {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
-    
   } else if (cart.length > 2) {
-      for (let i = 0; i < cart.length; i++ ) {
-        list.push(`${cart[i].itemName} at $${cart[i].itemPrice}, `)
+      for (let i = 0; i < cart.length -1; i++ ) {
+        list.push(` ${cart[i].itemName} at $${cart[i].itemPrice}`)
+      }
     }
-  }
-    return 
-      `In your cart, you have ${list} and 
-      ${cart[cart.length - 1].itemName} at 
-      $${cart[cart.length - 1].itemPrice}.`
-  }
+    return `In your cart, you have${list}, and ${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}.`
+}
   
 
 function total() {
