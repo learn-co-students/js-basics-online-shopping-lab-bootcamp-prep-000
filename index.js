7de13733f9b07a41d781e2e0f9c3cb625e043ed4
Joan in DC <joanrigdon@gmail.com>
2019-01-15 01:33:13 +0000

    Done.

--- index.js
+++ index.js
@@ -50,7 +50,14 @@ function total() {
   
 
 function removeFromCart(item) {
-  // write your code here
+  for (let i = 0; i < cart.length; i++ ) {
+    console.log(cart[i].itemName === item)
+    if (cart[i].itemName === item) {
+      cart.splice(i, 1);
+      return cart  
+    }
+  }
+  return 'That item is not in your cart.'
 }
 
 function placeOrder(cardNumber) {
