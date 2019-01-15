8cd5a7250b77694c81e644dea978a1b0a1f65053
Sajad Gholamzadehrizi <sajad1993gh@gmail.com>
2019-01-15 01:59:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var cart = [];
 
-function getCart() {
+function getCart(name) {
+  var object= { itemName: "daikon", itemPrice: Math.floor(Math.random() * (100 - 1 + 1 )) + 1 }
+  cart.push(object)
  return cart;
+   // return itemName + " has been added to your cart."
 }
-
 function setCart(c) {
   cart = c;
   return cart;
 }
 
-function addToCart(item) {
+function addToCart(itemName) {
  // write your code here
+ return itemName + " has been added to your cart."
 }
 
 function viewCart() {
