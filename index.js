a87f55c00a0d43f2501b6da3857f3eac5fc5667f
Jonathan Koss <jonathan_koss@yahoo.com>
2019-01-23 20:03:02 +0000

    Done.

--- index.js
+++ index.js
@@ -10,76 +10,146 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  cart.push({
-    itemName: item,
-    itemPrice: Math.floor(Math.random() * 100) + 1
-  });
-  return `${item} has been added to your cart.`;
-}
-
-
-function viewCart() {
-  if(cart.length === 0) {
-    return `Your shopping cart is empty.`;
-  }
-  else{
-    let array = [];
-    let output = `In your cart, you have `;
-    for(let i = 0; i < cart.length; i++) {
-      array.push(`${getCart()[i].itemName} at $${getCart()[i].itemPrice}`);
-    }
-    if(cart.length === 1) {
-      output += `${array[0]}.`;
-    }
-    else if(cart.length === 2) {
-      output += `${array[0]}, and ${array[1]}.`;
-    }
-    else if(cart.length > 2) {
-      let end = array.pop();
-      let middle = array.join(', ');
-      output += `${middle}, and ${end}.`;
-    }
-    return output;
-  }
-}
-
-
-function total() {
-  let sum = 0;
-  for(let i = 0; i < cart.length; i++) {
-    sum += cart[i].itemPrice;
-  }
-  return sum;
-}
-
- function removeFromCart(item) {
-  let itemNotInCart = true;
-  for(let i = 0; i < cart.length; i++) {
-  if(item === getCart()[i].itemName) {
-    cart.splice(i, 1);
-  }
- }
- if(itemNotInCart) {
-   return `That item is not in your cart.`;
- }
- return cart;
-}
-
-
-
-function placeOrder(cardNumber) {
-  if(arguments.length === 0) {
-    return `Sorry, we don't have a credit card on file for you.`;
-  }
-  else{
-    let cartTotal = total();
-    cart.length = 0;
-    return`Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`;
-    
-  }
+  cart.push({itemName: item,
+    itemPrice: Math.floor(Math.random() * 100) + 1});
+    getCart;
 }
 
 /*
+describe("addToCart()", function() {
+  it("can add items to the cart", function() {
+    addToCart("apples");
+
+    expect(getCart().length).toEqual(1);
+
+    addToCart("bananas");
+
+    expect(getCart().length).toEqual(2);
+  });
+
+  it("turns items into JavaScript objects before adding them to the cart", function() {
+    addToCart("carrots");
+
+    let itemConstructor = getCart()[0].constructor;
+
+    expect(itemConstructor).toEqual(Object);
+  });
+
+  it("properly structures the cart item as object with a key of `itemName` and the corresponding value { itemName: itemName } format", function() {
+    addToCart("daikon");
+
+    let itemName = getCart()[0].itemName;
+
+    expect(itemName).toEqual("daikon");
+  });
+
+  it("sets the price (integer between 1 and 100) on the cart object using the key `price`", function() {
+    addToCart("eggplant");
+
+    let itemPrice = getCart()[0].itemPrice;
+
+    expect(itemPrice).toBeLessThanOrEqualTo(100)
+                     .toBeGreaterThanOrEqualTo(1);
+  });
+
+  it("chooses the price at random", function() {
+    // Note: this test has a 1-in-10,000 chance of a false negative.
+    addToCart("figs");
+    addToCart("grapes");
+    addToCart("halloumi");
+
+    let pricesArray = getCart().map(cartItem => cartItem.itemPrice);
+    let areAnyPricesEqual = pricesArray[0] === pricesArray[1] && pricesArray[1] === pricesArray[2];
+
+    expect(areAnyPricesEqual).toBe(false);
+  });
+
+  it("returns a message indicating that the item has been added", function() {
+    expect(addToCart("ice cream")).toEqual("ice cream has been added to your cart.");
+
+    expect(addToCart("juice")).toEqual("juice has been added to your cart.");
+  });
+});
+
+describe("viewCart()", function() {
+  it("prints 'Your shopping cart is empty.' if the cart is empty", function() {
+    expect(viewCart()).toEqual("Your shopping cart is empty.")
+  });
+
+  it("correctly prints a one-item cart", function() {
+    addToCart("lemons");
+    expect(viewCart()).toEqual(`In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`);
+  });
+
+  it("correctly prints a two-item cart", function() {
+    addToCart("mango");
+    addToCart("nuts");
+
+    expect(viewCart()).toEqual(
+      `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}, and ${getCart()[1].itemName} at $${getCart()[1].itemPrice}.`
+    );
+  });
+
+  it("correctly prints a three-or-more-item cart", function() {
+    addToCart("orange");
+    addToCart("pear");
+    addToCart("quince");
+
+    
+
+    expect(viewCart()).toEqual(
+      `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}, ${getCart()[1].itemName} at $${getCart()[1].itemPrice}, and ${getCart()[2].itemName} at $${getCart()[2].itemPrice}.`
+    );
+  });
+});
+
+describe("total()", function() {
+  it("adds up the price of all items in the cart", function() {
+    addToCart("sorghum");
+    addToCart("tarragon");
+
+    const sorghumCost = getCart()[0].itemPrice;
+    const tarragonCost = getCart()[1].itemPrice;
+
+    let totalCost = sorghumCost + tarragonCost;
+
+    expect(total()).toBe(totalCost);
+
+    addToCart("urchin");
+
+    const urchinCost = getCart()[2].itemPrice;
+
+    totalCost += urchinCost;
+
+    expect(total()).toBe(totalCost);
+  });
+});
+
+describe("removeFromCart()", function() {
+  it("removes the specified item from the cart", function() {
+    addToCart("vanilla");
+    addToCart("watermelon");
+    addToCart("yams");
+
+    removeFromCart("watermelon");
+
+    const firstItemName = getCart()[0].itemName;
+    const secondItemName = getCart()[1].itemName;
+
+    expect(firstItemName).toEqual("vanilla");
+    expect(secondItemName).toEqual("yams");
+
+    removeFromCart("yams");
+
+    expect(getCart().length).toEqual(1);
+  });
+
+  it("alerts you if you're trying to remove an item that isn't in your cart", function() {
+    // Repeat item name from previous test to prevent hard-coding.
+    expect(removeFromCart("yams")).toEqual("That item is not in your cart.");
+  });
+});
+
 describe("placeOrder()", function() {
   it("doesn't place the order if a credit card number is not provided", function() {
     
