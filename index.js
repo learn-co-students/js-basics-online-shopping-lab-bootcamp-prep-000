2b062579057b722c2082dea2e826e765064fe58f
kendall willard <kendallwillard@hotmail.com>
2019-01-28 19:39:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 var cart = [];
 
 function getCart() {
@@ -11,17 +13,62 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
- var myItems = {item: Math.floor(Math.random() * 101) }
- var result = item + " has been added to your cart."
+ var itemPrice = Math.floor(Math.random() * 101)
+ var myItems = {}
+ var itemName = item
+ Object.assign(myItems, {itemName, itemPrice})
+ var result = `${item} has been added to your cart.`
+ cart.push(myItems)
  return result
 }
 
 function viewCart() {
   // write your code here
+  var myCart = {};
+  myCart = getCart();
+  var result = ""
+  var i = 0;
+  
+  if(cart.length <= 0) {
+    return "Your shopping cart is empty."
+  } 
+  
+  //If cart has one item
+  else if (cart.length === 1) {
+    result = `In your cart, you have ${myCart[0].itemName} at $${myCart[0].itemPrice}.`
+    return result
+  } 
+  
+  // If cart has two items
+  else if (cart.length === 2) {
+    result = `In your cart, you have ${myCart[0].itemName} at $${myCart[0].itemPrice}, and ${myCart[1].itemName} at $${myCart[1].itemPrice}.`
+    return result
+  }
+  
+  // If cart has multiple items
+  else {
+    result = `In your cart, you have ${myCart[0].itemName} at $${myCart[0].itemPrice}, `
+    for(i = 1; i < myCart.length -1; i++) {
+      result += `${myCart[i].itemName} at $${myCart[i].itemPrice}, `
+    }
+    
+    result += `and ${myCart[i].itemName} at $${myCart[i].itemPrice}.`
+  }
+  return result
+  
 }
 
 function total() {
   // write your code here
+  var total = 0
+  var myCart = {}
+  myCart = getCart()
+  if(myCart.length > 0 ) {
+    for( var i = 0; i < myCart.length; i++ ) {
+    total += parseInt(myCart.itemPrice[i])
+    }
+  }
+  return total
 }
 
 function removeFromCart(item) {
