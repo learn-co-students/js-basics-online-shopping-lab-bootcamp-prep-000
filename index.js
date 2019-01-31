047d38d00e0d117234a43b195d6d8547f1e1bb0e
kendall willard <kendallwillard@hotmail.com>
2019-01-31 22:31:31 +0000

    Done.

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ function placeOrder(cardNumber) {
   
   else {
    cart = []
-  
+  var totalCost = total()
    var result = `Your total cost is ${totalCost}, which will be charged to the card ${cardNumber}.`
   }
   
