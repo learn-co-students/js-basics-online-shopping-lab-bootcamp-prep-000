b61c906b0f7485a63c2d31a9ce1858565d8a7efd
Jonathan Koss <jonathan_koss@yahoo.com>
2019-02-03 02:22:51 +0000

    Done.

--- index.js
+++ index.js
@@ -63,34 +63,22 @@ function removeFromCart(item) {
   if(itemNotInCart) {
     return 'That item is not in your cart.';
   }
+  return cart;
 }
 
+function placeOrder(cardNumber) {
+  if(arguments.length === 0) {
+   return "Sorry, we don't have a credit card on file for you."; 
+  }
+  if(arguments.length === 1) {
+    let cartTotal = total();
+    cart.length = 0;
+    return `Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`;
+  }
+}
+
+
 /*
-describe("removeFromCart()", function() {
-  it("removes the specified item from the cart", function() {
-    addToCart("vanilla");
-    addToCart("watermelon");
-    addToCart("yams");
-
-    removeFromCart("watermelon");
-
-    const firstItemName = getCart()[0].itemName;
-    const secondItemName = getCart()[1].itemName;
-
-    expect(firstItemName).toEqual("vanilla");
-    expect(secondItemName).toEqual("yams");
-
-    removeFromCart("yams");
-
-    expect(getCart().length).toEqual(1);
-  });
-
-  it("alerts you if you're trying to remove an item that isn't in your cart", function() {
-    // Repeat item name from previous test to prevent hard-coding.
-    expect(removeFromCart("yams")).toEqual("That item is not in your cart.");
-  });
-});
-
 describe("placeOrder()", function() {
   it("doesn't place the order if a credit card number is not provided", function() {
     
