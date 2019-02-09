164a6078dc61c75dd453358c85810022314249fe
Sherief Farid <sherief@farid.cloud>
2019-02-09 03:05:11 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,77 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var itemName
+  var itemPrice
+  var newItem = {};
+  newItem[`itemName`] = item
+  var price = Math.floor(Math.random()*100)
+  newItem[`itemPrice`] = price
+  cart.push(newItem)
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  var message = "In your cart, you have"
+  var itemName
+  var itemPrice
+  getCart()
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  } else if (cart.length === 1) {
+    message = `${message} ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  } else {
+    var i = 0
+    for (i=0; i < cart.length ; i++) {
+      if (i===cart.length-1) {
+        message = `${message} and ${cart[i].itemName} at $${cart[i].itemPrice}.`
+      } else {
+          message = `${message} ${cart[i].itemName} at $${cart[i].itemPrice},`
+      }
+    }
+  }
+  return message
 }
 
 function total() {
-  // write your code here
+  var totalCost = 0;
+  for (var i=0;i<cart.length;i++) {
+    totalCost = cart[i].itemPrice + totalCost
+  }
+  return totalCost
 }
 
 function removeFromCart(item) {
-  // write your code here
-}
 
-function placeOrder(cardNumber) {
-  // write your code here
+  var findItem = false;
+  var output
+  for (var i=0; i< cart.length;i++){
+
+    if (item === cart[i].itemName) {
+      findItem = true;
+      cart.splice(i,1)
+    }
+ 
+    }
+  if (findItem) {
+    output = cart
+  } else {
+    output = 'That item is not in your cart.'
+  }
+  
+return output
+} 
+
+
+function placeOrder(cardNumber = 0) {
+  var message
+
+  if (cardNumber === 0) {
+    message =  `Sorry, we don\'t have a credit card on file for you.`
+  } else {
+    var totalPrice = total()
+    cart.splice(0)
+    message =  `Your total cost is $${totalPrice}, which will be charged to the card ${cardNumber}.`
+  }
+  return message
 }
