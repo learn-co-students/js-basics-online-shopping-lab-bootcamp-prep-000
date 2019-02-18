dab81090d26c691389f8ffd231791ef4dc82237d
Kash Reid-Bashir <kash@reid-bashir.com>
2019-02-18 14:49:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,55 @@
-var cart = [];
+var katzDeli = [];
 
-function getCart() {
- return cart;
+function takeANumber(currentLine, name)
+// The above code creates the main function and highlights the two main components - the current line and the queue joiners name
+{
+var placeInLine = currentLine.length + 1;
+// This line calculates the place in the line for the new joiner
+
+var outputString = “Welcome, ” + name +”. "You are number" ” + placeInLine + ” in line.”;
+// This line defines the output sentence which will be returned when called, informing the recipient of their current place in the queue
+
+currentLine.push(name);
+// This line adds the current entrant to the line to the main retained listing
+
+return outputString;
+// This line returns the sentence specified above to inform individuals of their place in the queue.
+
 }
 
-function setCart(c) {
-  cart = c;
-  return cart;
+function nowServing(currentLine){
+var outputString;
+if (currentLine.length === 0){
+outputString = “There is nobody waiting to be served!”
+}
+else{
+outputString = “Currently serving ” + currentLine[0] + “.”;
+currentLine.shift();
+}
+return outputString;
+}
+// This function defines who is currently being served and outputs the fact that either no one waiting to be served or the name of the individual currently being served and their number in the queue.
+
+
+
+function currentLine(line){
+var outputString;
+if (line.length === 0){
+outputString = “The line is currently empty.”
+}
+else{
+outputString = “The line is currently: ”
+for(var i = 0; i < line.length ; i++){
+if (i === 0){
+
+outputString = outputString + (i+1) + “. ” + line[i];
+}
+else{
+outputString = outputString + “, ” + (i+1) + “. ” + line[i];
+}
+}
+}
+return outputString;
 }
 
-function addToCart(item) {
- // write your code here
-}
-
-function viewCart() {
-  // write your code here
-}
-
-function total() {
-  // write your code here
-}
-
-function removeFromCart(item) {
-  // write your code here
-}
-
-function placeOrder(cardNumber) {
-  // write your code here
-}
+// This set of code defines the length of the line as things stand. It counts up using a focussed numeric fucntion
