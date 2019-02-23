ac61e5ab1b0b8c1adcad8f84f99eccb3cae0c8fd
Codey Pinero <codey.pinero@gmail.com>
2019-02-23 16:21:55 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,30 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var obj = { itemName: item, itemPrice: Math.floor(Math.random() * 100) };
+  cart.push(obj)
+  
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+const array = [];
+
+for (let i = 0; i < cart.length; i++) {
+array.push(` ${cart[i]} at $${Math.floor(Math.random() * 100)}`)
+
+  if (cart[i] === array.length) {
+  array.push(` and ${array}.`);
+	} else {
+	array.join(', ')
+      }
+  }
+  
+  if (array.length === 0) {
+console.log("Your shopping cart is empty.");
+    } else {
+  return 'In your cart, you have array'
+  }
 }
 
 function total() {
