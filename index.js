01f4b627903dd2e6f031c161550cc0e3c3e9c82b
whimsical-navigator-7194 <jonesandjay123@yahoo.com.tw>
2019-02-25 09:44:03 +0000

    Done.

--- index.js
+++ index.js
@@ -11,20 +11,83 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var itemName = item;
+ var itemPrice = Math.floor(Math.random()*100);
+ cart.push({itemName, itemPrice});
+ return itemName + " has been added to your cart.";
 }
 
 function viewCart() {
   // write your code here
+  var message = "";
+  if(cart.length > 0){
+    message = "In your cart, you have ";
+    for (var i = 0; i < cart.length; i++) {
+        var itemName = cart[i].itemName;
+        var itemPrice = cart[i].itemPrice;
+        message += itemName + " at $"+itemPrice;
+        if(i == cart.length-1){
+          message += ".";
+        }
+        else if(i == cart.length-2){
+          message += ", and ";
+        }
+        else{
+          message += ", ";
+        }
+    }
+  }
+  else{
+    message = "Your shopping cart is empty.";
+  }
+  return message;
 }
 
 function total() {
   // write your code here
+  var price = 0;
+  
+  if(cart.length > 0){
+    for (var i = 0; i < cart.length; i++) {
+        price += cart[i].itemPrice;
+    }
+  }
+  return price;
 }
 
 function removeFromCart(item) {
   // write your code here
+  var indexOf = 0;
+  var found = false;
+  if(cart.length > 0){
+    for (var i = 0; i < cart.length; i++) {
+        if(cart[i].itemName == item)
+        {
+          indexOf = i;
+          found = true;
+        }
+    }
+    if(found){
+      cart.splice(indexOf, 1);
+      return cart;
+    }
+    else{
+      return "That item is not in your cart.";
+    }
+  }
+  return "That item is not in your cart.";
 }
 
 function placeOrder(cardNumber) {
   // write your code here
+  if(cardNumber === "" || cardNumber === undefined){
+    return "Sorry, we don't have a credit card on file for you.";
+  }
+  else{
+    var message = "Your total cost is $"+total()+", which will be charged to the card "+cardNumber+".";
+    cart = [];
+    return message;
+  }
+  
+  
 }
