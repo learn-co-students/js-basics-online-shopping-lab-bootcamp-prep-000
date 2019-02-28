ad31fa9af5e95b365c2979ffe0122a08e36562d5
Monika Tadross <monikk.tadross@gmail.com>
2019-02-28 04:10:39 +0000

    Done.

--- index.js
+++ index.js
@@ -21,23 +21,20 @@ return `${itemName} has been added to your cart.`;
 function viewCart() {
   var messageArray = []
   var message = `In your cart, you have `
-  messageArray.push(`${cart[i].itemName} at $${cart[i].itemPrice}`) 
   if (cart.length === 0) {
     return `Your shopping cart is empty.`
-  } else { 
-      if (cart.length === 1) {
-        `${cart[0].itemName} at $${cart[0].itemPrice}`
+  } else if (cart.length === 1) {
+        messageArray.push(`${cart[0].itemName} at $${cart[0].itemPrice}`)
      } else if (cart.length === 2) {
-       `${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}`
-     } else if (cart.length === 3); {
-       `${cart[0].itemName} at $${cart[0].itemPrice}, ${cart[1].itemName} at $${cart[1].itemPrice}, and ${cart[2].itemName} at $${cart[2].itemPrice}`
-     } else {
-       for(var i = 3; i < cart.length; i++) {
-        messageArray.push(`${cart[i].itemName} at $${cart[i].itemPrice}` )
+       messageArray.push(`${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}`)
+     } else{
+       for(var i = 0; i < cart.length; i++){
+        if (i < cart.length -1){
+        messageArray.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)
         }
-       }
-      }
-  }
+        else messageArray.push(` and ${cart[i].itemName} at $${cart[i].itemPrice}` )
+}
+}
 return message + messageArray.toString() + `.`;
 }
 
