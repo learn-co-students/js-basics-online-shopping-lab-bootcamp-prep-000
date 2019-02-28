099fad209047f56eff940270b0383313ef3a159a
Ivan Szebenszki <szib77@gmail.com>
2019-02-28 00:20:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var cart = [];
 
 function getCart() {
- return cart;
+  return cart
 }
 
 function setCart(c) {
@@ -11,20 +11,60 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+  var price = Math.floor((Math.random()*100)+1)
+  var newItem = Object.assign({}, {
+    itemName: item,
+    itemPrice: price
+  })
+  cart = [...cart, newItem]
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
+  
+  function getItemString(item) {
+    return `${item.itemName} at $${item.itemPrice}`
+  }
+  
+  var cart = getCart()
+  if (cart.length === 0) return 'Your shopping cart is empty.'
+  var items = []
+  if (cart.length === 1) return `In your cart, you have ${getItemString(cart[0])}.`
+  for(var i=0; i<cart.length; i++) {
+    i === cart.length - 1 
+      ? items.push(`and ${getItemString(cart[i])}`)
+      : items.push(`${getItemString(cart[i])}`)
+  }
+  return `In your cart, you have ${items.join(', ')}.`
 }
 
 function total() {
   // write your code here
+  var total = 0
+  var cart = getCart()
+  for(var i = 0;i<cart.length;i++) {
+    total += cart[i].itemPrice
+  }
+  return total
 }
 
 function removeFromCart(item) {
   // write your code here
+  var items = getCart().map(item => item.itemName)
+  var idx = items.indexOf(item) 
+  if (idx === -1) {
+    return 'That item is not in your cart.'
+  } else {
+    return getCart().splice(idx,1)
+  }
+  
 }
 
 function placeOrder(cardNumber) {
   // write your code here
+  if ([...arguments].length === 0) return `Sorry, we don't have a credit card on file for you.`
+  var s = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  cart = []
+  return s
 }
