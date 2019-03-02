2c2949155df7293d639a90dd90fb3963f31b18ba
David zhang <d.ydzdavid@yahoo.com>
2019-03-02 17:29:46 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Online Shopping
+he# Online Shopping
 
 ## Objectives
 + Model a shopping cart full of items as an array of objects in JavaScript.
--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function removeFromCart(item) {
 
 
 function placeOrder(cardNumber) {
-  if (cardNumber === undefined) {
+  if (cardNumber === null) {
     console.log('Sorry, we don\'t have a credit card on file for you.')
   } else {
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
