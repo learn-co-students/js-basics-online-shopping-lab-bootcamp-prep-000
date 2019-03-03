bedb5000583085214eafc3120f1ba853d6e2481c
Will Smith <smithw1994@gmail.com>
2019-03-03 20:37:59 +0000

    Done.

--- index.js
+++ index.js
@@ -45,9 +45,20 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].itemName == item) {
+      cart.splice(i, 1)
+      return cart
+    }
+  }
+  return 'That item is not in your cart.'
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined) {
+    return 'Sorry, we don\'t have a credit card on file for you.'
+  }
+  var tlstr = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  cart = []
+  return tlstr
 }
