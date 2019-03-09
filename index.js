288029e22233ccd0cb4f08bd790c00691dfed7d0
erin johnson <erin.nicole.johnson@gmail.com>
2019-03-09 17:28:53 +0000

    Done.

--- index.js
+++ index.js
@@ -9,22 +9,65 @@ function setCart(c) {
   return cart;
 }
 
+
 function addToCart(item) {
- // write your code here
+  let myItem = item.toString();
+  var shoppingObject = {
+    itemName:  myItem,
+    itemPrice: Math.floor(Math.random() * 101)  // needs to be between 1 & 100; key should be 'price' ?
+  }
+ cart.push(shoppingObject);
+ return (`${item} has been added to your cart.`);
 }
 
-function viewCart() {
-  // write your code here
-}
+function viewCart(itemName, price) {
+  var a = "In your cart, you have ";
+  var j = cart.length-1;
+  if (cart.length === 0) {
+    return "Your shopping cart is empty.";
+  }
+  if (cart.length === 1) {
+    return a + (`${cart[0]['itemName']} at $${cart[0]['itemPrice']}.`)
+  }
+  if (cart.length > 1) {
+   for (let i = 0; i<cart.length-1; i++) {
+     a += (`${cart[i]['itemName']} at $${cart[i]['itemPrice']}, `)
+   } return a + (`and ${cart[j]['itemName']} at $${cart[j]['itemPrice']}.`);
+  }
+};
+
 
 function total() {
-  // write your code here
-}
+  var mySum = [];
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  for (var i=0;i<cart.length; i++) {
+    mySum.push(cart[i]['itemPrice']);
+    }
+   return mySum.reduce(reducer);
+};
+
 
 function removeFromCart(item) {
-  // write your code here
-}
+  for (let i=0; i<cart.length; i++) {
+    if (cart[i].itemName === item) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+  return "That item is not in your cart."
+};
 
 function placeOrder(cardNumber) {
-  // write your code here
+  for (var i=0; i<cart.length; i++) {
+    var theTotal = cart[i].itemPrice;
+    for (let j=0;j<theTotal.length; j++) {
+      const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    }
+  }
+  if (cardNumber === undefined) {
+  return "Sorry, we don't have a credit card on file for you."
+  } else {
+    cart = [];
+    return (`Your total cost is $${theTotal}, which will be charged to the card ${cardNumber}.`)
+   } return cart;
 }
