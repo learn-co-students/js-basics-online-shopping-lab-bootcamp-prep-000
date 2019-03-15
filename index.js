eae1e03cd09195d3a4fdfa3cecf7c5a38e7576e6
Jonathan Koss <jonathan_koss@yahoo.com>
2019-03-15 17:15:44 +0000

    Done.

--- index.js
+++ index.js
@@ -21,6 +21,19 @@ function viewCart() {
  if(cart.length === 0) {
   return 'Your shopping cart is empty.';
 } 
+else {
+  
+  
+  if(cart.length === 1) {
+    
+  }
+  if(cart.length === 2) {
+    
+  }
+  if(cart.length > 2) {
+    return 'In your cart, you have';
+  }
+}
 }
 
 function total() {
