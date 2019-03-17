505dfadb8bee1254c57e8be16b935d380b0ce39b
Trevahr Robertson <trevahr@yahoo.com>
2019-03-17 22:52:20 +0000

    Done.

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function placeOrder(cardNumber) {
   }
 
 else{
- return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
+ return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   
   cart = []
   
