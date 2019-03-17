9643ba273b34d2611d3929afbd27d00e07ed7d1b
Trevahr Robertson <trevahr@yahoo.com>
2019-03-17 23:55:49 +0000

    Done.

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function viewCart() {
   }
 
   if( cart.length === 1){
-  return  `In your cart, you have ${cart[0].itemName}  at $${cart[0].itemPrice}.`;
+  return  `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
     
   }
 
@@ -38,7 +38,7 @@ firstItem.push(otherItems);
     if( i === (cart.length - 1)){
     var lastItem = `and ${cart[i].itemName} at $${cart[i].itemPrice}.`
     firstItem.push(lastItem);
-    return firstItem.join(" ")
+    return firstItem.join("");
 }
 }
 
