78fa06a1fba45d3cc4d8efb114e20681cae13b08
Trevahr Robertson <trevahr@yahoo.com>
2019-03-17 20:05:50 +0000

    Done.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var c = {itemName: `${item}`, itemPrice: (Math.floor((Math.random()*100)+1))}
 
     cart.push(c);
  
-    return `${item} has been added to your cart`; 
+    return `${item} has been added to your cart.`; 
 }
 
 function viewCart() {
