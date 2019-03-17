f132a293d879a90f74f539d7d064242a5224a3d9
Trevahr Robertson <trevahr@yahoo.com>
2019-03-17 22:51:39 +0000

    Done.

--- index.js
+++ index.js
@@ -45,10 +45,35 @@ function removeFromCart(item) {
   cart.splice(i, 1);
   return cart;
   }}
-   return ('That item is not in your cart.');
+   return 'That item is not in your cart.';
    } 
 
 
 function placeOrder(cardNumber) {
-  // write your code here
+
+  if(cardNumber === undefined){
+   return "Sorry, we don't have a credit card on file for you."; 
+  }
+
+else{
+ return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
+  
+  cart = []
+  
 }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
