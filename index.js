7ccc453ab533e7e5307e44a5c87d911823aa1aa2
Abdul <awsayed@alaska.edu>
2019-03-18 07:53:46 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,87 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ cart.push( { itemName: item, itemPrice: Math.floor( Math.random() * 101 ) } );
+ return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+
+  if (cart.length === 0) {
+    return `Your shopping cart is empty.`;
+  } else {
+    let temp = [];
+    for (let i = 0; i < cart.length; i += 1) {
+      temp.push(` ${cart[i].itemName} at $${cart[i].itemPrice}`);
+    }
+
+    console.log(temp);
+
+    if (temp.length === 1) {
+      temp[0] = `In your cart, you have${temp[0]}.`;
+    } else { temp[0] = `In your cart, you have${temp[0]}`;}
+
+    if (temp.length > 1) {
+      temp[(cart.length - 1)] = ` and${temp[(cart.length - 1)]}.`;
+    }
+
+    console.log(temp.toString());
+    return (temp.toString());
+
+  }
+
 }
 
 function total() {
-  // write your code here
+
+  let sum = 0;
+
+  for (let i = 0; i < cart.length; i += 1) {
+    sum = sum + cart[i].itemPrice;
+  }
+  console.log(sum);
+  return sum;
 }
 
-function removeFromCart(item) {
-  // write your code here
+
+ function removeFromCart(item) {
+
+    let deleted = false;
+
+    for (let i = 0; i < cart.length; i += 1) {
+
+      if (cart[i].itemName === item) {
+        cart.splice(i, 1);
+        deleted = true;
+      }
+
+    }
+
+    if (!deleted) {
+        console.log(`That item is not in your cart.`);
+        return `That item is not in your cart.`
+    }
+
+    console.log(cart);
+    return cart;
+  
 }
 
-function placeOrder(cardNumber) {
-  // write your code here
-}
+
+
+  function placeOrder(cardNumber) {
+
+
+    if (cardNumber === undefined) {
+        console.log("Sorry, we don't have a credit card on file for you.");
+        return "Sorry, we don't have a credit card on file for you.";
+    } else {
+
+        const sum = total();
+        cart = [];
+
+        console.log(`Your total cost is $${sum}, which will be charged to the card ${cardNumber}.`);
+
+        return `Your total cost is $${sum}, which will be charged to the card ${cardNumber}.`
+    }
+  }
