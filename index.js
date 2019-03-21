53193fd70ec2607edb07b643ad3ee743739ddd07
Greg Morgan <gregm9999@gmail.com>
2019-03-21 04:55:51 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,71 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ let newItem = {
+   "itemName": item,
+   "itemPrice": Math.floor(Math.random()*100)
+ }
+ getCart().push(newItem);
+ return `${newItem.itemName} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
-}
+  let ans = [];
+  if (getCart().length===1) {
+    for (let i=0; i< getCart().length; i++) {
+      ans.push(`In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`);
+    }
+    } else if (getCart().length >1){
+      ans.push(`In your cart, you have`);
+      for (let i=0; i< getCart().length-1; i++) {
+        ans.push(`${getCart()[i].itemName} at $${getCart()[i].itemPrice},`);
+      }
+        ans.push(`and ${getCart()[getCart().length-1].itemName} at $${getCart()[getCart().length-1].itemPrice}.`)
+    } else {
+      return "Your shopping cart is empty.";
+    }
+    return ans.join(" ");
+  }
 
 function total() {
-  // write your code here
+  let total=0;
+  for (let i=0; i< getCart().length; i++) {
+    total= total + getCart()[i].itemPrice;
+  }
+  return total;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  let itemToRemove = '';
+  for (let i=0; i< getCart().length; i++) {
+    if (getCart()[i].itemName=== item) {
+      itemToRemove=getCart()[i]
+    }
+  }
+  if (itemToRemove) {
+    let index = getCart().indexOf(itemToRemove);
+    getCart().splice(index, 1);
+  } else {
+    return "That item is not in your cart.";
+  }
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (!cardNumber) {
+    return "Sorry, we don't have a credit card on file for you.";
+  } else {
+    console.log("total=", total())
+    let cartTotal = total();
+    setCart([]);
+    return `Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`
+  }
 }
+
+// function searcher(item) {
+//   let result=''
+//   for (let i=0; i< getCart().length; i++) {
+//     if (getCart()[i].itemName=== item) {
+//       result=getCart()[i]
+//     }
+//   }
+// }
