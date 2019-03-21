6a1c12c4c6df520cef3a7c4e725432f7881b3135
Elliot svensen <elliotsvensen@hotmail.co.uk>
2019-03-21 12:10:02 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,59 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  let priceOfItem = (Math.floor(Math.random() * 100) + 1); 
+  let fullItem = {itemName: item, itemPrice: priceOfItem};
+  cart.push(fullItem);
+  return (item + ' has been added to your cart.');
 }
 
 function viewCart() {
-  // write your code here
+  let currentCart = [];
+  if (cart.length === 0){
+    return 'Your shopping cart is empty.';
+  } else if (cart.length === 1) {
+    return 'In your cart, you have ' + cart[0].itemName + ' at $' + cart[0].itemPrice + '.';
+  } else if (cart.length === 2){
+    return 'In your cart, you have ' + cart[0].itemName + ' at $' + cart[0].itemPrice + ', and ' + cart[1].itemName + ' at $' + cart[1].itemPrice + '.';
+  } else if (cart.length > 2) {
+    var i = 0;
+    while (i < 2) {
+      currentCart.push(' ' + cart[i].itemName + ' at $' + cart[i].itemPrice);
+      i++;
+    }
+    for (i = 2; i < cart.length; i++) { 
+      currentCart.push(' and '+ cart[i].itemName + ' at $' + cart[i].itemPrice);
+    }
+  }
+  return 'In your cart, you have' + currentCart + '.';
 }
 
 function total() {
-  // write your code here
+  let combined = 0;
+  var i = 0;
+  for (i = 0; i < cart.length; i++) {
+    combined = combined + cart[i].itemPrice;
+  }
+  return combined;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var i = 0;
+  for (i = 0; i < cart.length; i++){
+    if (cart[i].itemName === item) {
+      cart.splice(i, 1);
+      return;
+    }
+  }
+  return 'That item is not in your cart.';
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined){
+    return "Sorry, we don't have a credit card on file for you.";
+  } else {
+    let totalCost = total();
+    cart = [];
+    return "Your total cost is $" + totalCost + ", which will be charged to the card " + cardNumber + '.';
+  }
 }
