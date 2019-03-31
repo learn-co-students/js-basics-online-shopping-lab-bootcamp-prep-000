f79831227f37dc57c03ae51cf8e3130fcfeee99e
Jonathan Koss <jonathan_koss@yahoo.com>
2019-03-31 15:48:44 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-
+  cart.push({
+    itemName: item,
+    itemPrice: Math.floor(Math.random() * 100) + 1
+  });
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
