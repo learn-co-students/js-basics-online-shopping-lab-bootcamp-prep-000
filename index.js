3ba7b58357bc5a5bfff5d3314127e8235efb7035
Annia Forney <anniaforney@gmail.com>
2019-04-02 21:04:45 +0000

    Done.

--- index.js
+++ index.js
@@ -10,23 +10,26 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-[ itemName: "apple" , itemPrice: 2 },
-[ { itemName: "doritos", itemPrice: 5},
-[ { itemName: "nutella", itemPrice: 8}
-}]
+  [{ itemName: "apples", itemPrice: 2},
+  { itemName: "doritos", itemPrice: 5},
+  { itemName: "nutella", itemPrice: 8}];
+}
 
 function viewCart() {
-  // write your code here
+  console.log`In your cart, you have apples at ${2}, doritos at ${5} and nutella at ${8}`;
 }
 
-function total() {
-  // write your code here
+function total(item) {
+return total;
 }
 
-function removeFromCart(item) {
-  // write your code here
+function removeFromCart(doritos) {
+  var array = doritos;
+  array.prototype.splice(doritos);
+  return total;
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  console.log `Your total cost is ${'15'}, which will be charged to the card 12345678`;
+  return placeOrder();
 }
