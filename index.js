2b6b3843c09895d518077fdf05abdb803bb34e7e
Jason Beedle <jason_beedle@hotmail.com>
2019-04-06 13:33:44 +0000

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ function setCart(c) {
   return cart;
 }
 
+function getRandomInt(min, max) {
+    return min + Math.floor(Math.random() * (max - min + 1));
+}
+
 
 function addToCart(item) {
  // write your code here
