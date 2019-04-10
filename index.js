34b09e0dcb539b87223e559b46004d28e1fb9765
Jowita Lapies <jowita.lapies@gmail.com>
2019-04-10 08:07:24 +0000

    Done.

--- index.js
+++ index.js
@@ -11,11 +11,17 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var itemName = item;
+ var price = Math.floor(Math.random() * 100);
+ cart.push({[itemName]: itemName, itemPrice: price});
+ console.log(`${itemName} has been added to your cart.`);
+ return cart;
+
 }
 
 function viewCart() {
   // write your code here
-}
+
 
 function total() {
   // write your code here
