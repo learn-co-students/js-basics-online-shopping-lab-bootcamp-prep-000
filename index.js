3189f9282327e362a245bcc03e5b4747c63f7784
Alexandre Silva Dias <alexdias53@gmail.com>
2019-04-12 12:21:18 +0000

    Done.

--- index.js
+++ index.js
@@ -9,16 +9,35 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(item){
+  
+  var price = Math.floor(Math.random()*100) + 1;
+  var itemName = {};
+  itemName[item] = price
+  cart.push(itemName)
+  console.log(item + " has been added to your cart.");
+  return cart
+
+ 
 }
 
 function viewCart() {
-  // write your code here
-}
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.")
+  } else {
+    var items = [];
+    for (var i = 0; i < cart.length; i++) // it should loop over every item in your cart, returning the contents as one long, coherent statement in this format: 
+    {
+      for (var item in cart[i]) {
+        items.push(item + " at $" + cart[i][item])
+      }
+    }
+    console.log("In your cart, you have " + items.join(", ") + ".");
+  }
+  }
 
 function total() {
-  // write your code here
+  
 }
 
 function removeFromCart(item) {
