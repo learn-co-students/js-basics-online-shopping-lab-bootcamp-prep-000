1fe0607feb53906bbb7a5eb2e109ba737bf9c78c
ann.kazannik <ann.kazannik@gmail.com>
2019-04-15 20:16:30 +0000

    Done.

--- index.js
+++ index.js
@@ -45,10 +45,11 @@ function total() {
 function removeFromCart(item) {
   for (let i = 0; i<cart.length; i++) {
     if (cart[i].itemName === item) {
-      cart.splice(i, 1)
-      }
-    }
-    return "That item is not in your cart."
+      cart.splice(i, 1);
+      return cart;
+     }
+  }
+return  "That item is not in your cart.";
 }
 
 function placeOrder(cardNumber) {
