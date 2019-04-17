63a1f6e1b7693c909a7d8837cafae031364a1ec0
James Hayward <james.hayward6@icloud.com>
2019-04-17 23:43:49 +0000

    Done.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
-let object = {
+var object = {
   itemName: item,
   itemPrice: getRandomInt(100)
 };
@@ -23,36 +23,36 @@ let object = {
  return `${item} has been added to your cart.`
 }
 
-function viewCart() {
+ function viewCart() {
 //   // write your code here
 for (let i = 0; i < cart.length; i++) {
-  let cartSummary = [
-    `${cart[i].itemName} at \$${cart[i].itemPrice}`
-  ];
+  var cartSummary = [];
+    cartSummary.push(`${cart[i].itemName} at \$${cart[i].itemPrice}`);
+  }
   if (cart.length === 0) {
     return 'Your shopping cart is empty.';
   }
-  if (cartSummary.length === 1) {
+  if (cart.length === 1) {
     return `In your cart, you have ${cartSummary[0]}.`;
     }
     else {
-      let final = cartSummary.pop //remove the last entry and add to the end of the sentence.
+      var final = cartSummary.slice(0, cartSummary.length - 1) //remove the last entry and add to the end of the sentence.
       return `In your cart, you have ${cartSummary.join(', ')} and ${final}.`
     }
 }
-}
+
 
 // if (cart.length === 0) {
 //   return 'Your shopping cart is empty.'
 // } if (cart.length === 1) {
 //   return `In your cart, you have ${cartSummary[0]}.`
 //   } else {
-//     let cartSummary = [];
+// //    let cartSummary = [];
 //       for (let i = 0; i < cart.length; i++) {
-//         cartSummary.push(`${cart[i].itemName} at \$${cart[i].itemPrice}`)
-//         // let cartSummary = [
-//         //   `${cart[i].itemName} at \$${cart[i].itemPrice}`
-//         // ]
+// //        cartSummary.push(`${cart[i].itemName} at \$${cart[i].itemPrice}`)
+//         let cartSummary = [
+//           `${cart[i].itemName} at \$${cart[i].itemPrice}`
+//         ]
 //         let final = cartSummary.pop //remove the last entry and add to the end of the sentence.
 //         return `In your cart, you have ${cartSummary.join(', ')} and ${final}.`
 // //    return `In your cart, you have ${cart[0].itemName} at \$${cart[0].itemPrice}.`; //opens the values inside the cart array
