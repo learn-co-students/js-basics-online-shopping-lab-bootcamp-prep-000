1e16b70cd47ce3a5bf0da98283fa0b29953d7126
Xinran Che <gracexinran@gmail.com>
2019-04-17 06:30:09 +0000

    Done.

--- index.js
+++ index.js
@@ -10,35 +10,60 @@ function setCart(c) {
 }
 
 
-function addToCart(item) {
-  var newitem = {itemName: item, itemPrice: Math.floor(Math.random() * 100) + 1};
-  getCart().push(newitem);
-  return `${item} has been added to your cart.`;
+// function addToCart(item) {
+//   var newitem = {itemName: item, itemPrice: Math.floor(Math.random() * 100) + 1};
+//   getCart().push(newitem);
+//   return `${item} has been added to your cart.`;
+// }
+//
+function addToCart(item){
+  getCart().push({itemName:item, itemPrice:Math.floor(Math.random()*100)+1});
+  return `${item} has been added to your cart.`
 }
 
-function viewCart() {
-  var i;
-  var string = "";
-  for(i=0; i<getCart().length-1; i++){
-    string += `${getCart()[i].itemName} at $${getCart()[i].itemPrice}, `;
-  }
-  if(getCart().length>1){
-    return `In your cart, you have ${string}and ${getCart()[(getCart().length)-1].itemName} at $${getCart()[(getCart().length)-1].itemPrice}.`;
-  }else if(getCart().length===1){
-    return `In your cart, you have ${getCart()[i].itemName} at $${getCart()[i].itemPrice}.`
+// function viewCart() {
+//   var i;
+//   var string = "";
+//   for(i=0; i<getCart().length-1; i++){
+//     string += `${getCart()[i].itemName} at $${getCart()[i].itemPrice}, `;
+//   }
+//   if(getCart().length>1){
+//     return `In your cart, you have ${string}and ${getCart()[(getCart().length)-1].itemName} at $${getCart()[(getCart().length)-1].itemPrice}.`;
+//   }else if(getCart().length===1){
+//     return `In your cart, you have ${getCart()[i].itemName} at $${getCart()[i].itemPrice}.`
+//   }else{
+//     return "Your shopping cart is empty."
+//   }
+// }
+//
+function viewCart(){
+  var yourcart = "";
+  if(!getCart().length){
+    return "Your shopping cart is empty.";
+  }else if(getCart().length === 1){
+    return `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`
   }else{
-    return "Your shopping cart is empty."
+    for(var i=0; i<getCart().length-1;i++){
+      yourcart += `${getCart()[i].itemName} at $${getCart()[i].itemPrice}, `
+    };
+    return `In your cart, you have ${yourcart}and ${getCart()[getCart().length-1].itemName} at $${getCart()[getCart().length-1].itemPrice}.`;
   }
 }
 
-function total() {
-  var total = 0;
-  for(var i=0;i<getCart().length;i++){
-    total += getCart()[i].itemPrice;
+// function total() {
+//   var total = 0;
+//   for(var i=0;i<getCart().length;i++){
+//     total += getCart()[i].itemPrice;
+//   };
+//   return total;
+// }
+function total(){
+  var totalprice = 0;
+  for(var i=0; i<getCart().length; i++){
+    totalprice += getCart()[i].itemPrice;
   };
-  return total;
+  return totalprice;
 }
-
 function removeFromCart(item) {
   for (var i=0; i<getCart().length; i++){
   if(getCart()[i].itemName === item){
@@ -47,14 +72,33 @@ function removeFromCart(item) {
   }
   return "That item is not in your cart.";
 }
-
-
-function placeOrder(cardNumber) {
-  if(cardNumber>0){
-    var totalcost = total();
+// function removeFromCart(item){
+//   for(var i=0; i<=getCart().length; i++){
+//     if(i === getCart().length){
+//       return "That item is not in your cart."
+//     }
+//     else if(getCart()[i].itemName === item){
+//       return getCart().splice(i,1);
+//     }
+//   }
+// }
+//
+// function placeOrder(cardNumber) {
+//   if(cardNumber>0){
+//     var totalcost = total();
+//     getCart().splice(0,getCart().length);
+//     return `Your total cost is $${totalcost}, which will be charged to the card ${cardNumber}.`;
+//   }else{
+//     return "Sorry, we don't have a credit card on file for you.";
+//   }
+// }
+function placeOrder(creditcard){
+  if(!creditcard){
+    return "Sorry, we don't have a credit card on file for you."
+  }
+  else {
+    var totalprice = total();
     getCart().splice(0,getCart().length);
-    return `Your total cost is $${totalcost}, which will be charged to the card ${cardNumber}.`;
-  }else{
-    return "Sorry, we don't have a credit card on file for you.";
+    return `Your total cost is $${totalprice}, which will be charged to the card ${creditcard}.`
   }
 }
