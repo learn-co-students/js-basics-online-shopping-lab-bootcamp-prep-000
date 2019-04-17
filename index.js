3f5d0842c5aebf68be40bf249f00d6220ac06a3e
James Hayward <james.hayward6@icloud.com>
2019-04-17 00:39:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var cart = [];
 
-function getCart() {
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getCart() { //this updates the items included in the cart list
  return cart;
 }
 
@@ -11,10 +15,27 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ let shopping = {
+   itemName: item,
+   itemPrice: getRandomInt(100),
+ };
+ cart.push(shopping) //this adds the items to the cart
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
+if (cart.length === 0) {
+  return 'Your shopping cart is empty.'
+} else {
+for (let i = 0; i < cart.length; i++)
+    return `In your cart, you have ${cart.join(', ')}`;
+}
+    //  let i = 0;
+    //  while (i < whatIGot.length) {
+    //      whatIGot.push(`${i+1}. ${line[i]}`)
+    //      ++i
+    //    } return `The line is currently: ${whosLeft.join(', ')}`;
 }
 
 function total() {
@@ -23,8 +44,14 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
+  // delete cart.
 }
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (cardNumber === cardNumber) {
+    return 'Sorry, we don\'t have a credit card on file for you.'
+  } else {
+    return `Your total cost is $undefined, which will be charged to the card 73013897.`
+  }
 }
