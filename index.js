270a8dfad18109a14d43b096820253ce6f6d9c4e
Martin Zuleta <mzuleta4@cuc.edu.co>
2019-04-23 21:25:25 +0000

    Done.

--- index.js
+++ index.js
@@ -10,62 +10,86 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
- var price = Math.random() * (100 - 1) + 1
- getCart().push({ itemName: item, itemPrice: price })
- return getCart()[cart.length].itemName
- 
+  var item = generateCartItem(item)
+  getCart().push(item)
+  return `${item.itemName} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code 
-  let res = ''
-  if (cart.length === 0){
-    res = 'Your shopping cart is empty.'
-    return res
-  }else{
-    res = 'In your cart, you have '
-    for (let i = 0; i < cart.length; i++){
-      if(i===cart.length-1){
-        res += (`${cart.itemName} at $${cart[i].itemPrice}.`)
-      }else{
-        res += (`${cart[i].itemName} at $${cart[i].itemPrice}, `)
-      }
-    }
-  }
-  return res
+  return getCart().length === 0 ? "Your shopping cart is empty." : generateCartDescription()
 }
 
 function total() {
-  // write your code here
-  let sum = 0
-  for (let i = 0; i < cart.length; i++){
-    sum += cart[i].itemPrice
+  var sum = sumUpPrices()
+  return sum
+}
+
+function removeFromCart(itemName) {
+  var itemToRemove = searchCartForItemToRemove(itemName)
+  return itemToRemove ? removeItemFromCart(itemToRemove) : notifyUserThereIsNoItemToRemove()
+}
+
+function placeOrder(cardNumber) {
+  if (arguments[0] == undefined) {
+    return "Sorry, we don't have a credit card on file for you."
+  } else {
+    var sumToCharge = total()
+    setCart([])
+    return `Your total cost is $${sumToCharge}, which will be charged to the card ${cardNumber}.`
+  }
+}
+
+// helper functions
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateCartItem(itemName) {
+  return {
+    itemName:itemName,
+    itemPrice:getRandomInt(1, 100)
+  }
+}
+
+function generateCartDescription() {
+  var cartDescription = 'In your cart, you have '
+  if ( getCart().length >= 1 ) {
+    cartDescription += `${getCart()[0].itemName} at $${getCart()[0].itemPrice}`
+  }
+  if ( getCart().length >= 2 ) {
+    var middleCartItemsDescription = ''
+    for (var i=1; i<getCart().length -1; i++) {
+      middleCartItemsDescription += `, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`
+    }
+    cartDescription += `${middleCartItemsDescription}, and ${getCart()[getCart().length-1].itemName} at $${getCart()[getCart().length-1].itemPrice}`
+  }
+
+  return `${cartDescription}.`
+}
+
+function searchCartForItemToRemove(itemName) {
+  var searchResult
+  for (var i=0; i<getCart().length; i++) {
+    if (getCart()[i].itemName === itemName) {searchResult = getCart()[i]}
+  }
+  return searchResult
+}
+
+function sumUpPrices() {
+  var sum = 0
+  for (var i=0; i<getCart().length; i++) {
+    sum = sum + getCart()[i].itemPrice
   }
   return sum
 }
 
-function removeFromCart(item) {
-  // write your code here
-  for (let i = 0; i < cart.length; i++){
-    if(i===cart.length-1 && item != cart[i].itemName ){
-      return 'That item is not in your cart.'
-    }else{
-      if(cart[i].itemName === name){
-        cart.splice(i,1)
-      }
-    }
-  }
-  return cart
-  
+function notifyUserThereIsNoItemToRemove() {
+  return 'That item is not in your cart.'
 }
 
-function placeOrder(cardNumber) {
-  // write your code
-  if(cardNumber === '' || cardNumber === ' '){
-    return `Sorry, we don't have a credit card on file for you.`
-  }else{
-    return  `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
-  }
-  
+function removeItemFromCart(itemToRemove) {
+  var indexOfItemToRemove = cart.indexOf(itemToRemove)
+  //Array.prototype.splice()
+  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+  getCart().splice(indexOfItemToRemove,1)
 }
