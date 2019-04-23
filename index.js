9eeb524175e654b0c922e3d696274a528211c896
Huy do <huyddo40@gmail.com>
2019-04-23 14:33:58 +0000

    Done.

--- index.js
+++ index.js
@@ -10,14 +10,43 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+
+ var price = Math.floor(Math.random()*(100-1 +1))+1
+ var obj = Object.assign({},{itemName:item,itemPrice:price});
+ cart.push(obj)
+ return `${obj.itemName} has been added to your cart.` 
 }
 
 function viewCart() {
-  // write your code here
+  var str = ""
+ 
+  if (cart === undefined || cart.length === 0){
+    str +="Your shopping cart is empty."
+  } else {
+    var obj = {}
+    for (var i = 0; i < cart.length; i++){
+       obj.i = cart[i]
+    }
+    str +="In your cart, you have "
+    // for (var item in obj){
+      // const values = Object.values(item)
+      // const keys = Object.keys(item)
+    //const entries = Object.entries(obj)
+        
+    
+      // for (const [item, count] of entries){
+      // str += `${item} at $${count}.`
+      for (const key of obj){
+        let value = obj[key];
+        str += `${key} at $${value}.`
+      }
+    }
+  // }
+  return str
 }
 
 function total() {
+ 
   // write your code here
 }
 
