46112ffd8c4327546c3a0fd7c0da5447f38a2f81
Finn Thye <piqueen@gmail.com>
2019-04-25 22:56:55 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,36 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var newItem = new Object();
+  newItem.itemName = item;
+  newItem.itemPrice = Math.floor(Math.random() * 100);
+
+  cart.push(newItem);
+
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
   // write your code here
+  let statement;
+  
+  if (cart.length === 0) {
+    return 'Your shopping cart is empty.';
+  } else {
+    for(let i=0; i<cart.length; i++) {
+      statement += cart[i].itemName + "at" + cart[i].itemPrice
+      
+      
+      
+      // let current = cart[i];
+      // let currentInfo = `${current.itemName} at $${current
+      // .itemPrice}` ;
+      //statement += currentInfo;
+    }
+    
+    return `In your cart, you have ${statement}.`;
+  };
+  
 }
 
 function total() {
