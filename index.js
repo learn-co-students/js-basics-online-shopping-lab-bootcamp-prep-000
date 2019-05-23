2f65735435661a1069b2fc21e17a154bc7bc6923
Eric J Wilhite <ericjwilhite@gmail.com>
2019-05-23 17:02:35 +0000

    Done.

--- index.js
+++ index.js
@@ -10,15 +10,41 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+let price = Math.floor(Math.random() * 100);
+cart.push({
+  itemName: `${item}`,
+  itemPrice: price
+  
+});
+ return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  let cartSize = cart.length;
+  let list = "";
+  if(cartSize === 0) {
+    list += `Your shopping cart is empty`;
+  } else {
+    for(let i = 0;i < cartSize;i++) {
+      if (cart.length > 0 && i === 0) {
+        list += `In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}`;
+      } else if (i < cart.length - 1){
+        list += `, ${cart[i].itemName} at $${cart[i].itemPrice}`;
+      } else {
+        list += `, and ${cart[i].itemName} at $${cart[i].itemPrice}`;
+      } 
+    }
+  }
+  list = list + '.';
+  return list;
 }
 
 function total() {
-  // write your code here
+  let total = 0;
+  for(let i = 0;i < cart.length;i++) {
+    total += cart[i].itemPrice;
+  }
+  return total;
 }
 
 function removeFromCart(item) {
