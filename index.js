d89e16340f70629e32e70fc5767bb1abb568ec63
Michelle Lam <shelly2.lam@gmail.com>
2019-05-24 02:50:06 +0000

    Done.

--- index.js
+++ index.js
@@ -10,19 +10,52 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+ var objectCart = {itemName: `${item}`, itemPrice: getRandomInt(100)};
+ cart.push(objectCart);
+ return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+ var cartList = []
+  if (cart.length === 0){
+    return "Your shopping cart is empty.";
+  }
+   for (var i = 0; i < cart.length; i++) {
+     var object = cart[i]
+     var key = Object.keys(object)[0]
+     var item = object[key]
+     var key2 = Object.keys(object)[1]
+     var price = object[key2]
+     cartList.push(`${item} at $${price}`)
+   }
+   if (cart.length === 1) {
+      return `In your cart, you have ${cartList}.`
+   }
+  if (cart.length === 2) {
+    return `In your cart, you have ${cartList.join(", and ")}.`
+    }
+    else{
+      return `In your cart, you have ${cartList.slice(0, -1).join(", ")}, and ${cartList.slice(-1)}.`
+    }
 }
 
 function total() {
-  // write your code here
+  var priceList = [];
+  for (var i = 0; i < cart.length; i++) {
+  var sum = 0
+  var object = cart[i];
+  var key2 = Object.keys(cart)[1];
+  var price = (object[key2]);
+  var total = sum + parseInt(price);
+  return total;
+}
 }
 
 function removeFromCart(item) {
-  // write your code here
+  if 
 }
 
 function placeOrder(cardNumber) {
