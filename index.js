f8a0e511945c38dbc689b65de23ff482174d1ae9
Austin Ruby <austinjruby@gmail.com>
2019-05-28 21:36:33 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,55 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var newObject = {};
+  newObject.itemName = item;
+  newObject.itemPrice = Math.round(Math.random() * 100);
+  cart.push(newObject);
+  return `${newObject["itemName"]} has been added to your cart.`
 }
-
 function viewCart() {
-  // write your code here
+  if(cart.length === 0) {
+    return "Your shopping cart is empty.";
+  }
+  else if(cart.length === 1) {
+    return `In your cart, you have ${cart[0]["itemName"]} at $${cart[0]["itemPrice"]}.`;
+  }
+  else{
+    return `In your cart, you have ${cart
+		  		.map((currentValue, index, array) => {
+			  		if(index !== array.length - 1) {
+				  		return `${array[index]["itemName"]} at $${array[index]["itemPrice"]}, `;
+					  	}
+					  else {
+						  return `and ${array[index]["itemName"]} at $${array[index]["itemPrice"]}.`;
+                      }
+					  })
+				  .join("")}`
+  }
 }
 
 function total() {
-  // write your code here
+  return cart.reduce((acc, currVal) => acc + currVal.itemPrice, 0);
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var index = cart.findIndex(x => x.itemName === item);
+  console.log(index);
+  if(index === -1) {
+    return "That item is not in your cart."
+  }
+  else {
+    cart.splice(index, 1);
+  }
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(typeof(cardNumber) !== "number") {
+    return "Sorry, we don't have a credit card on file for you.";
+  }
+  else {
+    var totalCost = total();
+    cart = [];
+    return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`;
+  }
 }
