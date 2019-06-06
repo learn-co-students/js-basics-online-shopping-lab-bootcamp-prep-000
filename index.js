438122d7cb60c9d75a441a1fc3e7a13e80439f3c
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-06 00:40:25 +0000

    Done.

--- index.js
+++ index.js
@@ -33,9 +33,9 @@ if(cart.length === 0){
    let i = 0;
    
    for( i;i < cart.length-1;i++){
-     startingString += `${cart[i].itemName} at $${cart[i].itemPrice} `;
+     startingString += `${cart[i].itemName} at $${cart[i].itemPrice}, `;
    }
-startingString += `,and ${cart[i].itemName} at $${cart[i].itemPrice}.`;
+startingString += `and ${cart[i].itemName} at $${cart[i].itemPrice}.`;
 return startingString;
 }
 }
@@ -45,13 +45,37 @@ return startingString;
 
 function total() {
   // write your code here
-  
+  var addPrice =0;
+  for(let i=0;i<cart.length;i++){
+    addPrice += cart[i].itemPrice;
+  }
+  return addPrice;
 }
 
 function removeFromCart(item) {
   // write your code here
-}
+  for(let i=0; i < cart.length;i++){
+    if(cart[i].itemName === item){
+      cart.splice(i,1);
+      return cart;
+    }
+  
+  }
+    return `That item is not in your cart.`;
+  }
+
 
 function placeOrder(cardNumber) {
   // write your code here
+  function empty(){
+    cart=[];
+  }
+    if(placeOrder(cardNumber) === placeOrder(cardNumber)){
+      empty();
+      return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    }
+   else if(placeOrder() === placeOrder()){
+     
+     return `Sorry, we don't have a credit card on file for you.`;
+   }
 }
