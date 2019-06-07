58e604346dcf0eb24d08bd090e2563fe6e90d86f
Jonathan Koss <jonathan_koss@yahoo.com>
2019-06-07 01:53:40 +0000

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ function setCart(c) {
   return cart;
 }
 
+function addToCart(item) {
+  cart.push({
+    itemName: item,
+    itemPrice: Math.floor(Math.random() * 100) + 1
+  });
+  return `${item} has been added to your cart.`;
+}
+
+function viewCart() {
+  if(cart.length === 0) {
+    return "Your shopping cart is empty.";
+  }
+}
