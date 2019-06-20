37d8ac765d39073fb588d37ad7eb643e2392717c
Brandon Fries <brandonfries1993@gmail.com>
2019-06-20 22:34:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,72 @@
 var cart = [];
 
-function getCart() {
- return cart;
-}
 
-function setCart(c) {
-  cart = c;
+function getCart() {
   return cart;
 }
 
+function setCart(c) {
+   cart = c;
+  return cart; 0
+}
+
+
 function addToCart(item) {
- // write your code here
+  var itemPrice = Math.floor(Math.random() * 100);
+  var itemName = {itemName: itemName, itemPrice: itemPrice};
+  cart.push({
+    [item] : itemPrice,
+    itemName: item,
+    itemPrice: itemPrice
+  })
+  return(item + " has been added to your cart.")
+
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  }
+  var newitems = [];
+  for (var i = 0; i < cart.length; i++) {
+    var obj = Object.keys(cart[i])[0]
+     newitems.push( obj + " at $" + cart[i][obj])
+  }
+  var string = "In your cart, you have "
+  if (newitems.length === 1) {
+  string += newitems + "."
+} else if (newitems.length === 2) {
+  string += (newitems[0] + ", and " + newitems[1] + ".")
+} else if (newitems.length > 2) {
+  var lastitem = newitems.pop()
+  var miditems = newitems.join(", ")
+  string += (miditems + ", and " + lastitem + ".")
+}
+return string
 }
 
-function total() {
-  // write your code here
-}
+ function total() {
+   var b = 0
+   for (var i = 0; i < cart.length; i ++) {
+     b +=cart[i].itemPrice
+   }
+return b
+ }
 
-function removeFromCart(item) {
-  // write your code here
-}
-
-function placeOrder(cardNumber) {
-  // write your code here
-}
+ function removeFromCart(item) {
+   for (var i = 0; i < cart.length; i++) {
+     if(cart[i].itemName === item) {
+       cart.splice(i,1)
+       return cart
+     }
+   }
+   return 'That item is not in your cart.'
+ }
+ function placeOrder(cardNumber) {
+   if (cardNumber === undefined) {
+     return "Sorry, we don't have a credit card on file for you."
+   }
+   var cost = total();
+   cart = [];
+   return 'Your total cost is $' + cost + ', which will be charged to the card ' + cardNumber  + '.'
+ }
