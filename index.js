e519bc12b714a5b64fb30161169a8f173e479d60
Tinus Strydom <tinuss@loans-inc.co.za>
2019-06-29 14:08:48 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,64 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  item = {itemName: item, itemPrice: Math.floor(Math.random(1)*101)};
+  cart.push(item);
+  return `${item.itemName} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  var text = "In your cart, you have ";
+  if(cart.length === 0){
+    return "Your shopping cart is empty.";
+  }else if(cart.length === 1){
+    text += cart[0].itemName+' at $'+ cart[0].itemPrice+'.';
+    return text;
+  }else if(cart.length === 2){
+    for(var i = 0; i < cart.length; i++){
+       text += cart[i].itemName+' at $'+ cart[i].itemPrice+', and ';
+     }
+      text = text.slice(0,text.length-6);
+      text += ".";
+      return text;
+    }else if(cart.length >= 3){
+   for(var j = 0; j < cart.length; j++){
+       text += cart[j].itemName+' at $'+cart[j].itemPrice;
+       if(j > 0){
+         text += ', and ';
+       }else{
+         text += ', ';
+       }
+     }
+     text = text.slice(0,text.length-6);
+     text += ".";
+     return text;
+ }
 }
 
 function total() {
-  // write your code here
+  var total = 0;
+  for(var i =0; i < cart.length;i++){
+    total += cart[i].itemPrice;
+  }
+  return total;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for(var i =0; i < cart.length; i++){
+    if(item === cart[i].itemName){
+      cart.splice(i,1);
+      return cart;
+    }
+  }
+  return "That item is not in your cart.";
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(cardNumber === undefined){
+    return "Sorry, we don't have a credit card on file for you.";
+  }else{
+    var payment = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    cart = [];
+    return payment;
+  }
 }
