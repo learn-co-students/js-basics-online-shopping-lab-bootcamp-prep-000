2b1afaf001d7a753541ca597261cf98efa08ef8d
Luis Ascencio <luismiguelascencio@gmail.com>
2019-07-03 15:57:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var cart = [];
 
 function getCart() {
- return cart;
+  return cart;
 }
 
 function setCart(c) {
@@ -10,21 +10,63 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  const price = Math.floor(Math.random() * 100);
+
+  cart.push({ itemName: item, itemPrice: price });
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  var cart_string = "In your cart, you have";
+  if (cart.length === 0) {
+    return "Your shopping cart is empty.";
+  } else {
+    const items_prices = [];
+    for (let index = 0; index < cart.length; index++) {
+      var cart_item_price = cart[index];
+      var item = Object.values(cart_item_price)[0];
+      var price = Object.values(cart_item_price)[1];
+      items_prices.push(`${item} at \$${price}`);
+    }
+    return `In your cart, you have ${items_prices
+      .join(", ")
+      .replace(/, ([^,]*)$/, ", and $1")}.`;
+  }
 }
 
 function total() {
-  // write your code here
+  var total = 0;
+  for (let index = 0; index < cart.length; index++) {
+    const cart_item_price = cart[index];
+    var price = Object.values(cart_item_price)[1];
+    total += price;
+  }
+  return total;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  let itemInCart = false;
+
+  for (let index = 0; index < cart.length; index++) {
+    if (cart[index].itemName === item) {
+      cart.splice(index, 1);
+      itemInCart = true;
+    }
+  }
+
+  if (!itemInCart) {
+    return "That item is not in your cart.";
+  }
+
+  return cart;
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (!cardNumber) {
+    return "Sorry, we don't have a credit card on file for you.";
+  } else {
+    var total_to_charge = total();
+    cart.length = 0;
+    return `Your total cost is $${total_to_charge}, which will be charged to the card ${cardNumber}.`;
+  }
 }
