3048d792c173e24c911852486819560c87ab6599
Jack OMalley <john.omalley@wework.com>
2019-07-23 01:18:44 +0000

    Done.

--- index.js
+++ index.js
@@ -9,22 +9,68 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+ var itemPrice = Math.floor(Math.random()*100)
+ var newItem = {itemName,itemPrice}
+ cart.push(newItem);
+ return `${itemName} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  var finalString = ["In your cart, you have"];
+  var theFinalString = []
+  if(cart.length === 0){
+    return "Your shopping cart is empty."
+  }
+  var sentenceArray = []
+  var priceArray = []
+  for(var i = 0; i < cart.length; i++){
+    var keys = Object.keys(cart[i])[0]
+    var values = Object.keys(cart[i])[1]
+    sentenceArray.push(cart[i][keys])
+    priceArray.push(cart[i][values])
+     }
+  for(i = 0; i < cart.length; i++){
+    if(cart.length === 1){
+      return `${finalString} ${sentenceArray[i]} at $${priceArray[i]}.`
+    }
+    else if(i === cart.length - 1){
+      return `${finalString} and ${sentenceArray[i]} at $${priceArray[i]}.`
+    }
+    else{
+      finalString = finalString + " " + `${sentenceArray[i]} at $${priceArray[i]},`
+    }
+  }
 }
 
 function total() {
-  // write your code here
+  var totalPrice = 0
+  for(var i = 0; i < cart.length; i++){
+    var values = Object.keys(cart[i])[1]
+    totalPrice += cart[i][values]
+  }
+  return totalPrice
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (var i = 0; i < cart.length; i++){
+    if (item === cart[i].itemName) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+    return "That item is not in your cart."
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(!cardNumber){
+    return "Sorry, we don't have a credit card on file for you."
+  }
+  else{
+    var finalWord = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    for(var i = 0; i <cart.length; i++){
+    cart.pop(cart[i])
+    }
+    return finalWord
+  }
 }
