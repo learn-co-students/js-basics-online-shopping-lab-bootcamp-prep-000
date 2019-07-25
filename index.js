5b09ba643d3d4309f9ed42d95b89a2b7a5ff564b
Ali Pfeffer <alpfefs@gmail.com>
2019-07-25 20:54:59 +0000

    Done.

--- index.js
+++ index.js
@@ -10,12 +10,28 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  // write your code here
+  var itemPrice = Math.floor(Math.random()*100)
+  cart.push({[item]: itemPrice})
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
+  var string = 'In your cart, you have '
+  for (var i = 0; i < cart.length; i++) {
+    if (cart.length === 0) {
+       string = 'Your shopping cart is empty'
+       } else if (i < cart.length - 1) {
+      string += `${Object.keys(cart[i])} at $${Object.values(cart[i])}, `
+    }
+    else if (i === cart.length - 1) {
+      string += `and ${Object.keys(cart[i])} at $${Object.values(cart[i])}.`
+    }
+  }
+  return string
 }
+
 
 function total() {
   // write your code here
