edcbd51cd5e5600ada0f467767025d3477270914
asynchronous-widget-3412 <rikeiwilliams@gmail.com>
2019-07-30 16:40:04 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -89,7 +89,7 @@ describe("viewCart()", function() {
     addToCart("pear");
     addToCart("quince");
 
-    ;
+    
 
     expect(viewCart()).toEqual(
       `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}, ${getCart()[1].itemName} at $${getCart()[1].itemPrice}, and ${getCart()[2].itemName} at $${getCart()[2].itemPrice}.`
@@ -125,7 +125,7 @@ describe("removeFromCart()", function() {
     addToCart("watermelon");
     addToCart("yams");
 
-    removeFromCart("watermelon");
+   removeFromCart("watermelon");
 
     const firstItemName = getCart()[0].itemName;
     const secondItemName = getCart()[1].itemName;
