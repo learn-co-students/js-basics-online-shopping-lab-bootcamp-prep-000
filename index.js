e6a71156d93c7819c79e7e66720d9a38e55335d8
fortuitous-map-5512 <danielionita88@yahoo.com>
2019-08-19 20:03:09 +0000

    Done.

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
-  let cart = [{itemName : itemPrice}];
+  getCart();
   if(cart.length === 0) {return 'Your shopping cart is empty.'}
   for(let i = 0; i < cart.length; i++){
     let items=[];
