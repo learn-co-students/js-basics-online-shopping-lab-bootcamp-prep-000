715189c1b910145bf57ddfbd944c02d80407ac1d
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2019-08-29 20:17:13 +0000

    Done.

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function viewCart() {
                 }
                
                 else if (i===cart.length-2){
-                 myString+= " and"
+                 myString+= "and"
                 }
                 else {
               
@@ -53,12 +53,37 @@ function viewCart() {
 
 function total() {
   // write your code here
+  var t=0;
+  for(var i = 0; i < cart.length; i++){
+       t+= cart[i].itemPrice;
+  }
+     return t;
 }
 
 function removeFromCart(item) {
   // write your code here
+     for (var i = 0; i <cart.length; i++) {
+    if (cart[i].itemName===item) {
+      cart.splice(i, 1);
+          return cart;
+    }  
+  }
+     return "That item is not in your cart."
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(cardNumber) {
+  
+   for (var i = cart.length; i>= 0; i--) {
+     var newCart;
+     newCart= `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+       cart.pop();
+       return newCart;
+    }
 }
+    else if (!cardNumber){
+      return  `Sorry, we don\'t have a credit card on file for you.`
+    }
+
+}
+
