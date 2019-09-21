1968b0f6739c1e0e8b68d6af0eabeeccc234b013
Mark Kauffman <markkauffman22@gmail.com>
2019-09-21 22:34:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var cart = [];
 
 function getCart() {
- return cart;
+  return cart;
 }
 
 function setCart(c) {
@@ -9,8 +9,18 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+  // make random price for itemObj
+  var itemPrice = Math.floor(Math.random() * 100);
+
+  // make into an object - { itemName: name of the item, itemPrice: price of the item, }
+  var itemObj = {itemName:itemName, itemPrice:itemPrice};
+ 
+  // add to cart array
+  cart.push(itemObj);
+ 
+  //return message indicating that the item has been added to cart
+  return (itemName + ' has been added to your cart.');
 }
 
 function viewCart() {
