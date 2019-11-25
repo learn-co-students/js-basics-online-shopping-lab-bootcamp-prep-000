493f950dd083aaeb9c140b499f956f21dcb346fd
comical-tag-7436 <shawtynow2008@gmail.com>
2019-11-25 23:37:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
 var cart = [];
 
-function getCart() {
- return cart;
+function setCart(newCart) {
+  cart = newCart;
+ -15,3 +15,36  function total() {
+
+  return t
 }
 
-function setCart(c) {
-  cart = c;
-  return cart;
+function getCart() {
+  return cart
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * 100)
+  cart.push(`${item}: ${price}`)
+  console.log(`${item} has been added to your cart.`)
+  return cart
 }
 
 function viewCart() {
-  // write your code here
-}
 
-function total() {
-  // write your code here
-}
+  if (cart.length === 0) {
+    console.log ("Your shopping cart is empty.");
+  }else {
+    var new_cart = "In your cart, you have ";
+    for (var i = 0; i < cart.length; i++) {
+      for (var item in cart[i]){
+       new_cart += `${item} at $${cart[i][item]}`
 
-function removeFromCart(item) {
-  // write your code here
-}
+      if (i === cart.length -1){
+        new_cart += ".";
+      }else {
+        new_cart += ", ";
+      }
+    }
+  }
+  console.log(new_cart)
 
-function placeOrder(cardNumber) {
-  // write your code here
 }
