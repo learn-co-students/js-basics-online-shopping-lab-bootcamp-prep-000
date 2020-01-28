a34070f392f2e904bcc76808c2d4cb970e154338
Richard Vernon Dyson <dyson77z@gmail.com>
2020-01-28 22:19:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
-var cart = [];
-
-function getCart() {
- return cart;
+function takeANumber(line,name){
+  var result = "";
+  line.push(name);
+  for (var i=0; i< line.length; i ++ ){
+    var number = i + 1;
+    if(String(line[i])===String(name)){
+     result= `Welcome, ${name}. You are number ${number} in line.`;
+     return result;
+    }
+  }
 }
 
-function setCart(c) {
-  cart = c;
-  return cart;
+function nowServing(line){
+  var current = String(line[0]);
+  if(line.length === 0){
+    return "There is nobody waiting to be served!";
+  }else{
+    line.shift();/*To delete the first in line */
+    return "Currently serving " + current +".";  
+  }
 }
 
-function addToCart(item) {
- // write your code here
-}
-
-function viewCart() {
-  // write your code here
-}
-
-function total() {
-  // write your code here
-}
-
-function removeFromCart(item) {
-  // write your code here
-}
-
-function placeOrder(cardNumber) {
-  // write your code here
+function currentLine(line){
+  var last = "";
+  var number = 0;
+  
+  if(line.length === 0)
+  {
+   return  "The line is currently empty.";
+  }else{
+  for(var i = 0; i < line.length; i ++){
+   number = i + 1;
+   
+   if(number===line.length){
+     last = last + number + '. ' + line[i];   
+   }else{
+     last = last + number + '. ' + line[i] + ", ";
+   } 
+  }
+  return `The line is currently: ${last}`;
+ }
 }
