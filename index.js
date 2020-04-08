2d94a76231a56a44161dd967a2d370856aa53080
Jake Gonchar <jake098877@gmail.com>
2020-04-08 20:59:06 +0000

    Done.

--- index.js
+++ index.js
@@ -37,8 +37,9 @@ function addToCart(item) {
 }
 //addToCart()
 function viewCart() {
+
 var carts = "In your cart, you have "
-var items
+var items = ""
 var item
 var price
 var itemOne
@@ -46,28 +47,43 @@ var itemTwo
 var length = cart.length
   if (cart.length === 0) {
     return "Your shopping cart is empty."
-  }
-
-  else {
-    for (var i = 0; i < cart.length; i++) {
-        item = cart[i].itemName
-        price = cart[i].itemPrice
+}     else if  (length === 1) {
+        item = cart[0].itemName
+        price = cart[0].itemPrice
         items = carts + item + " at " + "$" + price
-        if (length === 1) {
+
           return carts + item + " at " + "$" + price + "."
-        }
-        if (length === 2) {
-          itemTwo = " and " + cart[i+1].itemName + " at " + "$" + cart[i+1].itemPrice
-          return carts + item + " at " + "$" + price + "," + itemTwo + "."
+}        else if (length === 2) {
+  //debugger
+ itemOne = cart[0]
+itemTwo = cart[1]
+          var sentence =  itemOne.itemName + " at " + "$" + itemOne.itemPrice + ", and " + itemTwo.itemName + " at " + "$" + itemTwo.itemPrice + "."
+          return carts + sentence
         }
 
-        if (length > 2) {
-          items = item + " at " + "$" + price
-        }
-        }
-        return carts + items +"."
-      }
-    }
+        else if (length > 2) {
+          for (var i = 0; i < cart.length; i++) {
+            //cart[i]
+            if (length - 1 === i) {
+//debugger
+              items = items + "and " + cart[i].itemName + " at " + "$" + cart[i].itemPrice + "."
+              return carts + items
+            }
+            item = cart[i].itemName
+            price = cart[i].itemPrice
+            items += item + " at " + "$" + price + ", "
+
+          }
+          }
+}
+//debugger
+
+
+        //return carts + items +"."
+
+
+viewCart()
+
     /*if (i + 1 === length) {
       carts = carts + `${item} at $${price}.`
     return carts
@@ -111,9 +127,37 @@ function total() {
 }
 total()
 function removeFromCart(item) {
-  delete cart.item
+for (var i = 0; i < cart.length; i++) {
+if (item === cart[i].itemName) {
+  var item = cart[i].itemName
+  cart.splice(i,1)
+  return cart
 }
 
+
+
+}
+  return 'That item is not in your cart.'
+
+
+}
 function placeOrder(cardNumber) {
+var price
+var totalz = 0
+  if (cardNumber) {
+    total()
+    for (var i = 0; i < cart.length; i++) {
+      price = cart[i].itemPrice
+      totalz = totalz + price
+      //debugger
+    delete cart[i]
+    }
+    cart = []
+    return `Your total cost is $${totalz}, which will be charged to the card ${cardNumber}.`
+  }
+  else {
+    return "Sorry, we don\'t have a credit card on file for you."
+  }
+
   // write your code here
 }
