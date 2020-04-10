5e8cb9f5f8b56e76abae447024e40fab5cd67663
Mark Cascardi <mcascardi@gmail.com>
2020-04-10 07:02:50 +0000

    Done.

--- index.js
+++ index.js
@@ -9,16 +9,38 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+  const price = (Math.floor(Math.random() * Math.floor(100)))
+  const newItem = { itemName: itemName, itemPrice: price }
+  cart.push(newItem)
+  return `${itemName} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  if ( cart.length === 0 ) {
+    return `Your shopping cart is empty.`
+  }
+  let bar
+  if ( cart.length === 1 ) {
+    bar = viewItem(cart[0])
+  } else {
+    let foo = []
+    for ( let i = 0; i < cart.length - 1; i++) {
+      foo.push(viewItem(cart[i]))
+    }
+    foo.push(`and ${viewItem(cart[cart.length - 1])}`)
+    bar = foo.join(", ")
+  }
+  return `In your cart, you have ${bar}.`
+}
+
+function viewItem(item) {
+  return `${item.itemName} at $${item.itemPrice}`
 }
 
 function total() {
   // write your code here
+  return
 }
 
 function removeFromCart(item) {
