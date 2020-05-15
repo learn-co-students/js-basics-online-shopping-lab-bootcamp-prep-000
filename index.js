214788f8bffbd0b4ec976948c6d969f22c0ca7cc
Jastien Sandhu <superjastin@gmail.com>
2020-05-15 03:15:02 +0000

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,63 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var totals = {itemName: item, itemPrice: Math.floor(Math.random()*100) }
+  cart.push(totals)
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  if(cart.length === 0)
+  {
+    return "Your shopping cart is empty."
+  }else if (cart.length === 1) {
+
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+
+  }else if (cart.length === 2){
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
+  }else{
+    var test = "In your cart, you have "
+    for (let i = 0; i < cart.length - 1; i++){
+      test += `${cart[i].itemName} at $${cart[i].itemPrice}, `
+    }
+    return test + `and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`
+
+
+  }
+
 }
 
+
 function total() {
-  // write your code here
+  let totals = 0
+  for (let i = 0; i < cart.length; i++){
+    totals = totals + cart[i].itemPrice
+  }
+  return totals
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for(let i = 0; i < cart.length; i++){
+    if(item === cart[i].itemName){
+      cart.splice(i,1)
+      return cart
+    }
+  }
+  if(item != cart.itemName){
+    return "That item is not in your cart."
+  }
+
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(cardNumber === undefined){
+    return "Sorry, we don't have a credit card on file for you."
+  }
+  let totes = total()
+    cart = []
+    return `Your total cost is $${totes}, which will be charged to the card ${cardNumber}.`
+
+
+
 }
