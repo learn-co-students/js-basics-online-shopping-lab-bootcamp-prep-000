5cfe9e886816d167cdb57658ede11c17467c7692
Ladeeluc <laura.hatch.lh@gmail.com>
2020-06-02 21:44:34 -0500

    My init changes

--- index.js
+++ index.js
@@ -23,6 +23,36 @@ function viewCart() {
   if (cart.length === 0) {
     return `Your shopping cart is empty.`
   }
+  var item_status_string = 'In your cart, you have ';
+  for(let i = 0; i < cart.length; i++) {
+    item_status_string = item_status_string + cart[i].itemName + " at " + "$" + cart[i].itemPrice + "."
+  }
+  var item_status_string_two = `and`
+  for(let i = 0; i < cart.length; i++) {
+    item_status_string_two = "In your cart, you have" + cart[i].itemName + " at " + "$" + cart[i].itemPrice + item_status_string_two + "."
+  }
+
+  return item_status_string.concat(item_status_string_two);
+
+} 
+// write your code here
+
+  /**
+   * cart = [
+   *  {
+   *    itemName: "bannas",
+   *    itemPrice: 12
+   *  },
+   *  {
+   *    itemName: "apples",
+   *    itemPrice: 14
+   *  },
+   *  {
+   *    itemName: "melons",
+   *    itemPrice: 15
+   *  },
+   * ]
+   */
 
   //First Objective
   //In your cart, you have lemons at $65.
@@ -30,11 +60,6 @@ function viewCart() {
   //In your cart, you have mango at $16, and nuts at $29.
   //Third Objective
   //In your cart, you have orange at $7, pear at $51, and quince at $79.
-  for(let i = 0; i < cart.length; i++) {
-    
-  }
-} 
-  // write your code here
 
 
 
