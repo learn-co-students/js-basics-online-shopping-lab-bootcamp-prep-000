0b2c5fa95d34638e88a5ece89bde61390da64347
Ladeeluc <laura.hatch.lh@gmail.com>
2020-06-02 19:26:48 -0500

    Nate is pushing up the code

--- index.js
+++ index.js
@@ -9,16 +9,35 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(apples) {
-  "bananas", itemPrice: 17
-  Math.random(1..100)
-  return apples
- // write your code here
+// looked at solution
+
+
+function addToCart(passedInItemNameString) {
+  var item = { itemName: passedInItemNameString , itemPrice: Math.floor(Math.random() * 100)+1}
+  cart.push(item)
+  return `${passedInItemNameString} has been added to your cart.`
+  // write your code here
 }
 
 function viewCart() {
+  if (cart.length === 0) {
+    return `Your shopping cart is empty.`
+  }
+
+  //First Objective
+  //In your cart, you have lemons at $65.
+  //Second Objective
+  //In your cart, you have mango at $16, and nuts at $29.
+  //Third Objective
+  //In your cart, you have orange at $7, pear at $51, and quince at $79.
+  for(let i = 0; i < cart.length; i++) {
+    
+  }
+} 
   // write your code here
-}
+
+
+
 
 function total() {
   // write your code here
