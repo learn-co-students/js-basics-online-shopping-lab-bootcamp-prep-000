af102c41262187d354925929715372afafa662c5
curious-metric-5246 <richardjharvey1@gmail.com>
2020-06-23 18:59:52 +0000

    Done.

--- index.js
+++ index.js
@@ -11,9 +11,23 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var itemObj = {itemName: item, itemPrice: Math.floor((Math.random() * 100) + 1)}
+ cart.push(itemObj);
+console.log(cart);
+return item + " has been added to your cart.";
 }
 
 function viewCart() {
+  var cartContents = "In your cart, you have ";
+  var i = 0;
+  if ( cart.length < 1){
+    return "Your shopping cart is empty.";
+  } else {
+  for (i = 0; i < (cart.length); i++){
+   cartContents = cartContents + cart.itemName[i] + " at $" + cart.itemPrice + ".";
+  }
+  console.log(cartContents);
+  }
   // write your code here
 }
 
@@ -28,3 +42,8 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
 }
+
+
+addToCart('grapes');
+console.log( cart.itemPrice);
+viewCart();
