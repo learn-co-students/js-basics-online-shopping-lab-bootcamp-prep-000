e14170c496badc2601a6caa7746b8cea3edea55a
Aurangzaib <aurangzaib.danial@gmail.com>
2020-08-04 01:50:35 +0500

    Done.

--- index.js
+++ index.js
@@ -10,21 +10,64 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ let itemDetails = {itemName: item, itemPrice: randomPrice()}
+ cart.push(itemDetails)
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length) {
+    return 'In your cart, you have ' + formattedItems()
+  } else {
+    return 'Your shopping cart is empty.'
+  }
 }
 
 function total() {
-  // write your code here
+  return cart.reduce((total, item) => (total + item.itemPrice), 0)
 }
 
 function removeFromCart(item) {
-  // write your code here
+  let itemDetails = cart.find(itemObject => itemObject.itemName === item )
+  if (itemDetails) {
+    let itemIndex = cart.indexOf(itemDetails)
+    cart.splice(itemIndex, 1)
+    return cart
+  } else {
+    return 'That item is not in your cart.'
+  }
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber) {
+    let message = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    cart = []
+    return message
+  } else {
+    return 'Sorry, we don\'t have a credit card on file for you.'
+  }
+}
+
+function randomPrice() {
+  return Math.floor(Math.random() * 100); 
+}
+
+function formattedItems() {
+  let formatted = cart.map(item => {
+    return `${item.itemName} at $${item.itemPrice}`
+  })
+  formatLastItem(formatted)
+  return formatted.join(', ')
+}
+// bananas at $17, pancake batter at $5, and eggs at $49.
+
+function formatLastItem(formatted) {
+  const lastIndex = formatted.length - 1
+  const lastItem = formatted[lastIndex]
+  
+  if (formatted.length === 1) {
+    formatted[lastIndex] = lastItem + '.'
+  } else {
+    formatted[lastIndex] = 'and ' + lastItem + '.'
+  }
 }
