2d4f5f56b70df86f03bdce8abdc33a6118ff843b
Jelil Abudu <abudujelilfaisal@gmail.com>
2020-10-10 21:15:56 +0000

    Done.

--- index.js
+++ index.js
@@ -11,14 +11,45 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ 
+ let o = { itemName: item, itemPrice: Math.floor((Math.random() * 100) + 1) };
+ cart.push(o);
+ return `${o.itemName} has been added to your cart.`;
 }
 
 function viewCart() {
   // write your code here
+  
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  }
+  else if (cart.length === 1 ) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
+  }
+  else {
+    let myCart = "In your cart, you have";
+    for (let i = 0; i < cart.length; i++) {
+      if (i <= (cart.length - 2)) {
+        myCart += ` ${cart[i].itemName} at $${cart[i].itemPrice},`;
+      }
+      else {
+        myCart += ` and ${cart[i].itemName} at $${cart[i].itemPrice}.`;
+      }
+    }
+    return myCart;
+  }
+  
 }
 
 function total() {
   // write your code here
+  let totalValue = 0;
+  
+  for (let i = 0; i < cart.length; i++){
+    totalValue += cart[i].itemPrice;
+  }
+  
+  return totalValue;
 }
 
 function removeFromCart(item) {
