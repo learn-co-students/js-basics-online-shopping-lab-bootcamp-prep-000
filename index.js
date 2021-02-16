07cb00b2219ff7b90747224820bc31cb61d011af
Peter Crawford <pcraw1027@gmail.com>
2021-02-16 03:31:05 +0000

    Done.

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (cardNumber === "") {
+  if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
     var cartTotal = total();
